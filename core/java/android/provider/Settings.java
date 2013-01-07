1f3a21b89c133158fde6e1f527bab4ac900fff29
Danesh M <daneshm90@gmail.com>
2013-01-07 20:08:00 +0200

    AudioDeviceInventory: launch default music player on headset connect [1/2]
    Change-Id: Ibbb0607a56ab4d9246c14632eb4199558866854a
    
    Do not start music app when headset is unplugged
    
    Change-Id: Ic56ba6c30a72deaf119da40e65ca83a9dcc35c43
    
    AudioService: don't launch music player while in call
    
    If the option to launch the music player when inserting headphones is
    enabled, do not launch the music player if the user is in a call, or
    receiving one, or making one.
    
    Change-Id: I1e1d173fdd246154299b77227f0e1cbed449246b
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    AudioService: Prevent crash when there is no music app
    
    Change-Id: I45f22430a926da9b9254de8b58acd304b21fad41
    Signed-off-by: Julian Veit <claymore1298@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6343,6 +6343,16 @@ public final class Settings {
         public static final String TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK = "touchscreen_gesture_haptic_feedback";
 
         /**
+         * Whether to launch default music player when headset plugged in
+         * @hide
+         */
+        public static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
+
+        /** @hide */
+        private static final Validator HEADSET_CONNECT_PLAYER_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -6508,6 +6518,7 @@ public final class Settings {
             QS_BLUR,
             QS_BLUR_ALPHA,
             QS_BLUR_INTENSITY,
+            HEADSET_CONNECT_PLAYER,
         };
 
         /**
@@ -6743,6 +6754,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUS_BAR_CUSTOM_HEADER_HEIGHT);
             PRIVATE_SETTINGS.add(LOCKSCREEN_CLOCK);
             PRIVATE_SETTINGS.add(LOCKSCREEN_INFO);
+            PRIVATE_SETTINGS.add(HEADSET_CONNECT_PLAYER);
         }
 
         /**
@@ -6965,6 +6977,7 @@ public final class Settings {
             VALIDATORS.put(QS_BLUR, QS_BLUR_VALIDATOR);
             VALIDATORS.put(QS_BLUR_ALPHA, QS_BLUR_ALPHA_VALIDATOR);
             VALIDATORS.put(QS_BLUR_INTENSITY, QS_BLUR_INTENSITY_VALIDATOR);
+            VALIDATORS.put(HEADSET_CONNECT_PLAYER, HEADSET_CONNECT_PLAYER_VALIDATOR);
         }
 
         /**
