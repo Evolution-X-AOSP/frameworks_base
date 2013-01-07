1d3c72bf62854d0da02c5fc9e31b35d2f8a9d934
Danesh M <daneshm90@gmail.com>
2013-01-07 20:08:00 +0200

    AudioDeviceInventory: launch default music player on headset connect [1/2]
    Change-Id: Ibbb0607a56ab4d9246c14632eb4199558866854a
    
    Do not start music app when headset is unplugged
    
    Change-Id: Ic56ba6c30a72deaf119da40e65ca83a9dcc35c43
    
    AudioService: don't launch music player while in call
    
    If the option to launch the music player when inserting headphones is
    enabled, do not launch the music player if the user is in a call, or
    receiving one, or making one.
    
    Change-Id: I1e1d173fdd246154299b77227f0e1cbed449246b
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    AudioService: Prevent crash when there is no music app
    
    @Stallix: Port to A12
    
    Change-Id: I45f22430a926da9b9254de8b58acd304b21fad41
    Signed-off-by: Julian Veit <claymore1298@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5640,6 +5640,12 @@ public final class Settings {
         public static final String DOZE_TRIGGER_DOUBLETAP = "doze_trigger_doubletap";
 
         /**
+         * Whether to launch default music player when headset plugged in
+         * @hide
+         */
+        public static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5768,6 +5774,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(VIBRATE_ON_CONNECT);
             PRIVATE_SETTINGS.add(VIBRATE_ON_CALLWAITING);
             PRIVATE_SETTINGS.add(VIBRATE_ON_DISCONNECT);
+            PRIVATE_SETTINGS.add(HEADSET_CONNECT_PLAYER);
         }
 
         /**
