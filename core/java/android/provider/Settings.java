5d1c575f9ae902eea496d6709f6a94558241e4f7
maxwen <max.weninger@gmail.com>
2013-10-05 13:15:36 +0200

    base: add support for adb over network [1/3]
    Change-Id: If8354c92a03da501c683638cc69097d287d74816

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -15758,6 +15758,12 @@ public final class Settings {
         public static final String DEVICE_IDLE_CONSTANTS_USER = "device_idle_constants_user";
 
         /**
+         * The TCP/IP port to run ADB on, or 0 for USB
+         * @hide
+         */
+        public static final String ADB_PORT = "adb_port";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -35,6 +35,7 @@ import android.content.pm.PackageItemInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources.Theme;
+import android.database.ContentObserver;
 import android.database.sqlite.SQLiteCompatibilityWalFlags;
 import android.database.sqlite.SQLiteGlobal;
 import android.hardware.display.DisplayManagerInternal;
@@ -337,6 +338,23 @@ public final class SystemServer {
 
     public boolean safeMode = false;
 
+    private class AdbPortObserver extends ContentObserver {
+        public AdbPortObserver() {
+            super(null);
+        }
+        @Override
+        public void onChange(boolean selfChange) {
+            try {
+                int adbPort = Settings.Global.getInt(mContentResolver,
+                        Settings.Global.ADB_PORT, 0);
+                // setting this will control whether ADB runs on TCP/IP or USB
+                SystemProperties.set("service.adb.tcp.port", Integer.toString(adbPort));
+            } catch (Exception e) {
+                Slog.e(TAG, "", e);
+            }
+        }
+    }
+
     /**
      * Start the sensor service. This is a blocking call and can take time.
      */
@@ -520,6 +538,7 @@ public final class SystemServer {
             startBootstrapServices();
             startCoreServices();
             startOtherServices();
+            startEvoAdditions();
             SystemServerInitThreadPool.shutdown();
         } catch (Throwable ex) {
             Slog.e("System", "******************************************");
@@ -2374,5 +2393,16 @@ public final class SystemServer {
 
     private static void traceEnd() {
         BOOT_TIMINGS_TRACE_LOG.traceEnd();
+    }
+
+    // Evolution X additions start
+    private void startEvoAdditions() {
+        Settings.Global.putInt(mContentResolver, Settings.Global.ADB_PORT,
+                Integer.parseInt(SystemProperties.get("service.adb.tcp.port", "0")));
+
+        // register observer to listen for settings changes
+        mContentResolver.registerContentObserver(
+                Settings.Global.getUriFor(Settings.Global.ADB_PORT),
+                false, new AdbPortObserver());
     }
 }
