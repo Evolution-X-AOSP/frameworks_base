b41250bb98a9d5ca30333496d12e12a0bf26cffd
Roman Birg <roman@cyngn.com>
2014-11-09 19:52:34 -0800

    SystemUI: Add quick settings pull down with one finger
    Logic is currently set to use 1/3 of right hand side of the statusbar to
    trigger a quick settings quick pull down, quickly.
    
    Change-Id: I3f536496f724f07d2419a5b2db5fa8f8261609e7
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    SystemUI: fix right hand side gestures on lockscreen
    
    two finger quick pulldown was not accounting for being in the keyguard
    adn was eating touch events when it shouldn't be.
    
    Change-Id: I625d48a4002aaaf2d55d7efaed65d892ab7aa13c
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    SystemUI: add Settings.System toggle for quick-quick settings pulldown
    
    Change-Id: I3c5dcfc23f39bf8be6c402e25f61e6f57a8add2c
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    From: Zhao Wei Liew <zhaoweiliew@gmail.com>
    SystemUI: statusbar: Fix QS quick pulldown feature
    
    Bring in the fixes from CM 13.
    
    This commit squashes the following patches from CM 13.0:
    68c40f32470d66e0f8973d33a4124a2181754fb0 SystemUI: fix QS quick pull down
    477f58dc1413943926f2b4428f5873c0bb04dab8 SystemUI: hook up settings observer in notificationpanel
    d2bec52c76a3bb2c9a7fa3533e40f782fa592826 SystemUI: Fix quick-quick settings pulldown settings
    eb10bc20db37978413362250c8546ae7ed865077 Add left QS quick pulldown (1/3)
    
    Change-Id: Ie122477261d96499c80640cf274d22fba6642a01
    
    From: Zhao Wei Liew <zhaoweiliew@gmail.com>
    (partial) SystemUI: Use Tuner API for CM settings
    Get rid of all the excess code by implementing TunerService.Tunable
    and observing any changes made to the settings through it.
    
    Remove UserContentObserver as the Tuner API handles user switches.
    
    Also remove some unused imports that were introduced in earlier
    CM commits, but were never removed since.
    
    [Pig]: Forward port to R
    [bgcngm]: Forward port to S
    
    Change-Id: I3c5dcfc23f39bf8be6c402e25f61e6f57a8add2c

--- packages/SystemUI/src/com/android/systemui/shade/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/shade/QuickSettingsController.java
@@ -38,6 +38,7 @@ import android.content.res.Resources;
 import android.graphics.Insets;
 import android.graphics.Rect;
 import android.graphics.Region;
+import android.provider.Settings;
 import android.util.IndentingPrintWriter;
 import android.util.Log;
 import android.util.MathUtils;
@@ -100,6 +101,7 @@ import com.android.systemui.statusbar.phone.StatusBarTouchableRegionManager;
 import com.android.systemui.statusbar.phone.dagger.CentralSurfacesComponent;
 import com.android.systemui.statusbar.policy.CastController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
+import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.LargeScreenUtils;
 
 import dagger.Lazy;
@@ -116,6 +118,9 @@ public class QuickSettingsController implements Dumpable {
     public static final String TAG = "QuickSettingsController";
 
     public static final int SHADE_BACK_ANIM_SCALE_MULTIPLIER = 100;
+
+    private static final String STATUS_BAR_QUICK_QS_PULLDOWN =
+            "system:" + Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN;
 
     private QS mQs;
     private final Lazy<NotificationPanelViewController> mPanelViewControllerLazy;
@@ -157,6 +162,7 @@ public class QuickSettingsController implements Dumpable {
     private final AccessibilityManager mAccessibilityManager;
     private final MetricsLogger mMetricsLogger;
     private final Resources mResources;
+    private final TunerService mTunerService;
 
     /** Whether the notifications are displayed full width (no margins on the side). */
     private boolean mIsFullWidth;
@@ -286,6 +292,8 @@ public class QuickSettingsController implements Dumpable {
     private int mLastNotificationsClippingTopBound;
     private int mLastNotificationsClippingTopBoundNssl;
 
+    private int mOneFingerQuickSettingsIntercept;
+
     private final Region mInterceptRegion = new Region();
     /** The end bounds of a clipping animation. */
     private final Rect mClippingAnimationEndBounds = new Rect();
@@ -342,7 +350,8 @@ public class QuickSettingsController implements Dumpable {
             DumpManager dumpManager,
             KeyguardFaceAuthInteractor keyguardFaceAuthInteractor,
             ShadeRepository shadeRepository,
-            CastController castController
+            CastController castController,
+            TunerService tunerService
     ) {
         mPanelViewControllerLazy = panelViewControllerLazy;
         mPanelView = panelView;
@@ -390,6 +399,7 @@ public class QuickSettingsController implements Dumpable {
 
         mLockscreenShadeTransitionController.addCallback(new LockscreenShadeTransitionCallback());
         dumpManager.registerDumpable(this);
+        mTunerService = tunerService;
     }
 
     @VisibleForTesting
@@ -583,7 +593,22 @@ public class QuickSettingsController implements Dumpable {
                         MotionEvent.BUTTON_SECONDARY) || event.isButtonPressed(
                         MotionEvent.BUTTON_TERTIARY));
 
-        return twoFingerDrag || stylusButtonClickDrag || mouseButtonClickDrag;
+        final float w = mQs.getView().getMeasuredWidth();
+        final float x = event.getX();
+        float region = w * 1.f / 4.f; // TODO overlay region fraction?
+        boolean showQsOverride = false;
+
+        switch (mOneFingerQuickSettingsIntercept) {
+            case 1: // Right side pulldown
+                showQsOverride = mQs.getView().isLayoutRtl() ? x < region : w - region < x;
+                break;
+            case 2: // Left side pulldown
+                showQsOverride = mQs.getView().isLayoutRtl() ? w - region < x : x < region;
+                break;
+        }
+        showQsOverride &= mBarState == StatusBarState.SHADE;
+
+        return twoFingerDrag || showQsOverride || stylusButtonClickDrag || mouseButtonClickDrag;
     }
 
 
@@ -2148,7 +2173,8 @@ public class QuickSettingsController implements Dumpable {
     }
 
     /** */
-    public final class QsFragmentListener implements FragmentHostManager.FragmentListener {
+    public final class QsFragmentListener implements FragmentHostManager.FragmentListener,
+            TunerService.Tunable {
         /** */
         @Override
         public void onFragmentViewCreated(String tag, Fragment fragment) {
@@ -2180,17 +2206,26 @@ public class QuickSettingsController implements Dumpable {
             mShadeTransitionController.setQs(mQs);
             mNotificationStackScrollLayoutController.setQsHeader((ViewGroup) mQs.getHeader());
             mQs.setScrollListener(mQsScrollListener);
+            mTunerService.addTunable(this, STATUS_BAR_QUICK_QS_PULLDOWN);
             updateExpansion();
         }
 
         /** */
         @Override
         public void onFragmentViewDestroyed(String tag, Fragment fragment) {
+            mTunerService.removeTunable(this);
             // Manual handling of fragment lifecycle is only required because this bridges
             // non-fragment and fragment code. Once we are using a fragment for the notification
             // panel, mQs will not need to be null cause it will be tied to the same lifecycle.
             if (fragment == mQs) {
                 mQs = null;
+            }
+        }
+
+        @Override
+        public void onTuningChanged(String key, String newValue) {
+            if (STATUS_BAR_QUICK_QS_PULLDOWN.equals(key)) {
+                mOneFingerQuickSettingsIntercept = TunerService.parseInteger(newValue, 0);
             }
         }
     }
