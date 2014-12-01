c193fc04b8cdaabe77a94a050e06bf5c8c66a0e4
Jon Evans <jon@craftyjon.com>
2014-11-30 20:55:42 -0500

    Wake on plug [1/2]
    Change-Id: I5e50a7fd351857d76c1c2fd13c6e737d8902a2f3
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5777,6 +5777,12 @@ public final class Settings {
                 "notification_guts_kill_app_button";
 
         /**
+         * Whether to wake the display when plugging or unplugging the charger
+         * @hide
+         */
+        public static final String WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
