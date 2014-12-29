9627714e9f00b24104becf8165a76c8a8774d71b
Stevespear426 <stephen.k.spear@gmail.com>
2014-12-29 16:03:42 -0200

    AOKP custom system animations (1/2)
    - Work by AOKP team and ported by @cristianomatos @crdroidandroid
    - neobuddy89: Moved to global.
    
    From: Michael Ifland <msifland@gmail.com>
    Date: Mon, 21 Sep 2015 02:23:37 -0600
    Subject: [PATCH] Animations: Add option 'Random' to System Animations
    
    Change-Id: Ia4a7c1c50a72d202095f4cff2f6ce449e3bd0589
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14315,6 +14315,25 @@ public final class Settings {
         public static final String DISABLE_TRANSITION_ANIMATIONS = "disable_transition_animations";
 
         /**
+         * AOKP Custom System Animations
+         * @hide
+         */
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
+                "activity_open",
+                "activity_close",
+                "task_open",
+                "task_close",
+                "task_to_front",
+                "task_to_back",
+                "wallpaper_open",
+                "wallpaper_close",
+                "wallpaper_intra_open",
+                "wallpaper_intra_close",
+                "task_open_behind",
+        };
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+
+        /**
          * Global settings that shouldn't be persisted.
          *
          * @hide
