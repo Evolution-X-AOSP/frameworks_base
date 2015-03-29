9324507400eeae26924c99c12bce44544a1d5723
maxwen <max.weninger@gmail.com>
2015-03-29 18:39:55 +0200

    base: allow disable of screenshot shutter sound [1/2]
    @Stallix - Forward port to A13.
    
    Change-Id: I47d52bba21170118af87d35376d81d7569587a2f

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -63,6 +63,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.os.UserHandle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.provider.Settings;
@@ -933,8 +934,11 @@ public class ScreenshotController {
                  }
                  break;
             case AudioManager.RINGER_MODE_NORMAL:
-                 // Play the shutter sound to notify that we've taken a screenshot
-                 playCameraSound();
+                 if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                     Settings.System.SCREENSHOT_SHUTTER_SOUND, 1, UserHandle.USER_CURRENT) == 1) {
+                     // Play the shutter sound to notify that we've taken a screenshot
+                     playCameraSound();
+                 }
                  break;
         }
 
@@ -980,8 +984,11 @@ public class ScreenshotController {
                  }
                  break;
             case AudioManager.RINGER_MODE_NORMAL:
-                 // Play the shutter sound to notify that we've taken a screenshot
-                 playCameraSound();
+                 if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                     Settings.System.SCREENSHOT_SHUTTER_SOUND, 1, UserHandle.USER_CURRENT) == 1) {
+                     // Play the shutter sound to notify that we've taken a screenshot
+                     playCameraSound();
+                 }
                  break;
         }
 
