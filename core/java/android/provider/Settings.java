3d87b18d5bebeb2d686440c493065094ec5cfcc7
LorDClockaN <davor@losinj.com>
2015-08-28 20:59:06 +0200

    Toast icon switch [1/2]
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Change-Id: I24968141051805ee8c9bd5819e5f801c0dd78ef3
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6332,6 +6332,13 @@ public final class Settings {
         public static final String BACK_GESTURE_HEIGHT = "back_gesture_height";
 
         /**
+         * Toast icon
+         * @hide
+         */
+        @Readable
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
