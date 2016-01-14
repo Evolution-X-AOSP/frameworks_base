12a7ac0b9f915dc8ac298593d09ddc92db3c928c
beanstown106 <nbenis106@gmail.com>
2016-01-14 05:56:50 -0500

    Fingerprint authentication vibration [1/2]
    Change-Id: Ib2444f91cdba9973dc3142bba8cf6d349e983c8b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/BiometricUnlockController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/BiometricUnlockController.java
@@ -23,6 +23,7 @@ import static com.android.systemui.flags.Flags.ONE_WAY_HAPTICS_API_MIGRATION;
 import static com.android.systemui.keyguard.WakefulnessLifecycle.UNKNOWN_LAST_WAKE_TIME;
 
 import android.annotation.IntDef;
+import android.content.Context;
 import android.content.res.Resources;
 import android.hardware.biometrics.BiometricFaceConstants;
 import android.hardware.biometrics.BiometricFingerprintConstants;
@@ -32,6 +33,8 @@ import android.metrics.LogMaker;
 import android.os.Handler;
 import android.os.PowerManager;
 import android.os.Trace;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.view.HapticFeedbackConstants;
 
 import androidx.annotation.Nullable;
@@ -178,6 +181,7 @@ public class BiometricUnlockController extends KeyguardUpdateMonitorCallback imp
     private final BiometricUnlockLogger mLogger;
     private final SystemClock mSystemClock;
     private final boolean mOrderUnlockAndWake;
+    private final Context mContext;
 
     private long mLastFpFailureUptimeMillis;
     private int mNumConsecutiveFpFailures;
@@ -288,7 +292,8 @@ public class BiometricUnlockController extends KeyguardUpdateMonitorCallback imp
             ScreenOffAnimationController screenOffAnimationController,
             VibratorHelper vibrator,
             SystemClock systemClock,
-            FeatureFlags featureFlags
+            FeatureFlags featureFlags,
+            Context context
     ) {
         mPowerManager = powerManager;
         mUpdateMonitor = keyguardUpdateMonitor;
@@ -319,6 +324,7 @@ public class BiometricUnlockController extends KeyguardUpdateMonitorCallback imp
         mFeatureFlags = featureFlags;
         mOrderUnlockAndWake = resources.getBoolean(
                 com.android.internal.R.bool.config_orderUnlockAndWake);
+        mContext = context;
 
         dumpManager.registerDumpable(getClass().getName(), this);
     }
@@ -769,8 +775,12 @@ public class BiometricUnlockController extends KeyguardUpdateMonitorCallback imp
                     HapticFeedbackConstants.CONFIRM
             );
         } else {
-            mVibratorHelper.vibrateAuthSuccess(
-                    getClass().getSimpleName() + ", type =" + type + "device-entry::success");
+            boolean FingerprintVib = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.FP_SUCCESS_VIBRATE, 1, UserHandle.USER_CURRENT) == 1;
+            if (FingerprintVib) {
+                mVibratorHelper.vibrateAuthSuccess(
+                        getClass().getSimpleName() + ", type =" + type + "device-entry::success");
+            }
         }
     }
 
@@ -790,8 +800,12 @@ public class BiometricUnlockController extends KeyguardUpdateMonitorCallback imp
                     HapticFeedbackConstants.REJECT
             );
         } else {
-            mVibratorHelper.vibrateAuthError(
-                    getClass().getSimpleName() + ", type =" + type + "device-entry::error");
+            boolean FingerprintVib = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.FP_ERROR_VIBRATE, 1, UserHandle.USER_CURRENT) == 1;
+            if (FingerprintVib) {
+                mVibratorHelper.vibrateAuthError(
+                        getClass().getSimpleName() + ", type =" + type + "device-entry::error");
+            }
         }
     }
 
