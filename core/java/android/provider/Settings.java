e12416b2af36644c227584bd1d31e3c3f702c999
Danesh M <daneshm90@gmail.com>
2016-03-09 11:48:43 -0800

    Move high touch sensitivity and hovering to InputService
    Doing so allows us to keep track of user changes and restore
    preferences.
    
    This commit is squash of the following commits from CM 13.0:
    7348be747940afe7c3ec6a2b133b5473bc18573a Move high touch sensitivity and hovering to InputService
    f9a9d50491dc583ea568a4f12e57a2f97b9baacb InputMethodManager : Move registration to systemReady
    
    Since lineage-17.1 services/core/java/com/android/server/InputMethodManagerService.java is moved to
    services/core/java/com/android/server/inputmethod/InputMethodManagerService.java
    
    Change-Id: I5a6af73129acefa6530ceb3f73cc4cd83a19a676
    Ticket-Id: CYNGNOS-1166

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5567,13 +5567,6 @@ public final class Settings {
         public static final String ARTWORK_MEDIA_BACKGROUND_ALPHA = "artwork_media_background_alpha";
 
         /**
-         * IMPORTANT: If you add a new public settings you also have to add it to
-         * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
-         * it to PRIVATE_SETTINGS below. Also add a validator that can validate
-         * the setting value. See an example above.
-         */
-
-        /**
          * The alpha value of the On-The-Go overlay.
          *
          * @hide
@@ -6422,6 +6415,21 @@ public final class Settings {
         public static final String TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK = "touchscreen_gesture_haptic_feedback";
 
         /**
+         * Whether the HighTouchSensitivity is activated or not.
+         * 0 = off, 1 = on
+         * @hide
+         */
+        public static final String HIGH_TOUCH_SENSITIVITY_ENABLE =
+                "high_touch_sensitivity_enable";
+
+        /**
+         * IMPORTANT: If you add a new public settings you also have to add it to
+         * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
+         * it to PRIVATE_SETTINGS below. Also add a validator that can validate
+         * the setting value. See an example above.
+         */
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6568,6 +6576,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUS_BAR_LOGO_COLOR_ACCENT);
             PRIVATE_SETTINGS.add(BATTERY_LEVEL_COLORS);
             PRIVATE_SETTINGS.add(TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK);
+            PRIVATE_SETTINGS.add(HIGH_TOUCH_SENSITIVITY_ENABLE);
         }
 
         /**
@@ -11850,6 +11859,12 @@ public final class Settings {
          */
         @Readable
         public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
+
+        /**
+         * Whether touch hovering is enabled on supported hardware
+         * @hide
+         */
+        public static final String FEATURE_TOUCH_HOVERING = "feature_touch_hovering";
 
         /**
          * These entries are considered common between the personal and the managed profile,
