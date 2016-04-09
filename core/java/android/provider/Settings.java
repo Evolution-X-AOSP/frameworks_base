a02a1ecc9bfe2b9179cb4539362fcfaa8c62777f
Lokesh Chamane <lokesh.c703@gmail.com>
2016-04-09 19:54:40 -0400

    Base: HeadsUp snooze function [1/2]
    5.1 introduced a snooze feature which activates when swiping up.
    Default snooze time is 1 minute. Let's make it configurable!
    
    Forward ported to marshmallow/Nougat By:@BeansTown106
    *pretty much a rewrite using the logic from my heads up timeout port
    *also added a second toast for 1 minute instead of saying 1 minutes
    included Make Heads Up snooze toast more informative by beanstown106 too
    *tell the user which app is snoozing
    *also center the toast to account for big app names
    Example: SystemUI - Peeking notifications are snoozing for 1 minute.
    
    Change-Id: Ifa4b5abeb669358938c8b5cd0643d3f2caeee436
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5179,6 +5179,12 @@ public final class Settings {
          */
         public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
 
+        /**
+         * Defines the global heads up notification snooze
+         * @hide
+         */
+        public static final String HEADS_UP_NOTIFICATION_SNOOZE = "heads_up_notification_snooze";
+
         /** End of Evolution X system settings**/
 
         /**
