10f586037fc60ad0450e2e6b7bc659431c9db82d
mar-v-in <github@rvin.mooo.com>
2016-10-18 21:51:21 +0800

    PM: Signature spoofing [1/2]
    * @neobuddy89: Moved to Global.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14457,6 +14457,14 @@ public final class Settings {
         public static final String LOCKSCREEN_DISABLE_POWER_MENU = "LOCKSCREEN_DISABLE_POWER_MENU";
 
         /**
+         * Whether applications can fake a signature.
+         * 1 = permit apps to fake signature
+         * 0 = disable this feature
+         * @hide
+         */
+        public static final String ALLOW_SIGNATURE_FAKE = "allow_signature_fake";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
