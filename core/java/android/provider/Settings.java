b62bc4cb93ebeda6a11cd067343379be85236658
beanstown106 <nbenis106@gmail.com>
2017-01-04 13:29:17 -0500

    Pixel Navbar animation toggle [1/2]
    *made to work with the exposure for themers to disable navbar animation
    
    Change-Id: If15aff30447a65b9199d1768802ce86f1e1e1615
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5050,6 +5050,12 @@ public final class Settings {
         public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
 
         /**
+         * Whether to enable the pixel navbar animation
+         * @hide
+         */
+        public static final String PIXEL_NAV_ANIMATION = "pixel_nav_animation";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
