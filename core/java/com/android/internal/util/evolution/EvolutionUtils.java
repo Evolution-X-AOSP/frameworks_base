868fad921e97f972e633a9e03968cf4ebf856751
maxwen <max.weninger@gmail.com>
2017-01-12 02:28:02 +0100

    Utils: Add isAvailableApp method
    Change-Id: Iefdaf3c0b2d5db688f3a3f2f56fbf08b535d37e4
    Signed-off-by: Joe Maples <joe@frap129.org>

--- core/java/com/android/internal/util/evolution/EvolutionUtils.java
+++ core/java/com/android/internal/util/evolution/EvolutionUtils.java
@@ -17,6 +17,8 @@
 package com.android.internal.util.evolution;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
@@ -32,6 +34,19 @@ public class EvolutionUtils {
     public static boolean isChineseLanguage() {
        return Resources.getSystem().getConfiguration().locale.getLanguage().startsWith(
                Locale.CHINESE.getLanguage());
+    }
+
+    public static boolean isAvailableApp(String packageName, Context context) {
+       Context mContext = context;
+       final PackageManager pm = mContext.getPackageManager();
+       try {
+           pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+           int enabled = pm.getApplicationEnabledSetting(packageName);
+           return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+               enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+       } catch (NameNotFoundException e) {
+           return false;
+       }
     }
 
     public static boolean deviceSupportsFlashLight(Context context) {
