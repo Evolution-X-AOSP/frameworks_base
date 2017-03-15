29f67d87acbd01027996feb644f20e6b80e853f2
Pranav Vashi <neobuddy89@gmail.com>
2017-03-15 07:36:19 +0530

    SystemUI: Make roaming indicator optional [1/2]
    * Roaming indicator on signal icon is useless most of the times
      since a carrier might not charge differently to user if in same
      country.
    * So, allow user to hide roaming indicator.
    * This commit will only allow to hide indicator not roaming warnings.
    
    [someone5678]
    * Adapt to SystemUI mobile connections pipeline
    
    Change-Id: I5d103555459a5e771b072a98258b8b14f6fd8479
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -59,8 +59,9 @@ import java.util.Map;
 /**
  * Monitors the mobile signal changes and update the SysUI icons.
  */
-public class MobileSignalController extends SignalController<MobileState, MobileIconGroup> 
+public class MobileSignalController extends SignalController<MobileState, MobileIconGroup>
         implements TunerService.Tunable {
+
     private static final SimpleDateFormat SSDF = new SimpleDateFormat("MM-dd HH:mm:ss.SSS");
     private static final int STATUS_HISTORY_SIZE = 64;
     private final TelephonyManager mPhone;
@@ -78,8 +79,11 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
             "system:" + Settings.System.DATA_DISABLED_ICON;
     private static final String SHOW_FOURG_ICON =
             "system:" + Settings.System.SHOW_FOURG_ICON;
+    private static final String ROAMING_INDICATOR_ICON =
+            "system:" + Settings.System.ROAMING_INDICATOR_ICON;
 
     private boolean mDataDisabledIcon;
+    private boolean mRoamingIconAllowed;
 
     private MobileIconGroup mDefaultIcons;
     private Config mConfig;
@@ -172,6 +176,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
 
         Dependency.get(TunerService.class).addTunable(this, DATA_DISABLED_ICON);
         Dependency.get(TunerService.class).addTunable(this, SHOW_FOURG_ICON);
+        Dependency.get(TunerService.class).addTunable(this, ROAMING_INDICATOR_ICON);
     }
 
     @Override
@@ -186,6 +191,11 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
                 mConfig = Config.readConfig(mContext);
                 setConfiguration(mConfig);
                 notifyListeners();
+                break;
+            case ROAMING_INDICATOR_ICON:
+                mRoamingIconAllowed =
+                    TunerService.parseIntegerSwitch(newValue, false);
+                updateTelephony();
                 break;
             default:
                 break;
@@ -521,7 +531,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         }
         mCurrentState.dataConnected = mCurrentState.isDataConnected();
 
-        mCurrentState.roaming = isRoaming();
+        mCurrentState.roaming = isRoaming() && mRoamingIconAllowed;
         if (isCarrierNetworkChangeActive()) {
             mCurrentState.iconGroup = TelephonyIcons.CARRIER_NETWORK_CHANGE;
         } else if (isDataDisabled() && mDataDisabledIcon) {
