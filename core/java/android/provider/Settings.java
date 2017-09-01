d91a0b621d2484964bf2b9ab63c3d6b12d42f8da
maxwen <max.weninger@gmail.com>
2017-09-01 15:54:53 +0200

    base: allow to swap volume buttons rotation based [1/2]
    Change-Id: Ia50d9d1d8a72a649dd86b99f9701b0de05252ebe

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6570,6 +6570,13 @@ public final class Settings {
         public static final String VOLUME_DIALOG_TIMEOUT = "volume_dialog_timeout";
 
         /**
+         * Change volume up and down handlign based on rotation
+         * @hide
+         */
+        @Readable
+        public static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
