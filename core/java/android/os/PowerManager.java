7abf98ff2b9df562c62e5ff3efaf214417550f3c
maxwen <max.weninger@gmail.com>
2017-09-02 14:27:50 +0800

    Add WakelockBlocker [1/2]
    ported by faust93
    
    neobuddy89: Moved to global settings.
    
    Change-Id: I93fc64127c63a2d7986dff4f9c2a9941eaa56102

--- core/java/android/os/PowerManager.java
+++ core/java/android/os/PowerManager.java
@@ -3282,4 +3282,17 @@ public final class PowerManager {
     public static void invalidateIsInteractiveCaches() {
         PropertyInvalidatedCache.invalidateCache(CACHE_KEY_IS_INTERACTIVE_PROPERTY);
     }
+
+    /**
+     * @hide
+     */
+    public String getSeenWakeLocks() {
+        try {
+            if (mService != null) {
+                return mService.getSeenWakeLocks();
+            }
+        } catch (RemoteException e) {
+        }
+        return null;
+    }
 }
