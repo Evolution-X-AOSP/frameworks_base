b2de83d8e88dc8fbc61564be75f667eee0980e67
Sam Mortimer <sam@mortimer.me.uk>
2017-09-04 18:56:52 +0200

    Allow to disable battery light
    Change-Id: Ia4a1bb45aacf3a25839e9873a7a8d2e131fbc0ea

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -32,6 +32,7 @@ import android.database.ContentObserver;
 import android.hardware.health.HealthInfo;
 import android.hardware.health.V2_1.BatteryCapacityLevel;
 import android.metrics.LogMaker;
+import android.content.res.Resources;
 import android.os.BatteryManager;
 import android.os.BatteryManagerInternal;
 import android.os.BatteryProperty;
@@ -196,6 +197,9 @@ public final class BatteryService extends SystemService {
 
     private Led mLed;
 
+    // Battery light customization
+    private boolean mBatteryLightEnabled;
+
     private boolean mSentLowBatteryBroadcast = false;
 
     private ActivityManagerInternal mActivityManagerInternal;
@@ -281,6 +285,8 @@ public final class BatteryService extends SystemService {
         }
 
         mBatteryInputSuspended = PowerProperties.battery_input_suspended().orElse(false);
+        mBatteryLightEnabled = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_intrusiveBatteryLed);
         mBatteryModProps = new BatteryProperties();
         mBatteryModProps.modLevel = -1;
         mBatteryModProps.modStatus = 1;
@@ -326,6 +332,41 @@ public final class BatteryService extends SystemService {
                         false, obs, UserHandle.USER_ALL);
                 updateBatteryWarningLevelLocked();
             }
+        } else if (phase == PHASE_BOOT_COMPLETED) {
+            SettingsObserver mObserver = new SettingsObserver(new Handler());
+            mObserver.observe();
+        }
+    }
+
+    private synchronized void updateLed() {
+        mLed.updateLightsLocked();
+    }
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.Global.getUriFor(
+                    Settings.Global.BATTERY_LIGHT_ENABLED),
+                    false, this, UserHandle.USER_ALL);
+            update();
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            update();
+        }
+
+        public void update() {
+            ContentResolver resolver = mContext.getContentResolver();
+            Resources res = mContext.getResources();
+            mBatteryLightEnabled = Settings.Global.getInt(resolver,
+                    Settings.Global.BATTERY_LIGHT_ENABLED, mContext.getResources().getBoolean(
+                        com.android.internal.R.bool.config_intrusiveBatteryLed) ? 1 : 0) == 1;
+            updateLed();
         }
     }
 
@@ -1405,7 +1446,9 @@ public final class BatteryService extends SystemService {
             }
             final int level = mHealthInfo.batteryLevel;
             final int status = mHealthInfo.batteryStatus;
-            if (level < mLowBatteryWarningLevel) {
+            if (!mBatteryLightEnabled) {
+                mBatteryLight.turnOff();
+            } else if (level < mLowBatteryWarningLevel) {
                 switch (mBatteryLowBehavior) {
                     case LOW_BATTERY_BEHAVIOR_SOLID:
                         // Solid red when low battery
