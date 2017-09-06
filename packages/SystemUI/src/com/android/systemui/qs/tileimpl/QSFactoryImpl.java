4c8ced6ca2d5db7e5d0aee154f82a8d6078cc0ca
fusionjack <dogfight60-fusionjack@yahoo.de>
2017-09-06 09:12:37 +0800

    Reboot/Recovery QS Tile
    @xyyx: Adapted and reworked to O
    @qjohn: Vectorized icons
    
    Change-Id: I0bef1d739ea929aff7ec46af82af30b27895c8c2
    Signed-off-by: Josh Fox(XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -43,6 +43,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.ReadingModeTile;
+import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
@@ -131,6 +132,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new SoundSearchTIle(mHost);
             case "sound":
                 return new SoundTile(mHost);
+            case "reboot":
+                return new RebootTile(mHost);
         }
 
         // Intent tiles.
