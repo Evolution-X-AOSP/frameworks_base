d0ea8c419b767348cabc4686340a73f41577d913
Konsta <konsta09@gmail.com>
2017-09-09 19:10:41 +0800

    Framework: Volume key cursor control [1/2]
    This feature is moved to framework so it also works with third
    party keyboards.
    
    Change-Id: I22e5c0fa2692af503e39e6dc265bcdf799efae28

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5626,6 +5626,15 @@ public final class Settings {
          */
         public static final String LOCKCLOCK_FONT_SIZE = "lockclock_font_size";
 
+        /**
+          * Volume keys control cursor in text fields (default is 0)
+          * 0 - Disabled
+          * 1 - Volume up/down moves cursor left/right
+          * 2 - Volume up/down moves cursor right/left
+          * @hide
+          */
+        public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
         /** End of Evolution X system settings**/
 
         /**
