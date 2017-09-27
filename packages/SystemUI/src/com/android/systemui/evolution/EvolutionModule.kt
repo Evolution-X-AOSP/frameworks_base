b8636ff5cee75a345d1a1909eaeb6429a4797f9c
Roman Birg <roman@cyngn.com>
2017-09-27 21:01:50 +0200

    SystemUI: Add Compass tile
    @neobuddy89:
    * Set default state to inactive
    * Remove slashed icon
    * Add check if device supports compass
    * Disable long click for Compass tile
    * Code clean up and adapt to Android 13
    * Combine compass check in tile itself
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/evolution/EvolutionModule.kt
+++ packages/SystemUI/src/com/android/systemui/evolution/EvolutionModule.kt
@@ -22,6 +22,7 @@ import com.android.systemui.qs.tiles.AntiFlickerTile
 import com.android.systemui.qs.tiles.AODTile
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.CellularTile
+import com.android.systemui.qs.tiles.CompassTile
 import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.GloveModeTile
 import com.android.systemui.qs.tiles.HeadsUpTile
@@ -76,6 +77,12 @@ interface EvolutionModule {
     @IntoMap
     @StringKey(CellularTile.TILE_SPEC)
     fun bindCellularTile(cellularTile: CellularTile): QSTileImpl<*>
+
+    /** Inject CompassTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CompassTile.TILE_SPEC)
+    fun bindCompassTile(compassTile: CompassTile): QSTileImpl<*>
 
     /** Inject DataSwitchTile into tileMap in QSModule */
     @Binds
