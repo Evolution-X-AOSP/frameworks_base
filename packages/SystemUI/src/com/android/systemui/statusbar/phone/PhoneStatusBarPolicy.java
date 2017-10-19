5fc7ac24437497b182585ea48d070798dde6ffd2
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-18 20:33:08 -0400

    SystemUI: Bringup tuner statusbar changes
    Based on the following changes:
    
    Author: Steve Kondik <steve@cyngn.com>
    Date:   Wed Sep 14 05:08:02 2016 -0700
        systemui: Fix a few issues with Tuner
         * Allow launching PreferenceScreens directly by setting the
           "tuner" extra to a valid key.
         * Remove the silly warning. There are no dragons here.
         * Move CM stuff to CM manifest.
         * Fix the ActionBar title when navigating.
        Change-Id: Ideac31dbfd71d7c0aa7fc3c20395c24128c598fc
    
    Author: Gabriele M <moto.falcon.git@gmail.com>
    Date:   Sat Sep 16 19:35:28 2017 +0200
        SystemUI: Revert some Tuner changes
        These changes were done to start the status bar tuner directly without
        showing the main Tuner screen. However, they negatively affect the
        management of the fragments. Revert them so that a different solution
        can be implemented in a follow-up change.
        This reverts the following commits:
         - 338de6728d8910d450f8962a44bbe69feba1ced4
           "Handle onOptionsItemSelected for TunerActivity"
         - b3eba6af8e3253efc1332b55821fd08e4fbf77b0
           "SystemUI: Remove nav bar and status bar options from SystemUI Tuner"
         - 82b9f95f761e7b5cc94ea2542b30460e48f8fb02 (partially)
           "systemui: Fix a few issues with Tuner"
        BUGBASH-661
        Change-Id: I35182248566ff31ed08d2836d66d5bd3f947ee9a
    
    Author: Gabriele M <moto.falcon.git@gmail.com>
    Date:   Sat Sep 16 19:35:38 2017 +0200
        SystemUI: Add separate pref screen for the status bar tuner
        Keep the status bar preferences separate and allow to start them
        directly without loading the main tuner fragment.
        Change-Id: Ie1a0d9395b7bc01d585616a1976adeda3c457a0d
    
    Change-Id: Icc84575db18931d3a43bd9e640f2a55a51b460c1

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -170,7 +170,6 @@ public class PhoneStatusBarPolicy
     private BluetoothController mBluetooth;
     private AlarmManager.AlarmClockInfo mNextAlarm;
 
-    private boolean mNfcVisible;
     private NfcAdapter mAdapter;
     private final Context mContext;
 
@@ -357,7 +356,7 @@ public class PhoneStatusBarPolicy
         mIconController.setIconVisibility(mSlotScreenRecord, false);
 
         mIconController.setIcon(mSlotNfc, R.drawable.stat_sys_nfc,
-                mResources.getString(R.string.accessibility_status_bar_nfc));
+                mResources.getString(R.string.status_bar_nfc));
 
         mIconController.setIconVisibility(mSlotNfc, false);
         updateNfc();
@@ -432,12 +431,7 @@ public class PhoneStatusBarPolicy
     }
 
     private final void updateNfc() {
-        mNfcVisible =  getAdapter() != null && getAdapter().isEnabled();
-        if (mNfcVisible) {
-            mIconController.setIconVisibility(mSlotNfc, true);
-        } else {
-            mIconController.setIconVisibility(mSlotNfc, false);
-        }
+        mIconController.setIconVisibility(mSlotNfc, getAdapter() != null && getAdapter().isEnabled());
     }
 
     private final void updateVolumeZen() {
@@ -829,7 +823,6 @@ public class PhoneStatusBarPolicy
                 case NfcAdapter.ACTION_ADAPTER_STATE_CHANGED:
                     updateNfc();
                     break;
-
             }
         }
     };
