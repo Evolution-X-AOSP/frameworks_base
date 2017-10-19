554588f94ccef9eff9803b6726e983e521b7c739
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-18 20:33:08 -0400

    SystemUI: Bringup tuner navbar and statusbar changes
    Based on the following changes:
    
    Author: Steve Kondik <steve@cyngn.com>
    Date:   Mon Sep 12 12:49:59 2016 -0700
    
        systemui: Enable navbar tuner
    
         * And add an action for it.
    
        Change-Id: I23a51cc8e33f3498deec9ed60281423c66bbc4ca
    
    Author: Steve Kondik <steve@cyngn.com>
    Date:   Wed Sep 14 05:08:02 2016 -0700
    
        systemui: Fix a few issues with Tuner
    
         * Allow launching PreferenceScreens directly by setting the
           "tuner" extra to a valid key.
         * Remove the silly warning. There are no dragons here.
         * Move CM stuff to CM manifest.
         * Fix the ActionBar title when navigating.
    
        Change-Id: Ideac31dbfd71d7c0aa7fc3c20395c24128c598fc
    
    Author: Joey Rizzoli <joey@cyanogenmoditalia.it>
    Date:   Tue Nov 8 15:44:25 2016 +0100
    
        Tuner: allow power notif controls to be launched externally
    
        Change-Id: I8a9e1a2080640938c99125ab04986dfc99e004bf
        Signed-off-by: Joey Rizzoli <joey@cyanogenmoditalia.it>
    
    Author: Gabriele M <moto.falcon.git@gmail.com>
    Date:   Sat Sep 16 19:35:28 2017 +0200
    
        SystemUI: Revert some Tuner changes
    
        These changes were done to start the status bar tuner directly without
        showing the main Tuner screen. However, they negatively affect the
        management of the fragments. Revert them so that a different solution
        can be implemented in a follow-up change.
    
        This reverts the following commits:
    
         - 338de6728d8910d450f8962a44bbe69feba1ced4
           "Handle onOptionsItemSelected for TunerActivity"
    
         - b3eba6af8e3253efc1332b55821fd08e4fbf77b0
           "SystemUI: Remove nav bar and status bar options from SystemUI Tuner"
    
         - 82b9f95f761e7b5cc94ea2542b30460e48f8fb02 (partially)
           "systemui: Fix a few issues with Tuner"
    
        BUGBASH-661
    
        Change-Id: I35182248566ff31ed08d2836d66d5bd3f947ee9a
    
    Author: Gabriele M <moto.falcon.git@gmail.com>
    Date:   Sat Sep 16 19:35:38 2017 +0200
    
        SystemUI: Add separate pref screen for the status bar tuner
    
        Keep the status bar preferences separate and allow to start them
        directly without loading the main tuner fragment.
    
        Change-Id: Ie1a0d9395b7bc01d585616a1976adeda3c457a0d
    
    Author: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Date:   Thu Oct 13 19:19:03 2016 +0800
    
        SystemUI: tuner: Allow Tuner API for System settings
    
        We'd like to modify some AOSP System settings as well.
    
        Change-Id: I4e2e7bf680f20bce9619ff2adf243d0f8f4a9906
    
    BenzoEdit: Added lockscreen fragment
    
    Change-Id: Icc84575db18931d3a43bd9e640f2a55a51b460c1
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -24,8 +24,10 @@ import android.database.ContentObserver;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
+import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
+import android.provider.Settings.Global;
 import android.provider.Settings.Secure;
 import android.text.TextUtils;
 import android.util.ArrayMap;
@@ -142,31 +144,78 @@ public class TunerServiceImpl extends TunerService {
         setValue(TUNER_VERSION, newVersion);
     }
 
+    private boolean isGlobal(String key) {
+        return key.startsWith("global:");
+    }
+
+    private boolean isSystem(String key) {
+        return key.startsWith("system:");
+    }
+
+    private String chomp(String key) {
+        return key.replaceFirst("^(global|system):", "");
+    }
+
     @Override
     public String getValue(String setting) {
-        return Settings.Secure.getStringForUser(mContentResolver, setting, mCurrentUser);
+        if (isGlobal(setting)) {
+            return Settings.Global.getString(mContentResolver, chomp(setting));
+        } else if (isSystem(setting)) {
+            return Settings.System.getStringForUser(
+                    mContentResolver, chomp(setting), mCurrentUser);
+        } else {
+            return Settings.Secure.getStringForUser(mContentResolver, setting, mCurrentUser);
+        }
     }
 
     @Override
     public void setValue(String setting, String value) {
-         Settings.Secure.putStringForUser(mContentResolver, setting, value, mCurrentUser);
+        if (isGlobal(setting)) {
+            Settings.Global.putString(mContentResolver, chomp(setting), value);
+        } else if (isSystem(setting)) {
+            Settings.System.putStringForUser(
+                    mContentResolver, chomp(setting), value, mCurrentUser);
+        } else {
+            Settings.Secure.putStringForUser(mContentResolver, setting, value, mCurrentUser);
+        }
     }
 
     @Override
     public int getValue(String setting, int def) {
-        return Settings.Secure.getIntForUser(mContentResolver, setting, def, mCurrentUser);
+        if (isGlobal(setting)) {
+            return Settings.Global.getInt(mContentResolver, chomp(setting), def);
+        } else if (isSystem(setting)) {
+            return Settings.System.getIntForUser(
+                    mContentResolver, chomp(setting), def, mCurrentUser);
+        } else {
+            return Settings.Secure.getIntForUser(mContentResolver, setting, def, mCurrentUser);
+        }
     }
 
     @Override
     public String getValue(String setting, String def) {
-        String ret = Secure.getStringForUser(mContentResolver, setting, mCurrentUser);
+        String ret;
+        if (isGlobal(setting)) {
+            ret = Settings.Global.getString(mContentResolver, chomp(setting));
+        } else if (isSystem(setting)) {
+            ret = Settings.System.getStringForUser(
+                    mContentResolver, chomp(setting), mCurrentUser);
+        } else {
+            ret = Secure.getStringForUser(mContentResolver, setting, mCurrentUser);
+        }
         if (ret == null) return def;
         return ret;
     }
 
     @Override
     public void setValue(String setting, int value) {
-         Settings.Secure.putIntForUser(mContentResolver, setting, value, mCurrentUser);
+        if (isGlobal(setting)) {
+            Settings.Global.putInt(mContentResolver, chomp(setting), value);
+        } else if (isSystem(setting)) {
+            Settings.System.putIntForUser(mContentResolver, chomp(setting), value, mCurrentUser);
+        } else {
+            Settings.Secure.putIntForUser(mContentResolver, setting, value, mCurrentUser);
+        }
     }
 
     @Override
@@ -185,14 +234,21 @@ public class TunerServiceImpl extends TunerService {
             mTunables.add(tunable);
             mLeakDetector.trackCollection(mTunables, "TunerService.mTunables");
         }
-        Uri uri = Settings.Secure.getUriFor(key);
+        final Uri uri;
+        if (isGlobal(key)) {
+            uri = Settings.Global.getUriFor(chomp(key));
+        } else if (isSystem(key)) {
+            uri = Settings.System.getUriFor(chomp(key));
+        } else {
+            uri = Settings.Secure.getUriFor(key);
+        }
         if (!mListeningUris.containsKey(uri)) {
             mListeningUris.put(uri, key);
-            mContentResolver.registerContentObserver(uri, false, mObserver, mCurrentUser);
+            mContentResolver.registerContentObserver(uri, false, mObserver,
+                    isGlobal(key) ? UserHandle.USER_ALL : mCurrentUser);
         }
         // Send the first state.
-        String value = DejankUtils.whitelistIpcs(() -> Settings.Secure
-                .getStringForUser(mContentResolver, key, mCurrentUser));
+        String value = DejankUtils.whitelistIpcs(() -> getValue(key));
         tunable.onTuningChanged(key, value);
     }
 
@@ -212,7 +268,9 @@ public class TunerServiceImpl extends TunerService {
         }
         mContentResolver.unregisterContentObserver(mObserver);
         for (Uri uri : mListeningUris.keySet()) {
-            mContentResolver.registerContentObserver(uri, false, mObserver, mCurrentUser);
+            String key = mListeningUris.get(uri);
+            mContentResolver.registerContentObserver(uri, false, mObserver,
+                    isGlobal(key) ? UserHandle.USER_ALL : mCurrentUser);
         }
     }
 
@@ -222,7 +280,7 @@ public class TunerServiceImpl extends TunerService {
         if (tunables == null) {
             return;
         }
-        String value = Settings.Secure.getStringForUser(mContentResolver, key, mCurrentUser);
+        String value = getValue(key);
         for (Tunable tunable : tunables) {
             tunable.onTuningChanged(key, value);
         }
@@ -230,8 +288,7 @@ public class TunerServiceImpl extends TunerService {
 
     private void reloadAll() {
         for (String key : mTunableLookup.keySet()) {
-            String value = Settings.Secure.getStringForUser(mContentResolver, key,
-                    mCurrentUser);
+            String value = getValue(key);
             for (Tunable tunable : mTunableLookup.get(key)) {
                 tunable.onTuningChanged(key, value);
             }
@@ -254,7 +311,7 @@ public class TunerServiceImpl extends TunerService {
             if (ArrayUtils.contains(RESET_BLACKLIST, key)) {
                 continue;
             }
-            Settings.Secure.putStringForUser(mContentResolver, key, null, user);
+            setValue(key, null);
         }
     }
 
@@ -266,8 +323,9 @@ public class TunerServiceImpl extends TunerService {
         @Override
         public void onChange(boolean selfChange, java.util.Collection<Uri> uris,
                 int flags, int userId) {
-            if (userId == ActivityManager.getCurrentUser()) {
-                for (Uri u : uris) {
+            for (Uri u : uris) {
+                String key = mListeningUris.get(u);
+                if (userId == ActivityManager.getCurrentUser() || isGlobal(key)) {
                     reloadSetting(u);
                 }
             }
