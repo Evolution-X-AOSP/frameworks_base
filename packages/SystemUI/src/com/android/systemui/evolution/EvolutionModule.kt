26eb5ba6532a4ed73e3929ddd8f86b9607f2f813
Steve Kondik <steve@cyngn.com>
2017-11-05 16:37:52 +0100

    SystemUI: Add LiveDisplay tile
    Author: Steve Kondik <steve@cyngn.com>
    Date:   Sun Nov 5 16:37:52 2017 +0100
    
        SystemUI: LiveDisplay tile
    
        Change-Id: I53d492e5cb9998268104d5750705aa5ed55d9658
        Signed-off-by: Joey Rizzoli <joey@lineageos.org>
    
    Author: Unpublished <unpublished@gmx.net>
    Date:   Fri Dec 29 23:00:01 2017 +0100
    
        livedisplay: Allow tile creation before boot completed phase [2/2]
    
        Change-Id: Ibef44d4e07da9baf296796515288c9d42aba8608
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Thu Feb 1 16:39:16 2018 +0000
    Edit:   Refactor for Oreo
    
    Change-Id: Ibef44d4e07da9baf296796515288c9d42aba8608
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Thu Feb 1 17:01:43 2018 +0000
    
        LiveDisplayTile: Avoid NPE during boot up phase
    
         * During boot, with LineageHW not yet initialized there is no way to
           check the current LiveDisplay mode. In such cases, fallback to AUTO.
    
        Change-Id: Ifc004c86dfd68f73b8a362a1c833cbc3e7397811
    
    Author: Han Wang <416810799@qq.com>
    Date:   Tue Jun 4 18:03:59 2019 +0200
    
        LiveDisplayTile: Report unavailable on HWC2
    
        Change-Id: Ic10381e25046a2338bc6bda36ecbb5d5cefc1ab8
    
    commit 9d95e3e77af723c6c783ee528d8d0a6f96369af5
    Author: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Date:   Mon Aug 26 14:21:17 2019 -0300
    
        QSTileHost: Recreate tiles when LiveDisplay gets initialized
    
        Revert "LiveDisplayTile: Refresh state after livedisplay initialization"
    
        This reverts commit 42e327581e1006c30339c886ea381c162e3f5299.
    
        Change-Id: I9976076593684418c318e66803cf6e4f450975b2
    
    commit 6b67f3b98fb5b5db4daf44f43afb6323f5bed57c
    Author: DennySPB <dennyspb@gmail.com>
    Date:   Mon Sep 23 16:52:22 2019 +0300
    
        LiveDisplayTile: Fixup modes cycling for aosp implementation
    
        Signed-off-by: DennySPB <dennyspb@gmail.com>
        Change-Id: I5edcef553ae32b4eb72882ef81ffe56f5074fc0d
    
    commit 583bb96890ca118862513aa5bad9f71a97d4973b
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Fri Sep 6 23:41:14 2019 +0200
    
        LiveDisplay: Switch to ColorDisplayManager
    
        * ColorDisplayController is dead
    
        Change-Id: I9b5fdc3546bea7aabd65877c1862d9eaebbd4af3
    
    commit e34cc00e261f069ebe0858c07bb39896ba67b490
    Author: dianlujitao <dianlujitao@lineageos.org>
    Date:   Wed Aug 7 19:38:32 2019 +0800
    
        LiveDisplayTile: Refresh state after livedisplay initialization
    
         * SystemUI starts earlier than LiveDisplayConfig initialized, so
           there's no easy way to determin whether to make it available.
         * Instead, receive broadcast incicating initialization completed from
           LiveDisplayService and gray out on desire.
         * Outdoor mode gets fixed by the way.
    
        Change-Id: I55cea14c9248da1186e6c0c1b9e719a2bcf5444d
    
    commit 462732cc4fd2e13b9e3da7a327059a342bd5b17a
    Author: dianlujitao <dianlujitao@lineageos.org>
    Date:   Wed Aug 7 00:27:50 2019 +0800
    
        LiveDisplayTile: Enable for outdoor mode and skip night display on HWC2
    
        Change-Id: I00ab654033827388f8063eafb98a89315cb47aa9
    
    [Stallix] Adapt for A13 QPR3
    
    Change-Id: Ib22a230460fd1b22d94ff412bc30aa7abe3ba9b3

--- packages/SystemUI/src/com/android/systemui/evolution/EvolutionModule.kt
+++ packages/SystemUI/src/com/android/systemui/evolution/EvolutionModule.kt
@@ -21,6 +21,7 @@ import com.android.systemui.qs.tiles.AmbientDisplayTile
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.HeadsUpTile
+import com.android.systemui.qs.tiles.LiveDisplayTile
 import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.RebootTile
 import com.android.systemui.qs.tiles.SoundSearchTile
@@ -61,6 +62,12 @@ interface EvolutionModule {
     @IntoMap
     @StringKey(HeadsUpTile.TILE_SPEC)
     fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
+
+    /** Inject LiveDisplayTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(LiveDisplayTile.TILE_SPEC)
+    fun bindLiveDisplayTile(liveDisplayTile: LiveDisplayTile): QSTileImpl<*>
 
     /** Inject PowerShareTile into tileMap in QSModule */
     @Binds
