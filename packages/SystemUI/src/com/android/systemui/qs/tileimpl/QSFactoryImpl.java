ff932d3f90e81243a4ec3729e20345ca75e29f7d
Rituj Beniwal <ritujbeniwal@gmail.com>
2017-11-09 17:31:07 +0530

    base: Screen Stabilization [1/2]
    * New Settings menu integrated into QS itself (long click)
    * Thanks to SuperDroidBond for keeping this alive till Pie
    
    Change-Id: Ibccb8d78d79938f42241ea3690ea66c3aace89ea
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    
    ScreenStabilization Tile: Fix layout
    
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Change-Id: I973fd8dbef971ed726ebcf323fbfb35aed4495bf

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -63,6 +63,7 @@ import com.android.systemui.qs.tiles.ReadingModeTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
+import com.android.systemui.qs.tiles.ScreenStabilizationTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.SleepScreenTile;
 import com.android.systemui.qs.tiles.SmartPixelsTile;
@@ -138,6 +139,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<UsbTetherTile> mUsbTetherTileProvider;
     private final Provider<VolumeTile> mVolumeTileProvider;
     private final Provider<VpnTile> mVpnTileProvider;
+    private final Provider<ScreenStabilizationTile> mScreenStabilizationTileProvider;
 
     private QSTileHost mHost;
 
@@ -191,7 +193,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<CalcTile> calcTileProvider,
             Provider<CalendarTile> calendarTileProvider,
             Provider<CameraTile> cameraTileProvider,
-            Provider<CallTile> callTileProvider) {
+            Provider<CallTile> callTileProvider,
+            Provider<ScreenStabilizationTile> screenStabilizationTileProvider) {
         mWifiTileProvider = wifiTileProvider;
         mBluetoothTileProvider = bluetoothTileProvider;
         mCellularTileProvider = cellularTileProvider;
@@ -243,6 +246,7 @@ public class QSFactoryImpl implements QSFactory {
         mUsbTetherTileProvider = usbTetherTileProvider;
         mVolumeTileProvider = volumeTileProvider;
         mVpnTileProvider = vpnTileProvider;
+        mScreenStabilizationTileProvider = screenStabilizationTileProvider;
     }
 
     public void setHost(QSTileHost host) {
@@ -359,6 +363,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mCameraTileProvider.get();
             case "call":
                 return mCallTileProvider.get();
+            case "screenstabilization":
+                return mScreenStabilizationTileProvider.get();
         }
 
         // Intent tiles.
