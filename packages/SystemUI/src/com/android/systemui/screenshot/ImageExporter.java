119e56288245f1cf0b666844df9d75e74c2b685a
ezio84 <brabus84@gmail.com>
2017-12-19 14:37:35 +0100

    Screenshot: Append app name to filename
    Author: ezio84 <brabus84@gmail.com>
    Date:   Tue Dec 19 14:37:35 2017 +0100
    
        Screenshot: append app name to filename
    
        Kang Samsung's idea.
    
        Change-Id: Ibc6a52b5e2597e6014a2da6a4211febe17ec02c7
    
    Author: Wang Han <wanghan1995315@gmail.com>
    Date:   Fri May 11 12:47:40 2018 +0200
    
        SystemUI: Don't append app name to file on lockscreen
    
        When device is locked, current implementation will append the current
        app name to file when taking screenshot. This causes information leaks.
    
        To prevent this, we check for isKeyGuardLocked() and fall back not to
        append the app name when taking screenshot on lockscreen.
    
        Change-Id: I04498faf51986e1a3723a32befd97d29e1f3fe58
    
    Author: DroidFreak32 <rushabshah32@gmail.com>
    Date:   Thu Oct 4 11:33:07 2018 +0530
    
        Screenshot: Append app name after screenshot date.
    
        Change-Id: I2ee5d65006ff22dfa381aae50e5757b8e7596e91
    
    Author: ezio84 <brabus84@gmail.com>
    Date:   Sat Jun 16 16:57:19 2018 +0200
    
        GlobalScreenshot: Fix screenshot not saved with some languages
    
        like Virgin Islands English when taking a screenshot of the Settings app
    
        Change-Id: Ic04f66f5813b9597c96835d15c74509c93530a5c
    
    Author: Han Wang <416810799@qq.com>
    Date:   Mon May 25 11:18:01 2020 +0200
    
        SystemUI: Do not convert application name to ISO-8859-15
    
         * ISO-8859-15 does not handle CJK chars, so all of them are
           'unknown' and empty eventually.
    
        Change-Id: I27c689f5d737190d381146e49c1989e998b4f623
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Mon Aug 24 17:52:39 2020 +0200
    
        SystemUI: Use TaskStackChangeListener API for contextual screenshot package name
    
        * This should hopefully fix mislabeled screenshots.
    
        Change-Id: Ifde106c0e306a6700081e4d724c1cf0c06dd126c
    
    [someone5678]
    * Adapt to A14 QPR2
    
    Change-Id: Ic2a42536bf763a0c6f7fb9ac4bbbe84f73723f91
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/ImageExporter.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ImageExporter.java
@@ -65,6 +65,9 @@ public class ImageExporter {
 
     // ex: 'Screenshot_20201215-090626.png'
     private static final String FILENAME_PATTERN = "Screenshot_%1$tY%<tm%<td-%<tH%<tM%<tS.%2$s";
+    // ex: 'Screenshot_20201215-090626_Settings.png'
+    private static final String FILENAME_WITH_APP_NAME_PATTERN =
+            "Screenshot_%1$tY%<tm%<td-%<tH%<tM%<tS_%2$s.%3$s";
     // ex: 'Screenshot_20201215-090626-display-1.png'
     private static final String CONNECTED_DISPLAY_FILENAME_PATTERN =
             "Screenshot_%1$tY%<tm%<td-%<tH%<tM%<tS-display-%2$d.%3$s";
@@ -150,11 +153,12 @@ public class ImageExporter {
      * @param executor the thread for execution
      * @param bitmap the bitmap to export
      * @param displayId the display id the bitmap comes from.
+     * @param foregroundAppName the name of app running in foreground
      * @return a listenable future result
      */
     public ListenableFuture<Result> export(Executor executor, UUID requestId, Bitmap bitmap,
-            UserHandle owner, int displayId) {
-        return export(executor, requestId, bitmap, ZonedDateTime.now(), owner, displayId);
+            UserHandle owner, int displayId, String foregroundAppName) {
+        return export(executor, requestId, bitmap, ZonedDateTime.now(), owner, displayId, foregroundAppName);
     }
 
     /**
@@ -162,13 +166,14 @@ public class ImageExporter {
      *
      * @param executor the thread for execution
      * @param bitmap the bitmap to export
+     * @param foregroundAppName the name of app running in foreground
      *
      * @return a listenable future result
      */
     ListenableFuture<Result> export(Executor executor, UUID requestId, Bitmap bitmap,
-            ZonedDateTime captureTime, UserHandle owner, int displayId) {
+            ZonedDateTime captureTime, UserHandle owner, int displayId, String foregroundAppName) {
 
-        final Task task = new Task(mResolver, requestId, bitmap, captureTime, mCompressFormat,
+        final Task task = new Task(mResolver, requestId, bitmap, captureTime, foregroundAppName, mCompressFormat,
                 mQuality, /* publish */ true, owner, mFlags, displayId);
 
         return CallbackToFutureAdapter.getFuture(
@@ -221,7 +226,7 @@ public class ImageExporter {
         private final FeatureFlags mFlags;
 
         Task(ContentResolver resolver, UUID requestId, Bitmap bitmap, ZonedDateTime captureTime,
-                CompressFormat format, int quality, boolean publish, UserHandle owner,
+                String foregroundAppName, CompressFormat format, int quality, boolean publish, UserHandle owner,
                 FeatureFlags flags, int displayId) {
             mResolver = resolver;
             mRequestId = requestId;
@@ -230,7 +235,7 @@ public class ImageExporter {
             mFormat = format;
             mQuality = quality;
             mOwner = owner;
-            mFileName = createFilename(mCaptureTime, mFormat, displayId);
+            mFileName = createFilename(mCaptureTime, mFormat, displayId, foregroundAppName);
             mPublish = publish;
             mFlags = flags;
         }
@@ -375,12 +380,15 @@ public class ImageExporter {
     }
 
     @VisibleForTesting
-    static String createFilename(ZonedDateTime time, CompressFormat format, int displayId) {
+    static String createFilename(ZonedDateTime time, CompressFormat format, int displayId,  
+            String foregroundAppName) {
         if (displayId == Display.DEFAULT_DISPLAY) {
+            if (foregroundAppName != null) {
+                return String.format(FILENAME_WITH_APP_NAME_PATTERN, time, foregroundAppName, fileExtension(format));
+            }
             return String.format(FILENAME_PATTERN, time, fileExtension(format));
         }
-        return String.format(CONNECTED_DISPLAY_FILENAME_PATTERN, time, displayId,
-            fileExtension(format));
+        return String.format(CONNECTED_DISPLAY_FILENAME_PATTERN, time, displayId, fileExtension(format));
     }
 
     static ContentValues createMetadata(ZonedDateTime captureTime, CompressFormat format,
