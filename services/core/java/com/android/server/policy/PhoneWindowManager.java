36fbcfd7db9f4d46d0f759f38442ff8eb262439f
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2017-12-28 21:55:45 +0100

    Allow screen unpinning on devices without navbar
    Change-Id: Iedfc08f4d95bbee3c8578c0d2450b90739e63603
    
    Screen Pinning: Show correct text for on screen nav.
    
      Similar to I09c2ef661bff272cb4f7ca43bac0e45f4b20a4d4,
      we're not getting an instance of PhoneWindowManager which
      we can rely on to update dynamically.
    
      TICKET: OPO-393
    
    Change-Id: Iacf8221066461fb6940dd88432e665812545c3ff

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1833,6 +1833,14 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mDefaultDisplayPolicy.takeScreenshot(type, source);
     }
 
+    Runnable mBackLongPress = new Runnable() {
+        public void run() {
+            if (unpinActivity(false)) {
+                return;
+            }
+        }
+    };
+
     @Override
     public void showGlobalActions() {
         mHandler.removeMessages(MSG_DISPATCH_SHOW_GLOBAL_ACTIONS);
@@ -3492,6 +3500,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mPendingCapsLockToggle = false;
         }
 
+        if (keyCode == KeyEvent.KEYCODE_BACK && !down) {
+            mHandler.removeCallbacks(mBackLongPress);
+        }
+
         if (isUserSetupComplete() && !keyguardOn) {
             if (mModifierShortcutManager.interceptKey(event)) {
                 dismissKeyboardShortcutsMenu();
@@ -3879,6 +3891,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     return true;
                 }
         }
+
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (unpinActivity(true)) {
+                if (down && repeatCount == 0) {
+                    mHandler.postDelayed(mBackLongPress, 2000);
+                }
+            }
+        }
+
         if (isValidGlobalKey(keyCode)
                 && mGlobalKeyManager.handleGlobalKey(mContext, keyCode, event)) {
             return true;
@@ -4057,6 +4078,22 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 }
             } catch (Exception e) {
                 Slog.w(TAG, "Could not dispatch event to device key handler", e);
+            }
+        }
+        return false;
+    }
+
+    private boolean unpinActivity(boolean checkOnly) {
+        if (!hasNavigationBar()) {
+            try {
+                if (ActivityTaskManager.getService().isInLockTaskMode()) {
+                    if (!checkOnly) {
+                        ActivityTaskManager.getService().stopSystemLockTaskMode();
+                    }
+                    return true;
+                }
+            } catch (RemoteException e) {
+                // ignore
             }
         }
         return false;
