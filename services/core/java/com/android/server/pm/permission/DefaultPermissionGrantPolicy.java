374bae77e1285b66dcaafd8c3a5352800fcdbba2
Alex Cruz <du.alexcruz@gmail.com>
2018-01-18 23:14:52 -0500

    Fix Google dialer FC due to missing permissions
    In past verisons you could not download the Google dialer unless
    you were on a Nexus/Pixel. Now that we're able to download it on pretty
    much every device running 8.1, we need to protect the user from the user.
    
    Some users are smarter than others, is that simple. Some users will realize
    that the reason this FCs is because of missing permissions. Some won't be that
    smart and will come to us and report this as a bug. This commit fixes this.
    
    Change-Id: I125b42f5c68bd84aece41f2f9124716af06a88d6

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -868,6 +868,14 @@ public final class DefaultPermissionGrantPolicy {
             mPermissionGrantedCallback.onDefaultRuntimePermissionsGranted(userId);
         }
 
+        // Google dialer
+        PackageParser.Package googledialerPackage = getSystemPackage("com.google.android.dialer");
+        if (googledialerPackage != null && doesPackageSupportRuntimePermissions(googledialerPackage)) {
+            grantRuntimePermissions(googledialerPackage, PHONE_PERMISSIONS, true, userId);
+            grantRuntimePermissions(googledialerPackage, CONTACTS_PERMISSIONS, true, userId);
+            grantRuntimePermissions(googledialerPackage, SMS_PERMISSIONS, userId);
+        }
+
         // Mediascanner
         PackageParser.Package mediascannerPackage = getDefaultProviderAuthorityPackage(
                 "com.android.providers.media.MediaProvider", userId);
