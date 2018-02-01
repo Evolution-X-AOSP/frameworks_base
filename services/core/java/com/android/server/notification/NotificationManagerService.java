13cb6de72cdc22464654de6d0107cbd361a2a1ce
ezio84 <brabus84@gmail.com>
2018-02-01 16:49:32 +0100

    base: Add api to force show led lights
    We can use this to show a led color preview when using the
    color picker, or any other stuff.
    
    use -1 as color int to switch off
    
    Change-Id: Id59f480e74a30a875c6640fdfc2e746d98a8513c
    
    Led light preview: switch led off before changing the color
    
    on some devices this can avoid glitches
    
    Change-Id: I4ef51d6b074e344d48f4a814549db2dad9ef9d0a
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -5571,6 +5571,16 @@ public class NotificationManagerService extends SystemService {
             Slog.e(TAG, "exiting pullStats: bad request");
             return 0;
         }
+
+        @Override
+        public void forceShowLedLight(int color) {
+            forceShowLed(color);
+        }
+
+        @Override
+        public void forcePulseLedLight(int color, int onTime, int offTime) {
+            forcePulseLed(color, onTime, offTime);
+        }
     };
 
     protected void checkNotificationListenerAccess() {
@@ -7527,6 +7537,24 @@ public class NotificationManagerService extends SystemService {
         }
         record.setAudiblyAlerted(buzz || beep);
         return buzzBeepBlink;
+    }
+
+    private void forceShowLed(int color) {
+        if (color != -1) {
+            mNotificationLight.turnOff();
+            mNotificationLight.setColor(color);
+        } else {
+            mNotificationLight.turnOff();
+        }
+    }
+
+    private void forcePulseLed(int color, int onTime, int offTime) {
+        if (color != -1) {
+            mNotificationLight.turnOff();
+            mNotificationLight.setFlashing(color, LogicalLight.LIGHT_FLASH_TIMED, onTime, offTime);
+        } else {
+            mNotificationLight.turnOff();
+        }
     }
 
     @GuardedBy("mNotificationLock")
