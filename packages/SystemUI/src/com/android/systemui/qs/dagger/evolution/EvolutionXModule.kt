75fec045dea57f134a22fd9c439ab93537ca4d37
ezio84 <brabus84@gmail.com>
2018-02-09 18:20:23 +0100

    Add Google Sound Search tile
     * SoundSearchTile: Make default state inactive
     * Disable long click for SoundSearch tile
    
    Change-Id: Id893722185fa4f111f2730849d1c8abfffa121dc
    
    [someone5678] Adapt to QPR3
    
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/dagger/evolution/EvolutionXModule.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/evolution/EvolutionXModule.kt
@@ -22,6 +22,7 @@ import com.android.systemui.qs.tiles.AmbientDisplayTile
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.SoundTile
+import com.android.systemui.qs.tiles.SoundSearchTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
 import dagger.Binds
@@ -73,4 +74,10 @@ interface EvolutionXModule {
     @IntoMap
     @StringKey(AODTile.TILE_SPEC)
     fun bindAODTile(aodTile: AODTile): QSTileImpl<*>
+
+    /** Inject SoundSearchTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(SoundSearchTile.TILE_SPEC)
+    fun bindSoundSearchTile(soundSearchTile: SoundSearchTile): QSTileImpl<*>
 }
