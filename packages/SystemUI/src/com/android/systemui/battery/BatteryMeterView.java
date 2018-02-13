bf28259e0bd7ab215918d28640ade413037b424e
ezio84 <brabus84@gmail.com>
2018-02-14 00:24:18 +0100

    Battery Styles: Show a bolt ⚡ when charging
    * @neobuddy89: Only for hidden and text battery styles.
    
    Change-Id: I27f745e3458fda7c415a75d6e52f183e327cb498
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -246,6 +246,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
             mThemedDrawable.setBatteryLevel(mLevel);
             mCircleDrawable.setBatteryLevel(mLevel);
             mFullCircleDrawable.setBatteryLevel(mLevel);
+            updatePercentText();
         }
         if (mCharging != pluggedIn) {
             mCharging = pluggedIn;
@@ -253,7 +254,6 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
             mCircleDrawable.setCharging(mCharging);
             mFullCircleDrawable.setCharging(mCharging);
             updateShowPercent();
-        } else {
             updatePercentText();
         }
     }
@@ -321,7 +321,13 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
     }
 
     private void setPercentTextAtCurrentLevel() {
-        String percentText = NumberFormat.getPercentInstance().format(mLevel / 100f);
+        // Use the high voltage symbol ⚡ (u26A1 unicode) but prevent the system
+        // to load its emoji colored variant with the uFE0E flag
+        String bolt = "\u26A1\uFE0E";
+        CharSequence mChargeIndicator = mCharging && (mBatteryStyle == BATTERY_STYLE_HIDDEN ||
+            mBatteryStyle == BATTERY_STYLE_TEXT) ? (bolt + " ") : "";
+        String percentText = mChargeIndicator +
+            NumberFormat.getPercentInstance().format(mLevel / 100f);
         // Setting text actually triggers a layout pass (because the text view is set to
         // wrap_content width and TextView always relayouts for this). Avoid needless
         // relayout if the text didn't actually change.
