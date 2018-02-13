b9d33201dd79e0eab600a3f947ca39446dd6614c
ezio84 <brabus84@gmail.com>
2018-02-14 00:24:18 +0100

    Text battery style: show a ⚡when charging
    Change-Id: I74ad0ceef7f8617526e157ebd61747e3fe1a2042

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -262,6 +262,10 @@ public class BatteryMeterView extends LinearLayout implements
             updateShowPercent();
         }
         mLevel = level;
+        if (mCharging != pluggedIn) {
+            mCharging = pluggedIn;
+            updateShowPercent();
+        }
         updatePercentText();
         setContentDescription(
                 getContext().getString(charging ? R.string.accessibility_battery_level_charging
@@ -280,7 +284,13 @@ public class BatteryMeterView extends LinearLayout implements
 
     private void updatePercentText() {
         if (mBatteryPercentView != null) {
-            mBatteryPercentView.setText(
+            // Use the high voltage symbol ⚡ (u26A1 unicode) but prevent the system
+            // to load its emoji colored variant with the uFE0E flag
+            String bolt = "\u26A1\uFE0E";
+            CharSequence mChargeIndicator =
+                    mCharging && mDrawable.getMeterStyle() == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT
+                    ? (bolt + " ") : "";
+            mBatteryPercentView.setText(mChargeIndicator +
                     NumberFormat.getPercentInstance().format(mLevel / 100f));
         }
     }
@@ -325,8 +335,9 @@ public class BatteryMeterView extends LinearLayout implements
         if (mBatteryPercentView != null) {
             final Resources res = getContext().getResources();
             final int startPadding = res.getDimensionPixelSize(R.dimen.battery_level_padding_start);
-            mBatteryPercentView.setPaddingRelative(startPadding, 0,
-                    /*(mDrawable.getMeterStyle() != BatteryMeterDrawableBase.BATTERY_STYLE_TEXT ? endPadding : */0/*)*/, 0);
+            mBatteryPercentView.setPaddingRelative(
+                    mDrawable.getMeterStyle() == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT ? 0 : startPadding,
+                    0, 0, 0);
         }
     }
 
@@ -487,5 +498,6 @@ public class BatteryMeterView extends LinearLayout implements
 
         scaleBatteryMeterViews();
         updateShowPercent();
+        updatePercentText();
     }
 }
