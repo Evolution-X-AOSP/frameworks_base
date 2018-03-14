c38198381db4bf06deb356fe36a699a872b4aadf
Ali B <abittin@gmail.com>
2018-03-14 20:06:26 +0300

    base: Ambient Display battery toggle [1/2]
    Change-Id: Ieac818befa75c45aa35e0388a11681795aafb891
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5204,6 +5204,12 @@ public final class Settings {
         public static final String BLUETOOTH_SHOW_BATTERY = "bluetooth_show_battery";
 
         /**
+         * Whether to show battery percent on ambient bottom
+         * @hide
+         */
+        public static final String AMBIENT_BATTERY_PERCENT = "ambient_battery_percent";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
