41da9ba980faaf228ead0f3b2ef7e39d979451b4
ezio84 <brabus84@gmail.com>
2018-04-04 10:39:22 +0200

    QS: add right/left padding even if it's 0 for collapsed statusbar
    For devices without a rounded display we are setting rounded_corner_content_padding
    to 0 to avoid useless left/right padding in the statusbar.
    But this looks odd in the expanded statusbar / qs panel because other elements
    like the brightness slider buttons or the date have the padding anyway.
    
    We can handle those cases separately.
    
    Thanks @anirudhgupta109 for the suggestion.
    
    Change-Id: I77d6f10a8fb0c5a529f43ea6311bc98e37815106

--- packages/SystemUI/src/com/android/systemui/ScreenDecorations.java
+++ packages/SystemUI/src/com/android/systemui/ScreenDecorations.java
@@ -368,25 +368,28 @@ public class ScreenDecorations extends SystemUI implements Tunable {
         // screen decorations overlay.
         int padding = mContext.getResources().getDimensionPixelSize(
                 R.dimen.rounded_corner_content_padding);
-        if (padding != 0) {
-            setupStatusBarPadding(padding);
-        }
+        int qsPadding = mContext.getResources().getDimensionPixelSize(
+                R.dimen.qs_corner_content_padding);
+        setupStatusBarPadding(padding, qsPadding);
 
     }
 
-    private void setupStatusBarPadding(int padding) {
+    private void setupStatusBarPadding(int padding, int qsPadding) {
         // Add some padding to all the content near the edge of the screen.
         StatusBar sb = getComponent(StatusBar.class);
         View statusBar = (sb != null ? sb.getStatusBarWindow() : null);
         if (statusBar != null) {
-            TunablePadding.addTunablePadding(statusBar.findViewById(R.id.keyguard_header), PADDING,
-                    padding, FLAG_END);
-
             FragmentHostManager fragmentHostManager = FragmentHostManager.get(statusBar);
-            fragmentHostManager.addTagListener(CollapsedStatusBarFragment.TAG,
-                    new TunablePaddingTagListener(padding, R.id.status_bar));
-            fragmentHostManager.addTagListener(QS.TAG,
-                    new TunablePaddingTagListener(padding, R.id.header));
+            if (padding != 0) {
+                TunablePadding.addTunablePadding(statusBar.findViewById(R.id.keyguard_header), PADDING,
+                        padding, FLAG_END);
+                 fragmentHostManager.addTagListener(CollapsedStatusBarFragment.TAG,
+                        new TunablePaddingTagListener(padding, R.id.status_bar));
+            }
+            if (qsPadding != 0) {
+                fragmentHostManager.addTagListener(QS.TAG,
+                        new TunablePaddingTagListener(qsPadding, R.id.header));
+            }
         }
     }
 
