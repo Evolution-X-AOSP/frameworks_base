9d7c237a223f6f17690a9df2798e4b83fddcf774
calebcabob <calphonic@gmail.com>
2018-04-10 19:58:05 -0400

    Permissions: Grant Pixel's Wallpaper app access to SD
    Change-Id: If57e797dd2f8574d11258eeb207371a27622fcd1
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -875,6 +875,11 @@ public final class DefaultPermissionGrantPolicy {
             grantRuntimePermissions(googledialerPackage, CONTACTS_PERMISSIONS, true, userId);
             grantRuntimePermissions(googledialerPackage, SMS_PERMISSIONS, userId);
         }
+        // Pixel Wallpaper
+        PackageParser.Package pixelwallsPackage = getDefaultProviderAuthorityPackage("com.google.android.apps.wallpaper", userId);
+        if (pixelwallsPackage != null) {
+            grantRuntimePermissions(pixelwallsPackage, STORAGE_PERMISSIONS, true, userId);
+        }
 
         // Mediascanner
         PackageParser.Package mediascannerPackage = getDefaultProviderAuthorityPackage(
