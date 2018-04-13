ead07f706196e52dc934445f1b71b755a0c1b6a6
maxwen <max.weninger@gmail.com>
2018-04-13 03:32:51 +0200

    SystemUI: QS: CaffeineTile: Stop crashing during boot
    this triggers my ocd
    continues a579c2430f1a0bf088023b4283dcec37776c070d
    
    Change-Id: Idaab499be53faeb7819e2a059d5fd6bf1d5d2968
    Signed-off-by: Josh Fox(XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
@@ -176,6 +176,9 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
+        if (mWakeLock == null) {
+            return;
+        }
         state.value = mWakeLock.isHeld();
         if (state.value) {
             state.label = formatValueWithRemainingTime();
