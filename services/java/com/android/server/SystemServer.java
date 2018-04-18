0cfcc900c5eae5cc034484ea5cea3d677326a713
Adin Kwok <adin.kwok@carbonrom.org>
2018-04-18 01:05:27 -0700

    Smart Pixels: Switch to registered receiver
    Switching to a registered receiver allows to properly handle updates
    on enabling of battery saver mode and switching of users.
    
    Also only update screen filter with burn-in protection when the
    device is in an interactive state.
    
    Test: Service starts after rebooting with it enabled
          Service starts on battery saver mode (user toggle)
          Service starts on battery saver mode (auto-enabled)
          Service re-adjusts on user switch to current user settings
          Filter updates after selected timeout
    
    Change-Id: Iced17fd5cc49e0163754bf75782f8465b54e859b
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -164,6 +164,7 @@ import com.android.server.security.FileIntegrityService;
 import com.android.server.security.KeyAttestationApplicationIdProviderService;
 import com.android.server.security.KeyChainSystemService;
 import com.android.server.signedconfig.SignedConfigService;
+import com.android.server.smartpixels.SmartPixelsReceiver;
 import com.android.server.soundtrigger.SoundTriggerService;
 import com.android.server.soundtrigger_middleware.SoundTriggerMiddlewareService;
 import com.android.server.statusbar.StatusBarManagerService;
@@ -362,6 +363,8 @@ public final class SystemServer {
     private EntropyMixer mEntropyMixer;
     private DataLoaderManagerService mDataLoaderManagerService;
     private long mIncrementalServiceHandle = 0;
+
+    private SmartPixelsReceiver mSmartPixelsReceiver;
 
     private boolean mOnlyCore;
     private boolean mFirstBoot;
@@ -2553,6 +2556,9 @@ public final class SystemServer {
                 setIncrementalServiceSystemReady(mIncrementalServiceHandle);
                 t.traceEnd();
             }
+
+            mSmartPixelsReceiver = new SmartPixelsReceiver(context);
+
         }, t);
 
         t.traceEnd(); // startOtherServices
