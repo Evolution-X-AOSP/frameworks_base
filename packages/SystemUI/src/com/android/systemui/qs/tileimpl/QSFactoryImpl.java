38f125bf929decf5663ac5453b27f8723627e5e8
Marko Man <darkobas@gmail.com>
2018-04-18 13:22:52 +0200

    SystemUI: Add AOD QS tile
    Squashed with:
    
    Author: Marko Man <darkobas@gmail.com>
    Date:   Wed Apr 18 13:22:52 2018 +0200
    
        base: SystemUI: add qs AOD tile
    
        Change-Id: I0d9948dbd48e309d8fcc9b2c33be8b17810dc5b0
    
    Author: shagbag913 <sh4gbag913@gmail.com>
    Date:   Sun Mar 1 15:49:17 2020 -0500
    
        AODTile: disable tile when power save mode is on
    
        AOD is not allowed when power save mode is active, reflect that upon the
        tile.
    
        Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
        Change-Id: I3fc7f2a33d25a1616e8df5698ca2bc2a23d45cbb
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Sun Apr 12 19:04:24 2020 +0100
    
        AODTile: Explicitely disable long clicks
    
        Returning null in the getLongClickIntent() method is not enough to disable
        tile long clock and results in crashes:
    
          E AndroidRuntime: FATAL EXCEPTION: main
          E AndroidRuntime: Process: com.android.systemui, PID: 1391
          E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'java.lang.String android.content.Intent.resolveTypeIfNeeded(android.content.ContentResolver)' on a null object reference
          E AndroidRuntime:     at android.app.ApplicationPackageManager.queryIntentActivitiesAsUser(ApplicationPackageManager.java:1062)
          E AndroidRuntime:     at com.android.systemui.ActivityIntentHelper.getTargetActivityInfo(ActivityIntentHelper.java:70)
          E AndroidRuntime:     at com.android.systemui.ActivityIntentHelper.wouldLaunchResolverActivity(ActivityIntentHelper.java:47)
          E AndroidRuntime:     at com.android.systemui.statusbar.phone.StatusBar.startActivityDismissingKeyguard(StatusBar.java:2724)
          E AndroidRuntime:     at com.android.systemui.statusbar.phone.StatusBar.startActivityDismissingKeyguard(StatusBar.java:2709)
          E AndroidRuntime:     at com.android.systemui.statusbar.phone.StatusBar.startActivityDismissingKeyguard(StatusBar.java:2716)
          E AndroidRuntime:     at com.android.systemui.statusbar.phone.StatusBar.handleStartActivityDismissingKeyguard(StatusBar.java:3159)
          E AndroidRuntime:     at com.android.systemui.statusbar.phone.StatusBar.lambda$postStartActivityDismissingKeyguard$26$StatusBar(StatusBar.java:3155)
          E AndroidRuntime:     at com.android.systemui.statusbar.phone.-$$Lambda$StatusBar$CSd9n4rtnrfFyOdT2eTFRNUO5xM.run(Unknown Source:4)
          E AndroidRuntime:     at android.os.Handler.handleCallback(Handler.java:883)
          E AndroidRuntime:     at android.os.Handler.dispatchMessage(Handler.java:100)
          E AndroidRuntime:     at android.os.Looper.loop(Looper.java:214)
          E AndroidRuntime:     at android.app.ActivityThread.main(ActivityThread.java:7356)
          E AndroidRuntime:     at java.lang.reflect.Method.invoke(Native Method)
          E AndroidRuntime:     at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:491)
          E AndroidRuntime:     at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:930)
    
        Change-Id: Ia7d4da4723600c47bedbfeb4b9152924a50d52dd
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Tue Jun 9 21:47:24 2020 +0100
    
        AODTile: Rewrite AOD setting handling
    
        This fixes the tile status on clean installs, which was showing as
        active despite AOD being disabled.
    
        Change-Id: I148cfb34938165395bc52c830d1f2e374b5427df
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Thu Mar 18 12:22:21 2021 +0100
    
        SystemUI: AODTile: Use SecureSetting to change doze setting
    
        Change-Id: Ic39d26ac8a37041b75862627a497274b24f3362c
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Thu Mar 18 12:23:45 2021 +0100
    
        SystemUI: AODTile: Actually start using SecureSetting
    
        Change-Id: I37f0194fcb54db1cf3cae221f7f702d398834ced
    
    Change-Id: I5249106fea3518e8fa69990d1aa13c21cc542cb1

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -31,6 +31,7 @@ import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.AlarmTile;
 import com.android.systemui.qs.tiles.AmbientDisplayTile;
 import com.android.systemui.qs.tiles.AntiFlickerTile;
+import com.android.systemui.qs.tiles.AODTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CameraToggleTile;
@@ -126,6 +127,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<ReadingModeTile> mReadingModeTileProvider;
     private final Provider<AntiFlickerTile> mAntiFlickerTileProvider;
     private final Provider<DataSwitchTile> mDataSwitchTileProvider;
+    private final Provider<AODTile> mAODTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
@@ -177,7 +179,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<LiveDisplayTile> liveDisplayTileProvider,
             Provider<ReadingModeTile> readingModeTileProvider,
             Provider<AntiFlickerTile> antiFlickerTileProvider,
-            Provider<DataSwitchTile> dataSwitchTileProvider) {
+            Provider<DataSwitchTile> dataSwitchTileProvider,
+            Provider<AODTile> aodTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
 
@@ -225,6 +228,7 @@ public class QSFactoryImpl implements QSFactory {
         mReadingModeTileProvider = readingModeTileProvider;
         mAntiFlickerTileProvider = antiFlickerTileProvider;
         mDataSwitchTileProvider = dataSwitchTileProvider;
+        mAODTileProvider = aodTileProvider;
     }
 
     /** Creates a tile with a type based on {@code tileSpec} */
@@ -329,6 +333,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mAntiFlickerTileProvider.get();
             case "dataswitch":
                 return mDataSwitchTileProvider.get();
+            case "aod":
+                return mAODTileProvider.get();
         }
         // Custom tiles
         if (tileSpec.startsWith(CustomTile.PREFIX)) {
