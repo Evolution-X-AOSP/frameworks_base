6f408f3e1fa32669ca507bc12149f26bc6784dd0
maxwen <max.weninger@gmail.com>
2018-05-23 21:55:04 +0200

    SystemUI: prevent that dt2s triggers panel expand
    http://jira.omnirom.org/browse/OMNI-1466
    
    Change-Id: If304577d666ebf8423cca1298dfbe044843657cc
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -335,6 +335,11 @@ public class NotificationPanelView extends PanelView implements
                 if (mPowerManager != null) {
                     mPowerManager.goToSleep(e.getEventTime());
                 }
+                // quick pulldown can trigger those values
+                // on double tap - so reset them
+                mQsExpandImmediate = false;
+                requestPanelHeightUpdate();
+                setListening(false);
                 return true;
             }
         });
