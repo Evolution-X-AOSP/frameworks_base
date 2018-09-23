b600f1de69eb6276f5c33eeff165c632a85a74f1
ezio84 <brabus84@gmail.com>
2018-09-24 00:21:01 +0530

    base: Option for Up, Down as well for Network Traffic [1/2]
    [spezi77]
    * Adapt for OOS style network traffic indicator
    * For now skipped logic to show arrow indicators (follows in a separate commit)
    
    Co-authored-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5572,6 +5572,12 @@ public final class Settings {
         public static final String FORCE_SHOW_NAVBAR = "navbar_visibility";
 
         /**
+         * What to show in network traffic indicator in statusbar
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_TYPE = "network_traffic_type";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
