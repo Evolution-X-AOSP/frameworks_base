a76a40a934eae552b7ab5e66a10942bdd3b8a479
xiaohuin <xiaohuin@codeaurora.org>
2018-09-27 10:29:07 +0800

    SystemUI: fix notification overlap with PIN screen
    Don't expand notification if UE is SIM locked
    
    CRs-Fixed: 2324607
    Change-Id: I013a37bf0dd65282194cb50de924367fe3cb55d0
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -1241,7 +1241,8 @@ public class KeyguardViewMediator extends SystemUI {
             if (mOccluded != isOccluded) {
                 mOccluded = isOccluded;
                 mUpdateMonitor.setKeyguardOccluded(isOccluded);
-                mStatusBarKeyguardViewManager.setOccluded(isOccluded, animate
+                mStatusBarKeyguardViewManager.setOccluded(isOccluded,
+                        (KeyguardUpdateMonitor.getInstance(mContext).isSimPinSecure()?false:animate)
                         && mDeviceInteractive);
                 adjustStatusBarLocked();
             }
