2e229e4b2dbb8078cbfca4bda1e38e794c164126
alexxxdev <alexxxdev@yandex.ru>
2018-09-27 23:15:48 +0700

    SystemUI: fixed "Clock is double after reboot or update"
    Change-Id: Ic582f9d210a4d949f2660844014765a9b74f5000
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -111,7 +111,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
         Dependency.get(TunerService.class).addTunable(this,
                 StatusBarIconController.ICON_BLACKLIST);
-        mSettingsObserver.observe();
     }
 
     @Override
@@ -138,6 +137,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         showSystemIconArea(false);
         initEmergencyCryptkeeperText();
         initOperatorName();
+        mSettingsObserver.observe();
+        updateSettings(true);
     }
 
     @Override
