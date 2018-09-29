b96594ecad412169fd078a509ada410cdf4ee160
Pranav Vashi <neobuddy89@gmail.com>
2018-09-29 13:05:52 +0530

    Use FORCE_SHOW_NAVBAR to actually toggle navbar
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recents/ScreenPinningRequest.java
+++ packages/SystemUI/src/com/android/systemui/recents/ScreenPinningRequest.java
@@ -56,6 +56,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
+import com.android.internal.util.hwkeys.ActionUtils;
 import com.android.systemui.R;
 import com.android.systemui.broadcast.BroadcastDispatcher;
 import com.android.systemui.navigationbar.NavigationBarView;
@@ -345,11 +346,10 @@ public class ScreenPinningRequest implements View.OnClickListener,
          * @return whether there is a soft nav bar on specific display.
          */
         private boolean hasSoftNavigationBar(Context context, int displayId) {
-            if (displayId == DEFAULT_DISPLAY &&
-                    Settings.System.getIntForUser(context.getContentResolver(),
-                            Settings.System.FORCE_SHOW_NAVBAR, 0,
-                            UserHandle.USER_CURRENT) == 1) {
-                return true;
+            if (displayId == DEFAULT_DISPLAY) {
+                return Settings.System.getIntForUser(context.getContentResolver(),
+                                Settings.System.FORCE_SHOW_NAVBAR, ActionUtils.hasNavbarByDefault(context) ? 1 : 0,
+                                UserHandle.USER_CURRENT) == 1;
             }
             try {
                 return WindowManagerGlobal.getWindowManagerService().hasNavigationBar(displayId);
