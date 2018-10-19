30ea894dba654d9f12e219374e1adb7ddc31d97b
spezi77 <spezi7713@gmx.net>
2018-10-19 17:57:23 +0200

    GamingMode Tile: Show "What's This?" on long click.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/GamingModeTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/GamingModeTile.java
@@ -68,6 +68,10 @@ public class GamingModeTile extends QSTileImpl<BooleanState> {
             enableGamingMode();
             return;
         }
+        showGamingModeWhatsThisDialog();
+    }
+
+    private void showGamingModeWhatsThisDialog() {
         SystemUIDialog dialog = new SystemUIDialog(mContext);
         dialog.setTitle(R.string.gaming_mode_dialog_title);
         dialog.setMessage(R.string.gaming_mode_dialog_message);
@@ -95,7 +99,7 @@ public class GamingModeTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleLongClick() {
-        handleClick();
+	showGamingModeWhatsThisDialog();
     }
 
     private void handleState(boolean enabled) {
