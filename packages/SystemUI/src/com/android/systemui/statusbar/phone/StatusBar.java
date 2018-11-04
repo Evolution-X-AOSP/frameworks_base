4fc1a29504913e8ff6d87221e789b4654c46d909
ElDainosor <eldainosor@gmail.com>
2018-11-04 02:13:21 +0000

    Add more qs tiles styles [1/3]
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -364,6 +364,11 @@ public class StatusBar extends SystemUI implements DemoMode,
         "com.android.systemui.qstile.circletrim", // 1
         "com.android.systemui.qstile.dualtonecircletrim", // 2
         "com.android.systemui.qstile.squircletrim", // 3
+        "com.android.systemui.qstile.wavey", // 4
+        "com.android.systemui.qstile.pokesign", // 5
+        "com.android.systemui.qstile.ninja", // 6
+        "com.android.systemui.qstile.dottedcircle", // 7
+        "com.android.systemui.qstile.attemptmountain", // 8
     };
 
     /** If true, the system is in the half-boot-to-decryption-screen state.
