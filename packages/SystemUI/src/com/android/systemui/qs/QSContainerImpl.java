4e8e21b10d18c3f3af614f84d4e2dfe2e5c1def8
xyyx <xyyx@mail.ru>
2018-11-15 12:35:34 +0800

    QS: Fix opacity after reboot
    before after reboot we had:
    https://imgur.com/a/6WnJmwE
    
    now:
    https://imgur.com/a/jGspC2c
    
    Change-Id: I0b14cf1aa292ab91135614d87843de05745f2139
    Signed-off-by: phenomenall <venkysaihot@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
@@ -89,6 +89,7 @@ public class QSContainerImpl extends FrameLayout {
         super.onConfigurationChanged(newConfig);
         setBackgroundGradientVisibility(newConfig);
         updateResources();
+        updateSettings();
         mSizePoint.set(0, 0); // Will be retrieved on next measure pass.
     }
 
