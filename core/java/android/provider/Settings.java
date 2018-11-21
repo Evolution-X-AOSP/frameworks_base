9d46357b813b11a7c97377c83f15d504d3ccadc3
LorDClockaN <davor@losinj.com>
2018-11-21 15:49:47 +0100

    Volume dialog timeout [1/2]
    @neobuddy89:
    * Rewrite to avoid called getInt everytime changing volume! Really?
    * Removed validators - handle range in the settings.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6607,6 +6607,12 @@ public final class Settings {
         public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
 
         /**
+         * Volume dialog timeout. Value in seconds
+         * @hide
+         */
+        public static final String VOLUME_DIALOG_TIMEOUT = "volume_dialog_timeout";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -158,6 +158,8 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
     private static final String VOLUME_PANEL_ON_LEFT =
             Settings.Secure.VOLUME_PANEL_ON_LEFT;
+    public static final String VOLUME_DIALOG_TIMEOUT =
+            "system:" + Settings.System.VOLUME_DIALOG_TIMEOUT;
 
     private static final long USER_ATTEMPT_GRACE_PERIOD = 1000;
     private static final int UPDATE_ANIMATION_DURATION = 80;
@@ -321,6 +323,8 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     private final Lazy<SecureSettings> mSecureSettings;
     private int mDialogTimeoutMillis;
 
+    private int mTimeOutDesired, mTimeOut;
+
     public VolumeDialogImpl(
             Context context,
             VolumeDialogController volumeDialogController,
@@ -388,6 +392,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         if (!mShowActiveStreamOnly) {
             mTunerService.addTunable(mTunable, VOLUME_PANEL_ON_LEFT);
         }
+        mTunerService.addTunable(mTunable, VOLUME_DIALOG_TIMEOUT);
 
         initDimens();
 
@@ -802,6 +807,9 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                         mControllerCallbackH.onConfigurationChanged();
                     });
                 }
+            } else if (VOLUME_DIALOG_TIMEOUT.equals(key)) {
+                mTimeOutDesired = TunerService.parseInteger(newValue, 3);
+                mTimeOut = mTimeOutDesired * 1000;
             }
         }
     };
@@ -1616,8 +1624,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                     AccessibilityManager.FLAG_CONTENT_TEXT
                             | AccessibilityManager.FLAG_CONTENT_CONTROLS);
         }
-        return mAccessibilityMgr.getRecommendedTimeoutMillis(mDialogTimeoutMillis,
-                AccessibilityManager.FLAG_CONTENT_CONTROLS);
+        return mTimeOut;
     }
 
     protected void scheduleCsdTimeoutH(int timeoutMs) {
