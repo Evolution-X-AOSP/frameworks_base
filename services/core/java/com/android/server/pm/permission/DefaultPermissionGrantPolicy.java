bb38f6dd3c2f35cba355c38824bc7bf97047216e
Ali B <abittin@gmail.com>
2018-12-13 17:05:01 +0300

    Permissions: Grant Mediascanner access permissions to external storage
    12-13 16:52:37.579  2621  5891 E DatabaseUtils: Writing exception to parcel
    
    12-13 16:52:37.579  2621  5891 E DatabaseUtils: java.lang.SecurityException: Permission Denial: reading com.android.providers.media.MediaProvider uri content://media/external/audio/media/10579 from pid=1270, uid=1000 requires android.permission.READ_EXTERNAL_STORAGE, or grantUriPermission()
    
    12-13 16:52:37.579  2621  5891 E DatabaseUtils:         at android.content.ContentProvider.enforceReadPermissionInner(ContentProvider.java:634)
    
    12-13 16:52:37.579  2621  5891 E DatabaseUtils:         at com.android.providers.media.MediaProvider.enforceReadPermissionInner(MediaProvider.java:705)
    
    12-13 16:52:37.579  2621  5891 E DatabaseUtils:         at android.content.ContentProvider$Transport.enforceReadPermission(ContentProvider.java:503)
    
    12-13 16:52:37.579  2621  5891 E DatabaseUtils:         at android.content.ContentProvider$Transport.enforceFilePermission(ContentProvider.java:494)
    
    12-13 16:52:37.579  2621  5891 E DatabaseUtils:         at android.content.ContentProvider$Transport.openFile(ContentProvider.java:371)
    
    12-13 16:52:37.579  2621  5891 E DatabaseUtils:         at android.content.ContentProviderNative.onTransact(ContentProviderNative.java:229)
    
    12-13 16:52:37.579  2621  5891 E DatabaseUtils:         at android.os.Binder.execTransact(Binder.java:731)
    
    Change-Id: I093d544a7cfa755bf9aa1704ebb1cfd2f919325c
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -810,6 +810,11 @@ public final class DefaultPermissionGrantPolicy {
         // Google dialer
         grantSystemFixedPermissionsToSystemPackage("com.google.android.dialer", userId, PHONE_PERMISSIONS,
                 CONTACTS_PERMISSIONS, SMS_PERMISSIONS);
+
+        // Mediascanner
+        grantSystemFixedPermissionsToSystemPackage(
+                getDefaultProviderAuthorityPackage("com.android.providers.media.MediaProvider", userId), userId,
+                STORAGE_PERMISSIONS);
     }
 
     private String getDefaultSystemHandlerActivityPackageForCategory(String category, int userId) {
