68fb4a8dd847be2531e88706f705477bd3ed4be5
Ali B <abittin@gmail.com>
2018-12-13 17:05:01 +0300

    Permissions: Grant Mediascanner access permissions to external storage
    12-13 16:52:37.579  2621  5891 E DatabaseUtils: Writing exception to parcel
    
    12-13 16:52:37.579  2621  5891 E DatabaseUtils: java.lang.SecurityException: Permission Denial: reading com.android.providers.media.MediaProvider uri content://media/external/audio/media/10579 from pid=1270, uid=1000 requires android.permission.READ_EXTERNAL_STORAGE, or grantUriPermission()
    
    12-13 16:52:37.579  2621  5891 E DatabaseUtils:         at android.content.ContentProvider.enforceReadPermissionInner(ContentProvider.java:634)
    
    12-13 16:52:37.579  2621  5891 E DatabaseUtils:         at com.android.providers.media.MediaProvider.enforceReadPermissionInner(MediaProvider.java:705)
    
    12-13 16:52:37.579  2621  5891 E DatabaseUtils:         at android.content.ContentProvider$Transport.enforceReadPermission(ContentProvider.java:503)
    
    12-13 16:52:37.579  2621  5891 E DatabaseUtils:         at android.content.ContentProvider$Transport.enforceFilePermission(ContentProvider.java:494)
    
    12-13 16:52:37.579  2621  5891 E DatabaseUtils:         at android.content.ContentProvider$Transport.openFile(ContentProvider.java:371)
    
    12-13 16:52:37.579  2621  5891 E DatabaseUtils:         at android.content.ContentProviderNative.onTransact(ContentProviderNative.java:229)
    
    12-13 16:52:37.579  2621  5891 E DatabaseUtils:         at android.os.Binder.execTransact(Binder.java:731)
    
    Change-Id: I093d544a7cfa755bf9aa1704ebb1cfd2f919325c

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -985,6 +985,11 @@ final class DefaultPermissionGrantPolicy {
         grantSystemFixedPermissionsToSystemPackage(pm,
                 getDefaultProviderAuthorityPackage("com.google.android.flipendo", userId),
                 userId, SUSPEND_APP_PERMISSIONS);
+
+        // Mediascanner
+        grantSystemFixedPermissionsToSystemPackage(pm,
+                getDefaultProviderAuthorityPackage("com.android.providers.media.MediaProvider", userId), userId,
+                STORAGE_PERMISSIONS);
     }
 
     private String getDefaultSystemHandlerActivityPackageForCategory(PackageManagerWrapper pm,
