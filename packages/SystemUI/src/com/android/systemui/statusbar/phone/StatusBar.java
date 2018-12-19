bafa785625ebd293a72edd52b28ccebd86d91896
El Dainosor <eldainosor@gmail.com>
2018-12-19 22:39:06 +0530

    Add InkDrop QS Tile Style [1/3]
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -370,6 +370,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         "com.android.systemui.qstile.dottedcircle", // 7
         "com.android.systemui.qstile.attemptmountain", // 8
         "com.android.systemui.qstile.squaremedo", // 9
+        "com.android.systemui.qstile.inkdrop", // 10
     };
 
     /** If true, the system is in the half-boot-to-decryption-screen state.
