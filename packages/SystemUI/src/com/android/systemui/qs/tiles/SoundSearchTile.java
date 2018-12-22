f60f6281100b8c1caed79d8f20ecfd781970bf97
ezio84 <brabus84@gmail.com>
2018-12-22 01:19:35 +0000

    SoundSearchTile: Update intent flags for the apps
    Taken from: https://github.com/ezio84/abc_frameworks_base/commit/9bafbf08390786966719f1b90b9f24b2de17d1c9#diff-5888d398e209da47a36960bdd9feeeb5R50
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/SoundSearchTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/SoundSearchTile.java
@@ -77,16 +77,19 @@ public class SoundSearchTile extends QSTileImpl<BooleanState> {
         // Shazam
         if (EvolutionUtils.isPackageInstalled(mContext, "com.shazam.android") || EvolutionUtils.isPackageInstalled(mContext, "com.shazam.encore.android")) {
             Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             intent.setAction("com.shazam.android.intent.actions.START_TAGGING");
             mContext.startActivity(intent);
         // Soundhound
         } else if (EvolutionUtils.isPackageInstalled(mContext, "com.melodis.midomiMusicIdentifier.freemium") || EvolutionUtils.isPackageInstalled(mContext, "com.melodis.midomiMusicIdentifier")) {
             Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             intent.setAction("com.soundhound.android.ID_NOW_EXTERNAL");
             mContext.startActivity(intent);
         // Google Search Music
         } else if (EvolutionUtils.isPackageInstalled(mContext, "com.google.android.googlequicksearchbox")) {
             Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             intent.setAction("com.google.android.googlequicksearchbox.MUSIC_SEARCH");
             mContext.startActivity(intent);
         } else {
