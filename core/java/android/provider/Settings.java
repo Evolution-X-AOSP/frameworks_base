f82904dab1272ac9643b1a173ac9377a4664175c
Pranav Vashi <neobuddy89@gmail.com>
2018-12-29 23:21:23 +0530

    Add option to auto hide status-bar clock
    Inspired by:
    https://github.com/PotatoProject/frameworks_base/commit/dbaed27cd2633bd825a598e0b4615dacdb36cd00
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6132,6 +6132,7 @@ public final class Settings {
         /**
          * Whether to hide clock when launcher is visible
          * default: false
+         * @hide
          */
         public static final String STATUS_BAR_CLOCK_AUTO_HIDE = "status_bar_clock_auto_hide";
 
@@ -6174,6 +6175,18 @@ public final class Settings {
          * @hide
          */
         public static final String STATUS_BAR_CLOCK_DATE_FORMAT = "status_bar_clock_date_format";
+
+        /**
+         * Whether to auto hide clock
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_AUTO_HIDE_LAUNCHER = "status_bar_clock_auto_hide_launcher";
+
+        /** @hide */
+        public static final String STATUS_BAR_CLOCK_AUTO_HIDE_HDURATION = "status_bar_clock_auto_hide_hduration";
+
+        /** @hide */
+        public static final String STATUS_BAR_CLOCK_AUTO_HIDE_SDURATION = "status_bar_clock_auto_hide_sduration";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
