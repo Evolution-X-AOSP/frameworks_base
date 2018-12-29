214d7fd2f8a9a05a4b9988ac430669be9f5cf2dc
Pranav Vashi <neobuddy89@gmail.com>
2018-12-29 23:21:23 +0530

    Add option to auto hide status-bar clock
    Inspired by:
    https://github.com/PotatoProject/frameworks_base/commit/dbaed27cd2633bd825a598e0b4615dacdb36cd00
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5845,7 +5845,7 @@ public final class Settings {
          * default: false
          * @hide
          */
-        public static final String STATUS_BAR_CLOCK_AUTO_HIDE = "status_bar_clock_auto_hide";
+        public static final String STATUS_BAR_CLOCK_AUTO_HIDE_LAUNCHER = "status_bar_clock_auto_hide_launcher";
 
         /**
          * boolean value. toggles using arrow key locations on nav bar
@@ -6089,6 +6089,18 @@ public final class Settings {
          */
         @Readable
         public static final String FLASHLIGHT_ON_CALL_RATE = "flashlight_on_call_rate";
+
+        /**
+         * Whether to auto hide clock
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_AUTO_HIDE = "status_bar_clock_auto_hide";
+
+        /** @hide */
+        public static final String STATUS_BAR_CLOCK_AUTO_HIDE_HDURATION = "status_bar_clock_auto_hide_hduration";
+
+        /** @hide */
+        public static final String STATUS_BAR_CLOCK_AUTO_HIDE_SDURATION = "status_bar_clock_auto_hide_sduration";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
