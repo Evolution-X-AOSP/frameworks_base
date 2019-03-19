f169bdd088f53b1e38dbbe45327c8f450ca9e7f8
ElDainosor <eldainosor@gmail.com>
2019-03-19 01:41:09 -0300

    Add Lockscreen album art filter [1/2]
    @neobuddy89: Improvised and adapted for Android 10 and Android 11/12.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5791,6 +5791,12 @@ public final class Settings {
         public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
         /**
+         * Switch between filters for lockscreen album art
+         * @hide
+         */
+        public static final String LOCKSCREEN_ALBUMART_FILTER = "lockscreen_albumart_filter";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
