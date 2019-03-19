cea01aac108089b5ac2f194fbe8e1058830e4587
Niklas Brunlid <niklas.brunlid@sony.com>
2019-03-19 13:47:48 +0100

    Add a one minute timeout to global actions dialog
    The global actions menu can be started on top of store
    demo mode, videos and other scenarios where the display
    is on for extended periods. Close it automatically after
    one minute to avoid the possibility of OLED burn-in.
    
    Bug: 153119340
    Test: Manual.  See issue.
    Change-Id: I9efc39365e6b53fee35893fe0f407cc4358ff2eb

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -543,6 +543,8 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
             mDialog.show();
         }
         mWindowManagerFuncs.onGlobalActionsShown();
+
+        rescheduleBurninTimeout();
     }
 
     @VisibleForTesting
@@ -788,12 +790,23 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
                 mPowerAdapter, mUiEventLogger, mCentralSurfacesOptional,
                 mShadeController,
                 mKeyguardUpdateMonitor,
-                mLockPatternUtils);
+                mLockPatternUtils) {
+            @Override
+            public boolean dispatchTouchEvent(MotionEvent event) {
+                rescheduleBurninTimeout();
+                return super.dispatchTouchEvent(event);
+            }
+        };
 
         dialog.setOnDismissListener(this);
         dialog.setOnShowListener(this);
 
         return dialog;
+    }
+
+    private void rescheduleBurninTimeout() {
+        mHandler.removeMessages(MESSAGE_DISMISS);
+        mHandler.sendEmptyMessageDelayed(MESSAGE_DISMISS, BURNIN_DISMISS_DELAY);
     }
 
     @VisibleForTesting
@@ -2309,6 +2322,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
     private static final int MESSAGE_REFRESH = 1;
     private static final int DIALOG_DISMISS_DELAY = 300; // ms
     private static final int DIALOG_PRESS_DELAY = 850; // ms
+    private static final int BURNIN_DISMISS_DELAY = 60000; // ms
 
     @VisibleForTesting void setZeroDialogPressDelayForTesting() {
         mDialogPressDelay = 0; // ms
