7d91a476bc9b1d7994c06fb83776372551357dc2
AndyCGYan <GeForce8800Ultra@gmail.com>
2019-03-22 00:41:20 +0800

    Disable FP lockouts
    Both timed and permanent lockouts - GET THE FUCK OUT
    Now targeting LockoutFramework, introduced in Android 12
    
    Change-Id: I2d4b091f3546d4d7903bfb4d5585629212dc9915

--- services/core/java/com/android/server/biometrics/sensors/fingerprint/hidl/LockoutFrameworkImpl.java
+++ services/core/java/com/android/server/biometrics/sensors/fingerprint/hidl/LockoutFrameworkImpl.java
@@ -106,26 +106,11 @@ public class LockoutFrameworkImpl implements LockoutTracker {
     }
 
     @Override
-    public void addFailedAttemptForUser(int userId) {
-        mFailedAttempts.put(userId, mFailedAttempts.get(userId, 0) + 1);
-        mTimedLockoutCleared.put(userId, false);
-
-        if (getLockoutModeForUser(userId) != LOCKOUT_NONE) {
-            scheduleLockoutResetForUser(userId);
-        }
-    }
+    public void addFailedAttemptForUser(int userId) {}
 
     @Override
     @LockoutMode
     public int getLockoutModeForUser(int userId) {
-        final int failedAttempts = mFailedAttempts.get(userId, 0);
-        if (failedAttempts >= MAX_FAILED_ATTEMPTS_LOCKOUT_PERMANENT) {
-            return LOCKOUT_PERMANENT;
-        } else if (failedAttempts > 0
-                && !mTimedLockoutCleared.get(userId, false)
-                && (failedAttempts % MAX_FAILED_ATTEMPTS_LOCKOUT_TIMED == 0)) {
-            return LOCKOUT_TIMED;
-        }
         return LOCKOUT_NONE;
     }
 
