ceed1a23783d1aa601e405dc9bcc01a86c891ce1
ElDainosor <eldainosor@gmail.com>
2019-04-02 20:00:14 -0300

    base: Intensify the blur amount on LS cover art
    after 120bd6fd60e6c5c36b67d15453e6b544ba87f7a6 the filter ended up being pretty soft, so this makes it close to the previous iteration
    
    Change-Id: If80f69223d092f4571dd5c1a9b905a7d8e36b011

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1801,7 +1801,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                         artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), ImageHelper.toGrayscale(artworkBitmap));
                         break;
                     case 2:
-                        artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), ImageHelper.getBlurredImage(mContext, artworkBitmap));
+                        artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), ImageHelper.getBlurredImage(mContext, artworkBitmap, 7.0f));
                         break;
                     case 0:
                     default:
