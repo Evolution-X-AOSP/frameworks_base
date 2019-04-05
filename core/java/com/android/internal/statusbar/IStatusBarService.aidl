cc1b60803dfad0397fd1b7058241642309b695ed
Peter Cai <peter@typeblog.net>
2019-04-05 16:44:18 +0200

    Initial support for in-display fingerprint sensors
    * Initially based upon phh's Mi9 implementation,
      however a lot of changes have been made since.
    * All device specific stuff has been moved to
      IFingerprintInscreen HIDL interface and most
      of code has been improved and refactored.
    
    AOSiP edit: remove all ties to LineageContextConstants.Features.FOD as we dont want no damn sdk stuffs
    
    Change-Id: I1b56a69866b74fa71714b2422f00e82cafeb316a
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/com/android/internal/statusbar/IStatusBarService.aidl
+++ core/java/com/android/internal/statusbar/IStatusBarService.aidl
@@ -111,6 +111,10 @@ interface IStatusBarService
     void toggleCameraFlash();
     void restartUI();
 
+    // Used to show or hide in display fingerprint view
+    void showInDisplayFingerprintView();
+    void hideInDisplayFingerprintView();
+
     // Start SmartNav methods
     void toggleRecentApps();
     void toggleSplitScreen();
--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -101,6 +101,8 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_TOGGLE_FLASHLIGHT             = 51 << MSG_SHIFT;
     private static final int MSG_TOGGLE_NAVIGATION_EDITOR      = 52 << MSG_SHIFT;
     private static final int MSG_DISPATCH_NAVIGATION_EDITOR_RESULTS = 53 << MSG_SHIFT;
+    private static final int MSG_SHOW_IN_DISPLAY_FINGERPRINT_VIEW = 54 << MSG_SHIFT;
+    private static final int MSG_HIDE_IN_DISPLAY_FINGERPRINT_VIEW = 55 << MSG_SHIFT;
     private static final int MSG_TOGGLE_SETTINGS_PANEL         = 100 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
@@ -182,6 +184,8 @@ public class CommandQueue extends IStatusBar.Stub {
         default void toggleFlashlight() {}
         default void toggleNavigationEditor() {}
         default void dispatchNavigationEditorResults(Intent intent) {}
+        default void showInDisplayFingerprintView() { }
+        default void hideInDisplayFingerprintView() { }
     }
 
     @VisibleForTesting
@@ -628,6 +632,20 @@ public class CommandQueue extends IStatusBar.Stub {
         }
     }
 
+    @Override
+    public void showInDisplayFingerprintView() {
+        synchronized (mLock) {
+            mHandler.obtainMessage(MSG_SHOW_IN_DISPLAY_FINGERPRINT_VIEW).sendToTarget();
+        }
+    }
+
+    @Override
+    public void hideInDisplayFingerprintView() {
+        synchronized (mLock) {
+            mHandler.obtainMessage(MSG_HIDE_IN_DISPLAY_FINGERPRINT_VIEW).sendToTarget();
+        }
+    }
+
     private final class H extends Handler {
         private H(Looper l) {
             super(l);
@@ -913,6 +931,16 @@ public class CommandQueue extends IStatusBar.Stub {
                     Intent intent = (Intent) msg.obj;
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).dispatchNavigationEditorResults(intent);
+                    }
+                    break;
+                case MSG_SHOW_IN_DISPLAY_FINGERPRINT_VIEW:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).showInDisplayFingerprintView();
+                    }
+                    break;
+                case MSG_HIDE_IN_DISPLAY_FINGERPRINT_VIEW:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).hideInDisplayFingerprintView();
                     }
                     break;
             }
