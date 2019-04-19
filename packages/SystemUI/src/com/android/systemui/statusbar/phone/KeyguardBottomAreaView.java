aadebb1546d6ab8b0c105047d7d06b71ff7057a2
micky387 <mickaelsaibi@free.fr>
2019-04-19 21:55:49 +0200

    SystemUI: FOD: Use custom Bottom Area View for keyguard message
    Dreaming and Pulsing are not concerned
    
    Change-Id: I5ef1e6502a97589efcf40ab17c3f4dc21a1405d4
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -170,6 +170,8 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     private int mBurnInYOffset;
     private ActivityIntentHelper mActivityIntentHelper;
 
+    private boolean mDisplayFODView;
+
     public KeyguardBottomAreaView(Context context) {
         this(context, null);
     }
@@ -237,8 +239,16 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         mEnterpriseDisclosure = findViewById(
                 R.id.keyguard_indication_enterprise_disclosure);
         mIndicationText = findViewById(R.id.keyguard_indication_text);
-        mIndicationBottomMargin = getResources().getDimensionPixelSize(
-                R.dimen.keyguard_indication_margin_bottom);
+        mDisplayFODView = getResources()
+                .getBoolean(com.android.internal.R.bool.config_needCustomFODView);
+
+        if (mDisplayFODView) {
+            mIndicationBottomMargin = getResources().getDimensionPixelSize(
+                  R.dimen.op_keyguard_indication_margin_bottom);
+        } else {
+            mIndicationBottomMargin = getResources().getDimensionPixelSize(
+                  R.dimen.keyguard_indication_margin_bottom);
+        }
         mBurnInYOffset = getResources().getDimensionPixelSize(
                 R.dimen.default_burn_in_prevention_offset);
         updateCameraVisibility();
@@ -301,8 +311,13 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     @Override
     protected void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        mIndicationBottomMargin = getResources().getDimensionPixelSize(
-                R.dimen.keyguard_indication_margin_bottom);
+        if (mDisplayFODView) {
+            mIndicationBottomMargin = getResources().getDimensionPixelSize(
+                  R.dimen.op_keyguard_indication_margin_bottom);
+        } else {
+            mIndicationBottomMargin = getResources().getDimensionPixelSize(
+                  R.dimen.keyguard_indication_margin_bottom);
+        }
         mBurnInYOffset = getResources().getDimensionPixelSize(
                 R.dimen.default_burn_in_prevention_offset);
         MarginLayoutParams mlp = (MarginLayoutParams) mIndicationArea.getLayoutParams();
