c46e9240222f133169b1f5f7d1449e0ebbe8c013
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-08 11:52:38 -0300

    base: SwipeToScreenshot: Import MIUI implementation
    Reverse engineered from Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/9.2.25:user/release-keys
    
    Change-Id: I1d5938457b7cc4c5ef1fec235d27d8a40526961a
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -20415,7 +20415,7 @@ public class ActivityManagerService extends IActivityManager.Stub
     @Override
     public boolean isSwipeToScreenshotGestureActive() {
         synchronized (this) {
-            return mIsSwipeToScreenshotEnabled;
+            return mIsSwipeToScreenshotEnabled && SystemProperties.getBoolean("sys.android.screenshot", false);
         }
     }
 }
