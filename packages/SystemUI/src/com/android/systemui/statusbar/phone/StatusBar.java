9d9e20891e2328edaafabe42b13408e0b89e50f2
argraur <argraur@gmail.com>
2019-05-11 21:57:59 +0000

    SystemUI: increase LS album art blur strength
    Signed-off-by: argraur <argraur@gmail.com>
    Change-Id: If9969152089ae55d8298043b9cd639227431c7b3

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1805,10 +1805,10 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                         artworkDrawable = new BitmapDrawable(ImageHelper.getColoredBitmap(aw, mContext.getResources().getColor(R.color.sammy_minutes_accent)));
                         break;
                     case 3:
-                        artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), ImageHelper.getBlurredImage(mContext, artworkBitmap, 7.0f));
+                        artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), ImageHelper.getBlurredImage(mContext, artworkBitmap, 25.0f));
                         break;
                     case 4:
-                        artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), ImageHelper.getGrayscaleBlurredImage(mContext, artworkBitmap, 7.0f));
+                        artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), ImageHelper.getGrayscaleBlurredImage(mContext, artworkBitmap, 25.0f));
                         break;
                     case 0:
                     default:
