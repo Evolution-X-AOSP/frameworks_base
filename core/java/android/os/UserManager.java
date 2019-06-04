babdfaeb7b5e29e0080376726ab09e171a8c6afb
Stallix <thestallix@gmail.com>
2019-06-04 14:57:19 +0530

    Revert "fwb: Disable multi user for all devices"
    This reverts commit cbaf7f6e1dff37394451be71db2cdf1be5a1fc0d.

--- core/java/android/os/UserManager.java
+++ core/java/android/os/UserManager.java
@@ -1139,10 +1139,9 @@ public class UserManager {
      * @return whether the device supports multiple users.
      */
     public static boolean supportsMultipleUsers() {
-        return false;
-        /*return getMaxSupportedUsers() > 1
+        return getMaxSupportedUsers() > 1
                 && SystemProperties.getBoolean("fw.show_multiuserui",
-                Resources.getSystem().getBoolean(R.bool.config_enableMultiUserUI));*/
+                Resources.getSystem().getBoolean(R.bool.config_enableMultiUserUI));
     }
 
     /**
@@ -2634,8 +2633,6 @@ public class UserManager {
      * @return a value greater than or equal to 1
      */
     public static int getMaxSupportedUsers() {
-        return 1;
-        /*
         // Don't allow multiple users on certain builds
         if (android.os.Build.ID.startsWith("JVP")) return 1;
         if (ActivityManager.isLowRamDeviceStatic()) {
@@ -2646,7 +2643,7 @@ public class UserManager {
             }
         }
         return SystemProperties.getInt("fw.max_users",
-                Resources.getSystem().getInteger(R.integer.config_multiuserMaximumUsers));*/
+                Resources.getSystem().getInteger(R.integer.config_multiuserMaximumUsers));
     }
 
     /**
