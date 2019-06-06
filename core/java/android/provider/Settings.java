da51c6cb67d524d8f0c8a26ffef615550be08915
Anushek Prasal <anushekprasal@gmail.com>
2019-06-06 14:18:24 +0530

    base: Add toggle to disable charging animation [1/2]
    Change-Id: I8982623ce5d231d1a29dfa934179214a6745faae
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5249,6 +5249,12 @@ public final class Settings {
         public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
 
         /**
+         * Whether to show charging animation
+         * @hide
+         */
+        public static final String CHARGING_ANIMATION = "charging_animation";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
