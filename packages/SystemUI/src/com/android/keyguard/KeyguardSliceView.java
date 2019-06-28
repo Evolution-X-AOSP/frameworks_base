c80a5cb6ee2a1094431cdd828ead86be2015a906
Stallix <thestallix@gmail.com>
2019-06-29 02:07:27 +0530

    base: Fix unnecessary truncation in Slice
    AshutoshSundresh: Adapted for Android 11:
    KeyguardSliceButton -----> KeyguardSliceTextView
    
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/keyguard/KeyguardSliceView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardSliceView.java
@@ -513,8 +513,8 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
             int childCount = getChildCount();
             for (int i = 0; i < childCount; i++) {
                 View child = getChildAt(i);
-                if (child instanceof KeyguardSliceTextView) {
-                    ((KeyguardSliceTextView) child).setMaxWidth(width / 3);
+                if (child instanceof KeyguardSliceTextView && childCount > 5) {
+                    ((KeyguardSliceTextView) child).setMaxWidth(width / childCount);
                 }
             }
             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
