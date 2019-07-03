b39b38dfd84e4199bd6cd419e51acfde060565ea
Danny Baumann <dannybaumann@web.de>
2019-07-03 15:12:22 +0200

    SystemUI: Add VPN tile
    Author: Danny Baumann <dannybaumann@web.de>
    Date:   Wed Jul 3 15:12:22 2019 +0200
    
        SystemUI: Add VPN tile
    
        Allows quickly connecting/disconnecting to/from VPNs.
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Wed Oct 7 22:51:51 2020 +0100
    Edit    Update to Android 11 APIs
    
    [Stallix] Adapt for A13 QPR3
    
    Change-Id: Iaa9413bf1071805be8764d92bc5e2dd3131fdaed
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/dagger/evolution/EvolutionXModule.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/evolution/EvolutionXModule.kt
@@ -37,6 +37,7 @@ import com.android.systemui.qs.tiles.SoundSearchTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
 import com.android.systemui.qs.tiles.VolumeTile
+import com.android.systemui.qs.tiles.VpnTile
 
 import dagger.Binds
 import dagger.Module
@@ -146,6 +147,12 @@ interface EvolutionXModule {
     @IntoMap
     @StringKey(UsbTetherTile.TILE_SPEC)
     fun bindUsbTetherTile(usbTetherTile: UsbTetherTile): QSTileImpl<*>
+
+    /** Inject VpnTile into tileMap in QSModule */
+    @Binds  
+    @IntoMap
+    @StringKey(VpnTile.TILE_SPEC)
+    fun bindVpnTile(vpnTile: VpnTile): QSTileImpl<*>
 
     /** Inject VolumeTile into tileMap in QSModule */
     @Binds
