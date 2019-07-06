f247dd2c23e07b8a2db193b79e57f672a9edd2c2
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-07-05 23:26:19 -0300

    LiveDisplay: Refactor for HWC devices and cleanup
    * Integrate LiveDisplay backend into night display for legacyd evices
    * Cleanup code
    * Use config_nightDisplayAvailable instead of config_setColorTransformAccelerated to keep consistency
    
    Change-Id: I7a1f892166c34e6745bb5ff55a8b2fcab707f560

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4662,33 +4662,6 @@ public final class Settings {
         public static final Validator POCKET_JUDGE_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
-         * Color temperature of the display during the day
-         */
-        public static final String DISPLAY_TEMPERATURE_DAY = "display_temperature_day";
-
-        /** @hide */
-        public static final Validator DISPLAY_TEMPERATURE_DAY_VALIDATOR =
-                new SettingsValidators.InclusiveIntegerRangeValidator(0, 100000);
-
-        /**
-         * Color temperature of the display at night
-         */
-        public static final String DISPLAY_TEMPERATURE_NIGHT = "display_temperature_night";
-
-        /** @hide */
-        public static final Validator DISPLAY_TEMPERATURE_NIGHT_VALIDATOR =
-                new SettingsValidators.InclusiveIntegerRangeValidator(0, 100000);
-
-        /**
-         * Display color temperature adjustment mode, one of DAY (default), NIGHT, or AUTO.
-         */
-        public static final String DISPLAY_TEMPERATURE_MODE = "display_temperature_mode";
-
-        /** @hide */
-        public static final Validator DISPLAY_TEMPERATURE_MODE_VALIDATOR =
-                new SettingsValidators.InclusiveIntegerRangeValidator(0, 4);
-
-        /**
          * Automatic outdoor mode
          * 0 = 0ff, 1 = on
          */
@@ -4785,16 +4758,6 @@ public final class Settings {
                         return true;
                     }
                 };
-
-        /**
-         * Did we tell about how they can stop breaking their eyes?
-         * @hide
-         */
-        public static final String LIVE_DISPLAY_HINTED = "live_display_hinted";
-
-        /** @hide */
-        public static final Validator LIVE_DISPLAY_HINTED_VALIDATOR =
-                new SettingsValidators.InclusiveIntegerRangeValidator(-3, 1);
 
          /**
           * Screenshod sound enable, This is the noise made when taking a screesnhot
@@ -5980,9 +5943,6 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD);
             PRIVATE_SETTINGS.add(SHOW_VOLTE_ICON);
             PRIVATE_SETTINGS.add(POCKET_JUDGE);
-            PRIVATE_SETTINGS.add(DISPLAY_TEMPERATURE_DAY);
-            PRIVATE_SETTINGS.add(DISPLAY_TEMPERATURE_NIGHT);
-            PRIVATE_SETTINGS.add(DISPLAY_TEMPERATURE_MODE);
             PRIVATE_SETTINGS.add(DISPLAY_AUTO_OUTDOOR_MODE);
             PRIVATE_SETTINGS.add(DISPLAY_READING_MODE);
             PRIVATE_SETTINGS.add(DISPLAY_CABC);
@@ -5990,7 +5950,6 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DISPLAY_AUTO_CONTRAST);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_ADJUSTMENT);
             PRIVATE_SETTINGS.add(DISPLAY_PICTURE_ADJUSTMENT);
-            PRIVATE_SETTINGS.add(LIVE_DISPLAY_HINTED);
             PRIVATE_SETTINGS.add(SCREENSHOT_SOUND);
             PRIVATE_SETTINGS.add(SWIPE_TO_SCREENSHOT);
             PRIVATE_SETTINGS.add(THEME_AUTOMATIC_TIME_IS_NIGHT);
@@ -6158,9 +6117,6 @@ public final class Settings {
             VALIDATORS.put(STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD, STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD_VALIDATOR);
             VALIDATORS.put(SHOW_VOLTE_ICON, SHOW_VOLTE_ICON_VALIDATOR);
             VALIDATORS.put(POCKET_JUDGE, POCKET_JUDGE_VALIDATOR);
-            VALIDATORS.put(DISPLAY_TEMPERATURE_DAY, DISPLAY_TEMPERATURE_DAY_VALIDATOR);
-            VALIDATORS.put(DISPLAY_TEMPERATURE_NIGHT, DISPLAY_TEMPERATURE_NIGHT_VALIDATOR);
-            VALIDATORS.put(DISPLAY_TEMPERATURE_MODE, DISPLAY_TEMPERATURE_MODE_VALIDATOR);
             VALIDATORS.put(DISPLAY_AUTO_OUTDOOR_MODE, DISPLAY_AUTO_OUTDOOR_MODE_VALIDATOR);
             VALIDATORS.put(DISPLAY_READING_MODE, DISPLAY_READING_MODE_VALIDATOR);
             VALIDATORS.put(DISPLAY_CABC, DISPLAY_CABC_VALIDATOR);
@@ -6168,7 +6124,6 @@ public final class Settings {
             VALIDATORS.put(DISPLAY_AUTO_CONTRAST, DISPLAY_AUTO_CONTRAST_VALIDATOR);
             VALIDATORS.put(DISPLAY_COLOR_ADJUSTMENT, DISPLAY_COLOR_ADJUSTMENT_VALIDATOR);
             VALIDATORS.put(DISPLAY_PICTURE_ADJUSTMENT, DISPLAY_PICTURE_ADJUSTMENT_VALIDATOR);
-            VALIDATORS.put(LIVE_DISPLAY_HINTED, LIVE_DISPLAY_HINTED_VALIDATOR);
             VALIDATORS.put(SCREENSHOT_SOUND, SCREENSHOT_SOUND_VALIDATOR);
             VALIDATORS.put(SWIPE_TO_SCREENSHOT, SWIPE_TO_SCREENSHOT_VALIDATOR);
             VALIDATORS.put(THEME_AUTOMATIC_TIME_IS_NIGHT, THEME_AUTOMATIC_TIME_IS_NIGHT_VALIDATOR);
