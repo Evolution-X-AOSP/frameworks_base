4cda32c436b21246ac7844cde8e32c62d33c508d
Chris Crump <chris@cypheros.co>
2019-07-20 09:46:35 -0400

    SystemUI: Update ambient indication margin when fod is in use
    Change-Id: I5d71aadeb47e141c7d83a2142196a6765a424cd1

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -138,6 +138,7 @@ import android.view.ThreadedRenderer;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+import android.view.ViewGroup.MarginLayoutParams;
 import android.view.ViewParent;
 import android.view.ViewTreeObserver;
 import android.view.WindowManager;
@@ -742,6 +743,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
     private IOverlayManager mOverlayManager;
     private boolean mKeyguardRequested;
     private boolean mIsKeyguard;
+    private boolean mIsFingerprintRunning;
     private LogMaker mStatusBarStateLog;
     private final LockscreenGestureLogger mLockscreenGestureLogger = new LockscreenGestureLogger();
     protected NotificationIconAreaController mNotificationIconAreaController;
@@ -770,6 +772,12 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                     if (dreaming) {
                         maybeEscalateHeadsUp();
                     }
+                }
+
+                @Override
+                public void onFingerprintRunningStateChanged(boolean running) {
+                    mIsFingerprintRunning = running;
+                    updateAmbientIndicationArea();
                 }
 
                 @Override
@@ -1146,6 +1154,9 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         if (mAmbientIndicationContainer != null) {
             ((AmbientIndicationContainer) mAmbientIndicationContainer).initializeView(this, mHandler);
         }
+
+        // set the initial padding for ambient indication
+        updateAmbientIndicationArea();
 
         // set the initial view visibility
         setAreThereNotifications();
@@ -1746,6 +1757,28 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
             }
         }
         setAreThereNotifications();
+    }
+
+    private void updateAmbientIndicationArea() {
+        if (mAmbientIndicationContainer == null) return;
+        int indicationBottomMargin = mContext.getResources().getDimensionPixelSize(
+                R.dimen.ambient_indication_margin_bottom);
+        int indicationBottomMarginFod = mContext.getResources().getDimensionPixelSize(
+                R.dimen.ambient_indication_margin_bottom_fingerprint_in_display);
+        MarginLayoutParams mlp = (MarginLayoutParams) mAmbientIndicationContainer.getLayoutParams();
+
+        int bottomMargin = hasInDisplayFingerprint() ? indicationBottomMarginFod : indicationBottomMargin;
+        boolean newLp = mlp.bottomMargin != bottomMargin;
+        if (newLp) {
+            mlp.bottomMargin = bottomMargin;
+            mAmbientIndicationContainer.setLayoutParams(mlp);
+        }
+    }
+
+    private boolean hasInDisplayFingerprint() {
+        return mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_needCustomFODView)
+                && mIsFingerprintRunning;
     }
 
     /**
