d19f744f525a11d5781a52e1b65e3954bd1efc71
maxwen <max.weninger@gmail.com>
2019-09-25 16:19:49 +0200

    base: navigation mode settings [1/2]
    to configure the height of the back gesture
    
    Change-Id: I6ebe0039e250f3b8f0b91b232df5941b58026499

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6328,6 +6328,9 @@ public final class Settings {
          */
         public static final String ALERT_SLIDER_PULSE = "alert_slider_pulse";
 
+        /** @hide */
+        public static final String BACK_GESTURE_HEIGHT = "back_gesture_height";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
