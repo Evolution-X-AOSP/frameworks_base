e084e3c4e0130d80e1e2bdd67835216235983106
Felipe Leme <felipeal@google.com>
2019-10-07 11:21:15 -0700

    (Minor) fix on ActivityManagerService.LocalService.getCurrentUserId()
    Bug: 142130022
    Test: manual verification with 'adb shell dumpsys user'
    
    Change-Id: I3aa7d847e9595e7b28428ad839234c993f7d2485
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -18139,7 +18139,7 @@ public class ActivityManagerService extends IActivityManager.Stub
 
         @Override
         public int getCurrentUserId() {
-            return mUserController.getCurrentUserIdLU();
+            return mUserController.getCurrentUserId();
         }
 
         @Override
