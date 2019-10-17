ffca6aa6314e9b5f0843d27a2494dd77b8ba082b
Ido Ben-Hur <idoybh2@gmail.com>
2019-10-17 04:15:53 +0300

    base: Allow toggling QS footer text [1/2]
    Change-Id: If765a1de25141de568d0dc7b8f1be1f1308fd4e6
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6577,6 +6577,12 @@ public final class Settings {
         public static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
 
         /**
+         * Whether to show qs footer text
+         * @hide
+         */
+        public static final String QS_FOOTER_TEXT_SHOW = "qs_footer_text_show";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
