70b2e3f468b478e255089ea31b239dc02eb5fd3b
maxwen <max.weninger@gmail.com>
2019-11-14 13:56:31 +0100

    FODCircleViewImpl: Optionally disable FOD night mode
    * AICP Edit: Fix for auto mode
    
    Change-Id: Ifea6718f99aa717d54208b5b07edd10119491e34
    Co-authored-by: Marko Man <darkobas@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleViewImpl.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleViewImpl.java
@@ -18,6 +18,9 @@ package com.android.systemui.biometrics;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.display.ColorDisplayManager;
+import android.os.Handler;
+import android.os.SystemProperties;
 import android.util.Slog;
 import android.view.View;
 
@@ -36,6 +39,9 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
 
     private FODCircleView mFodCircleView;
     private FodScreenOffHandler mFodScreenOffHandler;
+    private boolean mDisableNightMode;
+    private boolean mNightModeActive;
+    private int mAutoModeState;
 
     @Override
     public void start() {
@@ -74,11 +80,15 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
         } catch (RuntimeException e) {
             // do nothing
         }
+        mDisableNightMode = SystemProperties.getBoolean("persist.fod.night_mode_disabled", true);
     }
 
     @Override
     public void showInDisplayFingerprintView() {
         if (mFodCircleView != null) {
+            if (mDisableNightMode) {
+                disableNightMode();
+            }
             mFodCircleView.show();
         }
     }
@@ -86,7 +96,27 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
     @Override
     public void hideInDisplayFingerprintView() {
         if (mFodCircleView != null) {
+            if (mDisableNightMode) {
+                setNightMode(mNightModeActive, mAutoModeState);
+            }
             mFodCircleView.hide();
+        }
+    }
+
+    private void disableNightMode() {
+        ColorDisplayManager colorDisplayManager = mContext.getSystemService(ColorDisplayManager.class);
+        mAutoModeState = colorDisplayManager.getNightDisplayAutoMode();
+        mNightModeActive = colorDisplayManager.isNightDisplayActivated();
+        colorDisplayManager.setNightDisplayActivated(false);
+    }
+
+    private void setNightMode(boolean activated, int autoMode) {
+        ColorDisplayManager colorDisplayManager = mContext.getSystemService(ColorDisplayManager.class);
+        colorDisplayManager.setNightDisplayAutoMode(0);
+        if (autoMode == 0) {
+            colorDisplayManager.setNightDisplayActivated(activated);
+        } else if (autoMode == 1 || autoMode == 2) {
+            colorDisplayManager.setNightDisplayAutoMode(autoMode);
         }
     }
 }
