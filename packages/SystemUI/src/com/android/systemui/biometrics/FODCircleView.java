250cb2e254b939a58088ada891d26b2cc4240bd1
Stallix <team.evolutionx.rom@gmail.com>
2019-11-14 14:36:30 +0000

    Revert "FOD: Add pressed fp icon from MIUI"
    This reverts commit 0cc9aba2a00ecc5d116d9a3f8bb46354b7732225.

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -91,7 +91,7 @@ public class FODCircleView extends ImageView implements OnTouchListener {
 
             mHandler.post(() -> {
                 setDim(true);
-                setImageResource(R.drawable.fod_icon_pressed);
+                setImageDrawable(null);
 
                 invalidate();
             });
@@ -253,8 +253,7 @@ public class FODCircleView extends ImageView implements OnTouchListener {
         super.onDraw(canvas);
 
         if (mIsInsideCircle) {
-            //canvas.drawCircle(mWidth / 2, mHeight / 2, (float) (mWidth / 2.0f), mPaintFingerprint);
-            setImageResource(R.drawable.fod_icon_pressed);
+            canvas.drawCircle(mWidth / 2, mHeight / 2, (float) (mWidth / 2.0f), mPaintFingerprint);
         }
     }
 
@@ -325,7 +324,7 @@ public class FODCircleView extends ImageView implements OnTouchListener {
 
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             setDim(true);
-            setImageResource(R.drawable.fod_icon_pressed);
+            setImageDrawable(null);
         }
 
         return true;
