223900c6383e69c08b7d24b863b8a9c3c398846b
Stallix <team.evolutionx.rom@gmail.com>
2019-11-14 08:29:22 +0000

    Revert "SystemUI: FOD: Use custom Bottom Area View for keyguard message"
    This reverts commit aadebb1546d6ab8b0c105047d7d06b71ff7057a2.
    
    Change-Id: I116c0bc567afea7ffacf20d15ee075a92d652a80

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -174,8 +174,6 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     private boolean mIsPowerCameraGesture;
     private String mPreviousSource;
 
-    private boolean mDisplayFODView;
-
     public KeyguardBottomAreaView(Context context) {
         this(context, null);
     }
@@ -243,16 +241,8 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         mEnterpriseDisclosure = findViewById(
                 R.id.keyguard_indication_enterprise_disclosure);
         mIndicationText = findViewById(R.id.keyguard_indication_text);
-        mDisplayFODView = getResources()
-                .getBoolean(com.android.internal.R.bool.config_needCustomFODView);
-
-        if (mDisplayFODView) {
-            mIndicationBottomMargin = getResources().getDimensionPixelSize(
-                  R.dimen.op_keyguard_indication_margin_bottom);
-        } else {
-            mIndicationBottomMargin = getResources().getDimensionPixelSize(
-                  R.dimen.keyguard_indication_margin_bottom);
-        }
+        mIndicationBottomMargin = getResources().getDimensionPixelSize(
+                R.dimen.keyguard_indication_margin_bottom);
         mBurnInYOffset = getResources().getDimensionPixelSize(
                 R.dimen.default_burn_in_prevention_offset);
         updateCameraVisibility();
@@ -315,13 +305,8 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     @Override
     protected void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        if (mDisplayFODView) {
-            mIndicationBottomMargin = getResources().getDimensionPixelSize(
-                  R.dimen.op_keyguard_indication_margin_bottom);
-        } else {
-            mIndicationBottomMargin = getResources().getDimensionPixelSize(
-                  R.dimen.keyguard_indication_margin_bottom);
-        }
+        mIndicationBottomMargin = getResources().getDimensionPixelSize(
+                R.dimen.keyguard_indication_margin_bottom);
         mBurnInYOffset = getResources().getDimensionPixelSize(
                 R.dimen.default_burn_in_prevention_offset);
         MarginLayoutParams mlp = (MarginLayoutParams) mIndicationArea.getLayoutParams();
