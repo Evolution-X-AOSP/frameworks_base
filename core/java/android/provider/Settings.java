427a80353ea3bf78b9518fb4f5239822e1ed35f8
Anushek Prasal <anushekprasal@gmail.com>
2019-11-14 14:07:44 +0530

    base: Add refresh rate setting [1/2]
    Change-Id: Ie794574f624bf48f58876ac6761ce28dccf0912c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5385,6 +5385,11 @@ public final class Settings {
         public static final String DOZE_TRIGGER_DOUBLETAP = "doze_trigger_doubletap";
 
         /**
+         * @hide
+         */
+        public static final String REFRESH_RATE_SETTING = "refresh_rate_setting";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
