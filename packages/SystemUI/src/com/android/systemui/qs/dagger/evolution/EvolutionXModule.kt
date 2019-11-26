5e42d9200341030e883583d75db6642938f86eef
Christian Oder <myself5@carbonrom.org>
2019-11-26 23:35:01 +0100

    SystemUI: Introduce Data Switch QS Tile
    * Based on OnePlus' OxygenOS tile, reworked to work with AOSP toggling without requirements on proprietary telephony-ext features
    
    idoybh (YAAP): Adapt to A12
    
    [ghostrider-reborn]
    - Set secondary label to SIM1 or SIM2 instead of just "On"
    
    [Jyotiraditya Panda <jyotiraditya@aospa.co>]
    - Code cleanup and improvements
    
    [Andrew Fluck <andrew@aospa.co>]
    - Update SIMs QS icons
    
    Change-Id: Ie2e280c07f24f9da6b4ee218b72501a2713ce429
    Co-authored-by: Andrew Fluck <andrew@aospa.co>
    Signed-off-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/dagger/evolution/EvolutionXModule.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/evolution/EvolutionXModule.kt
@@ -21,6 +21,7 @@ import com.android.systemui.qs.tiles.AODTile
 import com.android.systemui.qs.tiles.AmbientDisplayTile
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.CompassTile
+import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.ReadingModeTile
@@ -53,6 +54,12 @@ interface EvolutionXModule {
     @IntoMap
     @StringKey(CompassTile.TILE_SPEC)
     fun bindCompassTile(compassTile: CompassTile): QSTileImpl<*>
+
+    /** Inject DataSwitchTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(DataSwitchTile.TILE_SPEC)
+    fun bindDataSwitchTile(dataSwitchTile: DataSwitchTile): QSTileImpl<*>
 
     /** Inject SoundTile into tileMap in QSModule */
     @Binds
