49da0bcf9febed9e6ab6cf9e01dd5b194e68dc0c
maxwen <max.weninger@gmail.com>
2019-11-27 16:27:40 +0100

    SystemUI: old style mobile data indicators [1/2]
    neobuddy89: Use Tuner API.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5460,7 +5460,6 @@ public final class Settings {
          */
         public static final String HEADS_UP_STOPLIST_VALUES = "heads_up_stoplist_values";
 
-
         /**
          * Which applications to disable heads up notifications for
          *
@@ -5810,6 +5809,11 @@ public final class Settings {
          * @hide
          */
         public static final String BATTERY_LIGHT_REALLYFULL_COLOR = "battery_light_reallyfull_color";
+
+        /**
+         * @hide
+         */
+        public static final String USE_OLD_MOBILETYPE = "use_old_mobiletype";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
