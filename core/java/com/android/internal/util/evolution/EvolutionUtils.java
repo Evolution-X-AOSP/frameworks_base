7b8215678b29cc516430ecda061aebbef5a44304
Anushek Prasal <anushekprasal@gmail.com>
2019-11-30 16:08:56 +0530

    EvolutionUtils: Update notch check logic
    Some devices with notch have set config_fillMainBuiltInDisplayCutout to false. This results in Utils returning true for hasNotch. Instead let's check if the bounding path of the cutout region is defined. Also return false if NoCutoutOverlay is active.
    
    Change-Id: Ib134312d1c49b8ad8c086a1271cc29f6df020f3c
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/com/android/internal/util/evolution/EvolutionUtils.java
+++ core/java/com/android/internal/util/evolution/EvolutionUtils.java
@@ -223,20 +223,9 @@ public class EvolutionUtils {
 
 	    // Check if device has a notch
     public static boolean hasNotch(Context context) {
-        int result = 0;
-        int resid;
-        int resourceId = context.getResources().getIdentifier(
-                "status_bar_height", "dimen", "android");
-        resid = context.getResources().getIdentifier("config_fillMainBuiltInDisplayCutout",
-                "bool", "android");
-        if (resid > 0) {
-            return context.getResources().getBoolean(resid);
-        }
-        if (resourceId > 0) {
-            result = context.getResources().getDimensionPixelSize(resourceId);
-        }
-        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
-        float px = 24 * (metrics.densityDpi / 160f);
-        return result > Math.round(px);
+        String displayCutout = context.getResources().getString(R.string.config_mainBuiltInDisplayCutout);
+        boolean maskDisplayCutout = context.getResources().getBoolean(R.bool.config_maskMainBuiltInDisplayCutout);
+        boolean displayCutoutExists = (!TextUtils.isEmpty(displayCutout) && !maskDisplayCutout);
+        return displayCutoutExists;
     }
 }
