ffd809ecd17dd35d920493b5c0f81a441cf291fc
Anushek Prasal <anushekprasal@gmail.com>
2019-12-06 17:42:40 +0530

    SystemUI: Vibrate on volume panel button long press

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -1502,6 +1502,7 @@ public class VolumeDialogImpl implements VolumeDialog,
     public boolean onLongClick(View v) {
         if (v == mSettingsIcon) {
             startSoundActivity();
+            mController.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_HEAVY_CLICK));
         }
         return false;
     }
