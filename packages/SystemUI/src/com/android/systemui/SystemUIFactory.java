6b7c9676c7a346c9f10962fcccb1ffe598b318e9
Stallix <team.evolutionx.rom@gmail.com>
2019-12-13 21:59:17 +0000

    Revert "SystemUI: Add method for getting estimated battery time"
    This reverts commit 63e2c8d8095c30ff22f7b4c13754d813bf56dd26.

--- packages/SystemUI/src/com/android/systemui/SystemUIFactory.java
+++ packages/SystemUI/src/com/android/systemui/SystemUIFactory.java
@@ -207,7 +207,7 @@ public class SystemUIFactory {
     @Singleton
     @Provides
     public EnhancedEstimates provideEnhancedEstimates(Context context) {
-        return new EnhancedEstimatesImpl(context);
+        return new EnhancedEstimatesImpl();
     }
 
     @Singleton
