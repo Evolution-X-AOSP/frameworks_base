Evolution-X-AOSP/frameworks_base

commit 748292f469bb3885ac95b865716d59e7427cb2d0

Use FORCE_SHOW_NAVBAR to actually toggle navbar
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -757,19 +757,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         final TunerService tunerService = Dependency.get(TunerService.class);
         tunerService.addTunable(this, SYSUI_ROUNDED_FWVALS);
 
-        mDisplayManager = mContext.getSystemService(DisplayManager.class);
-        mNeedsNavigationBar = mContext.getResources().getBoolean(
-                com.android.internal.R.bool.config_showNavigationBar);
-
-        // Allow a system property to override this. Used by the emulator.
-        // See also hasNavigationBar().
-        String navBarOverride = SystemProperties.get("qemu.hw.mainkeys");
-        if ("1".equals(navBarOverride)) {
-            mNeedsNavigationBar = false;
-        } else if ("0".equals(navBarOverride)) {
-            mNeedsNavigationBar = true;
-        }
-
         mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
         mDreamManager = IDreamManager.Stub.asInterface(
                 ServiceManager.checkService(DreamService.DREAM_SERVICE));
@@ -4238,7 +4225,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     private void updateNavigationBarVisibility() {
         if (mDisplayId == Display.DEFAULT_DISPLAY && mWindowManagerService != null) {
-            boolean forcedVisibility = mNeedsNavigationBar || Settings.System.getIntForUser(
+            boolean forcedVisibility = Settings.System.getIntForUser(
                 mContext.getContentResolver(), Settings.System.FORCE_SHOW_NAVBAR,
                  0, UserHandle.USER_CURRENT) == 1;
             boolean hasNavbar = getNavigationBarView() != null;
@@ -4852,7 +4839,6 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     protected NavigationBarController mNavigationBarController;
     private NavigationBarController.SystemUiVisibility mNavigationBarSystemUiVisibility;
-    private boolean mNeedsNavigationBar;
 
     // UI-specific methods
 

commit 2afed886e678604b4eda73aabee32a9f7e3af74a

SystemUI: Fix derp caused by 748292f469bb3885ac95b865716d59e7427cb2d0
* Bad editor

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -761,6 +761,8 @@ public class StatusBar extends SystemUI implements DemoMode,
         final TunerService tunerService = Dependency.get(TunerService.class);
         tunerService.addTunable(this, SYSUI_ROUNDED_FWVALS);
 
+        mDisplayManager = mContext.getSystemService(DisplayManager.class);
+
         mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
         mDreamManager = IDreamManager.Stub.asInterface(
                 ServiceManager.checkService(DreamService.DREAM_SERVICE));