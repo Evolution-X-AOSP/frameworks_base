4ab0b39f61041196660070f1c7af77f0ab9f6aa4
Stallix <team.evolutionx.rom@gmail.com>
2019-12-17 15:10:19 +0000

    RecorderConstants: Use OP Screen Recorder package name

--- core/java/com/android/internal/util/custom/recorder/RecorderConstants.java
+++ core/java/com/android/internal/util/custom/recorder/RecorderConstants.java
@@ -16,5 +16,5 @@
 package com.android.internal.util.custom.recorder;
 
 public class RecorderConstants {
-    public static final String APP_PACKAGE_NAME = "org.lineageos.recorder";
+    public static final String APP_PACKAGE_NAME = "com.oneplus.screenrecord";
 }
