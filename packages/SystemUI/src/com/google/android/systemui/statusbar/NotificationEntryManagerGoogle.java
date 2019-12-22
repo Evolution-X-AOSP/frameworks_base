a4c8fc59dbd89beb839a03b0462b3bd112f50faf
Dyneteve <dyneteve@pixelexperience.org>
2019-12-22 20:07:26 +0200

    SystemUI: Update NotificationEntryManagerGoogle
    Change-Id: Ib28ecf258854ac8b1a460b7e9c95fff6e0135eaa

--- packages/SystemUI/src/com/google/android/systemui/statusbar/NotificationEntryManagerGoogle.java
+++ packages/SystemUI/src/com/google/android/systemui/statusbar/NotificationEntryManagerGoogle.java
@@ -9,14 +9,17 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.service.notification.NotificationListenerService.RankingMap;
 import android.service.notification.StatusBarNotification;
+
 import com.android.systemui.settings.CurrentUserTracker;
 import com.android.systemui.statusbar.NotificationPresenter;
 import com.android.systemui.statusbar.notification.NotificationEntryManager;
 import com.android.systemui.statusbar.notification.stack.NotificationListContainer;
 import com.android.systemui.statusbar.policy.HeadsUpManager;
+
 import com.google.android.collect.Sets;
 
 import java.util.HashSet;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -58,7 +61,8 @@ public class NotificationEntryManagerGoogle extends NotificationEntryManager {
     @Override
     public void addNotification(StatusBarNotification statusBarNotification, RankingMap rankingMap) {
         super.addNotification(statusBarNotification, rankingMap);
-        if (mShouldBroadcastNotifications && mUserTracker.getCurrentUserId() == 0) {
+        boolean z = mUserTracker.getCurrentUserId() == 0;
+        if (mShouldBroadcastNotifications && z) {
             Intent intent = new Intent();
             intent.setPackage(mWallpaperPackage);
             intent.setAction("com.breel.wallpapers.NOTIFICATION_RECEIVED");
@@ -68,26 +72,19 @@ public class NotificationEntryManagerGoogle extends NotificationEntryManager {
     }
 
     private void checkNotificationBroadcastSupport() {
+        WallpaperInfo wallpaperInfo;
         mShouldBroadcastNotifications = false;
         WallpaperManager wallpaperManager = (WallpaperManager) mContext.getSystemService(WallpaperManager.class);
-        WallpaperInfo wallpaperInfo = wallpaperManager != null ? wallpaperManager.getWallpaperInfo() : null;
-        if (wallpaperInfo != null) {
+        if (wallpaperManager != null && (wallpaperInfo = wallpaperManager.getWallpaperInfo()) != null) {
             ComponentName component = wallpaperInfo.getComponent();
             String packageName = component.getPackageName();
             if (NOTIFYABLE_PACKAGES.contains(packageName)) {
                 mWallpaperPackage = packageName;
                 String className = component.getClassName();
-                String[] strArr = NOTIFYABLE_WALLPAPERS;
-                int length = strArr.length;
-                int i = 0;
-                while (true) {
-                    if (i >= length) {
-                        break;
-                    } else if (className.startsWith(strArr[i])) {
+                for (String startsWith : NOTIFYABLE_WALLPAPERS) {
+                    if (className.startsWith(startsWith)) {
                         mShouldBroadcastNotifications = true;
-                        break;
-                    } else {
-                        i++;
+                        return;
                     }
                 }
             }
