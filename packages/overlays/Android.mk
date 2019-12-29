3a470d93944ad983128f98350852165e18646a34
Pranav Vashi <neobuddy89@gmail.com>
2019-12-30 00:48:42 +0530

    Add cylinder icon shape overlay
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -90,6 +90,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconPackRoundedPixelThemePickerOverlay \
 	IconPackRoundedSettingsOverlay \
 	IconPackRoundedSystemUIOverlay \
+	IconShapeCylinderOverlay \
 	IconShapeRoundedRectOverlay \
 	IconShapeSquareOverlay \
 	IconShapeSquircleOverlay \
