34036e7f340d4fdda6765d6213ba98f4a6ad9850
ElDainosor <eldainosor@gmail.com>
2020-01-07 14:34:05 -0700

    SystemUI: Lockscreen clock styles: Add sammy accent clock and a variant [1/2]
    Inspired on a KLWP design i saw someday, this just adds accent on time because it looks nice
    The second design is an attempt to make it kind of similar to some designs i saw for Rainmeter
    
    Change-Id: I9aa7492582b1e0ea882311511aa47b89251dd3c2

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -289,11 +289,17 @@ public class KeyguardStatusView extends GridLayout implements
             mClockView.setFormat12Hour(Html.fromHtml("<strong>h</strong>:mm"));
             mClockView.setFormat24Hour(Html.fromHtml("<strong>kk</strong>:mm"));
         } else if (mClockSelection == 4) {
-            mClockView.setFormat12Hour("hh\nmm");
-            mClockView.setFormat24Hour("kk\nmm");
-        } else if (mClockSelection == 6) {
             mClockView.setFormat12Hour(Html.fromHtml("<strong>h:mm</strong>"));
             mClockView.setFormat24Hour(Html.fromHtml("<strong>kk:mm</strong>"));
+        } else if (mClockSelection == 5) {
+            mClockView.setFormat12Hour("hh\nmm");
+            mClockView.setFormat24Hour("kk\nmm");
+        } else if (mClockSelection == 7) {
+            mClockView.setFormat12Hour(Html.fromHtml("hh<br><font color=" + getResources().getColor(R.color.sammy_minutes_accent) + ">mm</font>"));
+            mClockView.setFormat24Hour(Html.fromHtml("kk<br><font color=" + getResources().getColor(R.color.sammy_minutes_accent) + ">mm</font>"));
+        } else if (mClockSelection == 8) {
+            mClockView.setFormat12Hour(Html.fromHtml("<font color='#454545'>hh</font><br><font color=" + getResources().getColor(R.color.sammy_minutes_accent) + ">mm</font>"));
+            mClockView.setFormat24Hour(Html.fromHtml("<font color='#454545'>kk</font><br><font color=" + getResources().getColor(R.color.sammy_minutes_accent) + ">mm</font>"));
         } else {
             mClockView.setFormat12Hour(Html.fromHtml("<strong>hh</strong><br>mm"));
             mClockView.setFormat24Hour(Html.fromHtml("<strong>kk</strong><br>mm"));
@@ -798,13 +804,19 @@ public class KeyguardStatusView extends GridLayout implements
             case 3: // default (bold)
                 mClockView.setVisibility(View.VISIBLE);
                 break;
-            case 4: // sammy
+            case 4: // default (small font)
                 mClockView.setVisibility(View.VISIBLE);
                 break;
-            case 5: // sammy (bold)
+            case 5: // sammy
                 mClockView.setVisibility(View.VISIBLE);
                 break;
-            case 6: // default (small font)
+            case 6: // sammy (bold)
+                mClockView.setVisibility(View.VISIBLE);
+                break;
+            case 7: // sammy (accent)
+                mClockView.setVisibility(View.VISIBLE);
+                break;
+            case 8: // sammy (accent alt)
                 mClockView.setVisibility(View.VISIBLE);
                 break;
         }
