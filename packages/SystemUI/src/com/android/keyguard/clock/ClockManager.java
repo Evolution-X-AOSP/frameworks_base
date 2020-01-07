24516c30eb94485843409a2bf2090d5e8cbc7d5c
maxwen <max.weninger@gmail.com>
2020-01-07 19:50:18 +0100

    SystemUI: custom keyguard clock kick off
    Blissroms edit: Stripped down version of the original,
    remove one line and two line clock.
    
    allow custom clock to be in the same position as the default
    clock. Add bold hour clock back from the days where we
    had custom keyguard clock already
    
    Change-Id: I11bb2b0b70327bfc824b126f07043db49262c13b
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -148,7 +148,7 @@ public final class ClockManager {
         LayoutInflater layoutInflater = injectionInflater.injectable(LayoutInflater.from(context));
 
         addBuiltinClock(() -> new DefaultClockController(res, layoutInflater, colorExtractor));
-        addBuiltinClock(() -> new BubbleClockController(res, layoutInflater, colorExtractor));
+        //addBuiltinClock(() -> new BubbleClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new AnalogClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new TypeClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new BinaryClockController(res, layoutInflater, colorExtractor));
@@ -331,7 +331,8 @@ public final class ClockManager {
                     .setTitle(plugin::getTitle)
                     .setId(id)
                     .setThumbnail(plugin::getThumbnail)
-                    .setPreview(() -> plugin.getPreview(mWidth, mHeight))
+                     // to make the preview bigger in ThemePicker
+                    .setPreview(() -> plugin.getPreview(mWidth / 2, mHeight / 2))
                     .build());
         }
 
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -545,6 +545,10 @@ public class NotificationPanelView extends PanelView implements
         return mKeyguardStatusView;
     }
 
+    public boolean hasCustomClockInBigContainer() {
+        return mKeyguardStatusView.hasCustomClockInBigContainer();
+    }
+
     private void setStatusBar(StatusBar bar) {
         mStatusBar = bar;
         mKeyguardBottomArea.setStatusBar(mStatusBar);
@@ -895,7 +899,7 @@ public class NotificationPanelView extends PanelView implements
                     (int) (mKeyguardStatusView.getHeight()
                             - mShelfHeight / 2.0f - mDarkIconSize / 2.0f),
                     clockPreferredY,
-                    hasCustomClock(),
+                    hasCustomClockInBigContainer(),
                     hasVisibleNotifications,
                     mInterpolatedDarkAmount,
                     mEmptyDragAmount,
