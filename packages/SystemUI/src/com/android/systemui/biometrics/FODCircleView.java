f30159f636d5191768d240794c8d5fab11ee082c
00day0 <therandomuser11@gmail.com>
2020-01-10 13:50:25 +0100

    FODCircleView: Dont change alpha of fod on AOD
    * looks ugly
    
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -419,8 +419,6 @@ public class FODCircleView extends ImageView {
     private void updateAlpha() {
         if (mIsCircleShowing) {
             setAlpha(1.0f);
-        } else {
-            setAlpha(mIsDreaming ? 0.5f : 1.0f);
         }
     }
 
