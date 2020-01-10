d34fcc92f6f868f56f39fcbbcd64abcc5f6c36de
Joey Huab <team.evolutionx.rom@gmail.com>
2020-01-10 06:54:45 +0000

    SystemUI: Set default QS columns (portrait) to 3 [1/2]

--- packages/SystemUI/src/com/android/systemui/qs/TileLayout.java
+++ packages/SystemUI/src/com/android/systemui/qs/TileLayout.java
@@ -93,7 +93,7 @@ public class TileLayout extends ViewGroup implements QSTileLayout {
         int rows;
         if (res.getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
             columns = Settings.System.getIntForUser(resolver,
-                    Settings.System.QS_COLUMNS_PORTRAIT, 4,
+                    Settings.System.QS_COLUMNS_PORTRAIT, 3,
                     UserHandle.USER_CURRENT);
             rows = Settings.System.getIntForUser(mContext.getContentResolver(),
                     Settings.System.QS_ROWS_PORTRAIT, 3,
