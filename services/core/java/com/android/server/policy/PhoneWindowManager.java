fc8ca60bebc49d154bdb57f4ba90eb3855fff521
Joey Huab <joey@evolution-x.org>
2020-01-16 17:18:22 +0000

    PhoneWindowManager: Fixes to Hardware key disable

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -4188,7 +4188,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 		&& (!mHapticOnAction)
                 && (policyFlags & WindowManagerPolicy.FLAG_VIRTUAL) != 0
                 && (!isNavBarVirtKey || mNavBarVirtualKeyHapticFeedbackEnabled)
-                && event.getRepeatCount() == 0;
+                && event.getRepeatCount() == 0
+                && !isHwKeysDisabled();
 
         if (!virtualKey) {
             if (isHwKeysDisabled() || keyguardOn() || isDozeMode()) {
