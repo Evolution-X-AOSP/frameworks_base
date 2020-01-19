f83fa029cea6258f5a5496813da26daae1b206fa
El Dainosor <eldainosor@gmail.com>
2020-01-19 07:44:18 +0100

    base: Let's shrink file headers bigger than the device size
    Custom headers and new cameras with bigger resolution sizes aren't a good match.
    This might put a stop to those who put a 4K image as their header and complains about SysUI struggling :)
    
    Change-Id: I4a9ade0008f532d84160f44c57efdaa3e1fdeb30
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/com/android/internal/util/evolution/ImageHelper.java
+++ core/java/com/android/internal/util/evolution/ImageHelper.java
@@ -38,7 +38,9 @@ import android.renderscript.Element;
 import android.renderscript.Allocation;
 import android.renderscript.ScriptIntrinsicBlur;
 import android.renderscript.RenderScript;
+import android.util.DisplayMetrics;
 import android.util.TypedValue;
+import android.view.WindowManager;
 
 public class ImageHelper {
 
@@ -120,6 +122,42 @@ public class ImageHelper {
         canvas.drawBitmap(bitmap, middleX - bitmap.getWidth() / 2,
                 middleY - bitmap.getHeight() / 2, paint);
         return new BitmapDrawable(context.getResources(), scaledBitmap);
+    }
+
+    public static Bitmap resizeMaxDeviceSize(Context context, Drawable image) {
+        Bitmap i2b = ((BitmapDrawable) image).getBitmap();
+        return resizeMaxDeviceSize(context, i2b);
+    }
+
+    public static Bitmap resizeMaxDeviceSize(Context context, Bitmap image) {
+        Bitmap imageToBitmap;
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager wm = context.getSystemService(WindowManager.class);
+        wm.getDefaultDisplay().getRealMetrics(metrics);
+        int maxHeight = metrics.heightPixels;
+        int maxWidth = metrics.widthPixels;
+        try {
+            imageToBitmap = RGB565toARGB888(image);
+            if (maxHeight > 0 && maxWidth > 0) {
+                int width = imageToBitmap.getWidth();
+                int height = imageToBitmap.getHeight();
+                float ratioBitmap = (float) width / (float) height;
+                float ratioMax = (float) maxWidth / (float) maxHeight;
+
+                int finalWidth = maxWidth;
+                int finalHeight = maxHeight;
+                if (ratioMax > ratioBitmap) {
+                    finalWidth = (int) ((float)maxHeight * ratioBitmap);
+                } else {
+                    finalHeight = (int) ((float)maxWidth / ratioBitmap);
+                }
+                imageToBitmap = Bitmap.createScaledBitmap(imageToBitmap, finalWidth, finalHeight, true);
+                return imageToBitmap;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return image;
     }
 
     public static Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
