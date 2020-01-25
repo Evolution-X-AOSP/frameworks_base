1d6a4cb6c65ae30c9ab72982dd752268c7e17f60
Joey Huab <joey@evolution-x.org>
2020-01-25 15:46:13 +0000

    Revert "base: refine partial screenshot handling"
    This reverts commit 425548b12afc21671f379f18820dfc25b121d591.

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -72,12 +72,10 @@ import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.util.Slog;
 import android.view.Display;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.SurfaceControl;
 import android.view.View;
-import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.animation.Interpolator;
@@ -538,10 +536,6 @@ class GlobalScreenshot {
 
     private MediaActionSound mCameraSound;
 
-    public static boolean mPartialShotStarted;
-    public static boolean mPartialShot;
-    private float mTouchDownX;
-    private float mTouchDownY;
 
     /**
      * @param context everything needs a context :(
@@ -659,7 +653,6 @@ class GlobalScreenshot {
     }
 
     void takeScreenshot(Runnable finisher, boolean statusBarVisible, boolean navBarVisible) {
-        mPartialShot = false;
         mDisplay.getRealMetrics(mDisplayMetrics);
         takeScreenshot(finisher, statusBarVisible, navBarVisible,
                 new Rect(0, 0, mDisplayMetrics.widthPixels, mDisplayMetrics.heightPixels));
@@ -671,33 +664,23 @@ class GlobalScreenshot {
     void takeScreenshotPartial(final Runnable finisher, final boolean statusBarVisible,
             final boolean navBarVisible) {
         mWindowManager.addView(mScreenshotLayout, mWindowLayoutParams);
-        mPartialShotStarted = false;
-        mPartialShot = true;
-        ViewConfiguration vc = ViewConfiguration.get(mContext);
-        final int touchSlop = vc.getScaledTouchSlop();
         mScreenshotSelectorView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 ScreenshotSelectorView view = (ScreenshotSelectorView) v;
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
-                        mPartialShotStarted = true;
-                        mTouchDownX = event.getRawX();
-                        mTouchDownY = event.getRawY();
                         view.startSelection((int) event.getX(), (int) event.getY());
                         return true;
                     case MotionEvent.ACTION_MOVE:
                         view.updateSelection((int) event.getX(), (int) event.getY());
                         return true;
                     case MotionEvent.ACTION_UP:
-                        float x = event.getRawX();
-                        float y = event.getRawY();
                         view.setVisibility(View.GONE);
                         mWindowManager.removeView(mScreenshotLayout);
-                        if (Math.abs(mTouchDownX - x) > touchSlop ||
-                            Math.abs(mTouchDownY - y) > touchSlop) {
-                            final Rect rect = view.getSelectionRect();
-                            if (rect != null && !rect.isEmpty()) {
+                        final Rect rect = view.getSelectionRect();
+                        if (rect != null) {
+                            if (rect.width() != 0 && rect.height() != 0) {
                                 // Need mScreenshotLayout to handle it after the view disappears
                                 mScreenshotLayout.post(new Runnable() {
                                     public void run() {
@@ -705,16 +688,11 @@ class GlobalScreenshot {
                                                 rect);
                                     }
                                 });
-                                view.stopSelection();
-                                return true;
                             }
                         }
-                        finisher.run();
+
                         view.stopSelection();
                         return true;
-                    case MotionEvent.ACTION_CANCEL:
-                        stopScreenshot();
-                        finisher.run();
                 }
 
                 return false;
@@ -943,11 +921,6 @@ class GlobalScreenshot {
     }
 
     static void notifyScreenshotError(Context context, NotificationManager nManager, int msgResId) {
-        // do nothing - it was a partial screenshot and no selection was made
-        if (mPartialShot && !mPartialShotStarted) {
-            return;
-        }
-
         Resources r = context.getResources();
         String errorMsg = r.getString(msgResId);
 
