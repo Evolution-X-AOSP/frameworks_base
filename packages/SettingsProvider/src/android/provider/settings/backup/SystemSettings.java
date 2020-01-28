fe2315706a7bb943bfee1d8b94343d451da6cb3d
Marko Man <darkobas@gmail.com>
2020-01-29 00:24:33 +0100

    base: Pulse light accent color option
    Change-Id: If402e96d7549aff7f758236e6f54a6034d08f9c2
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -91,5 +91,6 @@ public class SystemSettings {
         Settings.System.PULSE_AMBIENT_LIGHT,
         Settings.System.AMBIENT_NOTIFICATION_LIGHT_ENABLED,
         Settings.System.AMBIENT_NOTIFICATION_LIGHT,
+        Settings.System.AMBIENT_NOTIFICATION_LIGHT_ACCENT,
     };
 }
