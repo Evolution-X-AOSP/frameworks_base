52da32719ee6b11d6a87915a8e1e99505a826d28
Joey Huab <joey@evolution-x.org>
2020-01-28 10:19:56 +0000

    Revert "fingerprint: notify client when cancelling succeeded"
    This reverts commit fb4838fbb42992bc9124d92ba8415cb63a17e482.

--- services/core/java/com/android/server/biometrics/BiometricServiceBase.java
+++ services/core/java/com/android/server/biometrics/BiometricServiceBase.java
@@ -120,7 +120,6 @@ public abstract class BiometricServiceBase extends SystemService
         public int permanentLockout; // total number of permanent lockouts
     }
 
-    private final boolean mNotifyClient;
     private final boolean mCleanupUnusedFingerprints;
 
     /**
@@ -657,8 +656,6 @@ public abstract class BiometricServiceBase extends SystemService
         mPowerManager = mContext.getSystemService(PowerManager.class);
         mUserManager = UserManager.get(mContext);
         mMetricsLogger = new MetricsLogger();
-        mNotifyClient = mContext.getResources().getBoolean(
-                com.android.internal.R.bool.config_notifyClientOnFingerprintCancelSuccess);
         mCleanupUnusedFingerprints = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_cleanupUnusedFingerprints);
         mPostResetRunnableForAllClients = mContext.getResources().getBoolean(
@@ -912,11 +909,7 @@ public abstract class BiometricServiceBase extends SystemService
                             + ", fromClient: " + fromClient);
                     // If cancel was from BiometricService, it means the dialog was dismissed
                     // and authentication should be canceled.
-                    final int stopResult = client.stop(client.getToken() == token);
-                    if (mNotifyClient && (stopResult == 0)) {
-                        handleError(mHalDeviceId,
-                                BiometricConstants.BIOMETRIC_ERROR_CANCELED, 0);
-                    }
+                    client.stop(client.getToken() == token);
                 } else {
                     if (DEBUG) Slog.v(getTag(), "Can't stop client " + client.getOwnerString()
                             + " since tokens don't match. fromClient: " + fromClient);
