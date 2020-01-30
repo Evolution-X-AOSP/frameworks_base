821b745115b4868fbcdeae4f43a715c25e5513fa
Joey Huab <joey@evolution-x.org>
2020-01-30 12:07:46 +0000

    Revert "Move BT state to OFF when BLE app count is zero"
    This reverts commit ae70d0f30328b8cc3ecf7b1477629ce3df39aac7.

--- services/core/java/com/android/server/BluetoothManagerService.java
+++ services/core/java/com/android/server/BluetoothManagerService.java
@@ -763,7 +763,6 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
             checkPackage(callingUid, packageName);
         }
         ClientDeathRecipient r = mBleApps.get(token);
-        int st = BluetoothAdapter.STATE_OFF;
         if (r == null && enable) {
             ClientDeathRecipient deathRec = new ClientDeathRecipient(packageName);
             try {
@@ -790,21 +789,8 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
         if (appCount == 0 && mEnable) {
             disableBleScanMode();
         }
-        if(appCount == 0) {
-            try {
-                mBluetoothLock.readLock().lock();
-                if (mBluetooth != null) {
-                    st = mBluetooth.getState();
-                }
-                if (!mEnableExternal || (st == BluetoothAdapter.STATE_BLE_ON)) {
-                    if (DBG) Slog.d(TAG, "Move to BT state OFF");
-                    sendBrEdrDownCallback();
-                }
-            } catch (RemoteException e) {
-                Slog.e(TAG, "", e);
-            } finally {
-                mBluetoothLock.readLock().unlock();
-            }
+        if (appCount == 0 && !mEnableExternal) {
+            sendBrEdrDownCallback();
         }
         return appCount;
     }
