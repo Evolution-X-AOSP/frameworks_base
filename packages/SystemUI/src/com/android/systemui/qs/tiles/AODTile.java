11697060dd4be7b029be07ca82740af9c6e37802
shagbag913 <sh4gbag913@gmail.com>
2020-02-06 20:12:02 -0500

    SystemUI: AODTile: Open LS display settings on long click
    Change-Id: I7d3bdcca687c2a54d0c129965837ec14608fb832

--- packages/SystemUI/src/com/android/systemui/qs/tiles/AODTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/AODTile.java
@@ -55,6 +55,12 @@ public class AODTile extends QSTileImpl<State> {
 
     public static final String TILE_SPEC = "aod";
 
+    private static final ComponentName LS_DISPLAY_SETTINGS_COMPONENT = new ComponentName(
+            "com.android.settings", "com.android.settings.Settings$LockscreenDashboardActivity");
+
+    private static final Intent LS_DISPLAY_SETTINGS =
+            new Intent().setComponent(LS_DISPLAY_SETTINGS_COMPONENT);
+
     @Inject
     public AODTile(QSHost host,
             QsEventLogger uiEventLogger,
@@ -113,7 +119,7 @@ public class AODTile extends QSTileImpl<State> {
 
     @Override
     public Intent getLongClickIntent() {
-        return null;
+        return LS_DISPLAY_SETTINGS;
     }
 
     @Override
