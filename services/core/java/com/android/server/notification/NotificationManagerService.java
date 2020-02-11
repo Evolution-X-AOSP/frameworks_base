738f07ca47c5023b7744b3698221605ba94e642c
woongki min <wk0831.min@samsung.com>
2020-02-11 15:56:56 +0900

    Need to check current user when triggering notification lights
    Notification Lights also requires user check like sound and vibration.
    Otherwise, lights can occur without notifications.
    
    Test: runtest BuzzBeepBlinkTest
    
    Change-Id: I625db35796b7a479283ce51973ad2350559bd47f
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -6115,6 +6115,10 @@ public class NotificationManagerService extends SystemService {
         if (/*isInCall() || */mScreenOn) {
             return false;
         }
+        // check current user
+        if (!isNotificationForCurrentUser(record)) {
+            return false;
+        }
 
         return true;
     }
