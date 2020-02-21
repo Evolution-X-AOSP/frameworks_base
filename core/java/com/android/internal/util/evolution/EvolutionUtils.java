4a1ae67fb96e69b8f80850e00077e53a9bee6cd2
Ali B <abittin@gmail.com>
2020-02-21 13:22:57 +0300

    EvolutionUtils: add Doze package checks
    Blissroms edit: add check for custom doze as well!
    
    Change-Id: I3556f484a0d6498c1e7f11356bc5e790a4bac73e
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/com/android/internal/util/evolution/EvolutionUtils.java
+++ core/java/com/android/internal/util/evolution/EvolutionUtils.java
@@ -71,6 +71,10 @@ public class EvolutionUtils {
 
     public static final String INTENT_SCREENSHOT = "action_take_screenshot";
     public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
+    public static final String DOZE_PACKAGE_NAME = "Doze";
+    public static final String LINEAGE_DOZE_PACKAGE_NAME = "org.lineageos.settings.doze";
+    public static final String CUSTOM_DOZE_PACKAGE_NAME = "com.custom.ambient.display";
+    public static final String ONEPLUS_DOZE_PACKAGE_NAME = "OnePlusDoze";
 
     private static IStatusBarService mStatusBarService = null;
 
@@ -470,5 +474,12 @@ public class EvolutionUtils {
         if (DeviceUtils.deviceSupportsVibrator(context)) {
             ((Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE)).vibrate(pattern, -1);
         }
+    }
+
+    public static boolean isDozePackageAvailable(Context context) {
+        return isPackageInstalled(context, DOZE_PACKAGE_NAME) ||
+            isPackageInstalled(context, ONEPLUS_DOZE_PACKAGE_NAME) ||
+            isPackageInstalled(context, LINEAGE_DOZE_PACKAGE_NAME) ||
+            isPackageInstalled(context, CUSTOM_DOZE_PACKAGE_NAME);
     }
 }
