f4b762be8e7706d7e07a589d20fdadfe5710ba10
ezio84 <brabus84@gmail.com>
2020-03-01 10:23:20 +0100

    Add kill-app api (with new fresh implementation)
    We can kang what sysui assistant code does to check if launcher is running,
    using a TaskStackChangeListener to always know the running
    taskid and its component name. This is a solid and reliable way
    to easily kill the task and even remove it from recents without the
    risk to kill launcher or a different app than the foreground one.
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/com/android/internal/util/evolution/EvolutionUtils.java
+++ core/java/com/android/internal/util/evolution/EvolutionUtils.java
@@ -262,12 +262,45 @@ public class EvolutionUtils {
     }
 
     public static void toggleCameraFlash() {
-        IStatusBarService service = getStatusBarService();
-        if (service != null) {
-            try {
-                service.toggleCameraFlash();
-            } catch (RemoteException e) {
-                // do nothing.
+        FireActions.toggleCameraFlash();
+    }
+
+    public static void killForegroundApp() {
+        FireActions.killForegroundApp();
+    }
+
+    private static final class FireActions {
+        private static IStatusBarService mStatusBarService = null;
+
+        private static IStatusBarService getStatusBarService() {
+            synchronized (FireActions.class) {
+                if (mStatusBarService == null) {
+                    mStatusBarService = IStatusBarService.Stub.asInterface(
+                            ServiceManager.getService("statusbar"));
+                }
+                return mStatusBarService;
+            }
+        }
+
+        public static void killForegroundApp() {
+            IStatusBarService service = getStatusBarService();
+            if (service != null) {
+                try {
+                    service.killForegroundApp();
+                } catch (RemoteException e) {
+                    // do nothing.
+                }
+            }
+        }
+
+        public static void toggleCameraFlash() {
+            IStatusBarService service = getStatusBarService();
+            if (service != null) {
+                try {
+                    service.toggleCameraFlash();
+                } catch (RemoteException e) {
+                    // do nothing.
+                }
             }
         }
     }
