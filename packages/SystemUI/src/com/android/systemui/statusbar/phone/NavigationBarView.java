ebf5eb73809cf3749c00325a64651bbeacfe7918
Joey Huab <joey@evolution-x.org>
2020-03-08 11:10:18 +0000

    [SQUASH] Revert several unneeded commits
    [SQUASH] Remove back gesture haptic feedback toggle
    
    Revert "SystemUI: Increase back gesture vibration duration"
    
    This reverts commit bcb784dd22dcb6f1642e1ae2f478a133d9f6b092.
    
    Revert "base: Add back gesture haptic feedback toggle [1/2]"
    
    This reverts commit 352f21f8d0f89a9fef856281c28214d36a499a0d.
    
    Revert "SystemUI: Added animation of switching between the power menu and the extended power menu"
    
    This reverts commit 2c2a22d6a9a52956143a3ad94ad000880b177015.
    
    Revert "TextureView rebind GL texture, if AHB content has changed"
    
    This reverts commit 9ff733a610ca27f2d042a00e9116e2563bd229be.
    
    Revert "Allow screen unpinning on devices without navbar"
    
    This reverts commit 49456c947aff24615d8e84149da9905b17982457.
    
    Revert "Allow lid to send a generic COVER_CHANGED broadcast"
    
    This reverts commit 054e1daaa4c5f33dc0cc2a33f29cd0f7f37bee65.
    
    Revert "base: lightservice: strengthen useScaleBrightness checking"
    
    This reverts commit 110d28a5d2099f1284ac237c538e67461a90a24e.
    
    Revert "MediaCodec: Release lock before calling onFrameRendered()"
    
    This reverts commit 19ef6c8f772bb478f2a3e8ec43f07cc3ac4c357c.
    
    Revert "Fix AoD front scrim being opaque at DOZE_PULSING."
    
    This reverts commit 82409433fa588358a0b06368fd0e61bf4aebd106.
    
    Revert "Reland "Apply front scrim to doze pulsing""
    
    This reverts commit 0b1f05379ec1f737493cda7e3ea81c0163c913ec.
    
    Revert "base: conditionally skip scan /vendor/overlay for static overlays"
    
    This reverts commit 9d68dd1e5306e84fb2f5722e3f8377e3774f9eec.
    
    Revert "Keyguard: Hide check button when using PIN quick unlock"
    
    This reverts commit 00192d766fd7114c1b030f13d641b1606b178f24.
    
    Revert "base: media_seekbar: make it sexy"
    
    This reverts commit c39ec414e4a7af71a11979a9a47766707238eca2.
    
    Revert "SystemUI: Improve QS detail view layout"
    
    This reverts commit 5404d040a4f5ee2838326048445a46b6e253cd6a.
    
    Revert "SystemUI: Enable and fix QS detail view, adapt layout to 10"
    
    This reverts commit 312b37242d3ee50fa1c71e5cb6d56bdba6f8f770.
    
    [SQUASH] Revert several unneeded commits
    
    [SQUASH] SystemUI: QS: Remove LTE Tile
    
    Revert "SystemUI: QS: LteTile: Fix network modes for 10"
    
    This reverts commit 78f9d7f779134f464f2e061441c25796ecb98c99.
    
    Revert "SystemUI: QS: LteTile: Support more Network modes"
    
    This reverts commit 4426556433e4caf52c28101064f6f1d18233c136.
    
    Revert "SystemUI: QS: LteTile: multisim compatibility"
    
    This reverts commit cdd45b348fd4e6d4caeb017c42990f14e8f7bc6e.
    
    Revert "SystemUI: QS: Add LTE Tile"
    
    This reverts commit 4d261cf72bd6ce7d565256c0a1a35b9163e1a199.
    
    Revert "storage: Do not notify for volumes on non-removable disks"
    
    This reverts commit ee061aaafcba506d6e43d4ac4afe539855c805c8.
    
    Revert "Revert "Disable OPA edge flash animation""
    
    This reverts commit 3face3bf1bdc2c84e03fac3858b32187a46337a4.
    
    Revert "SystemUI: assist: disable old Assistant animations"
    
    This reverts commit 8c1697eb00251acd77ae23ef615c41674f6a307c.
    
    Revert "QS brightness slider: Update brightness thumb vector"
    
    This reverts commit e5526786daa19f90d87e6b2430301fc1160de9bc.
    
    Partially Revert "base: Q seekbar: Tweak it's size"
    
    This partially reverts commit d074e81ad31d73e1d795f73438d89d69e90697e3.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -739,10 +739,6 @@ public class NavigationBarView extends FrameLayout implements
         mEdgeBackGestureHandler.setStateForBackArrowGesture();
     }
 
-    public void updateBackGestureHaptic() {
-        mEdgeBackGestureHandler.setStateForBackGestureHaptic();
-    }
-
     public void updateDisabledSystemUiStateFlags() {
         int displayId = mContext.getDisplayId();
         mOverviewProxyService.setSystemUiStateFlag(SYSUI_STATE_SCREEN_PINNING,
