e03ea4d9c2349a8bc481d3303348bf76173c4602
Nico <nicorg2529@gmail.com>
2020-03-10 21:53:40 +0100

    Extended swipe: show a double arrow when almost triggered
    abcdu: adapted to our code
    Change-Id: If8905cb231e5ea0e993a590ce7f02669b46b8136
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -471,6 +471,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
         mIsExtendedSwipe = mGestureNavigationSettingsObserver.getIsExtendedSwipe();
         mLeftVerticalSwipeAction = mGestureNavigationSettingsObserver.getLeftLSwipeAction();
         mRightVerticalSwipeAction = mGestureNavigationSettingsObserver.getRightLSwipeAction();
+        if (mEdgeBackPlugin != null) {
+            mEdgeBackPlugin.setLongSwipeEnabled(mIsExtendedSwipe);
+        }
 
         final DisplayMetrics dm = res.getDisplayMetrics();
         final float defaultGestureHeight = res.getDimension(
@@ -663,6 +666,7 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
         mEdgeBackPlugin = edgeBackPlugin;
         mEdgeBackPlugin.setBackCallback(mBackCallback);
         mEdgeBackPlugin.setLayoutParams(createLayoutParams());
+        mEdgeBackPlugin.setLongSwipeEnabled(mIsExtendedSwipe);
         updateDisplaySize();
     }
 
