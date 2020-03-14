8fde823517e59755507e20c83872e312f18647f1
Lucchetto <lucchetto.tie@live.com>
2020-03-14 14:38:11 +0530

    SystemUI: QS blur improvements
    QSPanel blur: improve logic
    
    Change-Id: Ia0473ad58cb79dcca1391bede35efde9fdded5f8
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    
    QSPanel blur: remove blur bitmap's tint
    
        * And move the blur view behind the existing scrim views
    
    Change-Id: I00621d0e6619348b5bb9b9d1fa0df425e794a0e3
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    
    QSPanel blur: refactor screenshot routine
    
    Change-Id: I36d9a5984d07674ede1a3118045a6257edca467c
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    
    QSPanel blur: redraw blur on rotation
    
    Change-Id: I94fc2b4ce1b2ba426f5e9f99aa95450dca0d0800
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    
    QSPanel: remove navbar scrim
    
    Change-Id: I9d19079dbbe8f06f578b451ca450104796e82b58
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    
    QSPanel blur: increase bitmap scaling
    
    Change-Id: Icf56d91b6854bdc4a00fd1642eb7e1a772f5afaa
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    
    ImageUtilities: optimize blur performance
    
    Change-Id: Ie045eeb02a687bfa03bdae84cdc3b9e5c6cd1847
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    
    QSPanel blur: fix crash with protected layers
    
        * We are not allowed to screenshot protected layers and displays, so let's make the view clear instead of blurring"
    
    Change-Id: Id817a892311ded9668a696b49f47a8c13a61f811
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -241,7 +241,6 @@ public class NotificationPanelView extends PanelView implements
     protected FrameLayout mQsFrame;
     @VisibleForTesting
     protected KeyguardStatusView mKeyguardStatusView;
-    private View mQsNavbarScrim;
     protected NotificationsQuickSettingsContainer mNotificationContainerParent;
     protected NotificationStackScrollLayout mNotificationStackScroller;
     private boolean mAnimateNextPositionUpdate;
@@ -421,6 +420,13 @@ public class NotificationPanelView extends PanelView implements
             .setDuration(200)
             .setAnimationFinishListener(mAnimatorListenerAdapter)
             .setCustomInterpolator(PANEL_ALPHA.getProperty(), Interpolators.ALPHA_IN);
+    private final AnimationProperties PANEL_ALPHA_OUT_FAST_PROPERTIES = new AnimationProperties()
+            .setDuration(100)
+            .setCustomInterpolator(PANEL_ALPHA.getProperty(), Interpolators.ALPHA_OUT);
+    private final AnimationProperties PANEL_ALPHA_IN_FAST_PROPERTIES = new AnimationProperties()
+            .setDuration(100)
+            .setAnimationFinishListener(mAnimatorListenerAdapter)
+            .setCustomInterpolator(PANEL_ALPHA.getProperty(), Interpolators.ALPHA_IN);
     private final NotificationEntryManager mEntryManager =
             Dependency.get(NotificationEntryManager.class);
 
@@ -571,7 +577,6 @@ public class NotificationPanelView extends PanelView implements
         mNotificationStackScroller.setOnEmptySpaceClickListener(this);
         addTrackingHeadsUpListener(mNotificationStackScroller::setTrackingHeadsUp);
         mKeyguardBottomArea = findViewById(R.id.keyguard_bottom_area);
-        mQsNavbarScrim = findViewById(R.id.qs_navbar_scrim);
         mLastOrientation = getResources().getConfiguration().orientation;
         mPulseLightsView = (NotificationLightsView) findViewById(R.id.lights_container);
 
@@ -1906,10 +1911,6 @@ public class NotificationPanelView extends PanelView implements
                 mBarState != StatusBarState.KEYGUARD && (!mQsExpanded
                         || mQsExpansionFromOverscroll));
         updateEmptyShadeView();
-        mQsNavbarScrim.setVisibility(mBarState == StatusBarState.SHADE && mQsExpanded
-                && !mStackScrollerOverscrolling && mQsScrimEnabled
-                ? View.VISIBLE
-                : View.INVISIBLE);
         if (mKeyguardUserSwitcher != null && mQsExpanded && !mStackScrollerOverscrolling) {
             mKeyguardUserSwitcher.hideIfNotSimple(true /* animate */);
         }
@@ -1934,10 +1935,6 @@ public class NotificationPanelView extends PanelView implements
                 || mBarState == StatusBarState.KEYGUARD) {
             updateKeyguardBottomAreaAlpha();
             updateBigClockAlpha();
-        }
-        if (mBarState == StatusBarState.SHADE && mQsExpanded
-                && !mStackScrollerOverscrolling && mQsScrimEnabled) {
-            mQsNavbarScrim.setAlpha(getQsExpansionFraction());
         }
 
         if (mAccessibilityManager.isEnabled()) {
@@ -2998,6 +2995,16 @@ public class NotificationPanelView extends PanelView implements
             mPanelAlpha = alpha;
             PropertyAnimator.setProperty(this, PANEL_ALPHA, alpha,
                     alpha == 255 ? PANEL_ALPHA_IN_PROPERTIES : PANEL_ALPHA_OUT_PROPERTIES, animate);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setPanelAlphaFast(int alpha, boolean animate) {
+        if (mPanelAlpha != alpha) {
+            mPanelAlpha = alpha;
+            PropertyAnimator.setProperty(this, PANEL_ALPHA, alpha,
+                    alpha == 255 ? PANEL_ALPHA_IN_FAST_PROPERTIES : PANEL_ALPHA_OUT_FAST_PROPERTIES, animate);
             return true;
         }
         return false;
