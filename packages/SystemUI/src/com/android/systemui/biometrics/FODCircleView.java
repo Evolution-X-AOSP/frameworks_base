6786d9a4f2e57dcb2e2bfd27c0a8316ba2a523fa
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-23 11:34:17 -0300

    FOD: Implement FodScreenOffHandler
    Required for our screen off fod impl
    
    Change-Id: Ib5dc2743d63cf1c008e6940a22c8d23f1abe2103
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -52,6 +52,8 @@ import java.util.NoSuchElementException;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import com.android.internal.util.custom.fod.FodScreenOffHandler;
+
 public class FODCircleView extends ImageView {
     private final int mPositionX;
     private final int mPositionY;
@@ -121,6 +123,8 @@ public class FODCircleView extends ImageView {
         R.drawable.fod_icon_pressed_vivo_yellow_shadow
     };
 
+    private FodScreenOffHandler mFodScreenOffHandler;
+
     private IFingerprintInscreenCallback mFingerprintInscreenCallback =
             new IFingerprintInscreenCallback.Stub() {
         @Override
@@ -140,6 +144,7 @@ public class FODCircleView extends ImageView {
         @Override
         public void onDreamingStateChanged(boolean dreaming) {
             mIsDreaming = dreaming;
+            dispatchFodDreamingStateChanged();
             updateAlpha();
 
             if (dreaming) {
@@ -173,6 +178,7 @@ public class FODCircleView extends ImageView {
         @Override
         public void onScreenTurnedOff() {
             hide();
+            dispatchFodScreenStateChanged(false);
         }
 
         @Override
@@ -180,6 +186,13 @@ public class FODCircleView extends ImageView {
             if (mUpdateMonitor.isFingerprintDetectionRunning()) {
                 show();
             }
+            dispatchFodScreenStateChanged(true);
+        }
+
+        @Override
+        public void onBiometricRunningStateChanged(boolean running,
+            BiometricSourceType biometricSourceType) {
+            dispatchFodFingerprintRunningStateChanged(running);
         }
 
         @Override
@@ -192,8 +205,28 @@ public class FODCircleView extends ImageView {
         }
     };
 
-    public FODCircleView(Context context) {
+    private void dispatchFodScreenStateChanged(boolean interactive){
+        if (mFodScreenOffHandler != null){
+            mFodScreenOffHandler.onScreenStateChanged(interactive);
+        }
+    }
+
+    private void dispatchFodFingerprintRunningStateChanged(boolean running){
+        if (mFodScreenOffHandler != null){
+            mFodScreenOffHandler.onFingerprintRunningStateChanged(running);
+        }
+    }
+
+    private void dispatchFodDreamingStateChanged(){
+        if (mFodScreenOffHandler != null){
+            mFodScreenOffHandler.onDreamingStateChanged(mIsDreaming);
+        }
+    }
+
+    public FODCircleView(Context context, FodScreenOffHandler fodScreenOffHandler) {
         super(context);
+
+        mFodScreenOffHandler = fodScreenOffHandler;
 
         IFingerprintInscreen daemon = getFingerprintInScreenDaemon();
         if (daemon == null) {
