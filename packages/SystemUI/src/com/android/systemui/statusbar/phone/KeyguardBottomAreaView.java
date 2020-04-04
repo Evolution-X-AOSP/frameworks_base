ce643764e6a6e78ee5d01d5c5c08c37f6a96c4a3
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-04-04 13:12:10 -0300

    SystemUI: Keyguard: Only apply padding if fod view showing
    Change-Id: I3d2a5b0e861c746887db88f5bcc6b6a35e6e1570
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -73,6 +73,7 @@ import com.android.systemui.Dependency;
 import com.android.systemui.Interpolators;
 import com.android.systemui.R;
 import com.android.systemui.assist.AssistManager;
+import com.android.systemui.biometrics.FODCircleView;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.IntentButtonProvider;
 import com.android.systemui.plugins.IntentButtonProvider.IntentButton;
@@ -438,7 +439,8 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
 
     private boolean hasInDisplayFingerprint() {
         return mContext.getPackageManager().hasSystemFeature(FOD)
-                && mIsFingerprintRunning;
+                && mIsFingerprintRunning &&
+            FODCircleView.canUnlockWithFp(KeyguardUpdateMonitor.getInstance(mContext));
     }
 
     public boolean isLeftVoiceAssist() {
