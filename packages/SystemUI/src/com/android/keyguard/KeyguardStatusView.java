Evolution-X-AOSP/frameworks_base

commit c78b9cf9b397e9bf3755be75407aeb9ae2a2b095

base: Add full accent sammy clock style [1/2]
* And properly document selection

Signed-off-by: Joey Huab <joey@evolution-x.org>

packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -302,34 +302,37 @@ public class KeyguardStatusView extends GridLayout implements
     private void refreshTime() {
         mClockView.refresh();
 
-        if (mClockSelection == 0) {
+        if (mClockSelection == 0) { // default
             mClockView.setFormat12Hour(Patterns.clockView12);
             mClockView.setFormat24Hour(Patterns.clockView24);
-        } else if (mClockSelection == 1) {
+        } else if (mClockSelection == 1) { // bold
             mClockView.setFormat12Hour(Html.fromHtml("<strong>hh</strong>:mm"));
             mClockView.setFormat24Hour(Html.fromHtml("<strong>kk</strong>:mm"));
-        } else if (mClockSelection == 2) {
+        } else if (mClockSelection == 2) { // accent
             mClockView.setFormat12Hour(Html.fromHtml("<font color=" + getResources().getColor(R.color.accent_device_default_light) + ">hh:mm</font>"));
             mClockView.setFormat24Hour(Html.fromHtml("<font color=" + getResources().getColor(R.color.accent_device_default_light) + ">kk:mm</font>"));
-        } else if (mClockSelection == 3) {
+        } else if (mClockSelection == 3) { // accent hour
             mClockView.setFormat12Hour(Html.fromHtml("<font color=" + getResources().getColor(R.color.accent_device_default_light) + ">hh</font>:mm"));
             mClockView.setFormat24Hour(Html.fromHtml("<font color=" + getResources().getColor(R.color.accent_device_default_light) + ">kk</font>:mm"));
-        } else if (mClockSelection == 4) {
+        } else if (mClockSelection == 4) { // accent min
             mClockView.setFormat12Hour(Html.fromHtml("hh<font color=" + getResources().getColor(R.color.accent_device_default_light) + ">:mm</font>"));
             mClockView.setFormat24Hour(Html.fromHtml("kk<font color=" + getResources().getColor(R.color.accent_device_default_light) + ">:mm</font>"));
-        } else if (mClockSelection == 5) {
+        } else if (mClockSelection == 5) { // sammy
             mClockView.setFormat12Hour("hh\nmm");
             mClockView.setFormat24Hour("kk\nmm");
-        } else if (mClockSelection == 6) {
+        } else if (mClockSelection == 6) { // sammy bold
             mClockView.setFormat12Hour(Html.fromHtml("<strong>hh</strong><br>mm"));
             mClockView.setFormat24Hour(Html.fromHtml("<strong>kk</strong><br>mm"));
-        } else if (mClockSelection == 7) {
+        } else if (mClockSelection == 7) { // sammy accent
+            mClockView.setFormat12Hour(Html.fromHtml("<font color=" + getResources().getColor(R.color.accent_device_default_light) + ">hh<br>mm</font>"));
+            mClockView.setFormat24Hour(Html.fromHtml("<font color=" + getResources().getColor(R.color.accent_device_default_light) + ">kk<br>mm</font>"));
+        } else if (mClockSelection == 8) { // sammy accent hour
             mClockView.setFormat12Hour(Html.fromHtml("<font color=" + getResources().getColor(R.color.accent_device_default_light) + ">hh</font><br>mm"));
             mClockView.setFormat24Hour(Html.fromHtml("<font color=" + getResources().getColor(R.color.accent_device_default_light) + ">kk</font><br>mm"));
-        } else if (mClockSelection == 8) {
+        } else if (mClockSelection == 9) { // sammy accent min
             mClockView.setFormat12Hour(Html.fromHtml("hh<br><font color=" + getResources().getColor(R.color.accent_device_default_light) + ">mm</font>"));
             mClockView.setFormat24Hour(Html.fromHtml("kk<br><font color=" + getResources().getColor(R.color.accent_device_default_light) + ">mm</font>"));
-        } else if (mClockSelection == 9 || mClockSelection == 10) {
+        } else if (mClockSelection == 10 || mClockSelection == 11) { // text
             mTextClock.onTimeChanged();
         }
     }

core/java/android/provider/Settings.java
@@ -11355,7 +11355,7 @@ public final class Settings {
         public static final String LOCKSCREEN_CLOCK_SELECTION = "lockscreen_clock_selection";
 
         private static final Validator LOCKSCREEN_CLOCK_SELECTION_VALIDATOR =
-                new SettingsValidators.InclusiveIntegerRangeValidator(0, 10);
+                new SettingsValidators.InclusiveIntegerRangeValidator(0, 11);
 
         /**
          * Select which lockscreen date style to display

commit 7d48891588ae9005e24bc657a07de2695d60b33e

SystemUI: KeyguardStatusView: Fix derp caused by c78b9cf
* Reference: https://github.com/DerpLab/platform_frameworks_base/commit/9836a245002b4e7f8e7ebb6d454764ec0494e0f6

Signed-off-by: Joey Huab <joey@evolution-x.org>

packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -626,9 +626,9 @@ public class KeyguardStatusView extends GridLayout implements
         if (info == null) {
             final ContentResolver resolver = mContext.getContentResolver();
             boolean mClockSelection = Settings.Secure.getIntForUser(resolver,
-                    Settings.Secure.LOCKSCREEN_CLOCK_SELECTION, 0, UserHandle.USER_CURRENT) == 9
+                    Settings.Secure.LOCKSCREEN_CLOCK_SELECTION, 0, UserHandle.USER_CURRENT) == 10
                     || Settings.Secure.getIntForUser(resolver,
-                    Settings.Secure.LOCKSCREEN_CLOCK_SELECTION, 0, UserHandle.USER_CURRENT) == 10;
+                    Settings.Secure.LOCKSCREEN_CLOCK_SELECTION, 0, UserHandle.USER_CURRENT) == 11;
             int mTextClockAlign = Settings.System.getIntForUser(resolver,
                     Settings.System.TEXT_CLOCK_ALIGNMENT, 0, UserHandle.USER_CURRENT);
 
@@ -935,11 +935,10 @@ public class KeyguardStatusView extends GridLayout implements
         mSmallClockView = findViewById(R.id.clock_view);
         mTextClock = findViewById(R.id.custom_text_clock_view);
 
-        if (mClockSelection == 5 || mClockSelection == 6
-                || mClockSelection == 7 || mClockSelection == 8)
+        if (mClockSelection >= 5 && mClockSelection <= 9)
             mDefaultClockView.setLineSpacing(0, 0.8f);
 
-        if (mClockSelection != 9 && mClockSelection != 10) {
+        if (mClockSelection != 10 && mClockSelection != 11) {
             mTextClock.setVisibility(View.GONE);
             mSmallClockView.setVisibility(View.VISIBLE);
             params.addRule(RelativeLayout.BELOW, R.id.clock_view);
@@ -963,7 +962,7 @@ public class KeyguardStatusView extends GridLayout implements
 
         mTextClock = findViewById(R.id.custom_text_clock_view);
 
-        if (mClockSelection == 9 || mClockSelection == 10) {
+        if (mClockSelection == 10 || mClockSelection == 11) {
             switch (mTextClockAlignment) {
                 case 0:
                 default: