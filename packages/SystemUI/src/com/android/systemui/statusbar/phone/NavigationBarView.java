0b5977387c89ac69cb5a563e1f2d573fb1a9968c
Anushek Prasal <anushekprasal@gmail.com>
2020-04-14 17:09:09 +0530

    base: Make IME button space optional [1/2]
    Change-Id: Iacdc496d45e63124299e0354037873a9b30fe945
    Signed-off-by: rohan <purohit.rohan@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -49,6 +49,7 @@ import android.os.Bundle;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.SparseArray;
@@ -1054,7 +1055,7 @@ public class NavigationBarView extends FrameLayout implements
                     : getResources().getDimensionPixelSize(
                             com.android.internal.R.dimen.navigation_bar_height);
             int finalHeight = mShowGestureNavbar ? height : 0;
-            int frameHeight = mShowGestureNavbar ? getResources().getDimensionPixelSize(
+            int frameHeight = showIMESpace() ? getResources().getDimensionPixelSize(
                     com.android.internal.R.dimen.navigation_bar_frame_height) : 0;
             mBarTransitions.setBackgroundFrame(new Rect(0, frameHeight - finalHeight, w, h));
         }
@@ -1248,4 +1249,9 @@ public class NavigationBarView extends FrameLayout implements
         mDockedStackExists = exists;
         updateRecentsIcon();
     });
+
+    private boolean showIMESpace() {
+        return Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.NAVIGATION_BAR_IME_SPACE, 1, UserHandle.USER_CURRENT) != 0;
+    }
 }
