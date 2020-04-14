omnirom/android_frameworks_base_old

SystemUI: simplify qs tile helper
this triggered my OCD

Change-Id: Ibf332c9ed58c7d24755d61023bcff75eeffea62c

packages/SystemUI/src/com/android/systemui/qs/customize/TileQueryHelper.java
@@ -31,6 +31,7 @@ import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 import android.text.TextUtils;
 import android.util.ArraySet;
+import android.util.Log;
 import android.widget.Button;
 
 import com.android.systemui.Dependency;
@@ -46,6 +47,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 public class TileQueryHelper {
     private static final String TAG = "TileQueryHelper";
@@ -84,21 +87,14 @@ public class TileQueryHelper {
         String current = Settings.Secure.getString(mContext.getContentResolver(),
                 Settings.Secure.QS_TILES);
         String possible = mContext.getString(R.string.quick_settings_tiles_stock)
-                + "," + mContext.getString(R.string.quick_settings_tiles_extra)
-		+ "," + current;
-        final ArrayList<String> possibleTiles = new ArrayList<>();
+                + "," + mContext.getString(R.string.quick_settings_tiles_extra);
+
+        final Set<String> possibleTiles = new HashSet<>();
         if (current != null) {
-            // The setting QS_TILES is not populated immediately upon Factory Reset
             possibleTiles.addAll(Arrays.asList(current.split(",")));
-        } else {
-            current = "";
         }
-        String[] stockSplit =  possible.split(",");
-        for (String spec : stockSplit) {
-            if (!current.contains(spec)) {
-                possibleTiles.add(spec);
-            }
-        }
+        possibleTiles.addAll(Arrays.asList(possible.split(",")));
+
         if (Build.IS_DEBUGGABLE && !current.contains(GarbageMonitor.MemoryTile.TILE_SPEC)) {
             possibleTiles.add(GarbageMonitor.MemoryTile.TILE_SPEC);
         }

Evolution-X-AOSP/frameworks_base

Signed-off-by: Joey Huab <joey@evolution-x.org>

packages/SystemUI/src/com/android/systemui/qs/customize/TileQueryHelper.java
@@ -31,6 +31,7 @@ import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 import android.text.TextUtils;
 import android.util.ArraySet;
+import android.util.Log;
 import android.widget.Button;
 
 import com.android.systemui.Dependency;
@@ -45,7 +46,9 @@ import com.android.systemui.util.leak.GarbageMonitor;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class TileQueryHelper {
     private static final String TAG = "TileQueryHelper";
@@ -81,22 +84,15 @@ public class TileQueryHelper {
     }
 
     private void addCurrentAndStockTiles(QSTileHost host) {
-        String stock = mContext.getString(R.string.quick_settings_tiles_stock);
         String current = Settings.Secure.getString(mContext.getContentResolver(),
                 Settings.Secure.QS_TILES);
-        final ArrayList<String> possibleTiles = new ArrayList<>();
+        String stock = mContext.getString(R.string.quick_settings_tiles_stock);
+        final Set<String> possibleTiles = new HashSet<>();
         if (current != null) {
-            // The setting QS_TILES is not populated immediately upon Factory Reset
             possibleTiles.addAll(Arrays.asList(current.split(",")));
-        } else {
-            current = "";
         }
-        String[] stockSplit =  stock.split(",");
-        for (String spec : stockSplit) {
-            if (!current.contains(spec)) {
-                possibleTiles.add(spec);
-            }
-        }
+        possibleTiles.addAll(Arrays.asList(stock.split(",")));
+
         /*if (Build.IS_DEBUGGABLE && !current.contains(GarbageMonitor.MemoryTile.TILE_SPEC)) {
             possibleTiles.add(GarbageMonitor.MemoryTile.TILE_SPEC);
         }*/