BlissRoms/platform_frameworks_base

AccentUtils: add coloraccent to resource list
Change-Id: Ice12451c4caa08fabeb164cc217a2d5bd1d0590c

core/java/android/content/res/AccentUtils.java
@@ -29,7 +29,8 @@ public class AccentUtils {
                 || resName.contains("accent_device_default")
                 || resName.contains("material_pixel_blue_dark")
                 || resName.contains("material_pixel_blue_bright")
-                || resName.contains("gradient_start");
+                || resName.contains("gradient_start")
+                || resName.contains("colorAccent");
     }
 
     static boolean isResourceGradient(String resName) {

Evolution-X-AOSP/frameworks_base

Signed-off-by: Joey Huab <joey@evolution-x.org>

core/java/android/content/res/AccentUtils.java
@@ -25,7 +25,8 @@ public class AccentUtils {
 
     static boolean isResourceAccent(String resName) {
         return resName.contains("accent_device_default_light")
-                || resName.contains("accent_device_default_dark");
+                || resName.contains("accent_device_default_dark")
+                || resName.contains("colorAccent");
     }
 
     static boolean isResourceQSbgColor(String resName) {