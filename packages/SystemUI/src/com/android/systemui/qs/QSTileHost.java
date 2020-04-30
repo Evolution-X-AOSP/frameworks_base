14afd0d409fabb204be218c5cb69bb9b06267fc1
Kshitij Gupta <kshitijgm@gmail.com>
2020-04-30 12:34:55 +0300

    QSTileHost: Fix reloadAllTiles() public function
    Change-Id: I9fcea8a958a5e3560c4218d9c05e93b3d16489f2
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/qs/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSTileHost.java
@@ -210,7 +210,7 @@ public class QSTileHost implements QSHost, Tunable, PluginListener<QSFactory>, D
 
     public void reloadAllTiles() {
         // Force remove and recreate of all tiles.
-        String value = Dependency.get(TunerService.class).getValue(TILES_SETTING);
+        String value = mTunerService.getValue(TILES_SETTING);
         onTuningChanged(TILES_SETTING, "");
         onTuningChanged(TILES_SETTING, value);
     }
