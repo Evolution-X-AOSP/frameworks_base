a4a871a0776912081ce21c672a296a239fc4a07d
Sam Mortimer <sam@mortimer.me.uk>
2020-05-20 12:20:17 +0200

    SystemUI: Prevent loop with display timeout
    * When no secure lock screen is set and the display
      times out, it immediately turns on again
    * Do not trigger showing the keyguard when we don't
      actually need it
    
    Change-Id: I685329e74afb0af79b5d8843ba9d3af2ee6377d4
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -1378,11 +1378,10 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
                 // showing, not in the process of going away) then reset its state. Otherwise, let
                 // this fall through and explicitly re-lock the keyguard.
                 mPendingReset = true;
-            } else if (
-                    (offReason == WindowManagerPolicyConstants.OFF_BECAUSE_OF_TIMEOUT
+            } else if (((offReason == WindowManagerPolicyConstants.OFF_BECAUSE_OF_TIMEOUT
                             && timeout > 0)
-                            || (offReason == WindowManagerPolicyConstants.OFF_BECAUSE_OF_USER
-                            && !lockImmediately)) {
+                            || offReason == WindowManagerPolicyConstants.OFF_BECAUSE_OF_USER)
+                            && !lockImmediately) {
                 doKeyguardLaterLocked(timeout);
                 mLockLater = true;
             } else if (!mLockPatternUtils.isLockScreenDisabled(currentUser)) {
