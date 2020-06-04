38c7317e84646256b72d3a4f49c212b62d09a232
Joey Huab <joey@evolution-x.org>
2020-06-04 02:29:54 +0000

    [SQUASH] Revert several commits
    Revert "FODCircleView: Fix high brightness after unlocking"
    
    This reverts commit 006f61410f0fab17148d4bd4abe08f39954a2831.
    
    Revert "FodScreenOffHandler: Add hasEnrolledFingerprints callback"
    
    This reverts commit b0d41d3da21a84d867b1642ead6c5eb0021551dc.
    
    Revert "FOD: Implement FodScreenOffHandler"
    
    This reverts commit 6786d9a4f2e57dcb2e2bfd27c0a8316ba2a523fa.
    
    Revert "SystemUI: Conditionally hide extra biometric help on keyguard"
    
    This reverts commit 86e3d975ab1f94995832458685185d0d9da959ca.

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -145,8 +145,6 @@ public class KeyguardIndicationController implements StateListener,
 
     private KeyguardUpdateMonitorCallback mUpdateMonitorCallback;
 
-    private boolean mBiometricHelpShowOnlyWhenFailed = false;
-
     private final DevicePolicyManager mDevicePolicyManager;
     private boolean mDozing;
     private final ViewClippingUtil.ClippingParameters mClippingParams =
@@ -217,8 +215,6 @@ public class KeyguardIndicationController implements StateListener,
         mKeyguardUpdateMonitor.registerCallback(mTickReceiver);
         mStatusBarStateController.addCallback(this);
         mUnlockMethodCache.addListener(this);
-
-        mBiometricHelpShowOnlyWhenFailed = res.getBoolean(R.bool.config_biometricHelpShowOnlyWhenFailed);
     }
 
     public void setIndicationArea(ViewGroup indicationArea) {
@@ -930,9 +926,6 @@ public class KeyguardIndicationController implements StateListener,
         @Override
         public void onBiometricHelp(int msgId, String helpString,
                 BiometricSourceType biometricSourceType) {
-            if (mBiometricHelpShowOnlyWhenFailed && msgId != -1){
-                return;
-            }
             if (!mKeyguardUpdateMonitor.isUnlockingWithBiometricAllowed()) {
                 return;
             }
