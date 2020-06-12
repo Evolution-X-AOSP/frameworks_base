dd89285197c4a056540b8adab9b9d0a47062f593
Arian <arian.kulmer@web.de>
2020-06-12 17:34:45 +0200

    VolumeDialog: Move the media output selector next to the expandable indicator
    * Keep the non-expanded panel as slim as possible.
    
    Change-Id: Ic0d171c1f52ad8f50e14f161431e41d49da6cad6
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -576,12 +576,12 @@ public class VolumeDialogImpl implements VolumeDialog,
                 == BluetoothProfile.STATE_CONNECTED;
     }
 
-    public void initSettingsH() {
+    public void updateMediaOutputH() {
         if (mMediaOutputView != null) {
             mMediaOutputView.setVisibility(
                     mDeviceProvisionedController.isCurrentUserSetup() &&
                             mActivityManager.getLockTaskModeState() == LOCK_TASK_MODE_NONE &&
-                            isBluetoothA2dpConnected() ? VISIBLE : GONE);
+                            isBluetoothA2dpConnected() && mExpanded ? VISIBLE : GONE);
         }
         if (mMediaOutputIcon != null) {
             mMediaOutputIcon.setOnClickListener(v -> {
@@ -593,7 +593,10 @@ public class VolumeDialogImpl implements VolumeDialog,
                         true /* dismissShade */);
             });
         }
+    }
 
+    public void initSettingsH() {
+        updateMediaOutputH();
         if (mAllyStream == -1) {
             mAllyStream = mActiveStream;
         }
@@ -621,6 +624,8 @@ public class VolumeDialogImpl implements VolumeDialog,
                 if (mExpanded) mController.setActiveStream(mAllyStream);
                 mExpandRows.setExpanded(!mExpanded);
                 mExpanded = !mExpanded;
+
+                updateMediaOutputH();
             });
         }
     }
