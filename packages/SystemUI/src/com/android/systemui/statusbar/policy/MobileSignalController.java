b0eb8f1a71966666acd04dfc1eb8dcc98fbbe2a5
Ido Ben-Hur <idoybh2@gmail.com>
2020-06-23 03:34:59 +0300

    base: Allow hiding VoWiFi StatusBar icon [1/2]
    Stallix edits: Use Evo's current ContentObserver and ContentResolver formats and not use SystemUI Tuner
    
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -204,6 +204,9 @@ public class MobileSignalController extends SignalController<
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.DATA_DISABLED_ICON),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.VOWIFI_ICON),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -213,7 +216,8 @@ public class MobileSignalController extends SignalController<
                     || uri.equals(Settings.System.getUriFor(Settings.System.VOLTE_ICON))
                     || uri.equals(Settings.System.getUriFor(Settings.System.VOLTE_ICON_STYLE))
                     || uri.equals(Settings.System.getUriFor(Settings.System.USE_OLD_MOBILETYPE))
-                    || uri.equals(Settings.System.getUriFor(Settings.System.DATA_DISABLED_ICON))) {
+                    || uri.equals(Settings.System.getUriFor(Settings.System.DATA_DISABLED_ICON))
+                    || uri.equals(Settings.System.getUriFor(Settings.System.VOWIFI_ICON))) {
                 updateSettings();
             }
         }
@@ -229,6 +233,11 @@ public class MobileSignalController extends SignalController<
     private boolean volteEnabled() {
         return Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.VOLTE_ICON, 1, UserHandle.USER_CURRENT) == 1;
+    }
+
+    private boolean vowifiEnabled() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.VOWIFI_ICON, 1, UserHandle.USER_CURRENT) == 1;
     }
 
     private int volteStyle() {
@@ -568,7 +577,7 @@ public class MobileSignalController extends SignalController<
         int volteIcon = (volteEnabled() && mConfig.showVolteIcon
                 && isVolteSwitchOn()) ? getVolteResId() : 0;
         MobileIconGroup vowifiIconGroup = getVowifiIconGroup();
-        if (mConfig.showVowifiIcon && vowifiIconGroup != null) {
+        if (vowifiEnabled() && mConfig.showVowifiIcon && vowifiIconGroup != null) {
             typeIcon = vowifiIconGroup.mDataType;
             statusIcon = new IconState(true,
                     mCurrentState.enabled && !mCurrentState.airplaneMode ? statusIcon.icon : 0,
