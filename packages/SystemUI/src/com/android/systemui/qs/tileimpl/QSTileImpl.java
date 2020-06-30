b17de7902e4a86ce260f455691b06590cf24da3d
rohan <purohit.rohan@gmail.com>
2020-06-30 13:05:29 -0400

    SystemUI: fix active color for qs tile styles
    Change-Id: Id7fb3d6fc65abfba202e8263b791fef175ec3c19
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -452,6 +452,8 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
                 System.QS_PANEL_BG_COLOR, defaultColor, UserHandle.USER_CURRENT));
         int qsBackGroundColorWall = ColorUtils.getValidQsColor(System.getIntForUser(context.getContentResolver(),
                 System.QS_PANEL_BG_COLOR_WALL, defaultColor, UserHandle.USER_CURRENT));
+        int qsTileStyle = Settings.System.getIntForUser(context.getContentResolver(),
+                Settings.System.QS_TILE_STYLE, 0, UserHandle.USER_CURRENT);
 
         switch (state) {
             case Tile.STATE_UNAVAILABLE:
@@ -460,16 +462,21 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
             case Tile.STATE_INACTIVE:
                 return Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary);
             case Tile.STATE_ACTIVE:
-                if (setQsFromResources) {
-                    if (setQsUseNewTint)
-                        return Utils.getColorAttrDefaultColor(context, android.R.attr.colorAccent);
-                    else
-                        return Utils.getColorAttrDefaultColor(context, android.R.attr.colorPrimary);
+                if (qsTileStyle == 7 || qsTileStyle == 9 || qsTileStyle == 10 || qsTileStyle == 12 ||
+                     qsTileStyle == 13 || qsTileStyle == 14) {
+                    return Utils.getColorAttrDefaultColor(context, android.R.attr.colorAccent);
                 } else {
-                    if (setQsFromWall)
-                        return qsBackGroundColorWall;
-                    else
-                        return qsBackGroundColor;
+                    if (setQsFromResources) {
+                        if (setQsUseNewTint)
+                            return Utils.getColorAttrDefaultColor(context, android.R.attr.colorAccent);
+                        else
+                            return Utils.getColorAttrDefaultColor(context, android.R.attr.colorPrimary);
+                    } else {
+                        if (setQsFromWall)
+                            return qsBackGroundColorWall;
+                        else
+                            return qsBackGroundColor;
+                    }
                 }
             default:
                 Log.e("QSTile", "Invalid state " + state);
