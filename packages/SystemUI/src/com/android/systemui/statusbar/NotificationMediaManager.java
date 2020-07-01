3277079226ffebd2564b8c41ff5addd0381ae571
bigrushdog <randall.rushing@gmail.com>
2020-07-01 15:28:07 -0700

    Refactor killapp implementation into a Dependency controller class
    There is a lot of good reusable code here that will be used for
    future features. This makes the code universally accessable.
    
    Change-Id: Ic0849ae363a83c9e504771eb3f025b19decfebcd
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -79,6 +79,7 @@ import com.android.systemui.statusbar.phone.ShadeController;
 import com.android.systemui.statusbar.phone.StatusBar;
 import com.android.systemui.statusbar.phone.StatusBarWindowController;
 import com.android.systemui.statusbar.policy.KeyguardMonitor;
+import com.android.systemui.statusbar.policy.TaskHelper;
 import com.android.systemui.statusbar.VisualizerView;
 import com.android.systemui.SysUiServiceProvider;
 
@@ -311,7 +312,8 @@ public class NotificationMediaManager implements Dumpable {
                 && entry.notification.getKey().equals(mMediaNotificationKey)
                 && mMediaIsVisible
                 // skip media heads up if the player is already in foreground
-                && !entry.notification.getPackageName().toLowerCase().equals(mForegroundPackage);
+                && !entry.notification.getPackageName().toLowerCase()
+                        .equals(Dependency.get(TaskHelper.class).getForegroundApp());
     }
 
     public boolean isNewTrackNotification() {
