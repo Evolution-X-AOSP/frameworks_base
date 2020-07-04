53128738ffde3029e020bbe82f442d65d551b74d
Gobinda Joy <gobinda.joy@gmail.com>
2020-07-05 01:06:36 +0600

    NetworkTrafficSB: Better workaround for RequestLayout() spam
    * Remove updateVisibility() call from setVisibleState()
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
@@ -119,7 +119,6 @@ public class NetworkTrafficSB extends NetworkTraffic implements DarkReceiver, St
                 mSystemIconVisible = false;
                 break;
         }
-        update();
     }
 
     @Override
