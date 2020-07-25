06e2edc93072ac35fb83356f69d9e5db7fbb9225
rituj <ritujbeniwal@gmail.com>
2020-07-25 15:18:03 +0530

    base: Introduce automated DC Dimming [1/3]
    * Schedule based on sunset/sunrise
    * Add QS Tile to control DC Dimming
    * Disable the service on devices with no DC dimming support
    * Set config_deviceDcDimmingSysfsNode to point to the relevant sysfs
    kernel node
    
    [ghostrider-reborn: adapt to 12, remove brightness-based schedule]
    
    Co-authored-by: Andrew Fluck <andrew@aospa.co>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Iaebf6624a8311722de3443c0ff5b033381749d20

--- core/java/android/content/Context.java
+++ core/java/android/content/Context.java
@@ -6511,6 +6511,15 @@ public abstract class Context {
     public static final String LINEARMOTOR_VIBRATOR_SERVICE = "linearmotor";
 
     /**
+     * {@link android.hardware.display.DcDimManager} for accessing and setting locked apps state.
+     *
+     * @see #getSystemService
+     * @see android.hardware.display.DcDimmingManager
+     * @hide
+     */
+    public static final String DC_DIM_SERVICE = "dc_dim_service";
+
+    /**
      * Determine whether the given permission is allowed for a particular
      * process and user ID running in the system.
      *
