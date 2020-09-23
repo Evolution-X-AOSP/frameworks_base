2bde7bdfefc9d90308979258b4b7398f84e4151a
Evan Anderson <evananderson@aospa.co>
2020-09-22 20:59:54 -0500

    cycle ringer: Remove @SystemApi annotation that breaks public api
    This addition should not be annotated with @SystemApi because it creates an addition that fails the API test in user builds
    
    Change-Id: I9f3e88eeae1c341bd431128db255c9214a852b15
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9579,7 +9579,6 @@ public final class Settings {
         @SystemApi
         public static final int VOLUME_HUSH_MUTE = 2;
         /** @hide */
-        @SystemApi
         public static final int VOLUME_HUSH_CYCLE = 3;
 
         /**
