030c801279653cd591644d5bdef249ac493e1938
maxwen <max.weninger@gmail.com>
2020-09-28 15:40:30 +0200

    SystemUI: Add OmniSettingsService
    * Adapted for Evolution X usage.
    
    Change-Id: I7909aab11b352fb058bf122102d85b095418fbb9
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/Dependency.java
+++ packages/SystemUI/src/com/android/systemui/Dependency.java
@@ -44,6 +44,7 @@ import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.dock.DockManager;
 import com.android.systemui.dump.DumpManager;
+import com.android.systemui.evolution.EvolutionSettingsService;
 import com.android.systemui.fragments.FragmentService;
 import com.android.systemui.keyguard.ScreenLifecycle;
 import com.android.systemui.keyguard.WakefulnessLifecycle;
@@ -332,6 +333,7 @@ public class Dependency {
     @Inject Lazy<MediaOutputDialogFactory> mMediaOutputDialogFactory;
     @Inject Lazy<TaskHelper> mTaskHelper;
     @Inject Lazy<ScreenDecorations> mScreenDecorations;
+    @Inject Lazy<EvolutionSettingsService> mEvolutionSettingsService;
 
     @Inject
     public Dependency() {
@@ -532,6 +534,7 @@ public class Dependency {
         mProviders.put(Divider.class, mDivider::get);
 
         mProviders.put(MediaOutputDialogFactory.class, mMediaOutputDialogFactory::get);
+        mProviders.put(EvolutionSettingsService.class, mEvolutionSettingsService::get);
 
         mProviders.put(ScreenDecorations.class, mScreenDecorations::get);
 
