35599bd2bf5a1a9aa0f02d8ac2fcc4bc4a22dd91
Oliver Scott <olivercscott@gmail.com>
2020-10-02 13:40:28 -0400

    base: Bluetooth timeout feature (1/2)
    SDB - adapt for A12 FLAG_MUTABLE
    
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -16284,6 +16284,12 @@ public final class Settings {
         public static final String WIFI_OFF_TIMEOUT = "wifi_off_timeout";
 
         /**
+         * The amount of time in milliseconds before bluetooth is turned off
+         * @hide
+         */
+        public static final String BLUETOOTH_OFF_TIMEOUT = "bluetooth_off_timeout";
+
+        /**
          * Global settings that shouldn't be persisted.
          *
          * @hide
