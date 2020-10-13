e541e250c5769a33968a12d0752532fed4073486
BoredOutOfMyGit <BoredOutOfMyGit@users.noreply.github.com>
2020-10-13 16:59:40 +0000

    Optional screenshot type [1/2]
    * Stallix edit: Use better coding for 11
    
    Also includes
    
    ScreenshotTile: Use the same toggle from optional screenshot type
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5408,13 +5408,6 @@ public final class Settings {
                 "statusbar_battery_bar_blend_color_reverse";
 
         /**
-         * 0 - fullscreen
-         * 1 - partial
-         * @hide
-         */
-        public static final String SCREENSHOT_DEFAULT_MODE = "screenshot_default_mode";
-
-        /**
          * Whether to show heads up only for dialer and sms apps
          * @hide
          */
@@ -5983,6 +5976,12 @@ public final class Settings {
         public static final String AMBIENT_BATTERY_PERCENT = "ambient_battery_percent";
 
         /**
+         * Whether to use partial screenshot when using volume keys
+         * @hide
+         */
+        public static final String SCREENSHOT_TYPE = "screenshot_type";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -6172,6 +6171,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(ACCENT_COLOR);
             PRIVATE_SETTINGS.add(SYSTEMUI_PLUGIN_VOLUME);
             PRIVATE_SETTINGS.add(QS_TILES_BG_DISCO);
+            PRIVATE_SETTINGS.add(SCREENSHOT_TYPE);
         }
 
         /**
