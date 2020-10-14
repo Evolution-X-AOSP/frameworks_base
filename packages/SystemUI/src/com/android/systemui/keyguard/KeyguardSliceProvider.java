d669719d56f7b1c7cc442285c6be7c44635a4476
Varun Date <date.varun123@gmail.com>
2020-10-15 05:23:39 +1100

    KeyguardSlice: Fix ticker appearing for a split second on lockscreen
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: Ief1986d9e7351818a543c79f1cd699443067f02f
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -205,7 +205,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
         Slice slice;
         synchronized (this) {
             ListBuilder builder = new ListBuilder(getContext(), mSliceUri, ListBuilder.INFINITY);
-            if (needsMediaLocked()) {
+            if (needsMediaLocked() && mDozing) {
                 addMediaLocked(builder);
             } else {
                 builder.addRow(new RowBuilder(mDateUri).setTitle(mLastText));
