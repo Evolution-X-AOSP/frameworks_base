6dd411ab5325635e65da13b6b002334134d8d5fc
DennySPb <dennyspb@gmail.com>
2020-10-29 15:42:33 +0300

    SystemUI: trigger pulse light only for notifications
    store the doze trigger reason in a setting to check later
    if it has been triggered for a notification or any other reason
    
    Change-Id: Iecee97b04e5e943c766fb4794d89e036ded65b3a
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -28,6 +28,7 @@ import android.hardware.display.AmbientDisplayConfiguration;
 import android.metrics.LogMaker;
 import android.os.SystemClock;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.text.format.Formatter;
 import android.util.Log;
 
@@ -494,6 +495,9 @@ public class DozeTriggers implements DozeMachine.Part {
                     mDozeHost.isPulsingBlocked());
             return;
         }
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                Settings.System.PULSE_TRIGGER_REASON, reason,
+                UserHandle.USER_CURRENT);
         mMachine.requestPulse(reason);
     }
 
