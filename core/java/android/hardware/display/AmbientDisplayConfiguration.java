9d342f6c69b16020c563e84a9bb6993c0ba1b4a9
ezio84 <brabus84@gmail.com>
2020-11-09 22:40:50 +0900

    fixup! Show Ambient instead of lockscreen on wake gestures [1/2]
    * Stallix edit: Add mDeviceHasSoli check.

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -227,8 +227,9 @@ public class AmbientDisplayConfiguration {
         return Settings.Secure.getIntForUser(mContext.getContentResolver(), name, def, user) != 0;
     }
 
+    /** {@hide} */
     public boolean isAmbientGestureEnabled(int user) {
-        return Settings.System.getIntForUser(mContext.getContentResolver(),
+        return !mDeviceHasSoli && Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.AMBIENT_WAKE_GESTURES, 1, user) != 0;
     }
 
