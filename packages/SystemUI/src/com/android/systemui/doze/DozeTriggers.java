c60bb63a3f54eed415117fefab06db633ab5ab41
ezio84 <brabus84@gmail.com>
2020-11-09 22:40:50 +0900

    fixup! Show Ambient instead of lockscreen on wake gestures [1/2]
    * Stallix edit: Add mDeviceHasSoli check.

--- packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -289,7 +289,8 @@ public class DozeTriggers implements DozeMachine.Part {
     }
 
     private void gentleWakeUp(int reason) {
-        if (!mConfig.alwaysOnEnabled(UserHandle.USER_CURRENT) && mConfig.isAmbientGestureEnabled(UserHandle.USER_CURRENT)) {
+        if (!mConfig.deviceHasSoli() && !mConfig.alwaysOnEnabled(UserHandle.USER_CURRENT)
+                && mConfig.isAmbientGestureEnabled(UserHandle.USER_CURRENT)) {
             requestPulse(reason, true /* alreadyPerformedProxCheck */, null /* onPulseSupressedListener */);
             return;
         }
