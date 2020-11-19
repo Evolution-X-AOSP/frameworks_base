a5b65f8b2c342321b28ab233680b6c728cdaeb6f
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-19 20:46:32 -0300

    [SQUASH] Introduce PixelPropsUtils
    That will spoof build fingerprints on some g00gle apps
    
    * Also will enable some cool stuffs like:
      - Dynamic navbar on GBoard
      - SafetyHub
    
    and others.
    
    Thanks to @kdrag0n for the original idea at https://github.com/ProtonAOSP/android_frameworks_base/commit/5a54bfd846c7a26ba4820a361a6fec779edf8c5a
    
    Change-Id: I1078e7402833fec77edb751070c5144d08c85b6c
    Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    
    keystore: Block key attestation for SafetyNet
    
    SafetyNet (part of Google Play Services) opportunistically uses
    hardware-backed key attestation via KeyStore as a strong integrity
    check. This causes SafetyNet to fail on custom ROMs because the verified
    boot key and bootloader unlock state can be detected from attestation
    certificates.
    
    As a workaround, we can take advantage of the fact that SafetyNet's
    usage of key attestation is opportunistic (i.e. falls back to basic
    integrity checks if it fails) and prevent it from getting the
    attestation certificate chain from KeyStore. This is done by checking
    the stack for DroidGuard, which is the codename for SafetyNet, and
    pretending that the device doesn't support key attestation.
    
    Key attestation has only been blocked for SafetyNet specifically, as
    Google Play Services and other apps have many valid reasons to use it.
    For example, it appears to be involved in Google's mobile security key
    ferature.
    
    Change-Id: I5146439d47f42dc6231cb45c4dab9f61540056f6
    
    core: Make CTS/Play Integrity pass again
    
    The logic behind CTS and Play Integrity has been updated today it now
    checks the product and model names against the fingerprint and if
    they do not match the CTS profile will fail.
    
    Also while we are at it use a newer FP from Pixel XL and add logging
    for key attestation blocking for debugging.
    
    Test: Boot, check for CTS and Play Integrity
    
    [Stallix - Evolution X]
    Switch back to using the hOS' AttestationHooks implementation for
    Play Integrity spoofing checks.
    
    Change-Id: I089d5ef935bba40338e10c795ea7d181103ffd15
    Signed-off-by: Dyneteve <dyneteve@hentaios.com>
    
    * Stallix: Adapted for A13 and squashed changes with their respected authors from
      https://github.com/Evolution-X/frameworks_base/commits/tiramisu-bak-reverts-qpr2/core/java/com/android/internal/util/evolution/PixelPropsUtils.java.
      https://github.com/Evolution-X/frameworks_base/commits/tiramisu-qpr2/core/java/com/android/internal/util/evolution/PixelPropsUtils.java
      https://github.com/Evolution-X/frameworks_base/commits/tiramisu-qpr2/core/java/com/android/internal/util/evolution/AttestationHooks.java
    
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>
    Co-authored-by: Dyneteve <dyneteve@hentaios.com>

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -269,6 +269,8 @@ import com.android.server.statusbar.StatusBarManagerInternal;
 import com.android.server.uri.NeededUriGrants;
 import com.android.server.uri.UriGrantsManagerInternal;
 
+import com.android.internal.util.evolution.AttestationHooks;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileDescriptor;
@@ -1961,7 +1963,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        enforceTaskPermission("getFocusedRootTaskInfo()");
+        if (!AttestationHooks.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("getFocusedRootTaskInfo()");
+        }
         final long ident = Binder.clearCallingIdentity();
         try {
             synchronized (mGlobalLock) {
@@ -2991,7 +2995,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        enforceTaskPermission("registerTaskStackListener()");
+        if (!AttestationHooks.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("registerTaskStackListener()");
+        }
         mTaskChangeNotificationController.registerTaskStackListener(listener);
     }
 
