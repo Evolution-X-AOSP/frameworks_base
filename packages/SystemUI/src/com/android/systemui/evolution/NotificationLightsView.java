omnirom/android_frameworks_base_old

[1/2] SystemUI: Add custom color to ambient pulse notification lights
Add custom color to ambient pulse notification lights

Change-Id: Ie67310221c0a100497a9d36847915d52e3854de1

packages/SystemUI/src/com/android/systemui/omni/NotificationLightsView.java
@@ -43,6 +43,7 @@ public class NotificationLightsView extends RelativeLayout {
     private ValueAnimator mLightAnimator;
     private boolean mPulsing;
     private boolean mNotification;
+    private int color;
 
     public NotificationLightsView(Context context) {
         this(context, null);
@@ -84,12 +85,19 @@ public class NotificationLightsView extends RelativeLayout {
     }
 
     public void animateNotification(boolean mNotification) {
-        int defaultColor = Color.parseColor("#3980FF");
+        int defaultColor = getResources().getInteger(
+                com.android.internal.R.integer.config_ambientNotificationDefaultColor);
         boolean useAccent = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.OMNI_NOTIFICATION_PULSE_ACCENT,
                 0, UserHandle.USER_CURRENT) != 0;
-        int color = useAccent ?
-                Utils.getColorAccentDefaultColor(getContext()) : defaultColor;
+        if (useAccent) {
+            color = useAccent ?
+                    Utils.getColorAccentDefaultColor(getContext()) : defaultColor;
+        } else {
+            color = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.OMNI_NOTIFICATION_PULSE_COLOR, defaultColor,
+                    UserHandle.USER_CURRENT);
+        }
         StringBuilder sb = new StringBuilder();
         sb.append("animateNotification color ");
         sb.append(Integer.toHexString(color));

Evolution-X-AOSP/frameworks_base

Signed-off-by: DennySPb <dennyspb@gmail.com>
Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
Signed-off-by: Joey Huab <joey@evolution-x.org>

packages/SystemUI/src/com/android/systemui/evolution/NotificationLightsView.java
@@ -43,6 +43,7 @@ public class NotificationLightsView extends RelativeLayout {
     private ValueAnimator mLightAnimator;
     private boolean mPulsing;
     private boolean mNotification;
+    private int color;
 
     public NotificationLightsView(Context context) {
         this(context, null);
@@ -84,12 +85,19 @@ public class NotificationLightsView extends RelativeLayout {
     }
 
     public void animateNotification(boolean mNotification) {
-        int defaultColor = Color.parseColor("#3980FF");
+        int defaultColor = getResources().getInteger(
+                com.android.internal.R.integer.config_ambientNotificationDefaultColor);
         boolean useAccent = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.NOTIFICATION_PULSE_ACCENT,
                 0, UserHandle.USER_CURRENT) != 0;
-        int color = useAccent ?
-                Utils.getColorAccentDefaultColor(getContext()) : defaultColor;
+        if (useAccent) {
+            color = useAccent ?
+                    Utils.getColorAccentDefaultColor(getContext()) : defaultColor;
+        } else {
+            color = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.NOTIFICATION_PULSE_COLOR, defaultColor,
+                    UserHandle.USER_CURRENT);
+        }
         StringBuilder sb = new StringBuilder();
         sb.append("animateNotification color ");
         sb.append(Integer.toHexString(color));