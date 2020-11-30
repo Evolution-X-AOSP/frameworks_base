caaadcff5f423722f97342c69cbfbecd6bb15573
Lucchetto <lucchetto.tie@live.com>
2020-11-30 22:58:36 +0100

    SystemUI: Animate brightness slider
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/qs/QSAnimator.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSAnimator.java
@@ -31,6 +31,7 @@ import com.android.systemui.qs.TouchAnimator.Builder;
 import com.android.systemui.qs.TouchAnimator.Listener;
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.tuner.TunerService.Tunable;
+import com.android.systemui.util.Utils;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -287,18 +288,19 @@ public class QSAnimator implements Callback, PageListener, Listener, OnLayoutCha
         }
 
         View brightness = mQsPanel.getBrightnessView();
+        if (brightness != null && Utils.useQsMediaPlayer(null) && !mQsPanel.shouldUseHorizontalLayout()
+                && mQsPanel.isMediaHostVisible()) {
+            View mQsPanelMediaHostView = mQsPanel.getMediaHost().getHostView();
+            View mQuickQsPanelMediaHostView = mQuickQsPanel.getMediaHost().getHostView();
+            float translation = mQsPanelMediaHostView.getHeight() - mQuickQsPanelMediaHostView.getHeight();
+            mBrightnessAnimator = new TouchAnimator.Builder().addFloat(brightness, "translationY", translation, 0)
+                    .build();
+            mAllViews.add(brightness);
+        } else {
+            mBrightnessAnimator = null;
+        }
+
         if (mAllowFancy) {
-            // Make brightness appear static position and alpha in through second half.
-            if (brightness != null && !mQsPanel.isBrightnessViewBottom()) {
-                firstPageBuilder.addFloat(brightness, "translationY", heightDiff, 0);
-                mBrightnessAnimator = new TouchAnimator.Builder()
-                        .addFloat(brightness, "alpha", mIsQuickQsBrightnessEnabled ? 1 : 0, 1)
-                        .setStartDelay(.5f)
-                        .build();
-                mAllViews.add(brightness);
-            } else {
-                mBrightnessAnimator = null;
-            }
             mFirstPageAnimator = firstPageBuilder
                     .setListener(this)
                     .build();
@@ -399,6 +401,9 @@ public class QSAnimator implements Callback, PageListener, Listener, OnLayoutCha
             }
         }
         mLastPosition = position;
+        if (mBrightnessAnimator != null) {
+            mBrightnessAnimator.setPosition(position);
+        }
         if (mOnFirstPage && mAllowFancy) {
             mQuickQsPanel.setAlpha(1);
             mFirstPageAnimator.setPosition(position);
