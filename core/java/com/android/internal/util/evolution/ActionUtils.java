c0e54e9f961b60daece16cbbdce52bde070fa2ab
Danny Lin <danny@kdrag0n.dev>
2020-12-07 09:29:33 -0500

    ActionUtils: Add Power menu action
    Change-Id: I54d7c493c3e8aa78f78eca93f13ac1e45e4458f5
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/com/android/internal/util/evolution/ActionUtils.java
+++ core/java/com/android/internal/util/evolution/ActionUtils.java
@@ -169,6 +169,16 @@ public class ActionUtils {
         context.startActivity(intent);
     }
 
+    // Launch Power Menu dialog
+    public static void showPowerMenu() {
+        final IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.showGlobalActions();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
     private static final class FireActions {
         private static IStatusBarService mStatusBarService = null;
         private static IStatusBarService getStatusBarService() {
