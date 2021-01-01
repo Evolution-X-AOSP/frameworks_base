b85027c18db0f9e918419e36e5230c97966e21f3
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-01-02 00:09:44 +0530

    SystemUI: It's time to kill klondike-saur
    sashascurtu: Adapted to scrambled pin
    Change-Id: I06c64b79727379d8fa42ed6735893c9ee58dd583
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/keyguard/KeyguardPINView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardPINView.java
@@ -53,7 +53,6 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
     private ViewGroup mRow1;
     private ViewGroup mRow2;
     private ViewGroup mRow3;
-    private View mDivider;
     private int mDisappearYTranslation;
     private View[][] mViews;
     private boolean mScramblePin;
@@ -106,7 +105,6 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
         mRow1 = findViewById(R.id.row1);
         mRow2 = findViewById(R.id.row2);
         mRow3 = findViewById(R.id.row3);
-        mDivider = findViewById(R.id.divider);
         mViews = new View[][]{
                 new View[]{
                         mRow0, null, null
