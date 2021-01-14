aca44c9723fc1a1064a97e52e3c1dd355c88b2df
Ido Ben-Hur <idoybh2@gmail.com>
2021-01-14 05:47:20 +0200

    FODAnimation: Make sure we don't add the view twice
    Test: Rapidly press FOD many times until systemui crashes
    Log: https://del.dog/estapebure.log
    Change-Id: I0abec8bdf163332f74e9c77ab213aba1d85640f0
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODAnimation.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODAnimation.java
@@ -42,6 +42,7 @@ public class FODAnimation extends ImageView {
     private WindowManager mWindowManager;
     private boolean mIsKeyguard;
     private boolean mIsRecognizingAnimEnabled;
+    private boolean mIsAdded;
 
     private int mSelectedAnim;
     private String[] ANIMATION_STYLES_NAMES = {
@@ -141,11 +142,14 @@ public class FODAnimation extends ImageView {
     public void showFODanimation() {
         if (mAnimParams != null && !mShowing && mIsKeyguard) {
             mShowing = true;
-            if (this.getWindowToken() == null){
-                mWindowManager.addView(this, mAnimParams);
-                mWindowManager.updateViewLayout(this, mAnimParams);
+            if (!mIsAdded) {
+                mIsAdded = true;
+                if (this.getWindowToken() == null) {
+                    mWindowManager.addView(this, mAnimParams);
+                    mWindowManager.updateViewLayout(this, mAnimParams);
+                }
+                recognizingAnim.start();
             }
-            recognizingAnim.start();
         }
     }
 
@@ -159,6 +163,7 @@ public class FODAnimation extends ImageView {
             }
             if (this.getWindowToken() != null) {
                 mWindowManager.removeView(this);
+                mIsAdded = false;
             }
         }
     }
