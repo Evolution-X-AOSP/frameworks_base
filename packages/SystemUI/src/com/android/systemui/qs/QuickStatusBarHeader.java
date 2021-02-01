751f80dae9be3b3ddc08a6f8e3cb1d973f66790c
Pranav Vashi <neobuddy89@gmail.com>
2021-02-02 02:28:47 +0530

    QuickStatusBar: Check for privacy chip indicators earlier
    This fixes bug where indicator is visible in both quick status bar and QS panel
    We had similar fix on 10.0: https://github.com/crdroidandroid/android_frameworks_base/commit/145ed309bd032d2fbbebd2f42a2a4a3cf950bfdd
    
    Issue cause:
     getIgnoredIconSlots() checks for getChipEnabled()
     getChipEnabled() checks for mMicCameraIndicatorsEnabled || mAllIndicatorsEnabled
     Therefore set mMicCameraIndicatorsEnabled, mAllIndicatorsEnabled before getIgnoredIconSlots()
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -271,6 +271,10 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mSystemIconsView = findViewById(R.id.quick_status_bar_system_icons);
         mQuickQsStatusIcons = findViewById(R.id.quick_qs_status_icons);
         StatusIconContainer iconContainer = findViewById(R.id.statusIcons);
+
+        mAllIndicatorsEnabled = mPrivacyItemController.getAllIndicatorsAvailable();
+        mMicCameraIndicatorsEnabled = mPrivacyItemController.getMicCameraAvailable();
+
         // Ignore privacy icons because they show in the space above QQS
         iconContainer.addIgnoredSlots(getIgnoredIconSlots());
         iconContainer.setShouldRestrictIcons(false);
@@ -348,9 +352,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mRingerModeTextView.setSelected(true);
         mNextAlarmTextView.setSelected(true);
         updateSettings();
-
-        mAllIndicatorsEnabled = mPrivacyItemController.getAllIndicatorsAvailable();
-        mMicCameraIndicatorsEnabled = mPrivacyItemController.getMicCameraAvailable();
 
         updateResources();
 
