26161c4b5db075aa082181c9c6511c81f1a19f4d
Michael W <baddaemon87@gmail.com>
2021-02-04 17:49:58 +0100

    SystemUI: Screenshots: Refactor shutter sound logic
    * Commit 683acd1dcb9b26902fe2a2901d9691c1b22bf1b5 introduced some
      duplicated code
    * Move it to an own method to reduce the footprint in the AOSP code parts
      and to reduce duplication
    
    Revert "ScreenshotSound: Fix playing screenshot's sound when camera's sounds off."
    
    This reverts commit f0e1f48f14c9bf316925eb8cb35058454526b78a.
    
    Change-Id: I57eaaee4db401d16cc6ef65c68604cdb4053ca01

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -56,8 +56,6 @@ import android.graphics.drawable.InsetDrawable;
 import android.graphics.drawable.LayerDrawable;
 import android.media.AudioManager;
 import android.media.MediaActionSound;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
@@ -240,7 +238,7 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
     private float mCornerSizeX;
     private float mDismissDeltaY;
 
-    private Ringtone mScreenshotSound;
+    private MediaActionSound mCameraSound;
     private AudioManager mAudioManager;
     private Vibrator mVibrator;
 
@@ -342,9 +340,9 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
         mFastOutSlowIn =
                 AnimationUtils.loadInterpolator(mContext, android.R.interpolator.fast_out_slow_in);
 
-        // Setup the Screenshot sound
-        mScreenshotSound = RingtoneManager.getRingtone(mContext,
-                    Uri.parse("file://" + "/system/media/audio/ui/camera_click.ogg"));
+        // Setup the Camera shutter sound
+        mCameraSound = new MediaActionSound();
+        mCameraSound.load(MediaActionSound.SHUTTER_CLICK);
 
         // Grab system services needed for screenshot sound
         mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
@@ -778,24 +776,7 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
     private void saveScreenshotAndToast(Consumer<Uri> finisher) {
         // Play the shutter sound to notify that we've taken a screenshot
         mScreenshotHandler.post(() -> {
-            switch (mAudioManager.getRingerMode()) {
-                case AudioManager.RINGER_MODE_SILENT:
-                    // do nothing
-                    break;
-                case AudioManager.RINGER_MODE_VIBRATE:
-                    if (mVibrator != null && mVibrator.hasVibrator()) {
-                        mVibrator.vibrate(VibrationEffect.createOneShot(50,
-                                VibrationEffect.DEFAULT_AMPLITUDE));
-                    }
-                    break;
-                case AudioManager.RINGER_MODE_NORMAL:
-                    // Play the shutter sound to notify that we've taken a screenshot
-                    if (Settings.System.getInt(mContext.getContentResolver(),
-                            Settings.System.SCREENSHOT_SOUND, 0) == 1) {
-                        mScreenshotSound.play();
-                    }
-                    break;
-            }
+            playShutterSound();
         });
 
         saveScreenshotInWorkerThread(finisher, new ActionsReadyListener() {
@@ -850,31 +831,7 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
                     }
                 });
 
-                mScreenshotLayout.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        switch (mAudioManager.getRingerMode()) {
-                            case AudioManager.RINGER_MODE_SILENT:
-                                // do nothing
-                                break;
-                            case AudioManager.RINGER_MODE_VIBRATE:
-                                if (mVibrator != null && mVibrator.hasVibrator()) {
-                                    mVibrator.vibrate(VibrationEffect.createOneShot(50,
-                                            VibrationEffect.DEFAULT_AMPLITUDE));
-                                }
-                                break;
-                            case AudioManager.RINGER_MODE_NORMAL:
-                                // Play the shutter sound to notify that we've taken a screenshot
-                                if (Settings.System.getInt(mContext.getContentResolver(),
-                                        Settings.System.SCREENSHOT_SOUND, 0) == 1) {
-                                    if (mScreenshotSound != null) {
-                                        mScreenshotSound.play();
-                                    }
-                                }
-                                break;
-                            }
-                        }
-                });
+                playShutterSound();
 
                 mScreenshotPreview.setLayerType(View.LAYER_TYPE_HARDWARE, null);
                 mScreenshotPreview.buildLayer();
@@ -1345,6 +1302,27 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
                     new ColorDrawable(Color.BLACK), insetDrawable});
         } else {
             return insetDrawable;
+        }
+    }
+
+    private void playShutterSound() {
+        switch (mAudioManager.getRingerMode()) {
+            case AudioManager.RINGER_MODE_SILENT:
+                // do nothing
+                break;
+            case AudioManager.RINGER_MODE_VIBRATE:
+                if (mVibrator != null && mVibrator.hasVibrator()) {
+                    mVibrator.vibrate(VibrationEffect.createOneShot(50,
+                            VibrationEffect.DEFAULT_AMPLITUDE));
+                }
+                break;
+            case AudioManager.RINGER_MODE_NORMAL:
+                // Play the shutter sound to notify that we've taken a screenshot
+                if (Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.SCREENSHOT_SOUND, 0) == 1) {
+                    mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
+                }
+                break;
         }
     }
 }
