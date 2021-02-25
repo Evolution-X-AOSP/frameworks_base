Evolution-X-AOSP/frameworks_base

commit 76df544c918336056ab776344b2ce3b808063218

SystemUI: Handle cases that we can't address within FODCircleView
So, briefly, what went on in this pow-wow is:
    - created a callback to efficiently monitor calls to FODCircleView impl (LuK1337 had a lot of patience)
    - addressed FOD visibility on statusbar expansion/collapse checking top activity package name and class within StatusBar.java
    - addressed FOD visibility if an app gets launched after statusbar expansion from a notif
    - addressed FOD visibility over every launcher animation, where every stands for:
        back to home, open recents, back gesture, assistant gesture (tested against any navigation type available)
    - reduced animation time to 125ms

Change-Id: I407d7dff6398bc7c6ecc4bfda7b9cb31af90bfd4
Signed-off-by: Joey Huab <joey@evolution-x.org>

packages/SystemUI/src/com/android/systemui/biometrics/FODCircleViewImpl.java
@@ -30,8 +30,12 @@ import com.android.internal.R;
 import com.android.internal.util.evolution.fod.FodUtils;
 
 import com.android.systemui.SystemUI;
+import com.android.systemui.biometrics.FODCircleViewImplCallback;
 import com.android.systemui.statusbar.CommandQueue;
+import com.android.systemui.util.Assert;
 
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -40,10 +44,15 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
     private static final String TAG = "FODCircleViewImpl";
 
     private FODCircleView mFodCircleView;
+
+    private final ArrayList<WeakReference<FODCircleViewImplCallback>>
+            mCallbacks = new ArrayList<>();
     private final CommandQueue mCommandQueue;
     private boolean mDisableNightMode;
     private boolean mNightModeActive;
     private int mAutoModeState;
+
+    private boolean mIsFODVisible;
 
     @Inject
     public FODCircleViewImpl(Context context, CommandQueue commandQueue) {
@@ -61,6 +70,12 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
         mCommandQueue.addCallback(this);
         try {
             mFodCircleView = new FODCircleView(mContext);
+            for (int i = 0; i < mCallbacks.size(); i++) {
+                FODCircleViewImplCallback cb = mCallbacks.get(i).get();
+                if (cb != null) {
+                    cb.onFODStart();
+                }
+            }
         } catch (RuntimeException e) {
             Slog.e(TAG, "Failed to initialize FODCircleView", e);
         }
@@ -73,6 +88,13 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
             if (isNightLightEnabled()) {
                 disableNightMode();
             }
+            for (int i = 0; i < mCallbacks.size(); i++) {
+                FODCircleViewImplCallback cb = mCallbacks.get(i).get();
+                if (cb != null) {
+                    cb.onFODStatusChange(true);
+                }
+            }
+            mIsFODVisible = true;
             mFodCircleView.show();
         }
     }
@@ -88,6 +110,13 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
             if (isNightLightEnabled()) {
                 setNightMode(mNightModeActive, mAutoModeState);
             }
+            for (int i = 0; i < mCallbacks.size(); i++) {
+                FODCircleViewImplCallback cb = mCallbacks.get(i).get();
+                if (cb != null) {
+                    cb.onFODStatusChange(false);
+                }
+            }
+            mIsFODVisible = false;
             mFodCircleView.hide();
         }
     }
@@ -107,5 +136,31 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
         } else if (autoMode == 1 || autoMode == 2) {
             colorDisplayManager.setNightDisplayAutoMode(autoMode);
         }
+    }
+
+    public void registerCallback(FODCircleViewImplCallback callback) {
+        Assert.isMainThread();
+        Slog.v(TAG, "*** register callback for " + callback);
+        for (int i = 0; i < mCallbacks.size(); i++) {
+            if (mCallbacks.get(i).get() == callback) {
+                Slog.e(TAG, "Object tried to add another callback",
+                        new Exception("Called by"));
+                return;
+            }
+        }
+        mCallbacks.add(new WeakReference<>(callback));
+        removeCallback(null);
+        sendUpdates(callback);
+    }
+
+    public void removeCallback(FODCircleViewImplCallback callback) {
+        Assert.isMainThread();
+        Slog.v(TAG, "*** unregister callback for " + callback);
+        mCallbacks.removeIf(el -> el.get() == callback);
+    }
+
+    private void sendUpdates(FODCircleViewImplCallback callback) {
+        callback.onFODStart();
+        callback.onFODStatusChange(mIsFODVisible);
     }
 }

data/etc/com.android.systemui.xml
@@ -30,8 +30,9 @@
         <permission name="android.permission.GET_APP_OPS_STATS"/>
         <permission name="android.permission.INTERACT_ACROSS_USERS"/>
         <permission name="android.permission.MANAGE_DEBUGGING"/>
-        <permission name="android.permission.MANAGE_SOUND_TRIGGER" />
+        <permission name="android.permission.MANAGE_FINGERPRINT" />
         <permission name="android.permission.MANAGE_SENSOR_PRIVACY"/>
+        <permission name="android.permission.MANAGE_SOUND_TRIGGER" />
         <permission name="android.permission.MANAGE_USB"/>
         <permission name="android.permission.MANAGE_USERS"/>
         <permission name="android.permission.MASTER_CLEAR"/>

packages/SystemUI/AndroidManifest.xml
@@ -139,6 +139,7 @@
     <uses-permission android:name="android.permission.USE_FINGERPRINT" />
     <uses-permission android:name="android.permission.RESET_FINGERPRINT_LOCKOUT" />
     <uses-permission android:name="android.permission.MANAGE_BIOMETRIC" />
+    <uses-permission android:name="android.permission.MANAGE_FINGERPRINT" />
     <uses-permission android:name="android.permission.MANAGE_SLICE_PERMISSIONS" />
     <uses-permission android:name="android.permission.CONTROL_KEYGUARD_SECURE_NOTIFICATIONS" />
     <uses-permission android:name="android.permission.GET_RUNTIME_PERMISSIONS" />

packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -68,6 +68,7 @@ import java.util.TimerTask;
 public class FODCircleView extends ImageView implements TunerService.Tunable {
     private static final String DOZE_INTENT = "com.android.systemui.doze.pulse";
     private static final String FOD_GESTURE = "system:" + Settings.System.FOD_GESTURE;
+    private static final int FADE_ANIM_DURATION = 125;
 
     private final int mPositionX;
     private final int mPositionY;

packages/SystemUI/src/com/android/systemui/biometrics/FODCircleViewImplCallback.java
@@ -1,0 +1,25 @@
+/**
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.biometrics;
+
+public class FODCircleViewImplCallback {
+
+    public void onFODStart() {}
+
+    public void onFODStatusChange(boolean isVisible) {}
+
+}

packages/SystemUI/src/com/android/systemui/statusbar/notification/ActivityLaunchAnimator.java
@@ -20,9 +20,12 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.app.ActivityManager;
+import android.content.Context;
 import android.graphics.Matrix;
 import android.graphics.Rect;
+import android.hardware.fingerprint.IFingerprintService;
 import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.util.MathUtils;
 import android.view.IRemoteAnimationFinishedCallback;
 import android.view.IRemoteAnimationRunner;
@@ -34,6 +37,7 @@ import android.view.View;
 
 import com.android.internal.policy.ScreenDecorationsUtils;
 import com.android.systemui.Interpolators;
+import com.android.systemui.biometrics.FODCircleViewImpl;
 import com.android.systemui.statusbar.NotificationShadeDepthController;
 import com.android.systemui.statusbar.notification.row.ExpandableNotificationRow;
 import com.android.systemui.statusbar.notification.stack.NotificationListContainer;
@@ -70,6 +74,8 @@ public class ActivityLaunchAnimator {
     private boolean mAnimationPending;
     private boolean mAnimationRunning;
     private boolean mIsLaunchForActivity;
+    private IFingerprintService mFingerprintService;
+    private FODCircleViewImpl mFODCircleViewImpl;
 
     public ActivityLaunchAnimator(
             NotificationShadeWindowViewController notificationShadeWindowViewController,
@@ -77,7 +83,8 @@ public class ActivityLaunchAnimator {
             NotificationPanelViewController notificationPanel,
             NotificationShadeDepthController depthController,
             NotificationListContainer container,
-            Executor mainExecutor) {
+            Executor mainExecutor,
+            FODCircleViewImpl fodCircleViewImpl) {
         mNotificationPanel = notificationPanel;
         mNotificationContainer = container;
         mDepthController = depthController;
@@ -87,6 +94,9 @@ public class ActivityLaunchAnimator {
         mWindowCornerRadius = ScreenDecorationsUtils
                 .getWindowCornerRadius(mNotificationShadeWindowViewController.getView()
                         .getResources());
+        mFODCircleViewImpl = fodCircleViewImpl;
+        mFingerprintService = IFingerprintService.Stub.asInterface(
+                ServiceManager.getService(Context.FINGERPRINT_SERVICE));
     }
 
     public RemoteAnimationAdapter getLaunchAnimation(
@@ -263,6 +273,15 @@ public class ActivityLaunchAnimator {
             mNotificationShadeWindowViewController.setExpandAnimationRunning(running);
             mNotificationContainer.setExpandingNotification(running ? mSourceNotification : null);
             mAnimationRunning = running;
+            boolean isFPClientActive = false;
+            try {
+                isFPClientActive = mFingerprintService.isClientActive();
+            } catch (Exception e) {
+                // do nothing.
+            }
+            if (!isFPClientActive) {
+                mFODCircleViewImpl.hideInDisplayFingerprintView();
+            }
             if (!running) {
                 mCallback.onExpandAnimationFinished(mIsFullScreenLaunch);
                 applyParamsToNotification(null);

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -84,6 +84,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.hardware.display.DisplayManager;
+import android.hardware.fingerprint.IFingerprintService;
 import android.media.AudioAttributes;
 import android.metrics.LogMaker;
 import android.net.Uri;
@@ -170,6 +171,8 @@ import com.android.systemui.SystemUI;
 import com.android.systemui.SystemUIFactory;
 import com.android.systemui.ambientmusic.AmbientIndicationContainer;
 import com.android.systemui.assist.AssistManager;
+import com.android.systemui.biometrics.FODCircleViewImpl;
+import com.android.systemui.biometrics.FODCircleViewImplCallback;
 import com.android.systemui.broadcast.BroadcastDispatcher;
 import com.android.systemui.bubbles.BubbleController;
 import com.android.systemui.charging.WirelessChargingAnimation;
@@ -262,6 +265,7 @@ import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.Executor;
@@ -666,7 +670,34 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
     };
 
+    private ActivityManager mActivityManager;
     private FlashlightController mFlashlightController;
+    private boolean mFodVisibility;
+    private boolean mIsDreaming;
+    private FODCircleViewImpl mFODCircleViewImpl;
+    private String mTopPkgClass;
+    private FODCircleViewImplCallback mFODCircleViewImplCallback =
+            new FODCircleViewImplCallback() {
+                @Override
+                public void onFODStatusChange(boolean isVisible) {
+                    boolean isFPClientActive = false;
+                    try {
+                        isFPClientActive = mFingerprintService.isClientActive();
+                    } catch (Exception e) {
+                        // do nothing.
+                    }
+                    mFodVisibility = isVisible;
+                    if (!isFPClientActive) {
+                        // if the client is not active, we have to nullify mTopPkgClass before
+                        // checking it against current foreground activity
+                        mTopPkgClass = null;
+                        return;
+                    } else if (isVisible && !mIsKeyguard && !mIsDreaming) {
+                        mTopPkgClass = getForegroundPackageNameAndClass();
+                    }
+                }
+            };
+
     private KeyguardUserSwitcher mKeyguardUserSwitcher;
     private final UserSwitcherController mUserSwitcherController;
     private final NetworkController mNetworkController;
@@ -696,6 +727,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             new KeyguardUpdateMonitorCallback() {
                 @Override
                 public void onDreamingStateChanged(boolean dreaming) {
+                    mIsDreaming = dreaming;
                     if (dreaming) {
                         maybeEscalateHeadsUp();
                     }
@@ -720,6 +752,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     private Lazy<NotificationShadeDepthController> mNotificationShadeDepthControllerLazy;
     private final BubbleController mBubbleController;
     private final BubbleController.BubbleExpandListener mBubbleExpandListener;
+    private final IFingerprintService mFingerprintService;
     private boolean mShowNavBar;
 
     private ActivityIntentHelper mActivityIntentHelper;
@@ -811,7 +844,8 @@ public class StatusBar extends SystemUI implements DemoMode,
             Lazy<NotificationShadeDepthController> notificationShadeDepthControllerLazy,
             StatusBarTouchableRegionManager statusBarTouchableRegionManager,
             FlashlightController flashlightController,
-            TaskHelper taskHelper) {
+            TaskHelper taskHelper,
+            FODCircleViewImpl fodCircleViewImpl) {
         super(context);
         mNotificationsController = notificationsController;
         mLightBarController = lightBarController;
@@ -890,6 +924,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         mDismissCallbackRegistry = dismissCallbackRegistry;
         mFlashlightController = flashlightController;
         mTaskHelper = taskHelper;
+        mFODCircleViewImpl = fodCircleViewImpl;
 
         mBubbleExpandListener =
                 (isExpanding, key) -> {
@@ -898,6 +933,9 @@ public class StatusBar extends SystemUI implements DemoMode,
                 };
 
         DateTimeView.setReceiverHandler(timeTickHandler);
+
+        mFingerprintService = IFingerprintService.Stub.asInterface(
+                ServiceManager.getService(Context.FINGERPRINT_SERVICE));
     }
 
     @Override
@@ -1096,6 +1134,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                         }
                     }
                 }, OverlayPlugin.class, true /* Allow multiple plugins */);
+                mActivityManager = mContext.getSystemService(ActivityManager.class);
+                mFODCircleViewImpl.registerCallback(mFODCircleViewImplCallback);
     }
 
     // ================================================================================
@@ -1494,7 +1534,8 @@ public class StatusBar extends SystemUI implements DemoMode,
         mActivityLaunchAnimator = new ActivityLaunchAnimator(
                 mNotificationShadeWindowViewController, this, mNotificationPanelViewController,
                 mNotificationShadeDepthControllerLazy.get(),
-                (NotificationListContainer) mStackScroller, mContext.getMainExecutor());
+                (NotificationListContainer) mStackScroller, mContext.getMainExecutor(),
+                mFODCircleViewImpl);
 
         // TODO: inject this.
         mPresenter = new StatusBarNotificationPresenter(mContext, mNotificationPanelViewController,
@@ -2003,6 +2044,12 @@ public class StatusBar extends SystemUI implements DemoMode,
                 Log.v(TAG, "clearing notification effects from setExpandedHeight");
             }
             clearNotificationEffects();
+        }
+
+        if (isExpanded && mFodVisibility) {
+            mFODCircleViewImpl.hideInDisplayFingerprintView();
+        } else if (!isExpanded && getForegroundPackageNameAndClass().equals(mTopPkgClass)) {
+            mFODCircleViewImpl.showInDisplayFingerprintView();
         }
 
         if (!isExpanded) {
@@ -2889,6 +2936,11 @@ public class StatusBar extends SystemUI implements DemoMode,
     @Override
     public void onRecentsAnimationStateChanged(boolean running) {
         setInteracting(StatusBarManager.WINDOW_NAVIGATION_BAR, running);
+        if (!running && getForegroundPackageNameAndClass().equals(mTopPkgClass)) {
+            mFODCircleViewImpl.showInDisplayFingerprintView();
+        } else {
+            mFODCircleViewImpl.hideInDisplayFingerprintView();
+        }
     }
 
     protected BarTransitions getStatusBarTransitions() {
@@ -4973,5 +5025,19 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (mQuickStatusBarHeader != null) {
             mQuickStatusBarHeader.updateDataUsageImage();
         }
+    }
+
+    private String getForegroundPackageNameAndClass() {
+        List<ActivityManager.RunningTaskInfo> tasks = mActivityManager.getRunningTasks(1);
+        if (tasks.isEmpty()) {
+            return null;
+        }
+        ActivityManager.RunningTaskInfo currentTask = tasks.get(0);
+        ComponentName currentActivity = currentTask.topActivity;
+        if (currentActivity.getPackageName() != null) {
+            return currentActivity.getPackageName().trim() +
+                  currentActivity.getShortClassName().trim();
+        }
+        return null;
     }
 }

packages/SystemUI/src/com/google/android/systemui/statusbar/phone/StatusBarGoogle.java
@@ -19,6 +19,7 @@ import com.android.systemui.dagger.qualifiers.UiBackground;
 import com.android.systemui.Dependency;
 import com.android.systemui.InitController;
 import com.android.systemui.assist.AssistManager;
+import com.android.systemui.biometrics.FODCircleViewImpl;
 import com.android.systemui.broadcast.BroadcastDispatcher;
 import com.android.systemui.bubbles.BubbleController;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
@@ -192,7 +193,8 @@ public class StatusBarGoogle extends StatusBar {
             Lazy<NotificationShadeDepthController> notificationShadeDepthControllerLazy,
             StatusBarTouchableRegionManager statusBarTouchableRegionManager,
             FlashlightController flashlightController,
-            TaskHelper taskHelper) {
+            TaskHelper taskHelper,
+            FODCircleViewImpl fodCircleViewImpl) {
         super(context,
                 notificationsController,
                 lightBarController,
@@ -271,7 +273,8 @@ public class StatusBarGoogle extends StatusBar {
                 notificationShadeDepthControllerLazy,
                 statusBarTouchableRegionManager,
                 flashlightController,
-                taskHelper);
+                taskHelper,
+                fodCircleViewImpl);
         mSmartSpaceController = smartSpaceController;
     }
 

commit 9ed94b932a8e279d23ad3c07d2ec71f36f9edca2

[SQUASH] Remove FOD Night Light commits
* Broken after 76df544c918336056ab776344b2ce3b808063218.

Revert "FOD: allow user to disable night light when active [1/2]"

This reverts commit 3941ba2a15d1fa31761cda54d8beed2b26844a7f.

Revert "FODCircleViewImpl: Optionally disable FOD night mode"

This reverts commit bdf8a36d8ae56af0434edba49a8e0fe49503bd34.

core/java/android/provider/Settings.java
@@ -5660,12 +5660,6 @@ public final class Settings {
         public static final String FOD_COLOR = "fod_color";
 
         /**
-         * FOD night light
-         * @hide
-         */
-        public static final String FOD_NIGHT_LIGHT = "fod_night_light";
-
-        /**
          * Whether to display status area (date & weather information) on the cLock
          * @hide
          */

core/res/res/values/evolution_config.xml
@@ -184,9 +184,6 @@
     <!-- Default fod pressed color -->
     <integer name="config_fod_pressed_color">1</integer>
 
-    <!-- NightLight FOD -->
-    <bool name="disable_fod_night_light">false</bool>
-
     <!-- Default value for FOD animation package resources -->
     <string name="config_fodAnimationPackage" translatable="false">com.evolution.fod.animations</string>
 

core/res/res/values/evolution_symbols.xml
@@ -168,9 +168,6 @@
   <!-- Default fod pressed icon -->
   <java-symbol type="integer" name="config_fod_pressed_color" />
 
-  <!-- NightLight FOD -->
-  <java-symbol type="bool" name="disable_fod_night_light"/>
-
   <!-- Default value for FOD animation package resources -->
   <java-symbol type="string" name="config_fodAnimationPackage" />
 
@@ -214,7 +211,6 @@
 
   <!-- Screen recorder framerate limit -->
   <java-symbol type="integer" name="config_screenRecorderMaxFramerate" />
-
 
   <!-- Any package that has one of the package names defined below will be prevented
        from using specific sensors in order to stop battery drain -->

packages/SystemUI/Android.bp
@@ -74,7 +74,7 @@ android_library {
         "dagger2-2.19",
         "jsr330",
         "vendor.lineage.biometrics.fingerprint.inscreen-V1.0-java",
-        "airbnb-lottie",
+        "lottie",
     ],
     manifest: "AndroidManifest.xml",
 
@@ -151,7 +151,7 @@ android_library {
         "dagger2-2.19",
         "jsr330",
         "vendor.lineage.biometrics.fingerprint.inscreen-V1.0-java",
-        "airbnb-lottie",
+        "lottie",
     ],
     libs: [
         "android.test.runner",

packages/SystemUI/src/com/android/systemui/biometrics/FODCircleViewImpl.java
@@ -18,15 +18,9 @@ package com.android.systemui.biometrics;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.content.res.Resources;
-import android.hardware.display.ColorDisplayManager;
-import android.os.Handler;
-import android.os.SystemProperties;
-import android.provider.Settings;
 import android.util.Slog;
 import android.view.View;
 
-import com.android.internal.R;
 import com.android.internal.util.evolution.fod.FodUtils;
 
 import com.android.systemui.SystemUI;
@@ -48,9 +42,6 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
     private final ArrayList<WeakReference<FODCircleViewImplCallback>>
             mCallbacks = new ArrayList<>();
     private final CommandQueue mCommandQueue;
-    private boolean mDisableNightMode;
-    private boolean mNightModeActive;
-    private int mAutoModeState;
 
     private boolean mIsFODVisible;
 
@@ -79,15 +70,11 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
         } catch (RuntimeException e) {
             Slog.e(TAG, "Failed to initialize FODCircleView", e);
         }
-        mDisableNightMode = mContext.getResources().getBoolean(R.bool.disable_fod_night_light);
     }
 
     @Override
     public void showInDisplayFingerprintView() {
         if (mFodCircleView != null) {
-            if (isNightLightEnabled()) {
-                disableNightMode();
-            }
             for (int i = 0; i < mCallbacks.size(); i++) {
                 FODCircleViewImplCallback cb = mCallbacks.get(i).get();
                 if (cb != null) {
@@ -99,17 +86,9 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
         }
     }
 
-    private boolean isNightLightEnabled() {
-       return Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.FOD_NIGHT_LIGHT, mDisableNightMode ? 1 : 0) == 1;
-    }
-
     @Override
     public void hideInDisplayFingerprintView() {
         if (mFodCircleView != null) {
-            if (isNightLightEnabled()) {
-                setNightMode(mNightModeActive, mAutoModeState);
-            }
             for (int i = 0; i < mCallbacks.size(); i++) {
                 FODCircleViewImplCallback cb = mCallbacks.get(i).get();
                 if (cb != null) {
@@ -118,23 +97,6 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
             }
             mIsFODVisible = false;
             mFodCircleView.hide();
-        }
-    }
-
-    private void disableNightMode() {
-        ColorDisplayManager colorDisplayManager = mContext.getSystemService(ColorDisplayManager.class);
-        mAutoModeState = colorDisplayManager.getNightDisplayAutoMode();
-        mNightModeActive = colorDisplayManager.isNightDisplayActivated();
-        colorDisplayManager.setNightDisplayActivated(false);
-    }
-
-    private void setNightMode(boolean activated, int autoMode) {
-        ColorDisplayManager colorDisplayManager = mContext.getSystemService(ColorDisplayManager.class);
-        colorDisplayManager.setNightDisplayAutoMode(0);
-        if (autoMode == 0) {
-            colorDisplayManager.setNightDisplayActivated(activated);
-        } else if (autoMode == 1 || autoMode == 2) {
-            colorDisplayManager.setNightDisplayAutoMode(autoMode);
         }
     }
 