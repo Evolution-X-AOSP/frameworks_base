8451f331eccd44cc801226853f8b2fd954e5de42
Oliver Scott <olivercscott@gmail.com>
2021-02-25 11:29:35 -0500

    Wi-Fi timeout feature (1/2)
    Issue: calyxos#228
    Change-Id: I1ac6b19eb35af656afd38eac24bb37fe618859f1
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -16308,6 +16308,12 @@ public final class Settings {
          */
         public static final String NR_NSA_TRACKING_SCREEN_OFF_MODE =
                 "nr_nsa_tracking_screen_off_mode";
+
+        /**
+         * The amount of time in milliseconds before wifi is turned off
+         * @hide
+         */
+        public static final String WIFI_OFF_TIMEOUT = "wifi_off_timeout";
     }
 
     /**
