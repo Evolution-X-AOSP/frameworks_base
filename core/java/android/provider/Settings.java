0adfb0f533b0ce148b2785db3ad499074f1292bd
Oliver Scott <olivercscott@gmail.com>
2021-02-25 11:29:35 -0500

    Wi-Fi timeout feature
    Add Wi-Fi timeout setting
    
    Issue: calyxos#228
    Change-Id: I1ac6b19eb35af656afd38eac24bb37fe618859f1
    (cherry picked from commit 12a44815c0d1c64c2684782a87707675181e19e6)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -19684,6 +19684,12 @@ public final class Settings {
         public static final String REPAIR_MODE_ACTIVE = "repair_mode_active";
 
         /**
+         * The amount of time in milliseconds before wifi is turned off
+         * @hide
+         */
+        public static final String WIFI_OFF_TIMEOUT = "wifi_off_timeout";
+
+        /**
          * Settings migrated from Wear OS settings provider.
          * @hide
          */
