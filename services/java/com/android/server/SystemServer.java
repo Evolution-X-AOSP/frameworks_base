49e0b2f908468a8b4ed246f699463dbb4accacb7
Ido Ben-Hur <idoybh2@gmail.com>
2021-02-27 01:52:33 +0200

    base: Allow scheduling always on display [1/2]
    From sunset to sunrise or at a custom time
    
    Change-Id: Ib76ecb1855ac215de6d7cd2f2346abfce6cc2214

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -132,6 +132,7 @@ import com.android.server.cpu.CpuMonitorService;
 import com.android.server.criticalevents.CriticalEventLog;
 import com.android.server.devicepolicy.DevicePolicyManagerService;
 import com.android.server.devicestate.DeviceStateManagerService;
+import com.android.server.display.AutoAODService;
 import com.android.server.display.DisplayManagerService;
 import com.android.server.display.color.ColorDisplayService;
 import com.android.server.dreams.DreamManagerService;
@@ -1741,6 +1742,12 @@ public final class SystemServer implements Dumpable {
             t.traceBegin("StartLogcatManager");
             mSystemServiceManager.startService(LogcatManagerService.class);
             t.traceEnd();
+
+            if (context.getResources().getBoolean(R.bool.config_dozeAlwaysOnDisplayAvailable)) {
+                t.traceBegin("AutoAODService");
+                mSystemServiceManager.startService(AutoAODService.class);
+                t.traceEnd();
+            }
 
         } catch (Throwable e) {
             Slog.e("System", "******************************************");
