d0ff7027888c9f17b11d94b813c04f868284a798
Chenyang Zhong <zhongcy95@gmail.com>
2021-03-21 23:16:10 -0400

    SystemUI: unify statusbar manual & auto brightness code
    setTemporaryBrightness(float) can be used in both manual and auto
    brightness mode. This stays inline with current android11's
    BrightnessController and avoids incorrect behaviors when sliding
    statusbar in auto brightness mode.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I0e7d7d0ccd871c61d062d6a190e5a61287687e80
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -616,7 +616,6 @@ public class StatusBar extends SystemUI implements
     private int mInitialTouchY;
     private int mLinger;
     private int mQuickQsOffsetHeight;
-    private boolean mAutomaticBrightness;
     private boolean mBrightnessControl;
     private boolean mBrightnessChanged;
     private boolean mJustPeeked;
@@ -2322,33 +2321,19 @@ public class StatusBar extends SystemUI implements
                 Math.max(BRIGHTNESS_CONTROL_PADDING, raw));
         float value = (padded - BRIGHTNESS_CONTROL_PADDING) /
                 (1 - (2.0f * BRIGHTNESS_CONTROL_PADDING));
-        if (mAutomaticBrightness) {
-            float adj = (2 * value) - 1;
-            adj = Math.max(adj, -1);
-            adj = Math.min(adj, 1);
-            final float val = adj;
-            mDisplayManager.setTemporaryAutoBrightnessAdjustment(val);
-            AsyncTask.execute(new Runnable() {
-                public void run() {
-                    Settings.System.putFloatForUser(mContext.getContentResolver(),
-                            Settings.System.SCREEN_AUTO_BRIGHTNESS_ADJ, val,
-                            UserHandle.USER_CURRENT);
-                }
-            });
-        } else {
-            final float val = convertGammaToLinearFloat(
-                    Math.round(value * GAMMA_SPACE_MAX),
-                    mMinimumBacklight, mMaximumBacklight);
-            mDisplayManager.setTemporaryBrightness(mDisplayId, val);
-            AsyncTask.execute(new Runnable() {
-                @Override
-                public void run() {
-                    Settings.System.putFloatForUser(mContext.getContentResolver(),
-                            Settings.System.SCREEN_BRIGHTNESS_FLOAT, val,
-                            UserHandle.USER_CURRENT);
-                }
-            });
-        }
+
+        final float val = convertGammaToLinearFloat(
+                Math.round(value * GAMMA_SPACE_MAX),
+                mMinimumBacklight, mMaximumBacklight);
+        mDisplayManager.setTemporaryBrightness(mDisplayId, val);
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                Settings.System.putFloatForUser(mContext.getContentResolver(),
+                        Settings.System.SCREEN_BRIGHTNESS_FLOAT, val,
+                        UserHandle.USER_CURRENT);
+            }
+        });
     }
 
     private void brightnessControl(MotionEvent event) {
