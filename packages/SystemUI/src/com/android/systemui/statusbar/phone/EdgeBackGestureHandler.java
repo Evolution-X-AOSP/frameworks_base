33706065ce85bab22c3cb403bcf34f5ac25cf9f0
Advaith Bhat <advaithbhat9@gmail.com>
2021-04-07 07:59:10 +0000

    SystemUI: Use EFFECT_HEAVY_CLICK for back swipe gestures
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -433,7 +433,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
 
     private void vibrateTick() {
         AsyncTask.execute(() ->
-            mVibrator.vibrate(VibrationEffect.createOneShot(HAPTIC_DURATION, VibrationEffect.DEFAULT_AMPLITUDE)));
+            mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_HEAVY_CLICK)));
     }
 
     public void onSettingsChanged() {
