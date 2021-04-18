ad607497c83ed83a3aaf454381b8d3f24b2679ce
Danny Lin <danny@kdrag0n.dev>
2021-04-18 02:06:58 -0700

    SystemUI: Add ripple effect to QS auto-brightness toggle
    Instead of abusing ImageView, which is not meant to be an interactive
    widget, use ImageButton for the auto-brightness toggle. This allows us
    to make it consistent with the rest of the UI by playing a ripple
    animation on interaction. It should also improve accessibility by being
    more semantically accurate.
    
    To remove ImageButton's default background, border, and padding while
    keeping the ripple effect,
    
    Change-Id: I8814714c309e43842a1e55b722333015e185ff4b
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -49,7 +49,7 @@ import android.view.animation.BounceInterpolator;
 import android.view.animation.DecelerateInterpolator;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.OvershootInterpolator;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import com.android.internal.logging.MetricsLogger;
@@ -117,7 +117,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
     @Nullable
     protected View mBrightnessView;
-    protected ImageView mAutoBrightnessView;
+    protected ImageButton mAutoBrightnessView;
     @Nullable
     private BrightnessController mBrightnessController;
 
