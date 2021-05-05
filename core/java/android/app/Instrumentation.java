26d94fdacb85c5d275cd267eac511a6a8fc6ee5d
Danny Lin <danny@kdrag0n.dev>
2021-05-05 02:01:39 -0700

    PixelPropsUtils: Make build property spoofing more reliable
    Determining the package name from Zygote's post-fork process name is
    unreliable for several reasons:
      - If USAP is enabled, Zygote will fork with the USAP process name, and
        spoofing will never trigger when the forked process specializes into
        a specific app.
      - With the native fork loop backported from AOSP master,
        Zygote.setAppProcessName() is no longer used for most apps because
        the forking is handled by native code.
    
    Instrumentation#newApplication() is responsible for creating the
    Application singleton during each app's initialization, so hook it
    instead for a more reliable and robust trigger.
    
    Change-Id: Ic762d8853967bf78db614efaf46166e527cb397a
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -67,6 +67,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
+import com.android.internal.util.custom.PixelPropsUtils;
+
 /**
  * Base class for implementing application instrumentation code.  When running
  * with instrumentation turned on, this class will be instantiated for you
@@ -1181,6 +1183,8 @@ public class Instrumentation {
         app.attach(context);
         GmsHooks.initApplicationBeforeOnCreate(app);
         AttestationHooks.initApplicationBeforeOnCreate(app);
+        String packageName = app.getPackageName();
+        PixelPropsUtils.setProps(packageName);
         return app;
     }
     
@@ -1200,6 +1204,8 @@ public class Instrumentation {
         app.attach(context);
         GmsHooks.initApplicationBeforeOnCreate(app);
         AttestationHooks.initApplicationBeforeOnCreate(app);
+        String packageName = app.getPackageName();
+        PixelPropsUtils.setProps(packageName);
         return app;
     }
 
--- core/java/com/android/internal/os/Zygote.java
+++ core/java/com/android/internal/os/Zygote.java
@@ -47,8 +47,6 @@ import java.io.DataOutputStream;
 import java.io.FileDescriptor;
 import java.io.IOException;
 
-import com.android.internal.util.custom.PixelPropsUtils;
-
 /** @hide */
 public final class Zygote {
     /*
@@ -885,9 +883,6 @@ public final class Zygote {
         } else {
             Log.w(loggingTag, "Unable to set package name.");
         }
-
-        // Set pixel props
-        PixelPropsUtils.setProps(args.mPackageName);
     }
 
     private static final String USAP_ERROR_PREFIX = "Invalid command to USAP: ";
