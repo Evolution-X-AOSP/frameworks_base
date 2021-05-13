ee62ac5eff5a3a58d7166a5a5a0d935bf4e59263
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2021-05-14 00:53:09 +0200

    AutoBrightness: Add support for one shot auto-brightness [1/2]
    - Only update auto brightness one time when screen is turned on.
    - Can be useful on devices where sensor is not accurate.
    
    jhonboy121: adapt to 12, simplified conditionals
    
    Change-Id: I5187fdb7765550d4eaa999317ab95f487d44c8d8
    Co-authored-by: Eamon Powell <eamonpowell@outlook.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6135,6 +6135,13 @@ public final class Settings {
         public static final String STATUS_BAR_LOGO_COLOR_ACCENT = "status_bar_logo_color_accent";
 
         /**
+         * Whether auto brightness is applied one shot when screen is turned on.
+         * Default 0
+         * @hide
+         */
+        public static final String AUTO_BRIGHTNESS_ONE_SHOT = "auto_brightness_one_shot";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
