bbba09500c5ac52fc2eb3e8b3355a071fda3e500
Anushek Prasal <anushekprasal@gmail.com>
2021-05-31 16:44:52 +0530

    base: Introduce automated Sleep Mode [1/2]
    [cjh1249131356]:
    - Adapt to S
    - Dropped AOD toggle in favor of scheduled AOD feature
    - Dropped Aggressive battery toggle
    
    [joeyhuab]
    * Adapt to A13 QPR2 after b8d6c0a.
    * Drop Location toggle for A14 QPR2.
    
    Change-Id: I85fc36e6db7ceeca1a1031f71f3882a776864814
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- core/java/com/android/internal/util/evolution/EvolutionUtils.java
+++ core/java/com/android/internal/util/evolution/EvolutionUtils.java
@@ -16,15 +16,24 @@
 
 package com.android.internal.util.evolution;
 
+import static android.provider.Settings.Global.ZEN_MODE_OFF;
+import static android.provider.Settings.Global.ZEN_MODE_IMPORTANT_INTERRUPTIONS;
 import static android.view.DisplayCutout.BOUNDS_POSITION_LEFT;
 import static android.view.DisplayCutout.BOUNDS_POSITION_RIGHT;
 
 import android.Manifest;
 import android.app.ActivityManager;
+import android.app.ActivityThread;
 import android.app.AlertDialog;
 import android.app.IActivityManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.bluetooth.BluetoothAdapter;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.content.om.IOverlayManager;
 import android.content.om.OverlayInfo;
@@ -32,6 +41,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
+import android.database.ContentObserver;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.hardware.camera2.CameraAccessException;
@@ -39,10 +49,14 @@ import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.hardware.input.InputManager;
 import android.hardware.fingerprint.FingerprintManager;
+import android.hardware.SensorPrivacyManager;
+import android.media.AudioManager;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
+import android.net.Uri;
+import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Looper;
@@ -51,6 +65,7 @@ import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
@@ -62,10 +77,14 @@ import android.view.InputDevice;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.Surface;
+import android.widget.Toast;
 
-import com.android.internal.R;
+import com.android.internal.notification.SystemNotificationChannels;
 import com.android.internal.statusbar.IStatusBarService;
+import com.android.internal.util.ArrayUtils;
+import com.android.internal.R;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -422,6 +441,352 @@ public class EvolutionUtils {
                 e.printStackTrace();
             }
             return null;
+        }
+    }
+  
+       public static class SleepModeController {
+        private final Resources mResources;
+        private final Context mUiContext;
+
+        private Context mContext;
+        private AudioManager mAudioManager;
+        private NotificationManager mNotificationManager;
+        private WifiManager mWifiManager;
+        private SensorPrivacyManager mSensorPrivacyManager;
+        private BluetoothAdapter mBluetoothAdapter;
+        private int mSubscriptionId;
+        private Toast mToast;
+
+        private boolean mSleepModeEnabled;
+
+        private static boolean mWifiState;
+        private static boolean mCellularState;
+        private static boolean mBluetoothState;
+        private static boolean mSensorState;
+        private static int mRingerState;
+        private static int mZenState;
+
+        private static final String TAG = "SleepModeController";
+        private static final int SLEEP_NOTIFICATION_ID = 727;
+        public static final String SLEEP_MODE_TURN_OFF = "android.intent.action.SLEEP_MODE_TURN_OFF";
+
+        public SleepModeController(Context context) {
+            mContext = context;
+            mUiContext = ActivityThread.currentActivityThread().getSystemUiContext();
+            mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+            mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+            mWifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
+            mSensorPrivacyManager = (SensorPrivacyManager) mContext.getSystemService(Context.SENSOR_PRIVACY_SERVICE);
+            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            mSubscriptionId = SubscriptionManager.INVALID_SUBSCRIPTION_ID;
+            mResources = mContext.getResources();
+
+            mSleepModeEnabled = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.SLEEP_MODE_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
+
+            SettingsObserver observer = new SettingsObserver(new Handler(Looper.getMainLooper()));
+            observer.observe();
+            observer.update();
+        }
+
+        private TelephonyManager getTelephonyManager() {
+            int subscriptionId = mSubscriptionId;
+
+            // If mSubscriptionId is invalid, get default data sub.
+            if (!SubscriptionManager.isValidSubscriptionId(subscriptionId)) {
+                subscriptionId = SubscriptionManager.getDefaultDataSubscriptionId();
+            }
+
+            // If data sub is also invalid, get any active sub.
+            if (!SubscriptionManager.isValidSubscriptionId(subscriptionId)) {
+                int[] activeSubIds = SubscriptionManager.from(mContext).getActiveSubscriptionIdList();
+                if (!ArrayUtils.isEmpty(activeSubIds)) {
+                    subscriptionId = activeSubIds[0];
+                }
+            }
+
+            return mContext.getSystemService(
+                    TelephonyManager.class).createForSubscriptionId(subscriptionId);
+        }
+
+        private boolean isWifiEnabled() {
+            if (mWifiManager == null) {
+                mWifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
+            }
+            try {
+                return mWifiManager.isWifiEnabled();
+            } catch (Exception e) {
+                return false;
+            }
+        }
+
+        private void setWifiEnabled(boolean enable) {
+            if (mWifiManager == null) {
+                mWifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
+            }
+            try {
+                mWifiManager.setWifiEnabled(enable);
+            } catch (Exception e) {
+            }
+        }
+
+        private boolean isBluetoothEnabled() {
+            if (mBluetoothAdapter == null) {
+                mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            }
+            try {
+                return mBluetoothAdapter.isEnabled();
+            } catch (Exception e) {
+                return false;
+            }
+        }
+
+        private void setBluetoothEnabled(boolean enable) {
+            if (mBluetoothAdapter == null) {
+                mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            }
+            try {
+                if (enable) mBluetoothAdapter.enable();
+                else mBluetoothAdapter.disable();
+            } catch (Exception e) {
+            }
+        }
+
+        private boolean isSensorEnabled() {
+            if (mSensorPrivacyManager == null) {
+                mSensorPrivacyManager = (SensorPrivacyManager) mContext.getSystemService(Context.SENSOR_PRIVACY_SERVICE);
+            }
+            try {
+                return !mSensorPrivacyManager.isAllSensorPrivacyEnabled();
+            } catch (Exception e) {
+                return false;
+            }
+        }
+
+        private void setSensorEnabled(boolean enable) {
+            if (mSensorPrivacyManager == null) {
+                mSensorPrivacyManager = (SensorPrivacyManager) mContext.getSystemService(Context.SENSOR_PRIVACY_SERVICE);
+            }
+            try {
+                mSensorPrivacyManager.setAllSensorPrivacy(!enable);
+            } catch (Exception e) {
+            }
+        }
+
+        private int getZenMode() {
+            if (mNotificationManager == null) {
+                mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+            }
+            try {
+                return mNotificationManager.getZenMode();
+            } catch (Exception e) {
+                return -1;
+            }
+        }
+
+        private void setZenMode(int mode) {
+            if (mNotificationManager == null) {
+                mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+            }
+            try {
+                mNotificationManager.setZenMode(mode, null, TAG);
+            } catch (Exception e) {
+            }
+        }
+
+        private int getRingerModeInternal() {
+            if (mAudioManager == null) {
+                mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+            }
+            try {
+                return mAudioManager.getRingerModeInternal();
+            } catch (Exception e) {
+                return -1;
+            }
+        }
+
+        private void setRingerModeInternal(int mode) {
+            if (mAudioManager == null) {
+                mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+            }
+            try {
+                mAudioManager.setRingerModeInternal(mode);
+            } catch (Exception e) {
+            }
+        }
+
+        private void enable() {
+            if (!ActivityManager.isSystemReady()) return;
+
+            // Disable Wi-Fi
+            final boolean disableWifi = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.SLEEP_MODE_WIFI_TOGGLE, 1, UserHandle.USER_CURRENT) == 1;
+            if (disableWifi) {
+                mWifiState = isWifiEnabled();
+                setWifiEnabled(false);
+            }
+
+            // Disable Bluetooth
+            final boolean disableBluetooth = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.SLEEP_MODE_BLUETOOTH_TOGGLE, 1, UserHandle.USER_CURRENT) == 1;
+            if (disableBluetooth) {
+                mBluetoothState = isBluetoothEnabled();
+                setBluetoothEnabled(false);
+            }
+
+            // Disable Mobile Data
+            final boolean disableData = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.SLEEP_MODE_CELLULAR_TOGGLE, 1, UserHandle.USER_CURRENT) == 1;
+            if (disableData) {
+                mCellularState = getTelephonyManager().isDataEnabled();
+                getTelephonyManager().setDataEnabled(false);
+            }
+
+            // Disable Sensors
+            final boolean disableSensors = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.SLEEP_MODE_SENSORS_TOGGLE, 1, UserHandle.USER_CURRENT) == 1;
+            if (disableSensors) {
+                mSensorState = isSensorEnabled();
+                setSensorEnabled(false);
+            }
+
+            // Set Ringer mode (0: Off, 1: Vibrate, 2:DND: 3:Silent)
+            final int ringerMode = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.SLEEP_MODE_RINGER_MODE, 0, UserHandle.USER_CURRENT);
+            if (ringerMode != 0) {
+                mRingerState = getRingerModeInternal();
+                mZenState = getZenMode();
+                if (ringerMode == 1) {
+                    setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE);
+                    setZenMode(ZEN_MODE_OFF);
+                } else if (ringerMode == 2) {
+                    setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+                    setZenMode(ZEN_MODE_IMPORTANT_INTERRUPTIONS);
+                } else if (ringerMode == 3) {
+                    setRingerModeInternal(AudioManager.RINGER_MODE_SILENT);
+                    setZenMode(ZEN_MODE_OFF);
+                }
+            }
+
+            showToast(mResources.getString(R.string.sleep_mode_enabled_toast), Toast.LENGTH_LONG);
+            addNotification();
+        }
+
+        private void disable() {
+            if (!ActivityManager.isSystemReady()) return;
+
+            // Enable Wi-Fi
+            final boolean disableWifi = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.SLEEP_MODE_WIFI_TOGGLE, 1, UserHandle.USER_CURRENT) == 1;
+            if (disableWifi && mWifiState != isWifiEnabled()) {
+                setWifiEnabled(mWifiState);
+            }
+
+            // Enable Bluetooth
+            final boolean disableBluetooth = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.SLEEP_MODE_BLUETOOTH_TOGGLE, 1, UserHandle.USER_CURRENT) == 1;
+            if (disableBluetooth && mBluetoothState != isBluetoothEnabled()) {
+                setBluetoothEnabled(mBluetoothState);
+            }
+
+            // Enable Mobile Data
+            final boolean disableData = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.SLEEP_MODE_CELLULAR_TOGGLE, 1, UserHandle.USER_CURRENT) == 1;
+            if (disableData && mCellularState != getTelephonyManager().isDataEnabled()) {
+                getTelephonyManager().setDataEnabled(mCellularState);
+            }
+
+            // Enable Sensors
+            final boolean disableSensors = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.SLEEP_MODE_SENSORS_TOGGLE, 1, UserHandle.USER_CURRENT) == 1;
+            if (disableSensors && mSensorState != isSensorEnabled()) {
+                setSensorEnabled(mSensorState);
+            }
+
+            // Set Ringer mode (0: Off, 1: Vibrate, 2:DND: 3:Silent)
+            final int ringerMode = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.SLEEP_MODE_RINGER_MODE, 0, UserHandle.USER_CURRENT);
+            if (ringerMode != 0 && (mRingerState != getRingerModeInternal() ||
+                    mZenState != getZenMode())) {
+                setRingerModeInternal(mRingerState);
+                setZenMode(mZenState);
+            }
+
+            showToast(mResources.getString(R.string.sleep_mode_disabled_toast), Toast.LENGTH_LONG);
+            mNotificationManager.cancel(SLEEP_NOTIFICATION_ID);
+        }
+
+        private void addNotification() {
+            Intent intent = new Intent(SLEEP_MODE_TURN_OFF);
+            intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND | Intent.FLAG_RECEIVER_INCLUDE_BACKGROUND);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(mContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+            // Display a notification
+            Notification.Builder builder = new Notification.Builder(mContext, SystemNotificationChannels.SLEEP)
+                .setTicker(mResources.getString(R.string.sleep_mode_notification_title))
+                .setContentTitle(mResources.getString(R.string.sleep_mode_notification_title))
+                .setContentText(mResources.getString(R.string.sleep_mode_notification_content))
+                .setSmallIcon(R.drawable.ic_sleep)
+                .setWhen(java.lang.System.currentTimeMillis())
+                .setOngoing(true)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(false);
+
+            Notification notification = builder.build();
+            mNotificationManager.notify(SLEEP_NOTIFICATION_ID, notification);
+        }
+
+        private void showToast(String msg, int duration) {
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        if (mToast != null) mToast.cancel();
+                        mToast = Toast.makeText(mUiContext, msg, duration);
+                        mToast.show();
+                    } catch (Exception e) {
+                    }
+                }
+            });
+        }
+
+        private void setSleepMode(boolean enabled) {
+            if (mSleepModeEnabled == enabled) {
+                return;
+            }
+
+            mSleepModeEnabled = enabled;
+
+            if (mSleepModeEnabled) {
+                enable();
+            } else {
+                disable();
+            }
+        }
+
+        class SettingsObserver extends ContentObserver {
+            SettingsObserver(Handler handler) {
+                super(handler);
+            }
+
+            void observe() {
+                ContentResolver resolver = mContext.getContentResolver();
+                resolver.registerContentObserver(Settings.Secure.getUriFor(
+                        Settings.Secure.SLEEP_MODE_ENABLED), false, this,
+                        UserHandle.USER_ALL);
+            }
+
+            @Override
+            public void onChange(boolean selfChange, Uri uri) {
+                update();
+            }
+
+            void update() {
+                final boolean enabled = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                        Settings.Secure.SLEEP_MODE_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
+                setSleepMode(enabled);
+            }
         }
     }
 }
