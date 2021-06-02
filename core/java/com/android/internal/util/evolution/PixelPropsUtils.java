95a739911290cc740009107be071a1227db0b287
Joey Huab <joey@evolution-x.org>
2021-06-02 16:28:53 +0900

    PixelPropsUtils: Switch to Marlin for Google Photos
    * Unlimited backup.

--- core/java/com/android/internal/util/evolution/PixelPropsUtils.java
+++ core/java/com/android/internal/util/evolution/PixelPropsUtils.java
@@ -30,12 +30,12 @@ public class PixelPropsUtils {
 
     private static final Map<String, Object> propsToChange;
     private static final Map<String, Object> propsToChangePixel3XL;
+    private static final Map<String, Object> propsToChangeOGPixelXL;
 
     private static final String[] packagesToChange = {
             "com.breel.wallpapers20",
             "com.google.android.apps.customization.pixel",
             "com.google.android.apps.fitness",
-            "com.google.android.apps.photos",
             "com.google.android.apps.recorder",
             "com.google.android.apps.subscriptions.red",
             "com.google.android.apps.tachyon",
@@ -59,6 +59,10 @@ public class PixelPropsUtils {
             "com.google.android.googlequicksearchbox"
     };
 
+    private static final String[] packagesToChangeOGPixelXL = {
+            "com.google.android.apps.photos"
+    };
+
     static {
         propsToChange = new HashMap<>();
         propsToChange.put("BRAND", "google");
@@ -74,6 +78,13 @@ public class PixelPropsUtils {
         propsToChangePixel3XL.put("PRODUCT", "crosshatch");
         propsToChangePixel3XL.put("MODEL", "Pixel 3 XL");
         propsToChangePixel3XL.put("FINGERPRINT", "google/crosshatch/crosshatch:11/RQ2A.210505.002/7246365:user/release-keys");
+        propsToChangeOGPixelXL = new HashMap<>();
+        propsToChangeOGPixelXL.put("BRAND", "google");
+        propsToChangeOGPixelXL.put("MANUFACTURER", "Google");
+        propsToChangeOGPixelXL.put("DEVICE", "marlin");
+        propsToChangeOGPixelXL.put("PRODUCT", "marlin");
+        propsToChangeOGPixelXL.put("MODEL", "Pixel XL");
+        propsToChangeOGPixelXL.put("FINGERPRINT", "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys");
     }
 
     public static void setProps(String packageName) {
@@ -95,6 +106,16 @@ public class PixelPropsUtils {
                 Log.d(TAG, "Defining props for: " + packageName);
             }
             for (Map.Entry<String, Object> prop : propsToChangePixel3XL.entrySet()) {
+                String key = prop.getKey();
+                Object value = prop.getValue();
+                setPropValue(key, value);
+            }
+        }
+        if (Arrays.asList(packagesToChangeOGPixelXL).contains(packageName)){
+            if (DEBUG){
+                Log.d(TAG, "Defining props for: " + packageName);
+            }
+            for (Map.Entry<String, Object> prop : propsToChangeOGPixelXL.entrySet()) {
                 String key = prop.getKey();
                 Object value = prop.getValue();
                 setPropValue(key, value);
