21a12c6ee5291f9921e895fbf70318e1e2a95247
daniml3 <danimoral1001@gmail.com>
2021-06-04 20:42:38 +0200

    core: check if the logical light is null before using it
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I9dbaa23e4e349284f117262e195b8692bd1017ca
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -7062,20 +7062,24 @@ public class NotificationManagerService extends SystemService {
     }
 
     private void forceShowLed(int color) {
-        if (color != -1) {
-            mNotificationLight.turnOff();
-            mNotificationLight.setColor(color);
-        } else {
-            mNotificationLight.turnOff();
+        if (mNotificationLight != null) {
+            if (color != -1) {
+                mNotificationLight.turnOff();
+                mNotificationLight.setColor(color);
+            } else {
+                mNotificationLight.turnOff();
+            }
         }
     }
 
     private void forcePulseLed(int color, int onTime, int offTime) {
-        if (color != -1) {
-            mNotificationLight.turnOff();
-            mNotificationLight.setFlashing(color, LogicalLight.LIGHT_FLASH_TIMED, onTime, offTime);
-        } else {
-            mNotificationLight.turnOff();
+        if (mNotificationLight != null) {
+            if (color != -1) {
+                mNotificationLight.turnOff();
+                mNotificationLight.setFlashing(color, LogicalLight.LIGHT_FLASH_TIMED, onTime, offTime);
+            } else {
+                mNotificationLight.turnOff();
+            }
         }
     }
 
