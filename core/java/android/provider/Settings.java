dfc43ba1551dffd75859531c6a7fb2c721bcf367
Ali B <abittin@gmail.com>
2021-06-27 14:06:41 +0300

    base: Add Settings Searchbar style (1/2)
    Change-Id: Ifa2022f69600f9468fbacd8002c035d1bed68bc7
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6444,6 +6444,14 @@ public final class Settings {
         public static final String ON_THE_GO_CAMERA = "on_the_go_camera";
 
         /**
+         * Whether to use the Android 12 searchbar in settings, the value is boolean.
+         * 0 = 0ff, 1 = on
+         *
+         * @hide
+         */
+        public static final String USE_NEW_SEARCHBAR = "use_new_searchbar";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
