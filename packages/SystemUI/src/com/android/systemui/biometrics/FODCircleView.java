9ffac95356c3d3aec156a93c35048b231cbdd64e
Ido Ben-Hur <idoybh2@gmail.com>
2021-07-03 02:25:24 +0300

    FODCircleView: Ignore assistant UI when showing bouncer
    This fixes fod not showing after using assistant from screen off for something that needs an unlock
    To reproduce use hotword on screen off and quickly ask for something such as: Open settings
    observe bouncer showing with no fod.
    
    Change-Id: I7940eb27d3991f8881fcc0ca7269726897571b35
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -248,6 +248,7 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
             mIsBouncer = isBouncer;
             if (mUpdateMonitor.isFingerprintDetectionRunning() && !mUpdateMonitor.userNeedsStrongAuth()) {
                 if (isPinOrPattern(mUpdateMonitor.getCurrentUser()) || !isBouncer) {
+                    mIsAssistantVisible = false;
                     show();
                 } else {
                     hide();
