defc5fda8422c2bd7b594697322cf26d31c27908
AshutoshSundresh <ashutoshsundresh@gmail.com>
2021-07-03 10:31:04 +0530

    PixelPropsUtils: Do not spoof the device as a Pixel for Pixel Buds
    So that the app shows in the app drawer
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/com/android/internal/util/evolution/PixelPropsUtils.java
+++ core/java/com/android/internal/util/evolution/PixelPropsUtils.java
@@ -29,37 +29,34 @@ public class PixelPropsUtils {
     private static final boolean DEBUG = false;
 
     private static final Map<String, Object> propsToChange;
+    private static final Map<String, Object> propsToChangePixelXL;
     private static final Map<String, Object> propsToChangePixel3XL;
-    private static final Map<String, Object> propsToChangeOGPixelXL;
+    private static final Map<String, Object> propsToChangeOnePlus9Pro;
 
     private static final String[] packagesToChange = {
             "com.breel.wallpapers20",
             "com.google.android.apps.customization.pixel",
             "com.google.android.apps.fitness",
+            "com.google.android.apps.maps",
             "com.google.android.apps.recorder",
+            "com.google.android.apps.safetyhub",
             "com.google.android.apps.subscriptions.red",
             "com.google.android.apps.tachyon",
+            "com.google.android.apps.turbo",
             "com.google.android.apps.turboadapter",
+            "com.google.android.apps.wallpaper",
             "com.google.android.apps.wallpaper.pixel",
             "com.google.android.as",
             "com.google.android.dialer",
+            "com.google.android.gms",
             "com.google.android.gms.location.history",
             "com.google.android.inputmethod.latin",
             "com.google.android.soundpicker",
             "com.google.pixel.dynamicwallpapers",
-            "com.google.pixel.livewallpaper",
-            "com.google.android.apps.safetyhub",
-            "com.google.android.apps.turbo",
-            "com.google.android.apps.wallpaper",
-            "com.google.android.apps.maps",
-            "com.google.android.gms"
+            "com.google.pixel.livewallpaper"
     };
 
-    private static final String[] packagesToChangePixel3XL = {
-            "com.google.android.googlequicksearchbox"
-    };
-
-    private static final String[] packagesToChangeOGPixelXL = {
+    private static final String[] packagesToChangePixelXL = {
             "com.google.android.apps.photos",
             "com.samsung.accessory.fridaymgr",
             "com.samsung.accessory.berrymgr",
@@ -72,6 +69,14 @@ public class PixelPropsUtils {
             "com.samsung.android.waterplugin"
     };
 
+    private static final String[] packagesToChangePixel3XL = {
+            "com.google.android.googlequicksearchbox"
+    };
+
+    private static final String[] packagesToChangeOnePlus9Pro = {
+            "com.google.android.apps.wearables.maestro.companion"
+    };
+
     static {
         propsToChange = new HashMap<>();
         propsToChange.put("BRAND", "google");
@@ -80,6 +85,13 @@ public class PixelPropsUtils {
         propsToChange.put("PRODUCT", "redfin");
         propsToChange.put("MODEL", "Pixel 5");
         propsToChange.put("FINGERPRINT", "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys");
+        propsToChangePixelXL = new HashMap<>();
+        propsToChangePixelXL.put("BRAND", "google");
+        propsToChangePixelXL.put("MANUFACTURER", "Google");
+        propsToChangePixelXL.put("DEVICE", "marlin");
+        propsToChangePixelXL.put("PRODUCT", "marlin");
+        propsToChangePixelXL.put("MODEL", "Pixel XL");
+        propsToChangePixelXL.put("FINGERPRINT", "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys");
         propsToChangePixel3XL = new HashMap<>();
         propsToChangePixel3XL.put("BRAND", "google");
         propsToChangePixel3XL.put("MANUFACTURER", "Google");
@@ -87,13 +99,13 @@ public class PixelPropsUtils {
         propsToChangePixel3XL.put("PRODUCT", "crosshatch");
         propsToChangePixel3XL.put("MODEL", "Pixel 3 XL");
         propsToChangePixel3XL.put("FINGERPRINT", "google/crosshatch/crosshatch:11/RQ3A.210805.001.A1/7474174:user/release-keys");
-        propsToChangeOGPixelXL = new HashMap<>();
-        propsToChangeOGPixelXL.put("BRAND", "google");
-        propsToChangeOGPixelXL.put("MANUFACTURER", "Google");
-        propsToChangeOGPixelXL.put("DEVICE", "marlin");
-        propsToChangeOGPixelXL.put("PRODUCT", "marlin");
-        propsToChangeOGPixelXL.put("MODEL", "Pixel XL");
-        propsToChangeOGPixelXL.put("FINGERPRINT", "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys");
+        propsToChangeOnePlus9Pro = new HashMap<>();
+        propsToChangeOnePlus9Pro.put("BRAND", "OnePlus");
+        propsToChangeOnePlus9Pro.put("MANUFACTURER", "OnePlus");
+        propsToChangeOnePlus9Pro.put("DEVICE", "OnePlus9Pro");
+        propsToChangeOnePlus9Pro.put("PRODUCT", "OnePlus9Pro_EEA");
+        propsToChangeOnePlus9Pro.put("MODEL", "LE2123");
+        propsToChangeOnePlus9Pro.put("FINGERPRINT", "OnePlus/OnePlus9Pro_EEA/OnePlus9Pro:11/RKQ1.201105.002/2107082109:user/release-keys");
     }
 
     public static void setProps(String packageName) {
@@ -110,6 +122,16 @@ public class PixelPropsUtils {
                 setPropValue(key, value);
             }
         }
+        if (Arrays.asList(packagesToChangePixelXL).contains(packageName)){
+            if (DEBUG){
+                Log.d(TAG, "Defining props for: " + packageName);
+            }
+            for (Map.Entry<String, Object> prop : propsToChangePixelXL.entrySet()) {
+                String key = prop.getKey();
+                Object value = prop.getValue();
+                setPropValue(key, value);
+            }
+        }
         if (Arrays.asList(packagesToChangePixel3XL).contains(packageName)){
             if (DEBUG){
                 Log.d(TAG, "Defining props for: " + packageName);
@@ -120,11 +142,11 @@ public class PixelPropsUtils {
                 setPropValue(key, value);
             }
         }
-        if (Arrays.asList(packagesToChangeOGPixelXL).contains(packageName)){
+        if (Arrays.asList(packagesToChangeOnePlus9Pro).contains(packageName)){
             if (DEBUG){
                 Log.d(TAG, "Defining props for: " + packageName);
             }
-            for (Map.Entry<String, Object> prop : propsToChangeOGPixelXL.entrySet()) {
+            for (Map.Entry<String, Object> prop : propsToChangeOnePlus9Pro.entrySet()) {
                 String key = prop.getKey();
                 Object value = prop.getValue();
                 setPropValue(key, value);
