c45411ef3426047ad213374eda2da4b0521b64f3
Ido Ben-Hur <idoybh2@gmail.com>
2021-07-11 14:40:52 +0300

    SystemUI: Show battery level for all devices
    There is absolutely no need to restrict it to audio devices
    If the device doesn't support % it will return a negative value anyway
    
    Change-Id: I6745adbe89dd5ad38e856966ef3d61c067dfc55b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -470,18 +470,11 @@ public class PhoneStatusBarPolicy
                 mResources.getString(R.string.accessibility_quick_settings_bluetooth_on);
         boolean bluetoothVisible = false;
         int batteryLevel = -1;
-        if (mBluetooth != null) {
-            if (mBluetooth.isBluetoothConnected()
-                    && (mBluetooth.isBluetoothAudioActive()
-                    || !mBluetooth.isBluetoothAudioProfileOnly())) {
-                bluetoothVisible = mBluetooth.isBluetoothEnabled();
-                batteryLevel = mBluetooth.getBatteryLevel();
-                contentDescription = mResources.getString(
-                        R.string.accessibility_bluetooth_connected);
-            }
+        if (mBluetooth != null && mBluetooth.isBluetoothConnected()) {
+            bluetoothVisible = mBluetooth.isBluetoothEnabled();
+            batteryLevel = mShowBluetoothBattery ? mBluetooth.getBatteryLevel() : -1;
             contentDescription = mResources.getString(
                     R.string.accessibility_bluetooth_connected);
-            bluetoothVisible = mBluetooth.isBluetoothEnabled();
         }
 
         mIconController.setBluetoothIcon(mSlotBluetooth,
