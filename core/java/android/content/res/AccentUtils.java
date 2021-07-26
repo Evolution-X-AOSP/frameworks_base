0be819d4d008714db3b2f3e13e098608ef1ffa71
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-07-26 15:15:24 +0530

    AccentUtils: Improve code
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/content/res/AccentUtils.java
+++ core/java/android/content/res/AccentUtils.java
@@ -7,21 +7,20 @@ import android.util.Log;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-/** @hide */
+/**
+ * @hide
+ */
 public class AccentUtils {
     private static final String TAG = "AccentUtils";
 
-    private static ArrayList<String> accentResources = new ArrayList<>(
+    private static final ArrayList<String> accentResources = new ArrayList<>(
             Arrays.asList("accent_device_default_light",
-                          "accent_device_default_dark"));
+                    "accent_device_default_dark"));
 
     private static final String ACCENT_COLOR_PROP = "persist.sys.theme.accentcolor";
 
     static boolean isResourceAccent(String resName) {
-        for (String ar : accentResources)
-            if (resName.contains(ar))
-                return true;
-        return false;
+        return accentResources.stream().anyMatch(resName::contains);
     }
 
     public static int getNewAccentColor(int defaultColor) {
@@ -31,8 +30,7 @@ public class AccentUtils {
     private static int getAccentColor(int defaultColor, String property) {
         try {
             String colorValue = SystemProperties.get(property, "-1");
-            return "-1".equals(colorValue)
-                    ? defaultColor : colorValue.equals("ff1a73e8")
+            return colorValue.equals("-1") || colorValue.equals("ff1a73e8")
                     ? defaultColor : Color.parseColor("#" + colorValue);
         } catch (Exception e) {
             Log.e(TAG, "Failed to set accent: " + e.getMessage() +
