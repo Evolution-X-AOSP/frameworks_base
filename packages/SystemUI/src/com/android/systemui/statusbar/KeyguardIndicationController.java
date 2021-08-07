ebd063fb32778522c71d164bfcccabd7540e146e
dmd79 <davidevinavil@gmail.com>
2021-08-08 00:07:45 +0200

    LockscreenCharging: Add divisor current overlay
    Change-Id: I821e19b9e4ff08ef18ce35ec95f0b994159f4ec2

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -208,6 +208,7 @@ public class KeyguardIndicationController {
     private boolean mAlternateFastchargeInfoUpdate;
 
     private int mChargingCurrent;
+    private int mBatteryCurrentDivider;
     private double mChargingVoltage;
     private float mTemperature;
 
@@ -297,6 +298,8 @@ public class KeyguardIndicationController {
         mKeyguardLogger = keyguardLogger;
         mScreenLifecycle.addObserver(mScreenObserver);
         mAlternateBouncerInteractor = alternateBouncerInteractor;
+        mBatteryCurrentDivider = mContext.getResources()
+                .getInteger(R.integer.config_battCurrentDivider);
 
         mFaceAcquiredMessageDeferral = faceHelpMessageDeferral;
         mCoExFaceAcquisitionMsgIdsToShow = new HashSet<>();
@@ -1058,8 +1061,7 @@ public class KeyguardIndicationController {
             Settings.System.LOCKSCREEN_BATTERY_INFO, 0, UserHandle.USER_CURRENT) == 1;
         if (showbatteryInfo) {
             if (mChargingCurrent > 0) {
-                current = (mChargingCurrent < 5 ? (mChargingCurrent * 1000)
-                        : (mChargingCurrent < 4000 ? mChargingCurrent : (mChargingCurrent / 1000)));
+                current = (mChargingCurrent / mBatteryCurrentDivider);
                 batteryInfo = batteryInfo + current + "mA";
             }
             if (mChargingVoltage > 0 && mChargingCurrent > 0) {
