ecb12b40cb6cdbaac77a7a1a5f5959a21c56870d
AshutoshSundresh <ashutoshsundresh@gmail.com>
2021-08-10 14:33:44 +0530

    LockIcon: Don't center on bouncer
    The animation is really janky
    
    - Also follow up https://github.com/ShapeShiftOS/android_frameworks_base/commit/79accfd3bffffc524bb96dc52f0d9dff83e10c14 with the new lock alignments
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -378,7 +378,7 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
             }
         }
 
-        if (!lockVisible)
+        if (!lockVisible || !mLockIcon)
             return;
 
         final ContentResolver resolver = mContext.getContentResolver();
@@ -391,10 +391,7 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
         FrameLayout.LayoutParams paramsContainer =
             (FrameLayout.LayoutParams) mLockIconContainer.getLayoutParams();
 
-        if (mBouncer.isShowing()) {
-            mLockIconContainer.setPaddingRelative(0, 0, 0, 0);
-            paramsContainer.gravity = Gravity.TOP | Gravity.CENTER_HORIZONTAL;
-        } else if (mCustomClockSelectionType) {
+        if (mCustomClockSelectionType) {
             mLockIconContainer.setPaddingRelative((int) mContext.getResources()
                     .getDimension(R.dimen.custom_clock_left_padding) + 8, 0, 0, 0);
             paramsContainer.gravity = Gravity.TOP | Gravity.LEFT;
