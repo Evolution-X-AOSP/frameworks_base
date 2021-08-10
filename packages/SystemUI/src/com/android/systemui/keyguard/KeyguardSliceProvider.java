464e7b05d07527a05c29ff3864582d4727383fb5
AshutoshSundresh <ashutoshsundresh@gmail.com>
2021-08-10 14:16:27 +0530

    keyguard: Fix null text on 12 clock after dismissing media
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -515,7 +515,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
         }
 
         // Set new track info from playing media notification
-        if (isTwelveClockSelected) {
+        if (isTwelveClockSelected && title != null) {
             StringBuffer evenSB = new StringBuffer(" ");
             evenSB.append(title);
             mMediaTitle = evenSB;
