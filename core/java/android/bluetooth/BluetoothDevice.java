b6a7d37d6e238a23dced447ae087032bb908a033
Hansong Zhang <hsz@google.com>
2021-08-18 16:35:00 -0700

    Bluetooth: Fix formatting in getAlias()
    Bug: 180747689
    Test: manual
    Change-Id: Ic309f4aad116fd424d5d0d0e2016d61be8826b78
    (cherry picked from commit 3c4a917cb9481f4b70c26b6a06e798e65e5895ce)

--- core/java/android/bluetooth/BluetoothDevice.java
+++ core/java/android/bluetooth/BluetoothDevice.java
@@ -1232,7 +1232,10 @@ public final class BluetoothDevice implements Parcelable {
             if (alias == null) {
                 return getName();
             }
-            return alias;
+            return alias
+                    .replace('\t', ' ')
+                    .replace('\n', ' ')
+                    .replace('\r', ' ');
         } catch (RemoteException e) {
             Log.e(TAG, "", e);
         }
