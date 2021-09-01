6d625b928704054acceeb173fc1fca6f93864790
Anushek Prasal <anushekprasal@gmail.com>
2021-09-01 20:43:19 +0800

    base: Make IME button space optional [1/2]
    [cjh1249131356 Edited]:
    - Expose hidden height instead of 0dp, so devices with rounded corner can adjust its height.
    - Hide left/right virtual buttons as well.
    
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Change-Id: I2b64bd01079d07333dc0455793f358b190e38cb3
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6563,6 +6563,12 @@ public final class Settings {
         public static final String NAVIGATION_BAR_MENU_ARROW_KEYS = "navigation_bar_menu_arrow_keys";
 
         /**
+         * Whether to show IME space
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_IME_SPACE = "navigation_bar_ime_space";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
