11bdd4620ff82656ffe074b0058092a0889c4e09
kubersharma001 <kubersharma001@gmail.com>
2021-09-20 06:02:49 +0000

    PixelPropsUtils: Remove some packages
    Change-Id: I08a0aea75702a0a17717faccfa42f61e36ace9de
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/com/android/internal/util/evolution/PixelPropsUtils.java
+++ core/java/com/android/internal/util/evolution/PixelPropsUtils.java
@@ -60,20 +60,15 @@ public class PixelPropsUtils {
     private static final String[] packagesToChangePixel5a = {
             "com.breel.wallpapers20",
             "com.google.android.apps.customization.pixel",
-            "com.google.android.apps.fitness",
             "com.google.android.apps.maps",
-            "com.google.android.apps.recorder",
             "com.google.android.apps.safetyhub",
             "com.google.android.apps.subscriptions.red",
-            "com.google.android.apps.tachyon",
             "com.google.android.apps.turbo",
             "com.google.android.apps.turboadapter",
             "com.google.android.apps.wallpaper",
             "com.google.android.apps.wallpaper.pixel",
             "com.google.android.as",
-            "com.google.android.dialer",
             "com.google.android.inputmethod.latin",
-            "com.google.android.soundpicker",
             "com.google.pixel.dynamicwallpapers",
             "com.google.pixel.livewallpaper"
     };
