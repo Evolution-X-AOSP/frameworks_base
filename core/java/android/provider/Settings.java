1b820733950cc33304021fc28923d3516e23fd9c
SuperDroidBond <superdroidbond@yahoo.com>
2021-10-04 11:07:34 +0530

    Ambient Pulse Lights: add layout style for top n bottom [1/2]
    Co-Authored-By: bezke <1642612+bezke@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6321,6 +6321,12 @@ public final class Settings {
         public static final String PULSE_AMBIENT_LIGHT_WIDTH = "pulse_ambient_light_width";
 
         /**
+         * Width of Ambient edge light layout style
+         * @hide
+         */
+        public static final String PULSE_LIGHT_LAYOUT_STYLE = "pulse_light_layout_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
