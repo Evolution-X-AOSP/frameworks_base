cbbe190b432cfde183c5aca8b0b4b3dff0e7411d
Jason Edson <jaysonedson@gmail.com>
2021-10-22 09:59:20 -0700

    QuickSettings: Allow changing vibration duration [1/2]
    Change-Id: I400365571cb9ff1948e03509eb547bf842255f4f
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11810,6 +11810,13 @@ public final class Settings {
         public static final String QUICK_SETTINGS_TILES_VIBRATE = "quick_settings_vibrate";
 
         /**
+         * Change quick settings tiles vibration duration (in milliseconds)
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES_VIBRATE_DURATION =
+                "quick_settings_vibrate_duration";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
