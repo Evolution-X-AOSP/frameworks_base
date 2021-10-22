94bae04feaa70a4fff8fb6ad84dc43d678ef21ca
Jason Edson <jaysonedson@gmail.com>
2021-10-22 09:59:20 -0700

    QuickSettings: Allow changing vibration duration [1/2]
    Change-Id: I400365571cb9ff1948e03509eb547bf842255f4f
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -227,5 +227,6 @@ public class SecureSettings {
         Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
         Settings.Secure.DOZE_ON_CHARGE,
         Settings.Secure.QUICK_SETTINGS_TILES_VIBRATE,
+        Settings.Secure.QUICK_SETTINGS_TILES_VIBRATE_DURATION,
     };
 }
