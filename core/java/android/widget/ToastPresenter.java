1c0a51895200bd9698d31028405dd1faa3987f8b
SagarMakhar <sagarmakhar@gmail.com>
2021-11-05 13:30:21 +0000

    Fixup legacy toast icon visibility
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/widget/ToastPresenter.java
+++ core/java/android/widget/ToastPresenter.java
@@ -254,8 +254,8 @@ public class ToastPresenter {
                 horizontalMargin, verticalMargin, removeWindowAnimations);
 
         ImageView appIcon = (ImageView) mView.findViewById(android.R.id.icon);
-        if ((Settings.System.getInt(mContext.getContentResolver(), Settings.System.TOAST_ICON, 1) == 1)) {
-            if (appIcon != null) {
+        if (appIcon != null) {
+            if ((Settings.System.getInt(mContext.getContentResolver(), Settings.System.TOAST_ICON, 1) == 1)) {
                 PackageManager pm = mContext.getPackageManager();
                 Drawable icon = null;
                 try {
@@ -264,6 +264,8 @@ public class ToastPresenter {
                     // nothing to do
                 }
                 appIcon.setImageDrawable(icon);
+            } else {
+                appIcon.setVisibility(View.GONE);
             }
         }
 
