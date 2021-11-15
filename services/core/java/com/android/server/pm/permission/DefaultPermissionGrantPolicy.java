a0d446c119ee808eda698c9910af46a4413ed829
Sagarrokade006 <sagarrokade006@gmail.com>
2021-11-15 15:32:15 +0000

    Permissions: Grant Download Provider access permissions to external storage
    msg: java.lang.SecurityException: Permission Denial: writing com.android.providers.downloads.DownloadProvider uri content://downloads/all_downloads/1 from pid=22101, uid=10126 requires android.permission.ACCESS_ALL_DOWNLOADS, or grantUriPermission()
    stacktrace: java.lang.SecurityException: Permission Denial: writing com.android.providers.downloads.DownloadProvider uri content://downloads/all_downloads/1 from pid=22101, uid=10126 requires android.permission.ACCESS_ALL_DOWNLOADS, or grantUriPermission()
    	at android.os.Parcel.createExceptionOrNull(Parcel.java:2425)
    	at android.os.Parcel.createException(Parcel.java:2409)
    	at android.os.Parcel.readException(Parcel.java:2392)
    	at android.database.DatabaseUtils.readExceptionFromParcel(DatabaseUtils.java:190)
    	at android.database.DatabaseUtils.readExceptionFromParcel(DatabaseUtils.java:142)
    	at android.content.ContentProviderProxy.delete(ContentProviderNative.java:629)
    	at android.content.ContentResolver.delete(ContentResolver.java:2333)
    	at android.content.ContentResolver.delete(ContentResolver.java:2299)
    	at com.google.android.libraries.gsa.util.a.c(SourceFile:1)
    	at com.google.android.apps.gsa.staticplugins.t.g.i(SourceFile:4)
    	at com.google.android.apps.gsa.staticplugins.t.g.e(SourceFile:29)
    	at com.google.android.apps.gsa.staticplugins.t.m.k(SourceFile:23)
    	at com.google.android.apps.gsa.staticplugins.t.m.f(SourceFile:1)
    	at com.google.android.apps.gsa.staticplugins.t.j.a(SourceFile:6)
    	at com.google.android.apps.gsa.search.core.i.e.run(SourceFile:2)
    	at com.google.android.libraries.gsa.k.a.k.run(SourceFile:2)
    	at com.google.android.apps.gsa.shared.util.c.a.bt.b(SourceFile:1)
    	at com.google.android.apps.gsa.shared.util.c.a.bw.run(SourceFile:1)
    	at com.google.android.libraries.g.ab.run(SourceFile:1)
    	at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1167)
    	at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:641)
    	at com.google.android.libraries.g.n.run(SourceFile:2)
    	at com.google.android.libraries.g.d.run(SourceFile:2)
    	at java.lang.Thread.run(Thread.java:920)
    Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -951,6 +951,10 @@ final class DefaultPermissionGrantPolicy {
         grantPermissionsToSystemPackage(pm, getDefaultSystemHandlerServicePackage(pm,
                         commonServiceAction, userId), userId, NOTIFICATION_PERMISSIONS);
 
+        // DownloadProviders
+        grantSystemFixedPermissionsToSystemPackage(pm,"com.android.providers.downloads.DownloadProvider", userId,
+                STORAGE_PERMISSIONS);
+
         // Mediascanner
         grantSystemFixedPermissionsToSystemPackage(pm,
                 getDefaultProviderAuthorityPackage("com.android.providers.media.MediaProvider", userId), userId,
