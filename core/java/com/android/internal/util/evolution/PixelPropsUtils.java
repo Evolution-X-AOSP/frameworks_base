52006d48074f9e0cc3f4a5f4f1ee9dd42897ba1a
Anay Wadhera <awadhera@berkeley.edu>
2021-12-10 13:11:49 -0800

    PixelPropsUtils: Remove spoofing for currently supported Pixels
    Change-Id: I3a426f8671f841c16e5af7c0a2a204d19c502464
    Reviewed-on: https://review.statixos.com/c/android_frameworks_base/+/6410
    Tested-by: Anay Wadhera <anay1018@gmail.com>
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    
    Change-Id: I926b23324e669e24079c78fecc0c7a0b77c15e13
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/com/android/internal/util/evolution/PixelPropsUtils.java
+++ core/java/com/android/internal/util/evolution/PixelPropsUtils.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2020 The Pixel Experience Project
  *               2021-2022 crDroid Android Project
+ *               2019-2022 The Evolution X Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,6 +30,8 @@ import java.util.Map;
 
 public class PixelPropsUtils {
 
+    public static final String PACKAGE_GMS = "com.google.android.gms";
+    private static final String DEVICE = "org.evolution.device";
     private static final String TAG = PixelPropsUtils.class.getSimpleName();
     private static final boolean DEBUG = false;
 
@@ -78,6 +81,20 @@ public class PixelPropsUtils {
             "com.google.ar.core"
     };
 
+    // Codenames for currently supported Pixels by Google
+    private static final String[] pixelCodenames = {
+            "oriole",
+            "raven",
+            "redfin",
+            "barbet",
+            "bramble",
+            "sunfish",
+            "coral",
+            "flame",
+            "bonito",
+            "sargo"
+    };
+
     private static volatile boolean sIsGms = false;
 
     static {
@@ -110,11 +127,13 @@ public class PixelPropsUtils {
         if (packageName == null) {
             return;
         }
-        if (Arrays.asList(packagesToKeep).contains(packageName)) {
-            return;
+        if (packageName.equals(PACKAGE_GMS)) {
+            sIsGms = true;
         }
-        if (packageName.startsWith("com.google.")
-                || Arrays.asList(extraPackagesToChange).contains(packageName)) {
+        boolean isPixelDevice = Arrays.asList(pixelCodenames).contains(SystemProperties.get(DEVICE));
+        if (!isPixelDevice &&
+            ((packageName.startsWith("com.google.") && !Arrays.asList(packagesToKeep).contains(packageName))
+                || Arrays.asList(extraPackagesToChange).contains(packageName))) {
             Map<String, Object> propsToChange = propsToChangePixel6;
 
             if (Arrays.asList(packagesToChangePixel5).contains(packageName)) {
@@ -138,9 +157,6 @@ public class PixelPropsUtils {
                 }
                 if (DEBUG) Log.d(TAG, "Defining " + key + " prop for: " + packageName);
                 setPropValue(key, value);
-            }
-            if (packageName.equals("com.google.android.gms")) {
-                sIsGms = true;
             }
             // Set proper indexing fingerprint
             if (packageName.equals("com.google.android.settings.intelligence")) {
