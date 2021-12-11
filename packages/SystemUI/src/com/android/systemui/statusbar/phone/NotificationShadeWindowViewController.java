8c77766ce948518862253306262bb8c28e6e50fc
jhonboy121 <alfredmathew05@gmail.com>
2021-12-11 19:54:55 +0530

    SystemUI: NotificationShadeWindowViewController: initialize all the setting variables
    * Include fix for Double tap to wake on doze after being changed in b4400a2.
    * This for one fixes double tap to wake not working in aod after a reboot / sysui restart
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationShadeWindowViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationShadeWindowViewController.java
@@ -198,27 +198,24 @@ public class NotificationShadeWindowViewController {
     public void setupExpandedStatusBar() {
         mStackScrollLayout = mView.findViewById(R.id.notification_stack_scroller);
 
+        final AmbientDisplayConfiguration configuration = new AmbientDisplayConfiguration(mView.getContext());
+        mDoubleTapEnabled = mSecureSettings.getIntForUser(
+            Settings.Secure.DOZE_DOUBLE_TAP_GESTURE, 1, UserHandle.USER_CURRENT) == 1;
+        mSingleTapEnabled = configuration.tapGestureEnabled(UserHandle.USER_CURRENT);
+
         final ContentObserver contentObserver = new ContentObserver(mHandler) {
             @Override
             public void onChange(boolean selfChange, Uri uri) {
-                final AmbientDisplayConfiguration configuration =
-                    new AmbientDisplayConfiguration(mView.getContext());
                 switch (uri.getLastPathSegment()) {
                     case Settings.Secure.DOZE_DOUBLE_TAP_GESTURE:
-                        mDoubleTapEnabled = configuration.doubleTapGestureEnabled(
-                                UserHandle.USER_CURRENT);
+                        mDoubleTapEnabled = Settings.Secure.getIntForUser(mView.getContext().getContentResolver(),
+                                Settings.Secure.DOZE_DOUBLE_TAP_GESTURE, 1, UserHandle.USER_CURRENT) == 1;
                         break;
                     case Settings.Secure.DOZE_TAP_SCREEN_GESTURE:
                         mSingleTapEnabled = configuration.tapGestureEnabled(UserHandle.USER_CURRENT);
                         break;
                     case Settings.System.QS_SHOW_AUTO_BRIGHTNESS_BUTTON:
-                        if (mAutoBrightnessIcon != null) {
-                            mShowAutoBrightnessButton = mSystemSettings.getIntForUser(
-                                Settings.System.QS_SHOW_AUTO_BRIGHTNESS_BUTTON, 0,
-                                UserHandle.USER_CURRENT) == 1;
-                            mAutoBrightnessIcon.setVisibility(mShowAutoBrightnessButton
-                                    ? View.VISIBLE : View.GONE);
-                        }
+                        updateAutoBrightnessIconVisibility();
                         break;
                 }
             }
@@ -231,6 +228,7 @@ public class NotificationShadeWindowViewController {
             Settings.Secure.DOZE_TAP_SCREEN_GESTURE,
             contentObserver, UserHandle.USER_ALL);
         if (mAutoBrightnessConfigEnabled) {
+            updateAutoBrightnessIconVisibility();
             mSystemSettings.registerContentObserverForUser(
                 Settings.System.QS_SHOW_AUTO_BRIGHTNESS_BUTTON,
                 contentObserver, UserHandle.USER_ALL);
@@ -585,4 +583,13 @@ public class NotificationShadeWindowViewController {
         }
     }
 
+    private void updateAutoBrightnessIconVisibility() {
+        if (mAutoBrightnessIcon != null) {
+            mShowAutoBrightnessButton = mSystemSettings.getIntForUser(
+                Settings.System.QS_SHOW_AUTO_BRIGHTNESS_BUTTON, 0,
+                UserHandle.USER_CURRENT) == 1;
+            mAutoBrightnessIcon.setVisibility(mShowAutoBrightnessButton
+                    ? View.VISIBLE : View.GONE);
+        }
+    }
 }
