4f6e1e52380c9c70d5c11d8f3ebd15a24219a17d
SagarMakhar <sagarmakhar@gmail.com>
2021-12-21 15:34:11 +0000

    Squashed patches for UDFPS customizations
    * for future picking purposes
    * added missing authors
    
    * (@Stallix) - Adapt for A13 QPR1
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: daniml3 <danimoral1001@gmail.com>
    Co-authored-by: DennySPb <dennyspb@gmail.com>
    Co-authored-by: kondors1995 <normandija1945@gmail.com>
    Co-authored-by: AnierinB <anierinb@evolution-x.org>
    Co-authored-by: 00day0 <therandomuser11@gmail.com>
    Co-authored-by: Raaj52 <mail2raaj52@gmail.com>
    Co-authored-by: Alexandru Scurtu <sasha.scurtu@gmail.com>
    Co-authored-by: Anushek <anushekprasal@gmail.com>
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Hikari-no-Tenshi <Hikari-no-Tenshi@users.noreply.github.com>
    Co-authored-by: TheScarastic <warabhishek@gmail.com>
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>
    Co-authored-by: nullxception <nullxception@users.noreply.github.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Akito Mizukito <akito@evolution-x.org>
    Co-authored-by: Marco Rizzo <marco.rizzo.00@outlook.com>
    Co-authored-by: Varun Date <date.varun123@gmail.com>
    Co-authored-by: firebird11 <hbgassel@gmail.com>
    Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Co-authored-by: spkal01 <kalligeross@gmail.com>
    Co-authored-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6476,6 +6476,21 @@ public final class Settings {
         public static final String QS_FOOTER_SHOW_SERVICES = "qs_footer_show_services";
 
         /**
+         * @hide
+         */
+        public static final String UDFPS_ANIM = "udfps_anim";
+
+        /**
+         * @hide
+         */
+        public static final String UDFPS_ANIM_STYLE = "udfps_anim_style";
+
+        /**
+         * @hide
+         */
+        public static final String UDFPS_ICON = "udfps_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
