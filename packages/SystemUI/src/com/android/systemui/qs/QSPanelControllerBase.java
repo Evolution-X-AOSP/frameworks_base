c68e158fc85137f558841e9163ac4a1ea981606a
maxwen <max.weninger@gmail.com>
2021-12-23 00:12:37 +0100

    SystemUI: Add QS tile layout settings [1/2]
    @neobuddy89:
    * Removed unused configs and imports
    * De-omnify with TunerService and TileUtils
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
@@ -26,6 +26,7 @@ import android.content.res.Configuration;
 import android.content.res.Configuration.Orientation;
 import android.metrics.LogMaker;
 import android.util.Log;
+import android.provider.Settings;
 import android.view.View;
 
 import com.android.internal.annotations.VisibleForTesting;
@@ -41,6 +42,7 @@ import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.logging.QSLogger;
 import com.android.systemui.qs.tileimpl.QSTileViewImpl;
 import com.android.systemui.util.LargeScreenUtils;
+import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.ViewController;
 import com.android.systemui.util.animation.DisappearParameters;
 
@@ -62,8 +64,18 @@ import kotlin.jvm.functions.Function1;
  * @param <T> Type of QSPanel.
  */
 public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewController<T>
-        implements Dumpable{
+        implements Dumpable, TunerService.Tunable {
     private static final String TAG = "QSPanelControllerBase";
+
+    private static final String QS_TILE_VERTICAL_LAYOUT =
+            "system:" + Settings.System.QS_TILE_VERTICAL_LAYOUT;
+    private static final String QS_LAYOUT_COLUMNS =
+            "system:" + Settings.System.QS_LAYOUT_COLUMNS;
+    private static final String QS_LAYOUT_COLUMNS_LANDSCAPE =
+            "system:" + Settings.System.QS_LAYOUT_COLUMNS_LANDSCAPE;
+    private static final String QS_TILE_LABEL_HIDE =
+            "system:" + Settings.System.QS_TILE_LABEL_HIDE;
+
     protected final QSHost mHost;
     private final QSCustomizerController mQsCustomizerController;
     private final boolean mUsingMediaPlayer;
@@ -72,6 +84,7 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
     private final UiEventLogger mUiEventLogger;
     protected final QSLogger mQSLogger;
     private final DumpManager mDumpManager;
+    private final TunerService mTunerService;
     protected final ArrayList<TileRecord> mRecords = new ArrayList<>();
     protected boolean mShouldUseSplitNotificationShade;
 
@@ -109,6 +122,10 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
                     if (previousSplitShadeState != mShouldUseSplitNotificationShade) {
                         onSplitShadeChanged(mShouldUseSplitNotificationShade);
                     }
+                    if (mView.getTileLayout() != null) {
+                        mView.getTileLayout().updateSettings();
+                        setTiles();
+                    }
                 }
             };
 
@@ -138,7 +155,8 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
             MetricsLogger metricsLogger,
             UiEventLogger uiEventLogger,
             QSLogger qsLogger,
-            DumpManager dumpManager
+            DumpManager dumpManager,
+            TunerService tunerService
     ) {
         super(view);
         mHost = host;
@@ -149,6 +167,7 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
         mUiEventLogger = uiEventLogger;
         mQSLogger = qsLogger;
         mDumpManager = dumpManager;
+        mTunerService = tunerService;
         mShouldUseSplitNotificationShade =
                 LargeScreenUtils.shouldUseSplitNotificationShade(getResources());
     }
@@ -188,11 +207,18 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
         switchTileLayout(true);
 
         mDumpManager.registerDumpable(mView.getDumpableTag(), this);
+
+        mTunerService.addTunable(this, QS_TILE_VERTICAL_LAYOUT);
+        mTunerService.addTunable(this, QS_LAYOUT_COLUMNS);
+        mTunerService.addTunable(this, QS_LAYOUT_COLUMNS_LANDSCAPE);
+        mTunerService.addTunable(this, QS_TILE_LABEL_HIDE);
     }
 
     @Override
     protected void onViewDetached() {
         mQSLogger.logOnViewDetached(mLastOrientation, mView.getDumpableTag());
+        mTunerService.removeTunable(mView);
+
         mView.removeOnConfigurationChangedListener(mOnConfigurationChangedListener);
         mHost.removeCallback(mQSHostCallback);
 
@@ -478,6 +504,23 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
         // We have to prevent the media container position from moving during the transition to have
         // a smooth translation animation without stuttering.
         mView.setShouldMoveMediaOnExpansion(!isOnSplitShadeLockscreen);
+    }
+
+    @Override
+    public void onTuningChanged(String key, String newValue) {
+        switch (key) {
+            case QS_TILE_VERTICAL_LAYOUT:
+            case QS_LAYOUT_COLUMNS:
+            case QS_LAYOUT_COLUMNS_LANDSCAPE:
+            case QS_TILE_LABEL_HIDE:
+                if (mView.getTileLayout() != null) {
+                    mView.getTileLayout().updateSettings();
+                    setTiles();
+                }
+                break;
+            default:
+                break;
+         }
     }
 
     /** */
