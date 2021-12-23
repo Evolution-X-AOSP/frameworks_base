809164da2aba7a954409136a6ba7c4ae9e82abe8
Ido Ben-Hur <idoybh2@gmail.com>
2021-12-23 17:24:28 +0200

    base: Add a toggle for secure tiles on keyguard [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12376,6 +12376,13 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_LYRIC = "status_bar_show_lyric";
 
         /**
+         * Whether to show QS auto brightness toggle button
+         * @hide
+         */
+        @Readable
+        public static final String QSTILE_REQUIRES_UNLOCKING = "qstile_requires_unlocking";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
