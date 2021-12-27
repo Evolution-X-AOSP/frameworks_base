409a678abf051177f0a13a2099a2ab074bb17fb3
Nauval Rizky <enuma.alrizky@gmail.com>
2021-12-27 17:05:57 +0700

    SystemUI: Add support for GameSpace
    This is an addon for GameSpace for broadcasting about game start/stop.
    Additionally, it also has special option for suppressing fullscreen
    intent like incoming call.
    
    This also squash commits:
    - SystemUI: Improve GameSpace lifecycle and broadcast handling
    - GameSpaceManager: Handle various case against "locking screen" scenario
    - GameSpace: Fix issue with multiple users [neobuddy89]
    - SystemUI: Update gamespace for A13 [Genkzsz11]
    - GameSpaceManager: restrict broadcast to MANAGE_GAME_MODE holders only
    - GameSpaceManager: Update TaskStack listener usage
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -242,6 +242,7 @@ import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController.DeviceProvisionedListener;
 import com.android.systemui.statusbar.policy.ExtensionController;
 import com.android.systemui.statusbar.policy.FlashlightController;
+import com.android.systemui.statusbar.policy.GameSpaceManager;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.policy.UserInfoControllerImpl;
 import com.android.systemui.statusbar.policy.UserSwitcherController;
@@ -508,6 +509,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
     private final StatusBarSignalPolicy mStatusBarSignalPolicy;
     private final StatusBarHideIconsForBouncerManager mStatusBarHideIconsForBouncerManager;
     private final Lazy<LightRevealScrimViewModel> mLightRevealScrimViewModelLazy;
+
+    protected GameSpaceManager mGameSpaceManager;
 
     /** Controller for the Shade. */
     @VisibleForTesting
@@ -888,6 +891,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
 
         mActivityIntentHelper = new ActivityIntentHelper(mContext);
         mActivityLaunchAnimator = activityLaunchAnimator;
+        mGameSpaceManager = new GameSpaceManager(mContext, mKeyguardStateController);
 
         // The status bar background may need updating when the ongoing call status changes.
         mOngoingCallController.addCallback((animate) -> maybeUpdateBarMode());
@@ -1524,6 +1528,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_SCREEN_CAMERA_GESTURE);
         mBroadcastDispatcher.registerReceiver(mBroadcastReceiver, filter, null, UserHandle.ALL);
+        mGameSpaceManager.observe();
     }
 
     protected QS createDefaultQSFragment() {
@@ -4206,6 +4211,11 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
     @Override
     public boolean isKeyguardSecure() {
         return mStatusBarKeyguardViewManager.isSecure();
+    }
+
+    @Override
+    public GameSpaceManager getGameSpaceManager() {
+        return mGameSpaceManager;
     }
 
     // End Extra BaseStatusBarMethods.
