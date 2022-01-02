0b62221367b5435841d3c3c96e7a33cc55843166
jhonboy121 <alfredmathew05@gmail.com>
2022-01-02 18:09:39 +0530

    SystemUI: Refactor notification counter
    * Update icons from NotificationIconAreaController when settings change and set initial state from StatusBarIconController
    * Also use value of config_statusBarShowNumber as the default
    
    @neobuddy89: Add required NPE checks to properly apply settings on boot.
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -21,6 +21,8 @@ import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_WIFI
 import android.annotation.Nullable;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.ArraySet;
 import android.view.View;
@@ -360,6 +362,8 @@ public interface StatusBarIconController {
 
         protected ArrayList<String> mBlockList = new ArrayList<>();
 
+        private final boolean mShowNotificationCount;
+
         public IconManager(
                 ViewGroup group,
                 StatusBarLocation location,
@@ -370,6 +374,10 @@ public interface StatusBarIconController {
             mGroup = group;
             mMobileContextProvider = mobileContextProvider;
             mContext = group.getContext();
+            mShowNotificationCount = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUSBAR_NOTIF_COUNT,
+                mContext.getResources().getBoolean(R.bool.config_statusBarShowNumber) ? 1 : 0,
+                UserHandle.USER_CURRENT) == 1;
             mLocation = location;
 
             reloadDimens();
@@ -441,6 +449,7 @@ public interface StatusBarIconController {
         protected StatusBarIconView addIcon(int index, String slot, boolean blocked,
                 StatusBarIcon icon) {
             StatusBarIconView view = onCreateStatusBarIconView(slot, blocked);
+            view.setShowCount(mShowNotificationCount);
             view.set(icon);
             mGroup.addView(view, index, onCreateLayoutParams());
             return view;
