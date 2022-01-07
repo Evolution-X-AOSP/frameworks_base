e05a28c4fb25f7471ea41407a4ecf83c478b6e31
Vachounet <vachounet@live.fr>
2022-01-07 15:15:18 +0530

    base: Custom (User Selected) UDFPS Icon [1/2]
    SDB: forward port to A12 & adapt for new implementation of UDFPS Icon
    
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Change-Id: Ic889f516648a7da9cc4a836aa5c1c49c94617d50

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6776,6 +6776,16 @@ public final class Settings {
         public static final String QQS_LAYOUT = "qqs_layout";
 
         /**
+         * @hide
+         */
+        public static final String OMNI_CUSTOM_FP_ICON_ENABLED = "custom_fp_icon_enabled";
+
+        /**
+         * @hide
+         */
+        public static final String OMNI_CUSTOM_FP_ICON = "custom_fingerprint_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
