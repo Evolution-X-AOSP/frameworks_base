44bfea7894a7ab06bd09f1607637e23ad1e9dbbb
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2022-01-10 09:47:15 +0530

    base: Allow disabling ripple effect on unlock [1/2]
    neobuddy89:
    * Reverse logic. Enabled animation by default.
    
    ArrowOS:
    * Add keys into backup and validators.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Idecaa59547159d0a2b0ecdb55a207377bd466710

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5534,6 +5534,12 @@ public final class Settings {
         public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
 
         /**
+         * Whether to enable the ripple animation on fingerprint unlock
+         * @hide
+         */
+        public static final String ENABLE_RIPPLE_EFFECT = "enable_ripple_effect";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -89,5 +89,7 @@ public class SystemSettings {
         Settings.System.DISPLAY_COLOR_MODE,
         Settings.System.ALARM_ALERT,
         Settings.System.NOTIFICATION_LIGHT_PULSE,
+        // Evolution X additions
+        Settings.System.ENABLE_RIPPLE_EFFECT,
     };
 }
