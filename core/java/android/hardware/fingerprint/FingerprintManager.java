3ec430fecd58d4a3b979972f0241247611fcc2d9
alibei <sebastiankotzias83@gmail.com>
2022-01-25 20:25:04 +0100

    FingerprintManager: Fix NPE due to sensorProps
    in some cases, senorProps can be null. Add a check to get around this.
    
    Signed-off-by: alibei <sebastiankotzias83@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/hardware/fingerprint/FingerprintManager.java
+++ core/java/android/hardware/fingerprint/FingerprintManager.java
@@ -1033,7 +1033,10 @@ public class FingerprintManager implements BiometricAuthenticator, BiometricFing
      */
     public boolean isPowerbuttonFps() {
         final FingerprintSensorPropertiesInternal sensorProps = getFirstFingerprintSensor();
-        return sensorProps.sensorType == TYPE_POWER_BUTTON;
+        if (sensorProps != null) {
+            return sensorProps.sensorType == TYPE_POWER_BUTTON;
+        }
+        return false;
     }
 
     /**
