0741c447c0ad43f0d33ecd80b895dd191614e086
spezi77 <spezi7713@gmx.net>
2022-01-28 17:56:51 +0100

    Gesture nav: Add Partial Screenshot action
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: Ie8045e35ecb19e4c07113865c6a092859aab9746

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1384,6 +1384,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             case 14: // Powermenu
                 EvolutionUtils.showPowerMenu();
                 break;
+            case 15: // Partial Screenshot
+                EvolutionUtils.takeScreenshot(false);
+                break;
         }
     }
 
