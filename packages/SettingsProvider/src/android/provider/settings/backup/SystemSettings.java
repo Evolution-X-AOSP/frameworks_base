61c2dd34a9a2b6aa3137fa0b487164f96fa21411
Joey Huab <joey@evolution-x.org>
2022-02-17 14:44:52 +0900

    [SQUASH] Remove several broken stuff [1/2]
    Revert "ColorBucket: Add custom overlays category to ThemeOverlayManager [2/3]"
    
    This reverts commit ff30b92422d19c8052257dd8c4069e4c475e620d.
    
    Revert "base: Ambient Music: Force disable ambient music components"
    
    * Moved to vendor: https://github.com/Evolution-X/vendor_evolution/commit/f9473e871a35ccea44e00908d6bd67bfee098dfc.
    
    This reverts commit 5af2ee5c17ba91fb5fc7e2512206d74ac0f88863.

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -106,7 +106,6 @@ public class SystemSettings {
         Settings.System.QQS_SHOW_BRIGHTNESS,
         Settings.System.QS_BRIGHTNESS_POSITION_BOTTOM,
         Settings.System.QS_SHOW_AUTO_BRIGHTNESS_BUTTON,
-        Settings.System.COLOR_BUCKET_OVERLAY,
         Settings.System.ENABLE_FLOATING_ROTATION_BUTTON,
         Settings.System.STATUS_BAR_NOTIF_COUNT,
         Settings.System.LOCKSCREEN_SMALL_CLOCK,
