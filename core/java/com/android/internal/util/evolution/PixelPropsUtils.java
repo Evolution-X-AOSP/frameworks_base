8a1ae6f7d1c45f3e0bbbb1860c89dfea1d2d076f
Joey Huab <joey@evolution-x.org>
2022-02-18 12:47:58 +0900

    PixelPropsUtils: Update Raven to 2nd February 2022 Security patch fingerprint
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/com/android/internal/util/evolution/PixelPropsUtils.java
+++ core/java/com/android/internal/util/evolution/PixelPropsUtils.java
@@ -149,7 +149,7 @@ public class PixelPropsUtils {
         propsToChangePixel6.put("DEVICE", "raven");
         propsToChangePixel6.put("PRODUCT", "raven");
         propsToChangePixel6.put("MODEL", "Pixel 6 Pro");
-        propsToChangePixel6.put("FINGERPRINT", "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys");
+        propsToChangePixel6.put("FINGERPRINT", "google/raven/raven:12/SQ1D.220205.004/8151327:user/release-keys");
         propsToChangePixel5 = new HashMap<>();
         propsToChangePixel5.put("BRAND", "google");
         propsToChangePixel5.put("MANUFACTURER", "Google");
