adf7c2510b7ed421cc2c7e9673b4ac384ca39d34
spezi77 <spezi7713@gmx.net>
2022-02-25 12:34:25 +0100

    SystemUI: Remove unnecessary showVoLTEIcon config.
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -143,7 +143,6 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
     private ImsManager mImsManager;
     private FeatureConnector<ImsManager> mFeatureConnector;
     private int mCallState = TelephonyManager.CALL_STATE_IDLE;
-    private boolean mShowVolteIcon;
 
     private boolean mDataDisabledIcon;
     private boolean mRoamingIconAllowed;
@@ -193,8 +192,6 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         mLastState.networkNameData = mCurrentState.networkNameData = networkName;
         mLastState.enabled = mCurrentState.enabled = hasMobileData;
         mLastState.iconGroup = mCurrentState.iconGroup = mDefaultIcons;
-
-        mShowVolteIcon = mContext.getResources().getBoolean(R.bool.config_display_volte);
 
         int phoneId = mSubscriptionInfo.getSimSlotIndex();
         mFeatureConnector = new FeatureConnector(mContext, phoneId,
@@ -552,7 +549,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
             }
         } else if (mImsManager != null && mVoLTEicon > 0 && isVolteAvailable()) {
             switch (mVoLTEicon) {
- 	        // OnePlus
+                // OnePlus
                 case 1:
                     resId = R.drawable.ic_volte;
                     break;
@@ -1216,9 +1213,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
     private final BroadcastReceiver mVolteSwitchObserver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             Log.d(mTag, "action=" + intent.getAction());
-            if (mShowVolteIcon) {
-                notifyListeners();
-            }
+            notifyListeners();
         }
     };
 }
