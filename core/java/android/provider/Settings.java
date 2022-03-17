c604590db808a3fd19aa28070810718663d253b6
LibXZR <i@xzr.moe>
2022-03-17 22:58:19 +0800

    base: Add support for application downgrade [1/2]
    Can be useful in some cases.
    
    Change-Id: I999337cc55e9675cedde35191a6438d90744e5aa
    Signed-off-by: LibXZR <i@xzr.moe>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -17920,6 +17920,12 @@ public final class Settings {
          * @hide
          */
         public static final String RESTRICTED_NETWORKING_MODE = "restricted_networking_mode";
+
+        /**
+         * Control whether application downgrade is allowed.
+         * @hide
+         */
+        public static final String PM_DOWNGRADE_ALLOWED = "pm_downgrade_allowed";
     }
 
     /**
