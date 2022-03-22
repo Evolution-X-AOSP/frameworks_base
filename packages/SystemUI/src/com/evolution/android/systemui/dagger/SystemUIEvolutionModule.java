81168f76cff6d36f908806cf22137acb1543c55d
Anay Wadhera <anay1018@gmail.com>
2022-03-23 01:06:26 +0900

    SystemUI: catch up to 12L SystemUIDefaultModule
    Change-Id: Ib316311e2eef30a7dbc296a30397b143bbf35aab
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/7113
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>

--- packages/SystemUI/src/com/evolution/android/systemui/dagger/SystemUIEvolutionModule.java
+++ packages/SystemUI/src/com/evolution/android/systemui/dagger/SystemUIEvolutionModule.java
@@ -37,8 +37,7 @@ import com.android.systemui.plugins.qs.QSFactory;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.power.EnhancedEstimates;
 import com.android.systemui.power.EnhancedEstimatesImpl;
-import com.android.systemui.power.PowerUI;
-import com.android.systemui.power.PowerNotificationWarnings;
+import com.android.systemui.power.dagger.PowerModule;
 import com.android.systemui.qs.dagger.QSModule;
 import com.android.systemui.qs.tileimpl.QSFactoryImpl;
 import com.android.systemui.recents.Recents;
@@ -82,6 +81,7 @@ import javax.inject.Named;
 
 @Module(includes = {
         MediaModule.class,
+        PowerModule.class,
         QSModule.class,
         VolumeModule.class
 })
@@ -173,12 +173,6 @@ public abstract class SystemUIEvolutionModule {
             ConfigurationController configurationController) {
         return new HeadsUpManagerPhone(context, statusBarStateController, bypassController,
                 groupManager, configurationController);
-    }
-
-    @SysUISingleton
-    @Provides
-    static PowerUI.WarningsUI provideWarningsUi(PowerNotificationWarnings controllerImpl) {
-        return controllerImpl;
     }
 
     @Binds
