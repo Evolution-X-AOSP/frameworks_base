fc62d1d0ea4ae8bda9c6e7e53704f8cad04ff637
Pranav Vashi <neobuddy89@gmail.com>
2022-03-27 10:08:48 +0530

    Bring back QQS brightness slider [@marshmello61: forward port to Android 13]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -91,7 +91,7 @@ public class QSPanel extends LinearLayout implements Tunable {
 
     protected boolean mExpanded;
     protected boolean mListening;
-    private boolean mIsAutomaticBrightnessAvailable = false;
+    protected boolean mIsAutomaticBrightnessAvailable = false;
 
     @Nullable protected QSTileHost mHost;
     private final List<OnConfigurationChangedListener> mOnConfigurationChangedListeners =
@@ -718,7 +718,7 @@ public class QSPanel extends LinearLayout implements Tunable {
         mCollapseExpandAction = action;
     }
 
-    private void updateBrightnessSliderPosition() {
+    protected void updateBrightnessSliderPosition() {
         if (mBrightnessView == null) return;
         removeView(mBrightnessView);
         addView(mBrightnessView, mTop ? 0 : 1);
