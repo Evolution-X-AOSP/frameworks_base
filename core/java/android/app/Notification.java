e3f656e7b04f87172e6fa42cbd3e64040d35138d
maxwen <max.weninger@gmail.com>
2022-04-02 00:28:00 +0200

    base: notifications: set emphasized button bg to accentColor
    makes sense cause standard action buttons text color is also accentColor
    Also remove textAllCaps from action buttons in general.
    No other button in the notif shade has textAllCaps
    so it looks out of place
    
    Change-Id: I9678c1e6be52a37832e8eb435cb83f8db174645f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -6300,7 +6300,7 @@ public class Notification implements Parcelable
             if (emphasizedMode) {
                 // change the background bgColor
                 CharSequence title = action.title;
-                int buttonFillColor = getColors(p).getSecondaryAccentColor();
+                int buttonFillColor = getStandardActionColor(p);
                 if (tombstone) {
                     buttonFillColor = setAlphaComponentByFloatDimen(mContext,
                             ContrastColorUtil.resolveSecondaryColor(
