9c4f101919d6e2f4fe3b4a2c7635719fd9e05662
Sudheer Shanka <sudheersai@google.com>
2022-04-05 17:20:12 -0700

    Update getUidProcessState to consider mPendingStartActivityUids.
    Bug: 226299593
    Test: atest tests/cts/hostside/src/com/android/cts/net/HostsideRestrictBackgroundNetworkTests.java
    Change-Id: Ib923eafb35c3e315e49dc7a896b5d3dcb8c6f896
    Merged-In: Ib923eafb35c3e315e49dc7a896b5d3dcb8c6f896

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -6789,6 +6789,9 @@ public class ActivityManagerService extends IActivityManager.Stub
         }
 
         synchronized (mProcLock) {
+            if (mPendingStartActivityUids.isPendingTopUid(uid)) {
+                return PROCESS_STATE_TOP;
+            }
             return mProcessList.getUidProcStateLOSP(uid);
         }
     }
