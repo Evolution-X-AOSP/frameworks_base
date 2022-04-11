fa8e33a687dfc7166481d18e365ddec019375b2d
SKULSHADY <anushekprasal@gmail.com>
2022-04-11 17:56:30 +0900

    base: Make IME button space optional [1/2]
    [cjh1249131356 Edited]:
    - Expose hidden height instead of 0dp, so devices with rounded corner can adjust its height.
    - Hide left/right virtual buttons as well.
    
    Co-authored-by: BasharatKar <basharatkar1@gmail.com>
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6353,6 +6353,12 @@ public final class Settings {
         public static final String HIDE_LOCKSCREEN_STATUS_BAR = "hide_lockscreen_status_bar";
 
         /**
+         * Whether to show IME space
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_IME_SPACE = "navigation_bar_ime_space";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
