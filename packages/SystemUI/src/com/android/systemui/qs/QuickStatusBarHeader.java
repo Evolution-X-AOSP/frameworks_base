2873c4975f4449eac5af9080c87d6b94384301c9
703joko <703joko@gmail.com>
2022-04-16 13:56:50 +0800

    Battery Styles: Introduce Battery Landscape [1/2]
    Inspiration from Ancient ROM, thanks to Kakek for helps.
    
    arcana: adapted to 12L
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I4eabcaf1d70db2809beeb0a89051cb75fd320376
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -19,6 +19,8 @@ import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 import static com.android.systemui.battery.BatteryMeterView.BATTERY_STYLE_CIRCLE;
 import static com.android.systemui.battery.BatteryMeterView.BATTERY_STYLE_DOTTED_CIRCLE;
 import static com.android.systemui.battery.BatteryMeterView.BATTERY_STYLE_FULL_CIRCLE;
+import static com.android.systemui.battery.BatteryMeterView.BATTERY_STYLE_RLANDSCAPE;
+import static com.android.systemui.battery.BatteryMeterView.BATTERY_STYLE_LANDSCAPE;
 
 import android.content.Context;
 import android.content.Intent;
@@ -353,7 +355,9 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
             }
             if (mBatteryRemainingIcon.getBatteryStyle() == BATTERY_STYLE_CIRCLE
                 || mBatteryRemainingIcon.getBatteryStyle() == BATTERY_STYLE_DOTTED_CIRCLE
-                || mBatteryRemainingIcon.getBatteryStyle() == BATTERY_STYLE_FULL_CIRCLE) {
+                || mBatteryRemainingIcon.getBatteryStyle() == BATTERY_STYLE_FULL_CIRCLE
+                || mBatteryRemainingIcon.getBatteryStyle() == BATTERY_STYLE_RLANDSCAPE
+                || mBatteryRemainingIcon.getBatteryStyle() == BATTERY_STYLE_LANDSCAPE) {
                 textColorSecondary = Utils.getColorAttrDefaultColor(mContext,
                         android.R.attr.textColorHint);
             }
