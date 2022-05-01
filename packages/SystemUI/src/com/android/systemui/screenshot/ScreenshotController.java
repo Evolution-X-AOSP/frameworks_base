5f56c7c48919cb5a627c011b47089a4e714e174d
LuK1337 <priv.luk@gmail.com>
2022-05-01 21:17:28 +0200

    SystemUI: Let screenshot window ignore screen decorations
    This lets us select whole screen during partial screenshot.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/4631
    Change-Id: Ia309552c7ea6b0b834bb8bddb8f83117c1870261

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -388,6 +388,7 @@ public class ScreenshotController {
         mWindow.requestFeature(Window.FEATURE_NO_TITLE);
         mWindow.requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS);
         mWindow.setBackgroundDrawableResource(android.R.color.transparent);
+        mWindow.setDecorFitsSystemWindows(false);
 
         mConfigChanges.applyNewConfig(context.getResources());
         reloadAssets();
