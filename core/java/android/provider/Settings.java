d6692c4200e348ea628fab7ec0778bb32b268937
June <june@eridan.me>
2022-05-02 02:09:23 -0400

    fwb: Add toggle for screenshot timestamp EXIF metadata [1/2]
    Signed-off-by: June <june@eridan.me>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11412,6 +11412,25 @@ public final class Settings {
         public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
 
         /**
+         * Toggle to control adding a timestamp to screenshot EXIF
+         * @hide
+         */
+        public static final String SCREENSHOT_TIMESTAMP_EXIF =
+                "screenshot_timestamp_exif";
+
+        /**
+         * Enables adding a timestamp to screenshot EXIF
+         * @hide
+         */
+        public static final int ENABLE_SCREENSHOT_TIMESTAMP_EXIF = 1;
+
+        /**
+         * Disables adding a timestamp to screenshot EXIF
+         * @hide
+         */
+        public static final int DISABLE_SCREENSHOT_TIMESTAMP_EXIF = 0;
+
+        /**
          * Whether or not to enable media resumption
          * When enabled, media controls in quick settings will populate on boot and persist if
          * resumable via a MediaBrowserService.
