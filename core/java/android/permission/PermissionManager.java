37ed33474624d3cfaae61dea6add0b38a54f0428
Timi Rautamäki <timi.rautamaki@gmail.com>
2022-05-04 12:31:56 +0000

    PermissionManager: fix NPE in getIndicatorExemptedPackages
    sLocationProviderPkgNames, sLocationExtraPkgNames and sExemptedPkgNames can be null
    because they're static.
    Also rename them from member to static variable to follow code style.
    
    Change-Id: I7d08e02047ccf183cc75ce18b3ebd2086b2831b3

--- core/java/android/permission/PermissionManager.java
+++ core/java/android/permission/PermissionManager.java
@@ -145,9 +145,9 @@ public final class PermissionManager {
 
     private List<SplitPermissionInfo> mSplitPermissionInfos;
 
-    private static String[] mLocationProviderPkgNames;
-    private static String[] mLocationExtraPkgNames;
-    private static String[] mExemptedPkgNames;
+    private static String[] sLocationProviderPkgNames;
+    private static String[] sLocationExtraPkgNames;
+    private static String[] sExemptedPkgNames;
 
     /**
      * Creates a new instance.
@@ -163,13 +163,6 @@ public final class PermissionManager {
         mPermissionManager = IPermissionManager.Stub.asInterface(ServiceManager.getServiceOrThrow(
                 "permissionmgr"));
         mLegacyPermissionManager = context.getSystemService(LegacyPermissionManager.class);
-
-        mLocationProviderPkgNames = context.getResources().getStringArray(
-                R.array.config_locationProviderPackageNames);
-        mLocationExtraPkgNames = context.getResources().getStringArray(
-                R.array.config_locationExtraPackageNames);
-        mExemptedPkgNames = context.getResources().getStringArray(
-                R.array.config_indicatorExemptedPackageNames);
     }
 
     /**
@@ -957,17 +950,17 @@ public final class PermissionManager {
                 pkgNames.add(exemptedPackage);
             }
         }
-        for (String pkgName: mLocationProviderPkgNames) {
+        for (String pkgName: sLocationProviderPkgNames) {
             if (pkgName != null) {
                 pkgNames.add(pkgName);
             }
         }
-        for (String pkgName: mLocationExtraPkgNames) {
+        for (String pkgName: sLocationExtraPkgNames) {
             if (pkgName != null) {
                 pkgNames.add(pkgName);
             }
         }
-        for (String pkgName: mExemptedPkgNames) {
+        for (String pkgName: sExemptedPkgNames) {
             if (pkgName != null) {
                 pkgNames.add(pkgName);
             }
@@ -987,6 +980,12 @@ public final class PermissionManager {
             for (int i = 0; i < EXEMPTED_ROLES.length; i++) {
                 INDICATOR_EXEMPTED_PACKAGES[i] = context.getString(EXEMPTED_ROLES[i]);
             }
+            sLocationProviderPkgNames = context.getResources().getStringArray(
+                    R.array.config_locationProviderPackageNames);
+            sLocationExtraPkgNames = context.getResources().getStringArray(
+                    R.array.config_locationExtraPackageNames);
+            sExemptedPkgNames = context.getResources().getStringArray(
+                    R.array.config_indicatorExemptedPackageNames);
         }
     }
     /**
