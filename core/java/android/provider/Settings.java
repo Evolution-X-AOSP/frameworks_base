43a403cd9e8be178d443b6b52d30afb23eaf2966
LuK1337 <priv.luk@gmail.com>
2022-05-06 21:26:57 +0900

    core: Add ENABLE_TASKBAR setting

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6510,6 +6510,12 @@ public final class Settings {
         public static final String SWIPE_TO_SCREENSHOT = "swipe_to_screenshot";
 
         /**
+         * Whether to enable taskbar.
+         * @hide
+         */
+        public static final String ENABLE_TASKBAR = "enable_taskbar";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
