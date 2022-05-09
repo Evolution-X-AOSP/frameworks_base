08ed6584cd1fce5d34c4f374d8124908b9657b46
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2022-05-09 09:59:44 +0530

    Add show recents option [1/2]
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -62,6 +62,7 @@ import android.view.WindowManager;
 import android.view.WindowMetrics;
 
 import com.android.internal.util.evolution.EvolutionUtils;
+import com.android.internal.util.hwkeys.ActionHandler;
 import com.android.internal.util.hwkeys.ActionUtils;
 import com.android.internal.config.sysui.SystemUiDeviceConfigFlags;
 import com.android.internal.policy.GestureNavigationSettingsObserver;
@@ -1053,6 +1054,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                 break;
             case 18: // Hush Mute
                 ActionUtils.triggerHushMute(mContext);
+                break;
+            case 19: // Recent apps
+                ActionHandler.toggleRecentApps();
                 break;
         }
     }
