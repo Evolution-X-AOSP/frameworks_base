83471c791e9c34db5a5590216dc13698071aabbd
Pranav Vashi <neobuddy89@gmail.com>
2022-05-19 08:02:41 +0530

    Remove Google games dashboard integration
    * We do not want two different game modes.
      Also, it starts automatically even when disabled.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/org/pixelexperience/systemui/dagger/SystemUIGoogleBinder.java
+++ packages/SystemUI/src/org/pixelexperience/systemui/dagger/SystemUIGoogleBinder.java
@@ -48,7 +48,6 @@ import com.evolution.android.systemui.theme.EvolutionThemeOverlayController;
 
 import org.pixelexperience.systemui.GoogleServices;
 import org.pixelexperience.systemui.columbus.ColumbusTargetRequestServiceWrapper;
-import org.pixelexperience.systemui.gamedashboard.GameMenuActivityWrapper;
 import org.pixelexperience.systemui.statusbar.dagger.StatusBarGoogleModule;
 
 import dagger.Binds;
@@ -243,15 +242,7 @@ public abstract class SystemUIGoogleBinder {
     public abstract SystemUI bindGoogleServices(GoogleServices sysui);
 
     /**
-     * Inject into GameMenuActivity.
-     */
-    @Binds
-    @IntoMap
-    @ClassKey(GameMenuActivityWrapper.class)
-    public abstract Activity bindGameMenuActivity(GameMenuActivityWrapper activity);
-
-    /**
-     * Inject into GameMenuActivity.
+     * Inject into ColumbusTargetRequestServiceWrapper.
      */
     @Binds
     @IntoMap
