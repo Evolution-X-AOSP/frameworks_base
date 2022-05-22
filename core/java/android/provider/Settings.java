f067e42251eb425b5b1162e281905e49bd57ad6f
bigrushdog <randall.rushing@gmail.com>
2022-05-22 01:18:36 -0700

    [SQUASH] base: Add Battery Bar [1/2]
    I'd like to thank and show respect to the following contributors for this
    fantastic feature
    
    Lokesh Chamane <lokesh.chamane@pac-rom.com>
    SpiritCroc <spiritcroc@gmail.com>
    cphelps76 <cphelps76@droidconcepts.com>
    
    commit b6c364e1a83734391f11830a2a77b87d8a125df2
    Author: Lokesh Chamane <lokesh.chamane@pac-rom.com>
    Date:   Sat May 14 20:31:42 2016 -0400
    
        BatteryBar: Gradient color [1/2]
    
        Change-Id: I1c18e5e57d77faac8d347a751f8a9d2e63f97517
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit 3c707ae49fc278a3224a399b88df036080c5d616
    Author: SpiritCroc <spiritcroc@gmail.com>
    Date:   Mon May 2 11:47:57 2016 +0200
    
        Battery bar enhancements [1/2]
    
        Add color settings for charging & low battery
        Add reversed alignment setting
    
        Change-Id: I0ff3831d046689a30941718e4e9e4d301a65ff53
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit 23599237626f3782a65aeb15e8b7bc2f8a81c116
    Author: cphelps76 <cphelps76@droidconcepts.com>
    Date:   Wed Mar 19 16:23:35 2014 +0000
    
        base: Add back battery bar [1/2]
    
        {x} Adds Battery bar with following features:
        {x} aligment
        {x} thickness
        {x} charging animation
    
        @mydongistiny: updated for oreo
        Change-Id: Ie9a168121dc569ec3af068a4bda6b46a4e896b2d
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    Change-Id: I5e54fecae3fe8ef4b8cd055d9305ed2c07f2bd3e
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6532,6 +6532,57 @@ public final class Settings {
         public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
 
         /**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String BATTERY_BAR_LOCATION = "battery_bar_location";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_COLOR = "battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_THICKNESS = "battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_STYLE = "battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_ANIMATE = "battery_bar_animate";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_CHARGING_COLOR = "battery_bar_charging_color";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_BATTERY_LOW_COLOR_WARNING = "battery_bar_battery_low_color_warning";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_HIGH_COLOR = "battery_bar_high_color";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_LOW_COLOR = "battery_bar_low_color";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_USE_GRADIENT_COLOR = "battery_bar_use_gradient_color";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
