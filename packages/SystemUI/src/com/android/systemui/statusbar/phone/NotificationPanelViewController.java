39bba40f9bac9ae8e7ce3511cb39406561744dd7
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-05-26 18:39:27 +0530

    SystemUI: Re-inflate keyguard statusbar on theme change
    Fixes lockscreen statusbar icons not refreshing on changing icon pack.
    Inspired by https://github.com/AOSPA/android_frameworks_base/commit/20dc3a97ffbce00f26ff2d96f027eefb826877ee
    
    Change-Id: I1d004c5a54fad5231dfe04e1d134a28293993d6d
    Reviewed-on: https://review.statixos.com/c/android_frameworks_base/+/7832
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -906,13 +906,6 @@ public class NotificationPanelViewController extends PanelViewController {
             }
         }
 
-        mKeyguardStatusBarViewController =
-                mKeyguardStatusBarViewComponentFactory.build(
-                        mKeyguardStatusBar,
-                        mNotificationPanelViewStateProvider)
-                        .getKeyguardStatusBarViewController();
-        mKeyguardStatusBarViewController.init();
-
         mNotificationContainerParent = mView.findViewById(R.id.notification_container_parent);
         updateViewControllers(
                 mView.findViewById(R.id.keyguard_status_view),
@@ -1021,6 +1014,13 @@ public class NotificationPanelViewController extends PanelViewController {
                 mKeyguardStatusViewComponentFactory.build(keyguardStatusView);
         mKeyguardStatusViewController = statusViewComponent.getKeyguardStatusViewController();
         mKeyguardStatusViewController.init();
+
+        mKeyguardStatusBarViewController =
+                mKeyguardStatusBarViewComponentFactory.build(
+                        mKeyguardStatusBar,
+                        mNotificationPanelViewStateProvider)
+                        .getKeyguardStatusBarViewController();
+        mKeyguardStatusBarViewController.init();
 
         if (mKeyguardUserSwitcherController != null) {
             // Try to close the switcher so that callbacks are triggered if necessary.
@@ -1157,6 +1157,15 @@ public class NotificationPanelViewController extends PanelViewController {
                 R.layout.keyguard_status_view, mNotificationContainerParent, false);
         mKeyguardStatusView = keyguardStatusView;
         mNotificationContainerParent.addView(keyguardStatusView, statusIndex);
+
+        // Re-inflate the keyguard status bar.
+        statusIndex = mView.indexOfChild(mKeyguardStatusBar);
+        mView.removeView(mKeyguardStatusBar);
+        mKeyguardStatusBar = (KeyguardStatusBarView) mLayoutInflater.inflate(
+                R.layout.keyguard_status_bar, mView, false);
+        mView.addView(mKeyguardStatusBar);
+        mKeyguardStatusBar.setVisibility(isOnKeyguard() ? View.VISIBLE : View.INVISIBLE);
+
         // When it's reinflated, this is centered by default. If it shouldn't be, this will update
         // below when resources are updated.
         mStatusViewCentered = true;
