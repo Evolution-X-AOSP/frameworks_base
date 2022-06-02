3ed74218fee6d32f6303366ae19f5671f4474575
cjh1249131356 <cjh1249131356@gmail.com>
2022-06-03 01:13:40 +0800

    base: Rework lock gesture feature
    Match OEM behavior:
    - Lock swipe gestures from bottom, left & right side
    - Require double swipe to unlock gestures
    - Auto relock gestures after intervals
    - Avoid showing NavigationBar / TaskBar when swiping from status bar / left & right side
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -1945,6 +1945,11 @@ public class DisplayPolicy {
         final Resources res = getCurrentUserResources();
         final int portraitRotation = displayRotation.getPortraitRotation();
 
+        final InsetsPolicy policy = mDisplayContent.getInsetsPolicy();
+        if (policy != null) {
+            policy.updateLockedStatus();
+        }
+
         if (hasStatusBar()) {
             mDisplayCutoutTouchableRegionSize = res.getDimensionPixelSize(
                     R.dimen.display_cutout_touchable_region_size);
@@ -2307,7 +2312,7 @@ public class DisplayPolicy {
         if (controlTarget.canShowTransient()) {
             // Show transient bars if they are hidden; restore position if they are visible.
             mDisplayContent.getInsetsPolicy().showTransient(SHOW_TYPES_FOR_SWIPE,
-                    isGestureOnSystemBar);
+                    isGestureOnSystemBar, swipeTarget == mStatusBar);
             controlTarget.showInsets(restorePositionTypes, false);
         } else {
             // Restore visibilities and positions of system bars.
@@ -3000,5 +3005,9 @@ public class DisplayPolicy {
      */
     boolean shouldAttachNavBarToAppDuringTransition() {
         return mShouldAttachNavBarToAppDuringTransition && mNavigationBar != null;
+    }
+
+    public Context getUiContext() {
+        return mUiContext;
     }
 }
