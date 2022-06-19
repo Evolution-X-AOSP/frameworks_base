789121bcef588df730e816ba33ad8bcbd759b1da
minarypenguin <alexfinhart@gmail.com>
2022-06-19 08:04:20 +0800

    base: Do not reset keyguard going away state
    - Fixes the flicker on turning of the screen when udfps is enabled
    
    Co-authored-by: Blaster4385 <venkateshchaturvedi12@gmail.com>
    Signed-off-by: minarypenguin <alexfinhart@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -100,6 +100,7 @@ import com.android.internal.policy.IKeyguardExitCallback;
 import com.android.internal.policy.IKeyguardStateCallback;
 import com.android.internal.policy.ScreenDecorationsUtils;
 import com.android.internal.util.LatencyTracker;
+import com.android.internal.util.evolution.udfps.UdfpsUtils;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.keyguard.KeyguardConstants;
 import com.android.keyguard.KeyguardDisplayManager;
@@ -1349,7 +1350,10 @@ public class KeyguardViewMediator extends CoreStartable implements Dumpable,
         // explicitly DO NOT want to call
         // mKeyguardViewControllerLazy.get().setKeyguardGoingAwayState(false)
         // here, since that will mess with the device lock state.
-        mUpdateMonitor.dispatchKeyguardGoingAway(false);
+        boolean isUdfps = deviceHasUdfps();
+        if (!isUdfps) {
+            mUpdateMonitor.dispatchKeyguardGoingAway(false);
+        }
 
         notifyStartedGoingToSleep();
     }
@@ -3337,5 +3341,9 @@ public class KeyguardViewMediator extends CoreStartable implements Dumpable,
 
             mInteractionJankMonitor.cancel(CUJ_LOCKSCREEN_OCCLUSION);
         }
+    }
+
+    private boolean deviceHasUdfps() {
+        return UdfpsUtils.hasUdfpsSupport(mContext);
     }
 }
