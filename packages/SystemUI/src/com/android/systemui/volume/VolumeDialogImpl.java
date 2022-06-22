c4ff05a46dcbf3099f9329f39a20646cb638a4ac
Christian Hoffmann <chrmhoffmann@gmail.com>
2022-06-22 21:50:42 +0200

    SystemUI: Avoid NPE in volume dialog
    Change-Id: I1bc6d46de01ddd4db6b32555e5557611550d007e

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -1641,7 +1641,9 @@ public class VolumeDialogImpl implements VolumeDialog,
                     mDialog.dismiss();
                     tryToRemoveCaptionsTooltip();
                     mExpanded = false;
-                    mExpandRows.setExpanded(mExpanded);
+                    if (mExpandRows != null) {
+                        mExpandRows.setExpanded(mExpanded);
+                    }
                     mAnimatingRows = 0;
                     mDefaultRow = null;
                     mIsAnimatingDismiss = false;
