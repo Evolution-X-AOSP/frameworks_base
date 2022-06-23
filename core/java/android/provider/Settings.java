1a4a49365c9e10f3bab0cd9c3e93290b53e96e6d
Kristian Angelov <kristiana2000@abv.bg>
2022-06-23 03:49:21 +0300

    Wallpaper: Make ImageWallpaper zooming tunable
    For some reason Google decided that permanently zooming the wallpaper by 10%
    and ruining its' quality in order to show a "zoom out depth animation" was a
    good design choice.
    Make this user configurable in order to get our pixels back.
    
    Change-Id: Ifbd50ea250b1a3ffb83b6fe7ba54e92939caeaa1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6691,6 +6691,13 @@ public final class Settings {
         public static final String CLICK_PARTIAL_SCREENSHOT = "click_partial_screenshot";
 
         /**
+         *  Toggle the wallpaper zoom effects
+         *  0 = 0ff, 1 = on
+         * @hide
+         */
+        public static final String DISPLAY_SETTINGS_WALLPAPER_ZOOM = "display_settings_wallpaper_zoom";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
