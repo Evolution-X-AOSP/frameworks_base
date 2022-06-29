4ea6312c3c196836b49101ad9565fe13edab049f
althafvly <althafvly@gmail.com>
2022-06-29 15:02:37 +0530

    Keyguard: Allow user configurable fingerprint wake-and-unlock
    Change-Id: I471908cfa27a54e0075394eed348cb15386f62ce
    (cherry picked from commit 598811ae53255de1a643c7b5950ec60bcc7136b3)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5627,6 +5627,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
         /**
+         * Whether to enable fingerprint wake-and-unlock.
+         * @hide
+         */
+        public static final String FINGERPRINT_WAKE_UNLOCK = "fingerprint_wake_unlock";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
