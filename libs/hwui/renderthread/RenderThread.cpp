ef869b5bfc054015a26e5aa44e675cc4f0106db5
Sergey Vasiliev <sdv22@live.ru>
2022-07-11 09:00:57 +0800

    hwui: Increase thread prio
    Change-Id: I908516edb79e84e28a8ef6455f489aba33012bbb

--- libs/hwui/renderthread/RenderThread.cpp
+++ libs/hwui/renderthread/RenderThread.cpp
@@ -448,7 +448,7 @@ void RenderThread::requestVsync() {
 }
 
 bool RenderThread::threadLoop() {
-    setpriority(PRIO_PROCESS, 0, PRIORITY_DISPLAY);
+    setpriority(PRIO_PROCESS, 0, PRIORITY_URGENT_DISPLAY);
     Looper::setForThread(mLooper);
     if (gOnStartHook) {
         gOnStartHook("RenderThread");
