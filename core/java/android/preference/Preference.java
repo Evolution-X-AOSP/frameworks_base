622915758036f9bb968f053d7bde79986a8a71ac
Joey Huab <joey@evolution-x.org>
2022-07-12 06:36:22 +0000

    [SQUASH] Revert several commits
    Revert "SystemUI: use google sans font for lockscreen clock"
    
    This reverts commit b95555c7eed42bed6095d210f385718dd51a9ae7.
    
    Revert "base: Smoother Upload and Download Animation"
    
    This reverts commit 51d992223b08c8dd22e5dad14794f7208948e1a4.
    
    Revert "Factor out preference-in-storage check to overridable method."
    
    This reverts commit 5026ec615ba9f2fa77d26afad79593fdce52ac8a.

--- core/java/android/preference/Preference.java
+++ core/java/android/preference/Preference.java
@@ -1575,14 +1575,6 @@ public class Preference implements Comparable<Preference> {
         mDefaultValue = defaultValue;
     }
 
-    /**
-     * Returns whether the preference can be found in persistent storage
-     * @hide
-     */
-    protected boolean isPersisted() {
-        return getSharedPreferences().contains(mKey);
-    }
-
     private void dispatchSetInitialValue() {
         if (getPreferenceDataStore() != null) {
             onSetInitialValue(true, mDefaultValue);
@@ -1591,7 +1583,7 @@ public class Preference implements Comparable<Preference> {
 
         // By now, we know if we are persistent.
         final boolean shouldPersist = shouldPersist();
-        if (!shouldPersist || !isPersisted()) {
+        if (!shouldPersist || !getSharedPreferences().contains(mKey)) {
             if (mDefaultValue != null) {
                 onSetInitialValue(false, mDefaultValue);
             }
