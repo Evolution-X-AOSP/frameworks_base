b5d4fa402526c616b3fae708b2cb4820ca0d8f1a
Joey Huab <joey@evolution-x.org>
2022-07-28 05:44:03 +0000

    BatteryService: Switch Warp charging status path to an overlay
    * Allows to support both pre oplus and post oplus rebase paths
    * Based on f557a85

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -175,6 +175,7 @@ public final class BatteryService extends SystemService {
     private boolean mWarpCharger;
     private boolean mHasWarpCharger;
     private boolean mLastWarpCharger;
+    private String mOpWarpChargerPath;
 
     private long mDischargeStartTime;
     private int mDischargeStartLevel;
@@ -801,8 +802,9 @@ public final class BatteryService extends SystemService {
     }
 
     private boolean isWarpCharger() {
+        mOpWarpChargerPath = mContext.getResources().getString(com.android.internal.R.string.config_opWarpChargerStatusPaths);
         try {
-            FileReader file = new FileReader("/sys/class/power_supply/battery/fastchg_status");
+            FileReader file = new FileReader(mOpWarpChargerPath);
             BufferedReader br = new BufferedReader(file);
             String state = br.readLine();
             br.close();
