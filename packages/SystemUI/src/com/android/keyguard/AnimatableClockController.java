8faba6989655585d5f185663fed1fd4c17f4375f
strwbrry143 <alexfinhart@gmail.com>
2022-08-05 14:14:04 +0800

    AnimatableClockController: Sync with fonts overlay changes
    Signed-off-by: strwbrry143 <alexfinhart@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/AnimatableClockController.java
+++ packages/SystemUI/src/com/android/keyguard/AnimatableClockController.java
@@ -212,8 +212,8 @@ public class AnimatableClockController extends ViewController<AnimatableClockVie
              || font.equalsIgnoreCase("bariol")
              || font.equalsIgnoreCase("comfortaa")
              || font.equalsIgnoreCase("coolstory")
-             || font.equalsIgnoreCase("jtleonor-bold")
-             || font.equalsIgnoreCase("linotte-bold")
+             || font.equalsIgnoreCase("jtleonor")
+             || font.equalsIgnoreCase("linotte")
              || font.equalsIgnoreCase("misans")
              || font.equalsIgnoreCase("nokiapure")) {
           return true;
