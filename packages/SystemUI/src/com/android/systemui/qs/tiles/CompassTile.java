12dffff17dee14f8295f9f65eff99f3523e455cd
timjosten <timjosten@gmail.com>
2022-08-18 18:10:47 +0000

    SystemUI: Fix QS compass tile spam & instability
    E ViewRootImpl: Accessibility content change on non-UI thread. Future Android versions will throw an exception.
    E ViewRootImpl: android.view.ViewRootImpl$CalledFromWrongThreadException: Only the original thread that created a view hierarchy can touch its views.
    E ViewRootImpl:  at android.view.ViewRootImpl$SendWindowContentChangedAccessibilityEvent.runOrPost(ViewRootImpl.java:10375)
    E ViewRootImpl:  at android.view.ViewRootImpl.postSendWindowContentChangedCallback(ViewRootImpl.java:9203)
    E ViewRootImpl:  at android.view.ViewRootImpl.notifySubtreeAccessibilityStateChanged(ViewRootImpl.java:9383)
    E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
    E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
    E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
    E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
    E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
    E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
    E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
    E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
    E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
    E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
    E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
    E ViewRootImpl:  at android.view.View.notifySubtreeAccessibilityStateChangedIfNeeded(View.java:14002)
    E ViewRootImpl:  at android.view.View.setRotation(View.java:17015)
    ...
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CompassTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CompassTile.java
@@ -64,6 +64,7 @@ public class CompassTile extends QSTileImpl<BooleanState> implements SensorEvent
 
     private ImageView mImage;
     private boolean mListeningSensors;
+    private Handler mHandler;
 
     @Inject
     public CompassTile(QSHost host,
@@ -77,6 +78,7 @@ public class CompassTile extends QSTileImpl<BooleanState> implements SensorEvent
     ) {
         super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
                 statusBarStateController, activityStarter, qsLogger);
+        mHandler = mainHandler;
         mSensorManager = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
         mAccelerationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mGeomagneticFieldSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
@@ -141,7 +143,7 @@ public class CompassTile extends QSTileImpl<BooleanState> implements SensorEvent
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
-        Float degrees = arg == null ? 0 :(float) arg;
+        final Float degrees = arg == null ? 0 : (Float) arg;
 
         state.value = mActive;
         state.icon = ResourceIcon.get(R.drawable.ic_qs_compass);
@@ -150,23 +152,28 @@ public class CompassTile extends QSTileImpl<BooleanState> implements SensorEvent
             state.state = Tile.STATE_ACTIVE;
             if (arg != null) {
                 state.label = formatValueWithCardinalDirection(degrees);
-
-                float target = 360 - degrees;
-                float relative = target - mImage.getRotation();
-                if (relative > 180) relative -= 360;
-
-                mImage.setRotation(mImage.getRotation() + relative / 2);
-
+                mHandler.post(() -> {
+                    if (mImage == null)
+                        return;
+                    float target = 360 - degrees;
+                    float relative = target - mImage.getRotation();
+                    if (relative > 180) relative -= 360;
+                    mImage.setRotation(mImage.getRotation() + relative / 2);
+                });
             } else {
                 state.label = mContext.getString(R.string.quick_settings_compass_init);
-                mImage.setRotation(0);
+                mHandler.post(() -> {
+                    if (mImage != null)
+                        mImage.setRotation(0);
+                });
             }
         } else {
             state.label = mContext.getString(R.string.quick_settings_compass_label);
             state.state = Tile.STATE_INACTIVE;
-            if (mImage != null) {
-                mImage.setRotation(0);
-            }
+            mHandler.post(() -> {
+                if (mImage != null)
+                    mImage.setRotation(0);
+            });
         }
     }
 
