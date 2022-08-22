e7848b40fc5cb9cfdff2870a447f589dfacc1984
Joey Huab <joey@evolution-x.org>
2022-08-22 22:14:40 +0000

    SystemUI: VoLTE changes
    StatusBarImsView: Adjust onDarkChanged
    
    * After 6c5ebb3.
    
    MobileSignalController: Adapt connectionReady changes
    
    * After https://android.googlesource.com/platform/frameworks/opt/net/ims/+/a93a7c4931a6da026d3c369297f3803ababfebaf.

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -271,7 +271,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
                 new ArrayList<Integer>(ImsFeature.STATE_LOG_MAP.keySet()),
                 new FeatureConnector.Listener<ImsManager> () {
                     @Override
-                    public void connectionReady(ImsManager manager)
+                    public void connectionReady(ImsManager manager, int subId)
                             throws ImsException {
                         Log.d(mTag, "ImsManager: connection ready.");
                         mImsManager = manager;
