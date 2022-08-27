2a343b9993a14af3ce774a82a0640211a30ef75d
jhonboy121 <alfredmathew05@gmail.com>
2022-08-27 12:30:58 +0000

    fixup! base: Add some static custom utilities
    [jhonboy121]: adapt to A12.1 and A13, simplify some code

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesCommandQueueCallbacks.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesCommandQueueCallbacks.java
@@ -605,6 +605,9 @@ public class CentralSurfacesCommandQueueCallbacks implements CommandQueue.Callba
 
     @Override
     public void toggleCameraFlash() {
+        if (CentralSurfaces.DEBUG) {
+            Log.d(CentralSurfaces.TAG, "Toggling camera flashlight");
+        }
         if (mFlashlightController.isAvailable()) {
             mFlashlightController.setFlashlight(!mFlashlightController.isEnabled());
         }
