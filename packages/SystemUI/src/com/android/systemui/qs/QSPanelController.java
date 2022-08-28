17fe27dc306b4bcc70ba307eaa93eed324e920b3
Pranav Vashi <neobuddy89@gmail.com>
2022-08-28 14:25:01 +0530

    SystemUI: Update and fix QS tile layout settings
    * Do not let every tom, dick and harry set max columns.
    * On config changes and settings update, check if dynamic row and columns update.
    * Update resources in tile layout when orientation changes to get proper tiles in landscape.
    * Request layout changes whenever column update happens.
    * Be smart in calculating how many max tiles should Quick QS show depending on no. of columns.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanelController.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanelController.java
@@ -114,6 +114,8 @@ public class QSPanelController extends QSPanelControllerBase<QSPanel> {
         mTunerService.addTunable(mView, QSPanel.QS_SHOW_BRIGHTNESS_SLIDER);
         mTunerService.addTunable(mView, QSPanel.QS_SHOW_AUTO_BRIGHTNESS);
         mTunerService.addTunable(mView, QSPanel.QS_BRIGHTNESS_SLIDER_POSITION);
+        mTunerService.addTunable(mView, QSPanel.QS_LAYOUT_COLUMNS);
+        mTunerService.addTunable(mView, QSPanel.QS_LAYOUT_COLUMNS_LANDSCAPE);
 
         mView.setBrightnessRunnable(() -> {
             mView.updateResources();
