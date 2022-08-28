c6f753e450d51ce3b69ab354f065102c843918a0
Pranav Vashi <neobuddy89@gmail.com>
2022-08-28 14:25:01 +0530

    SystemUI: Update and fix QS tile layout settings
    * Do not let every tom, dick and harry set max columns.
    * On config changes and settings update, check if dynamic row and columns update.
    * Update resources in tile layout when orientation changes to get proper tiles in landscape.
    * Request layout changes whenever column update happens.
    * Be smart in calculating how many max tiles should Quick QS show depending on no. of columns.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -77,6 +77,11 @@ public class QSPanel extends LinearLayout implements Tunable {
             Settings.Secure.QS_SHOW_BRIGHTNESS_SLIDER;
     public static final String QS_BRIGHTNESS_SLIDER_POSITION =
             Settings.Secure.QS_BRIGHTNESS_SLIDER_POSITION;
+    public static final String QS_LAYOUT_COLUMNS =
+            "system:" + Settings.System.QS_LAYOUT_COLUMNS;
+    public static final String QS_LAYOUT_COLUMNS_LANDSCAPE =
+            "system:" + Settings.System.QS_LAYOUT_COLUMNS_LANDSCAPE;
+
 
     private static final String TAG = "QSPanel";
 
@@ -385,6 +390,10 @@ public class QSPanel extends LinearLayout implements Tunable {
                             TunerService.parseIntegerSwitch(newValue, true) ? View.VISIBLE : View.GONE);
                 }
                 break;
+            case QS_LAYOUT_COLUMNS:
+            case QS_LAYOUT_COLUMNS_LANDSCAPE:
+                needsDynamicRowsAndColumns();
+                break;
             default:
                 break;
          }
@@ -464,6 +473,7 @@ public class QSPanel extends LinearLayout implements Tunable {
         super.onConfigurationChanged(newConfig);
         mOnConfigurationChangedListeners.forEach(
                 listener -> listener.onConfigurationChange(newConfig));
+        needsDynamicRowsAndColumns();
     }
 
     @Override
@@ -497,8 +507,12 @@ public class QSPanel extends LinearLayout implements Tunable {
         return false;
     }
 
-    private boolean needsDynamicRowsAndColumns() {
-        return true;
+    public void needsDynamicRowsAndColumns() {
+        if (mTileLayout != null) {
+            int columns = mTileLayout.getResourceColumns();
+            mTileLayout.setMinRows(mUsingHorizontalLayout ? 2 : 1);
+            mTileLayout.setMaxColumns(columns > 3 && mUsingHorizontalLayout ? columns / 2 : columns);
+        }
     }
 
     private void switchAllContentToParent(ViewGroup parent, QSTileLayout newLayout) {
@@ -695,10 +709,7 @@ public class QSPanel extends LinearLayout implements Tunable {
                 mBrightnessRunnable.run();
             }
             reAttachMediaHost(mediaHostView, horizontal);
-            if (needsDynamicRowsAndColumns()) {
-                mTileLayout.setMinRows(horizontal ? 2 : 1);
-                mTileLayout.setMaxColumns(horizontal ? mTileLayout.getResourceColumns() / 2 : mTileLayout.getResourceColumns());
-            }
+            needsDynamicRowsAndColumns();
             updateMargins(mediaHostView);
             if (mHorizontalLinearLayout == null) return;
             mHorizontalLinearLayout.setVisibility(horizontal ? View.VISIBLE : View.GONE);
