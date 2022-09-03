462dedaa8e2a4634eddf2e8dc08f2569b6a1c94a
NurKeinNeid <mralexman3000@gmail.com>
2022-09-03 16:15:36 +0200

    SystemUI: screenshot: Always show screenshot dismiss button
    * should not depend on accessibility mode imo
    
    Change-Id: I46881ed210132252b9c5c7d6be03ed845e33f082
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotView.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotView.java
@@ -74,6 +74,7 @@ import android.view.WindowInsets;
 import android.view.WindowManager;
 import android.view.WindowMetrics;
 import android.view.accessibility.AccessibilityManager;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AnimationUtils;
 import android.view.animation.Interpolator;
 import android.widget.FrameLayout;
@@ -129,7 +130,6 @@ public class ScreenshotView extends FrameLayout implements
     private final Interpolator mFastOutSlowIn;
     private final DisplayMetrics mDisplayMetrics;
     private final float mFixedSize;
-    private final AccessibilityManager mAccessibilityManager;
     private final GestureDetector mSwipeDetector;
 
     private int mNavMode;
@@ -207,8 +207,6 @@ public class ScreenshotView extends FrameLayout implements
 
         mDisplayMetrics = new DisplayMetrics();
         mContext.getDisplay().getRealMetrics(mDisplayMetrics);
-
-        mAccessibilityManager = AccessibilityManager.getInstance(mContext);
 
         mSwipeDetector = new GestureDetector(mContext,
                 new GestureDetector.SimpleOnGestureListener() {
@@ -566,10 +564,8 @@ public class ScreenshotView extends FrameLayout implements
                 mScreenshotPreview.setScaleX(currentScale);
                 mScreenshotPreview.setScaleY(currentScale);
                 mScreenshotPreview.setVisibility(View.VISIBLE);
-                if (mAccessibilityManager.isEnabled()) {
-                    mDismissButton.setAlpha(0);
-                    mDismissButton.setVisibility(View.VISIBLE);
-                }
+                mDismissButton.setAlpha(0);
+                mDismissButton.setVisibility(View.VISIBLE);
             }
         });
 
@@ -1091,10 +1087,7 @@ public class ScreenshotView extends FrameLayout implements
         mScrollChip.setVisibility(View.GONE);
         mScrollablePreview.setVisibility(View.GONE);
         mScrollingScrim.setVisibility(View.GONE);
-
-        if (mAccessibilityManager.isEnabled()) {
-            mDismissButton.setVisibility(View.VISIBLE);
-        }
+        mDismissButton.setVisibility(View.VISIBLE);
         mActionsContainer.setVisibility(View.VISIBLE);
         mActionsContainerBackground.setVisibility(View.VISIBLE);
         mScreenshotPreviewBorder.setVisibility(View.VISIBLE);
