f43e53c61c960cf0bf35aae8e8296dbb1954cc87
Pranav Vashi <neobuddy89@gmail.com>
2022-09-18 13:23:09 +0530

    SystemUI: Use collapsing toolbar for tuner prefs
    Match other parts of Settings.
    
    Change-Id: I1a8f041870e3d65b63c6e302033edeb8b017c3aa
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerActivity.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerActivity.java
@@ -31,6 +31,8 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceScreen;
 
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+
 import com.android.systemui.Dependency;
 import com.android.systemui.res.R;
 import com.android.systemui.demomode.DemoModeController;
@@ -38,8 +40,6 @@ import com.android.systemui.fragments.FragmentService;
 import com.android.systemui.util.settings.GlobalSettings;
 
 import javax.inject.Inject;
-
-import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 
 public class TunerActivity extends CollapsingToolbarBaseActivity implements
         PreferenceFragment.OnPreferenceStartFragmentCallback,
@@ -67,10 +67,7 @@ public class TunerActivity extends CollapsingToolbarBaseActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
-        setTheme(androidx.appcompat.R.style.Theme_AppCompat_DayNight);
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        setContentView(R.layout.tuner_activity);
 /*
         Toolbar toolbar = findViewById(R.id.action_bar);
         if (toolbar != null) {
