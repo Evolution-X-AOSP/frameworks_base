f9375f8325129ec9f09d409e93f94ab0a7cfacf2
vjspranav <pranavasri@live.in>
2022-09-20 17:34:48 +0000

    Allow changing system themes [1/2]
    * Currently Black,Vivid,Clear(WIP)

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -418,7 +418,7 @@ public class ThemeOverlayController extends CoreStartable implements Dumpable {
                 },
                 UserHandle.USER_ALL);
         mSecureSettings.registerContentObserverForUser(
-                Settings.Secure.getUriFor(Settings.Secure.SYSTEM_BLACK_THEME),
+                Settings.Secure.getUriFor(Settings.Secure.SYSTEM_CUSTOM_THEME),
                 false,
                 new ContentObserver(mBgHandler) {
                     @Override
@@ -687,10 +687,11 @@ public class ThemeOverlayController extends CoreStartable implements Dumpable {
 
         boolean nightMode = (mContext.getResources().getConfiguration().uiMode
                 & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
-        boolean isBlackTheme = mSecureSettings.getInt(Settings.Secure.SYSTEM_BLACK_THEME, 0) == 1
-                                && nightMode;
+        int customTheme = mSecureSettings.getInt(Settings.Secure.SYSTEM_CUSTOM_THEME, 0);
+        // If custom Theme is 0, that would mean use default system dark theme 
+        boolean isCustomTheme = nightMode && (customTheme != 0);
 
-        mThemeManager.setIsBlackTheme(isBlackTheme);
+        mThemeManager.setIsCustomTheme(isCustomTheme);
 
         if (mNeedsOverlayCreation) {
             mNeedsOverlayCreation = false;
@@ -702,7 +703,7 @@ public class ThemeOverlayController extends CoreStartable implements Dumpable {
                     managedProfiles);
         }
 
-        mThemeManager.applyBlackTheme(isBlackTheme);
+        mThemeManager.applyCustomTheme(customTheme, isCustomTheme);
     }
 
     private Style fetchThemeStyleFromSetting() {
