596f0051458716a5153ac216b600e4b4b71b41c2
Joey Huab <joey@evolution-x.org>
2022-09-24 08:13:42 +0000

    Revert "BatteryService: Add support for OEM fast charger detection"
    This reverts commit 6cdba3a7152c8c8d8edf0b4bcb0543370f70b74f.

--- packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
+++ packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
@@ -32,7 +32,6 @@ import static android.os.BatteryManager.EXTRA_DASH_CHARGER;
 import static android.os.BatteryManager.EXTRA_WARP_CHARGER;
 import static android.os.BatteryManager.EXTRA_VOOC_CHARGER;
 import static android.os.BatteryManager.EXTRA_TURBO_POWER;
-import static android.os.BatteryManager.EXTRA_OEM_FAST_CHARGER;
 
 import android.content.Context;
 import android.content.Intent;
@@ -55,7 +54,6 @@ public class BatteryStatus {
     public static final int CHARGING_WARP = 4;
     public static final int CHARGING_VOOC = 5;
     public static final int CHARGING_TURBO_POWER = 6;
-    public static final int CHARGING_OEM_FAST_CHARGER = 7;
 
     public final int status;
     public final int level;
@@ -71,13 +69,11 @@ public class BatteryStatus {
     public final boolean warpChargeStatus;
     public final boolean voocChargeStatus;
     public final boolean turboPowerStatus;
-    public final boolean oemChargeStatus;
 
     public BatteryStatus(int status, int level, int plugged, int health,
             int maxChargingCurrent, int maxChargingVoltage, float temperature,
             int maxChargingWattage, boolean dashChargeStatus, boolean warpChargeStatus,
-            boolean voocChargeStatus, boolean turboPowerStatus,
-            boolean oemChargeStatus, boolean present) {
+            boolean voocChargeStatus, boolean turboPowerStatus, boolean present) {
         this.status = status;
         this.level = level;
         this.plugged = plugged;
@@ -89,7 +85,6 @@ public class BatteryStatus {
         this.warpChargeStatus = warpChargeStatus;
         this.voocChargeStatus = voocChargeStatus;
         this.turboPowerStatus = turboPowerStatus;
-        this.oemChargeStatus = oemChargeStatus;
         this.present = present;
         this.temperature = temperature;
     }
@@ -103,7 +98,6 @@ public class BatteryStatus {
         warpChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_WARP_CHARGER, false);
         voocChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_VOOC_CHARGER, false);
         turboPowerStatus = batteryChangedIntent.getBooleanExtra(EXTRA_TURBO_POWER, false);
-        oemChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_OEM_FAST_CHARGER, false);
         present = batteryChangedIntent.getBooleanExtra(EXTRA_PRESENT, true);
         temperature = batteryChangedIntent.getIntExtra(EXTRA_TEMPERATURE, -1);
 
@@ -211,7 +205,6 @@ public class BatteryStatus {
                 warpChargeStatus ? CHARGING_WARP :
                 voocChargeStatus ? CHARGING_VOOC :
                 turboPowerStatus ? CHARGING_TURBO_POWER :
-                oemChargeStatus ? CHARGING_OEM_FAST_CHARGER :
                 maxChargingWattage <= 0 ? CHARGING_UNKNOWN :
                 maxChargingWattage < slowThreshold ? CHARGING_SLOWLY :
                         maxChargingWattage > fastThreshold ? CHARGING_FAST :
