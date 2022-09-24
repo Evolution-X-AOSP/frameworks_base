e12644eab6a6d44da77dbe06911886ae9e4aee36
Joey Huab <joey@evolution-x.org>
2022-09-24 20:25:16 +0000

    Revert "base: HeadsUp snooze function [1/2]"
    This reverts commit 8c7cd6b6fe95c669f8429f6c9906e0e8c8c75814.
    
    Revert "HeadsUp: Fix crash while formatting string"
    
    This reverts commit 55fc4333bc5a2142f53b48708bae92572337beb6.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6438,12 +6438,6 @@ public final class Settings {
         public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
 
         /**
-         * Defines the global heads up notification snooze
-         * @hide
-         */
-        public static final String HEADS_UP_NOTIFICATION_SNOOZE = "heads_up_notification_snooze";
-
-        /**
          * Applications list where heasdup should't show
          * @hide
          */
