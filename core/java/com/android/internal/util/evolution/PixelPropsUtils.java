1ae669c5e0397536775c605cf10d1ca2bee0f536
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-09-25 16:29:11 +0000

    PixelPropsUtils: Spoof device as Pixel for Wallpapers app
    * Partial pick of https://github.com/aosp-gr/frameworks_base/commit/962b965a4286e98ff77ace462dc4a164546237eb.
    
    Unlocks features like Pixel 6 wallpapers.

--- core/java/com/android/internal/util/evolution/PixelPropsUtils.java
+++ core/java/com/android/internal/util/evolution/PixelPropsUtils.java
@@ -33,6 +33,7 @@ public class PixelPropsUtils {
     private static final String PACKAGE_GMS = "com.google.android.gms";
     private static final String PACKAGE_FINSKY = "com.android.vending";
     private static final String PACKAGE_SETTINGS_SERVICES = "com.google.android.settings.intelligence";
+    private static final String PACKAGE_WALLPAPERS = "com.google.android.apps.wallpaper";
     private static final String SAMSUNG = "com.samsung.android.";
 
     private static final String DEVICE = "org.evolution.device";
@@ -51,6 +52,7 @@ public class PixelPropsUtils {
     private static final Map<String, ArrayList<String>> propsToKeep;
 
     private static final String[] packagesToChangePixel6Pro = {
+            PACKAGE_WALLPAPERS,
             "com.google.android.apps.subscriptions.red",
             "com.google.android.inputmethod.latin"
     };
