cd2f362b786d3a0fd3bc6cb70bd477517a30e14c
Jason Edson <jaysonedson@gmail.com>
2022-09-26 07:27:48 +0000

    EnhancedEstimates: Get estimates from Device Health Services
    Get battery estimates from com.google.android.apps.turbo (Turbo.apk)
    Code taken from android11 SystemUIGoogle.apk and cleaned up
    
    [jhonboy121]:
    *  use GlobalSettings class for acquiring settings
    *  use proper access modifiers and finalize many vars.
    *  simplify estimates processing logic
    *  use modern try catch method for properly closing cursor
    *  cache many variables used a lot to reduce runtime
    *  use EvolutionUtils class method to check whether package is installed
    
    Change-Id: Ic1d1cd4ed2865ffcaed1cab126f699ee4ddf32ab
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    
    Revert "EnhancedEstimates: Get estimates from Device Health Services"
    
    This reverts commit b43002a66bfc6d4bc9f55f2eaae1bdae84f998e4.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -17057,12 +17057,6 @@ public final class Settings {
         public static final String PULSE_PANEL = "pulse_panel";
 
         /**
-         * Google Hybrid Battery Warnings
-         * @hide
-         */
-        public static final String HYBRID_SYSUI_BATTERY_WARNING_FLAGS = "hybrid_sysui_battery_warning_flags";
-
-        /**
          * Global settings that shouldn't be persisted.
          *
          * @hide
---
+++ packages/SystemUI/src/com/android/systemui/power/EnhancedEstimatesImpl.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.power;
+
+import static com.android.internal.util.evolution.EvolutionUtils.isPackageInstalled;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.Settings;
+import android.util.KeyValueListParser;
+import android.util.Log;
+
+import com.android.settingslib.fuelgauge.Estimate;
+import com.android.settingslib.utils.PowerUtil;
+import com.android.systemui.dagger.SysUISingleton;
+import com.android.systemui.power.EnhancedEstimates;
+import com.android.systemui.util.settings.GlobalSettings;
+
+import java.time.Duration;
+
+import javax.inject.Inject;
+
+@SysUISingleton
+public final class EnhancedEstimatesImpl implements EnhancedEstimates {
+
+    private static final String TAG = "EnhancedEstimatesImpl";
+
+    private static final Estimate EMPTY_ESTIMATE = new Estimate(-1L, false, -1L);
+
+    private static final Duration DAY = Duration.ofDays(1L);
+    private static final long HOUR = Duration.ofHours(1L).toMillis();
+    private static final long THREE_HOURS = Duration.ofHours(3L).toMillis();
+    private static final long FIFTEEN_MINUTES = Duration.ofMinutes(15L).toMillis();
+
+    private final Context mContext;
+    private final GlobalSettings mGlobalSettings;
+    private final KeyValueListParser mParser;
+
+    @Inject
+    public EnhancedEstimatesImpl(
+        Context context,
+        GlobalSettings globalSettings
+    ) {
+        mContext = context;
+        mGlobalSettings = globalSettings;
+        mParser = new KeyValueListParser(',');
+    }
+
+    @Override
+    public boolean isHybridNotificationEnabled() {
+        final boolean isTurboInstalled = isPackageInstalled(
+            mContext,
+            "com.google.android.apps.turbo",
+            false /* ignoreState */
+        );
+        if (!isTurboInstalled) return false;
+        updateFlags();
+        return mParser.getBoolean("hybrid_enabled", true);
+    }
+
+    @Override
+    public Estimate getEstimate() {
+        final Uri build = new Uri.Builder()
+            .scheme("content")
+            .authority("com.google.android.apps.turbo.estimated_time_remaining")
+            .appendPath("time_remaining")
+            .build();
+        try (final Cursor query = mContext.getContentResolver().query(build, null, null, null, null)) {
+            if (query == null) return EMPTY_ESTIMATE;
+            try {
+                if (query.moveToFirst()) {
+                    long timeRemaining = -1L;
+                    final int usageColumnIndex = query.getColumnIndex("is_based_on_usage");
+                    final boolean isBasedOnUsage = usageColumnIndex != -1 && query.getInt(usageColumnIndex) != 0;
+                    final int batteryLifecolumnIndex = query.getColumnIndex("average_battery_life");
+                    if (batteryLifecolumnIndex != -1) {
+                        final long averageBatteryLife = query.getLong(batteryLifecolumnIndex);
+                        if (averageBatteryLife != -1L) {
+                            final long duration = Duration.ofMillis(averageBatteryLife).compareTo(DAY) >= 0
+                                ? HOUR : FIFTEEN_MINUTES;
+                            timeRemaining = PowerUtil.roundTimeToNearestThreshold(averageBatteryLife, duration);
+                        }
+                    }
+                    return new Estimate(
+                        query.getLong(query.getColumnIndex("battery_estimate")),
+                        isBasedOnUsage,
+                        timeRemaining
+                    );
+                }
+            } catch (Exception ex) {
+                // Catch and release
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Something went wrong when getting an estimate from Turbo", e);
+        }
+        return EMPTY_ESTIMATE;
+    }
+
+    @Override
+    public long getLowWarningThreshold() {
+        updateFlags();
+        return mParser.getLong("low_threshold", THREE_HOURS);
+    }
+
+    @Override
+    public long getSevereWarningThreshold() {
+        updateFlags();
+        return mParser.getLong("severe_threshold", HOUR);
+    }
+
+    @Override
+    public boolean getLowWarningEnabled() {
+        updateFlags();
+        return mParser.getBoolean("low_warning_enabled", false);
+    }
+
+    private void updateFlags() {
+        final String string = mGlobalSettings.getString("hybrid_sysui_battery_warning_flags");
+        try {
+            mParser.setString(string);
+        } catch (IllegalArgumentException ex) {
+            Log.e(TAG, "Bad hybrid sysui warning flags");
+        }
+    }
+}
