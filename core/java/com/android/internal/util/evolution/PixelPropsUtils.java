2e482b21143fe39c0be0c4462bc711ffaf0d3fb0
Joey Huab <joey@evolution-x.org>
2022-10-03 17:20:37 +0000

    PixelPropsUtils: Update fingerprints to October 2022 release

--- core/java/com/android/internal/util/evolution/PixelPropsUtils.java
+++ core/java/com/android/internal/util/evolution/PixelPropsUtils.java
@@ -148,7 +148,7 @@ public class PixelPropsUtils {
         propsToChangePixel6Pro.put("PRODUCT", "raven");
         propsToChangePixel6Pro.put("MODEL", "Pixel 6 Pro");
         propsToChangePixel6Pro.put(
-                "FINGERPRINT", "google/raven/raven:13/TP1A.220905.004/8927612:user/release-keys");
+                "FINGERPRINT", "google/raven/raven:13/TP1A.221005.002/9012097:user/release-keys");
         propsToChangePixel5 = new HashMap<>();
         propsToChangePixel5.put("BRAND", "google");
         propsToChangePixel5.put("MANUFACTURER", "Google");
@@ -156,7 +156,7 @@ public class PixelPropsUtils {
         propsToChangePixel5.put("PRODUCT", "redfin");
         propsToChangePixel5.put("MODEL", "Pixel 5");
         propsToChangePixel5.put(
-                "FINGERPRINT", "google/redfin/redfin:13/TP1A.220905.004/8927612:user/release-keys");
+                "FINGERPRINT", "google/redfin/redfin:13/TP1A.221005.002/9012097:user/release-keys");
         propsToChangePixelXL = new HashMap<>();
         propsToChangePixelXL.put("BRAND", "google");
         propsToChangePixelXL.put("MANUFACTURER", "Google");
