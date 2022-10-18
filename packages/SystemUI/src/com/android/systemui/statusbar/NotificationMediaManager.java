bb5f9ecbcec147d2e3fad8705dc24daffd66eaa7
Pranav Vashi <neobuddy89@gmail.com>
2022-10-19 00:41:10 +0530

    Pulse: Implement pulse color based album art [1/2]
    * Little hacky but works.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -252,9 +252,12 @@ public class NotificationMediaManager implements Dumpable {
         mMediaListeners.add(callback);
         callback.onPrimaryMetadataOrStateChanged(mMediaMetadata,
                 getMediaControllerPlaybackState(mMediaController));
+        callback.setMediaNotificationColor(mColorExtractor.getMediaBackgroundColor());
     }
 
     public void removeCallback(MediaListener callback) {
+        mColorExtractor.setMediaBackgroundColor(0);
+        callback.setMediaNotificationColor(0);
         mMediaListeners.remove(callback);
     }
 
@@ -328,6 +331,7 @@ public class NotificationMediaManager implements Dumpable {
         ArrayList<MediaListener> callbacks = new ArrayList<>(mMediaListeners);
         for (int i = 0; i < callbacks.size(); i++) {
             callbacks.get(i).onPrimaryMetadataOrStateChanged(mMediaMetadata, state);
+            callbacks.get(i).setMediaNotificationColor(mColorExtractor.getMediaBackgroundColor());
         }
     }
 
@@ -395,5 +399,7 @@ public class NotificationMediaManager implements Dumpable {
          */
         default void onPrimaryMetadataOrStateChanged(MediaMetadata metadata,
                 @PlaybackState.State int state) {}
+
+        default void setMediaNotificationColor(int color) {};
     }
 }
