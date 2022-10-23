8642eee444611a6d31fe6ca711965a080c97648d
spezi77 <spezi7713@gmx.net>
2022-10-23 19:16:39 +0000

    Global Actions: Update the default actions to match the stock experience.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -656,7 +656,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
 
         // make sure emergency affordance action is first, if needed
         if (Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.GLOBAL_ACTIONS_EMERGENCY, 0) == 1) {
+                Settings.System.GLOBAL_ACTIONS_EMERGENCY, 1) == 1) {
             addIfShouldShowAction(tempActions, new EmergencyAffordanceAction());
             addedKeys.add(GLOBAL_ACTION_KEY_EMERGENCY);
         }
@@ -693,12 +693,12 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
                 }
             } else if (GLOBAL_ACTION_KEY_SETTINGS.equals(actionKey)) {
                 if (Settings.System.getInt(mContext.getContentResolver(),
-                        Settings.System.GLOBAL_ACTIONS_SETTINGS, 0) != 0) {
+                        Settings.System.GLOBAL_ACTIONS_SETTINGS, 0) == 1) {
                     addIfShouldShowAction(tempActions, getSettingsAction());
                 }
             } else if (GLOBAL_ACTION_KEY_LOCKDOWN.equals(actionKey)) {
                 if (Settings.System.getInt(mContext.getContentResolver(),
-                        Settings.System.GLOBAL_ACTIONS_LOCKDOWN, 0) != 0) {
+                        Settings.System.GLOBAL_ACTIONS_LOCKDOWN, 1) == 1) {
                     addIfShouldShowAction(tempActions, new LockDownAction());
                 }
             } else if (GLOBAL_ACTION_KEY_VOICEASSIST.equals(actionKey)) {
@@ -736,7 +736,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
             } else if (GLOBAL_ACTION_KEY_EMERGENCY.equals(actionKey)) {
                 if (shouldDisplayEmergency()) {
                     if (Settings.System.getInt(mContext.getContentResolver(),
-                            Settings.System.GLOBAL_ACTIONS_EMERGENCY, 0) == 1) {
+                            Settings.System.GLOBAL_ACTIONS_EMERGENCY, 1) == 1) {
                         addIfShouldShowAction(tempActions, new EmergencyDialerAction());
                     }
                 }
