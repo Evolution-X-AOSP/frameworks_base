93b79cdaccae7f10c36622cf89906056be408509
Ido Ben-Hur <idoybh2@gmail.com>
2022-11-25 01:37:39 +0200

    base: Allow toggling NFC sounds [1/3]

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -248,5 +248,6 @@ public class SecureSettings {
         Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
         Settings.Secure.DOZE_ON_CHARGE,
         Settings.Secure.QUICK_SETTINGS_TILES_VIBRATE,
+        Settings.Secure.NFC_SOUNDS,
     };
 }
