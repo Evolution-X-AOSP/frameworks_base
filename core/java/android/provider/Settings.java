0ec841ced9224110f5cfebbb7c500365d6576815
Pzqqt <821026875@qq.com>
2022-12-06 22:09:22 +0800

    base: Android P system animation (1/2)
    Pzqqt: Remove hooks for task_open_enter, task_open_exit, task_close_enter,
    task_close_exit animations as they don't change much.
    
    Co-authored-by: zeromod <zeromoddev@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pzqqt <821026875@qq.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -18977,6 +18977,13 @@ public final class Settings {
         public static final String RESTRICTED_NETWORKING_MODE = "restricted_networking_mode";
 
         /**
+         * Android P animation style
+         * @hide
+         */
+        @Readable
+        public static final String PIE_ANIMATION_STYLE = "pie_animation_style";
+
+        /**
          * Enable blocking wakelock
          * @hide
          */
