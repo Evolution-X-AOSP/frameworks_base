cdacfd7ac5f1d4044302435338be610988707d7b
spkal01 <kalligeross@gmail.com>
2022-12-10 19:25:20 +0200

    SystemUI: Restore Combined signal icons support
    This reverts commits 36928c0838041c05d50e5effea1b83eb59080800 and 9273f914487160fceec0bd60e117061a2c75a91e
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarMobileView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarMobileView.java
@@ -66,6 +66,7 @@ public class StatusBarMobileView extends FrameLayout implements DarkReceiver,
     private int mVisibleState = STATE_HIDDEN;
     private DualToneHandler mDualToneHandler;
     private boolean mForceHidden;
+    private boolean mProviderModel;
 
     private boolean mOldStyleType;
     private ImageView mMobileTypeSmall;
@@ -84,13 +85,14 @@ public class StatusBarMobileView extends FrameLayout implements DarkReceiver,
      */
     public static StatusBarMobileView fromContext(
             Context context,
-            String slot
+            String slot,
+            boolean providerModel
     ) {
         LayoutInflater inflater = LayoutInflater.from(context);
         StatusBarMobileView v = (StatusBarMobileView)
                 inflater.inflate(R.layout.status_bar_mobile_signal_group, null);
         v.setSlot(slot);
-        v.init();
+        v.init(providerModel);
         v.setVisibleState(STATE_ICON);
         return v;
     }
@@ -129,12 +131,17 @@ public class StatusBarMobileView extends FrameLayout implements DarkReceiver,
         setMeasuredDimension(mMobileGroup.getMeasuredWidth(), mMobileGroup.getMeasuredHeight());
     }
 
-    private void init() {
+    private void init(boolean providerModel) {
+        mProviderModel = providerModel;
         mDualToneHandler = new DualToneHandler(getContext());
         mMobileGroup = findViewById(R.id.mobile_group);
         mMobile = findViewById(R.id.mobile_signal);
         mMobileType = findViewById(R.id.mobile_type);
-        mMobileRoaming = findViewById(R.id.mobile_roaming);
+        if (mProviderModel) {
+            mMobileRoaming = findViewById(R.id.mobile_roaming_large);
+        } else {
+            mMobileRoaming = findViewById(R.id.mobile_roaming);
+        }
         mMobileRoamingSpace = findViewById(R.id.mobile_roaming_space);
         mIn = findViewById(R.id.mobile_in);
         mOut = findViewById(R.id.mobile_out);
