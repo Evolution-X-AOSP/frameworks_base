4943cac5bde3fa851ab2c963130ea47f7b2b3d75
Ido Ben-Hur <idoybh2@gmail.com>
2022-12-14 01:50:18 +0200

    base: Allow enabling pocket judge on call via an overlay
    this works fine when the default dialer isn't gdialer (ex - vanilla)
    so allow overlaying to disable this behavior

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -680,6 +680,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private PocketLock mPocketLock;
     private boolean mPocketLockShowing;
     private boolean mIsDeviceInPocket;
+    private boolean mIsPocketDisabledOnCall;
     private final IPocketCallback mPocketCallback = new IPocketCallback.Stub() {
 
         @Override
@@ -2111,6 +2112,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 res.getBoolean(com.android.internal.R.bool.config_wakeOnAssistKeyPress);
         mWakeOnBackKeyPress =
                 res.getBoolean(com.android.internal.R.bool.config_wakeOnBackKeyPress);
+        mIsPocketDisabledOnCall =
+                res.getBoolean(com.android.internal.R.bool.config_pocketJudgeDisableOnCall);
 
         // Init display burn-in protection
         boolean burnInProtectionEnabled = context.getResources().getBoolean(
@@ -5467,9 +5470,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             return;
         }
 
-        TelecomManager telecomManager = getTelecommService();
-        if (telecomManager != null && telecomManager.isInCall()) {
-            return;
+        if (mIsPocketDisabledOnCall) {
+            TelecomManager telecomManager = getTelecommService();
+            if (telecomManager != null && telecomManager.isInCall()) {
+                return;
+            }
         }
 
         if (mPowerManager.isInteractive() && !isKeyguardShowingAndNotOccluded()){
