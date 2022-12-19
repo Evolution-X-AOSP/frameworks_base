c3e7a4818e8552f071d92506547c58b47bd9b68b
ShevT <evgeny.shishkov@gmail.com>
2022-12-19 16:24:06 +0300

    SystemUI: Add Screenshot tile
        OmniROM:
    
        From: maxwen <max.weninger@gmail.com>
        Date: Wed, 14 Oct 2020 17:17:31 +0200
        Subject: [PATCH] SystemUI: qs tiles revenge
    
        -screenshot
    
        [micky387]
         * edit to the new A12 Tiles API
         * Add secondaryLabel for the long press screenshot tile
    
        From: micky387 <mickaelsaibi@free.fr>
        Date: Wed, 14 Dec 2022 13:12:26 +0100
        Subject: [PATCH] SystemUI: Update ScreenshotTile after merge
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/dagger/evolution/EvolutionXModule.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/evolution/EvolutionXModule.kt
@@ -27,6 +27,7 @@ import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.ReadingModeTile
 import com.android.systemui.qs.tiles.RefreshRateTile
+import com.android.systemui.qs.tiles.ScreenshotTile
 import com.android.systemui.qs.tiles.SmartPixelsTile
 import com.android.systemui.qs.tiles.SoundTile
 import com.android.systemui.qs.tiles.SoundSearchTile
@@ -95,6 +96,12 @@ interface EvolutionXModule {
     @IntoMap
     @StringKey(RefreshRateTile.TILE_SPEC)
     fun bindRefreshRateTile(refreshRateTile: RefreshRateTile): QSTileImpl<*>
+
+    /** Inject ScreenshotTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(ScreenshotTile.TILE_SPEC)
+    fun bindScreenshotTile(screenshotTile: ScreenshotTile): QSTileImpl<*>
 
     /** Inject SyncTile into tileMap in QSModule */
     @Binds
