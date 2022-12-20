4fac3476d9ad4bc59a4c65b6a9bfdb84ba37af0a
Pranav Vashi <neobuddy89@gmail.com>
2022-12-21 00:33:37 +0530

    Fix issues with center clock
    * HeadsupApperanceController needs to hide/show current clock,
      and not the one it was inited with.
    * Let ClockController initialize mActiveClock properly.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ClockController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ClockController.java
@@ -17,6 +17,7 @@
 package com.android.systemui.statusbar.phone;
 
 import android.content.Context;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
@@ -53,7 +54,9 @@ public class ClockController implements TunerService.Tunable {
         mLeftClock = statusBar.findViewById(R.id.clock);
         mRightClock = statusBar.findViewById(R.id.clock_right);
 
-        mActiveClock = mLeftClock;
+        mClockPosition = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    STATUS_BAR_CLOCK, CLOCK_POSITION_LEFT, UserHandle.USER_CURRENT);
+        updateActiveClock();
 
         Dependency.get(TunerService.class).addTunable(this,
                 STATUS_BAR_CLOCK);
