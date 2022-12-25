0874377c6f3ba0e101ca2a97aad8f565e42f18b4
AnierinB <anierin@evolution-x.org>
2022-12-25 14:44:07 +0000

    base: Blacklist OPlusExtras for Parallel space
    Add DeviceExtras for pre oplus devices (6 series) while we're here
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- services/core/java/ink/kaleidoscope/server/ParallelSpaceManagerService.java
+++ services/core/java/ink/kaleidoscope/server/ParallelSpaceManagerService.java
@@ -111,7 +111,9 @@ public final class ParallelSpaceManagerService extends SystemService {
 
     private static final List<String> SPACE_BLACKLIST_PACKAGES = Arrays.asList(
         // To avoid third party apps starting it accidentally.
-        "com.android.launcher3"
+        "com.android.launcher3",
+        "org.evolution.oneplus.DeviceExtras",
+        "org.evolution.oplus.OPlusExtras"
     );
 
     /**
