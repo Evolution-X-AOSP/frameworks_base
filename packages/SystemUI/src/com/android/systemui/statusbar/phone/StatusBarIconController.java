c530ad06774c19937f6a2c3d54e1432a45dc47f4
Pranav Vashi <neobuddy89@gmail.com>
2022-12-28 00:41:14 +0530

    Network traffic mode for status bar [2/3]
    * Complete re-write with proper slot in status bar
    * Refactor stats calc based on https://github.com/crdroidandroid/android_lineage-sdk/commit/02eddd44ace2cdc32b980ef094808ee5beb63c9b
    * Retained optimized code and formatting
    
    Co-authored-by: SpiritCroc <dev@spiritcroc.de>
    Co-authored-by: Oliver Scott <olivercscott@gmail.com>
    Co-authored-by: Gobinda Joy <gobinda.joy@gmail.com>
    Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -17,6 +17,7 @@ package com.android.systemui.statusbar.phone;
 import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_BLUETOOTH;
 import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_ICON;
 import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_MOBILE_NEW;
+import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_NETWORK_TRAFFIC;
 import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_WIFI_NEW;
 
 import android.annotation.Nullable;
@@ -42,9 +43,11 @@ import com.android.systemui.plugins.DarkIconDispatcher.DarkReceiver;
 import com.android.systemui.statusbar.BaseStatusBarFrameLayout;
 import com.android.systemui.statusbar.StatusBarBluetoothView;
 import com.android.systemui.statusbar.StatusBarIconView;
+import com.android.systemui.statusbar.StatusBarNetworkTraffic;
 import com.android.systemui.statusbar.StatusIconDisplayable;
 import com.android.systemui.statusbar.connectivity.ui.MobileContextProvider;
 import com.android.systemui.statusbar.phone.PhoneStatusBarPolicy.BluetoothIconState;
+import com.android.systemui.statusbar.phone.PhoneStatusBarPolicy.NetworkTrafficState;
 import com.android.systemui.statusbar.phone.StatusBarSignalPolicy.CallIndicatorIconState;
 import com.android.systemui.statusbar.pipeline.mobile.ui.MobileUiAdapter;
 import com.android.systemui.statusbar.pipeline.mobile.ui.binder.MobileIconsBinder;
@@ -101,6 +104,9 @@ public interface StatusBarIconController {
 
     /** */
     void setBluetoothIcon(String slot, BluetoothIconState state);
+
+    /** */
+    void setNetworkTraffic(String slot, NetworkTrafficState state);
 
     /**
      * Notify this class that there is a new set of mobile icons to display, keyed off of this list
@@ -443,6 +449,9 @@ public interface StatusBarIconController {
 
                 case TYPE_BLUETOOTH:
                     return addBluetoothIcon(index, slot, holder.getBluetoothState());
+
+                case TYPE_NETWORK_TRAFFIC:
+                    return addNetworkTraffic(index, slot, holder.getNetworkTrafficState());
             }
 
             return null;
@@ -499,6 +508,13 @@ public interface StatusBarIconController {
             return view;
         }
 
+        protected StatusBarNetworkTraffic addNetworkTraffic(int index, String slot, NetworkTrafficState state) {
+            StatusBarNetworkTraffic view = onCreateStatusBarNetworkTraffic(slot);
+            view.applyNetworkTrafficState(state);
+            mGroup.addView(view, index, onCreateLayoutParams());
+            return view;
+        }
+
         private StatusBarIconView onCreateStatusBarIconView(String slot, boolean blocked) {
             return new StatusBarIconView(mContext, slot, null, blocked);
         }
@@ -521,6 +537,11 @@ public interface StatusBarIconController {
 
         private StatusBarBluetoothView onCreateStatusBarBluetoothView(String slot) {
             StatusBarBluetoothView view = StatusBarBluetoothView.fromContext(mContext, slot);
+            return view;
+        }
+
+        private StatusBarNetworkTraffic onCreateStatusBarNetworkTraffic(String slot) {
+            StatusBarNetworkTraffic view = StatusBarNetworkTraffic.fromContext(mContext, slot);
             return view;
         }
 
@@ -561,6 +582,9 @@ public interface StatusBarIconController {
                 case TYPE_BLUETOOTH:
                     onSetBluetoothIcon(viewIndex, holder.getBluetoothState());
                     return;
+                case TYPE_NETWORK_TRAFFIC:
+                    onSetNetworkTraffic(viewIndex, holder.getNetworkTrafficState());
+                    return;
                 default:
                     break;
             }
@@ -570,6 +594,13 @@ public interface StatusBarIconController {
             StatusBarBluetoothView view = (StatusBarBluetoothView) mGroup.getChildAt(viewIndex);
             if (view != null) {
                 view.applyBluetoothState(state);
+            }
+        }
+
+        public void onSetNetworkTraffic(int viewIndex, NetworkTrafficState state) {
+            StatusBarNetworkTraffic view = (StatusBarNetworkTraffic) mGroup.getChildAt(viewIndex);
+            if (view != null) {
+                view.applyNetworkTrafficState(state);
             }
         }
 
