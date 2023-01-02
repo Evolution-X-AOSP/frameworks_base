fbac45406d9606fba13f6bee889de9bd9ff77933
pioush.kumar-IN009423 <pioush.kumar@oppo.com>
2023-01-02 11:54:43 +0530

    Youtube application when moving to PIP animation was very bad
    Problem: {
        1. Open youtube video premium. Start any video.
        2. Press back make the video smaller.
        3. Then press home or back from navigation buttons.
        4. Observe animation surface is very weird.
    }
    
    Solution: {
        1. Youtube itself has set very small pip args. That causes surface to first scale then animate.
        2. So set the sourceHintRect to null. To show only color overlay and animate the task snapshot only.
        3. Will make better user effect.
    }
    
    Bug: 263842271
    Signed-off-by: pioush.kumar-IN009423 <pioush.kumar@oppo.com>
    Change-Id: I33e29c504a33be992ab4dc74d23240ad98720c91

--- services/core/java/com/android/server/wm/Task.java
+++ services/core/java/com/android/server/wm/Task.java
@@ -1588,16 +1588,20 @@ class Task extends TaskFragment {
                 || (oldParentFragment != null && oldParentFragment.isEmbedded()));
     }
 
-    private void clearPinnedTaskIfNeed() {
+    private void clearPinnedTaskIfNeed(boolean isTaskRemoved) {
         // The original task is to be removed, try remove also the pinned task.
         if (mChildPipActivity != null && mChildPipActivity.getTask() != null) {
             mTaskSupervisor.removeRootTask(mChildPipActivity.getTask());
+            if (isTaskRemoved && mChildPipActivity != null
+                    && mChildPipActivity.getWindowingMode() != WINDOWING_MODE_PINNED) {
+                mChildPipActivity.clearLastParentBeforePip();
+            }
         }
     }
 
     /** Completely remove all activities associated with an existing task. */
     void removeActivities(String reason, boolean excludingTaskOverlay) {
-        clearPinnedTaskIfNeed();
+        clearPinnedTaskIfNeed(false);
         // Broken down into to cases to avoid object create due to capturing mStack.
         if (getRootTask() == null) {
             forAllActivities((r) -> {
@@ -2603,7 +2607,7 @@ class Task extends TaskFragment {
         mRemoving = true;
 
         EventLogTags.writeWmTaskRemoved(mTaskId, reason);
-        clearPinnedTaskIfNeed();
+        clearPinnedTaskIfNeed(true);
         // If applicable let the TaskOrganizer know the Task is vanishing.
         setTaskOrganizer(null);
 
