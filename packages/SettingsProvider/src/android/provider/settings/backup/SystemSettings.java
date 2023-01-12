aaf75873247473ebe11d2420ae4dd7ecb2ba6c52
Joey Huab <joey@evolution-x.org>
2023-01-12 07:26:18 +0000

    [SQUASH] base: Nuke quick mute gesture [1/2]
    Revert "AudioManager: Set minimum index via QuickMute instead of muting"
    
    This reverts commit 6f2d87bd9146f6a2cd5a39e8948af70fc86b4db7.
    
    Revert "base: Add quick mute gesture [1/2]"
    
    This reverts commit fc37ad550a0cab11e403b34194c10cb650c2e9e9.

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -115,8 +115,6 @@ public class SystemSettings {
         Settings.System.ENABLE_FLOATING_ROTATION_BUTTON,
         Settings.System.QS_FOOTER_TEXT_SHOW,
         Settings.System.QS_FOOTER_TEXT_STRING,
-        Settings.System.VOLUME_BUTTON_QUICK_MUTE,
-        Settings.System.VOLUME_BUTTON_QUICK_MUTE_DELAY,
         Settings.System.RINGTONE_VIBRATION_PATTERN,
         Settings.System.CUSTOM_RINGTONE_VIBRATION_PATTERN,
         Settings.System.NOTIFICATION_VIBRATION_PATTERN,
