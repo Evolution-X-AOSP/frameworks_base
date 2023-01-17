fb72f70eabd7cfdd908cb0cea7643e4fdcab5c83
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-01-17 12:12:08 +0530

    [DNM][HACK] telephony: Force Class 0 SMS to Class 1
    This kills Flash SMS messages. Fuck you airtel
    
    Change-Id: Ifb0c9e8bae5c12868d178fbdaeceb2cc72a0ffb6
    Signed-off-by: Sageofd6path <mail2anirban95@gmail.com>

--- telephony/java/com/android/internal/telephony/gsm/SmsMessage.java
+++ telephony/java/com/android/internal/telephony/gsm/SmsMessage.java
@@ -1671,8 +1671,6 @@ public class SmsMessage extends SmsMessageBase {
         } else {
             switch (mDataCodingScheme & 0x3) {
             case 0:
-                messageClass = MessageClass.CLASS_0;
-                break;
             case 1:
                 messageClass = MessageClass.CLASS_1;
                 break;
