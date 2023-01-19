b54689aa4fcd3247b34389e687e4605445407c93
Logan-Developer <logan2.humbert@gmail.com>
2023-01-20 00:21:32 +0100

    SystemUI: Introduce Google TV remote controls keyguard affordance [1/2]
    This adds a button for launching the Remote controls from Google TV app
    at the bottom of the lockscreen for quick access.

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -325,7 +325,8 @@ public class SystemSettingsValidators {
                             "wallet",
                             "qr",
                             "camera",
-                            "flashlight"
+                            "flashlight",
+                            "remote"
                         );
                         final String[] split = value.split(";");
                         if (split.length != 2) return false;
