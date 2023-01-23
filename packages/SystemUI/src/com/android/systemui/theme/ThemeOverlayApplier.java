91183bceafd7335fe577fd0dff3ea83135b91a33
rezaadi0105 <rezaadipangestu5@gmail.com>
2023-01-23 22:19:38 +0700

    base: Data icon style [1/3]
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
@@ -139,6 +139,9 @@ public class ThemeOverlayApplier implements Dumpable {
     @VisibleForTesting
     static final String OVERLAY_CATEGORY_BRIGHTNESS_SLIDER =
             "android.theme.customization.brightness_slider";
+    @VisibleForTesting
+    static final String OVERLAY_CATEGORY_DATA =
+            "android.customization.sb_data";
 
     /*
      * All theme customization categories used by the system, in order that they should be applied,
@@ -158,7 +161,8 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_ICON_WIFI,
             OVERLAY_CATEGORY_NAVBAR,
             OVERLAY_CATEGORY_LOCK_CLOCK_FONT,
-            OVERLAY_CATEGORY_BRIGHTNESS_SLIDER);
+            OVERLAY_CATEGORY_BRIGHTNESS_SLIDER,
+            OVERLAY_CATEGORY_DATA);
 
     /* Categories that need to be applied to the current user as well as the system user. */
     @VisibleForTesting
@@ -171,7 +175,8 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_ICON_SYSUI,
             OVERLAY_CATEGORY_NAVBAR,
             OVERLAY_CATEGORY_LOCK_CLOCK_FONT,
-            OVERLAY_CATEGORY_BRIGHTNESS_SLIDER);
+            OVERLAY_CATEGORY_BRIGHTNESS_SLIDER,
+            OVERLAY_CATEGORY_DATA);
 
     /* Categories for all available custom themes */
     @VisibleForTesting
@@ -228,6 +233,7 @@ public class ThemeOverlayApplier implements Dumpable {
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_NAVBAR, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_LOCK_CLOCK_FONT, ANDROID_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_BRIGHTNESS_SLIDER, SYSUI_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_DATA, SYSUI_PACKAGE);
 
         dumpManager.registerDumpable(TAG, this);
     }
