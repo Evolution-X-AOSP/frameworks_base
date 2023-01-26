43c72dc0aa6b3ebee1b9bc3a59547388eae76765
minaripenguin <minaripenguin@users.noreply.github.com>
2023-01-26 17:06:40 +0800

    SystemUI: Combined signal icons toggle [1/2]
    * add runtime toggle support
    * replace methods where injection of feature flags is needed with system properties check to avoid the hassle of re-injecting feature flag to sysui, only keep parts where featureflag class is already injected
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -496,6 +496,18 @@ public class ThemeOverlayController extends CoreStartable implements Dumpable {
                 },
                 UserHandle.USER_ALL);
 
+        mSecureSettings.registerContentObserverForUser(
+                Settings.Secure.getUriFor(Settings.Secure.ENABLE_COMBINED_SIGNAL_ICONS),
+                false,
+                new ContentObserver(mBgHandler) {
+                    @Override
+                    public void onChange(boolean selfChange, Collection<Uri> collection, int flags,
+                            int userId) {
+                        restartSystemUI();
+                    }
+                },
+                UserHandle.USER_ALL);
+
         mSystemSettings.registerContentObserverForUser(
                 Settings.System.getUriFor(Settings.System.STATUS_BAR_CLOCK_SIZE),
                 false,
@@ -590,7 +602,11 @@ public class ThemeOverlayController extends CoreStartable implements Dumpable {
         mConfigurationController.addCallback(mConfigurationListener);
     }
 
-    private void reevaluateSystemTheme(boolean forceReload) {
+    private void restartSystemUI() {
+        android.os.Process.killProcess(android.os.Process.myPid());
+    }
+
+    protected void reevaluateSystemTheme(boolean forceReload) {
         final WallpaperColors currentColors = mCurrentColors.get(mUserTracker.getUserId());
         final int mainColor;
         if (currentColors == null) {
