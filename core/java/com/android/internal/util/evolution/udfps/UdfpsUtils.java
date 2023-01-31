7631f3334a5c47ad9057b36f4dc0e3ddc0bfff7c
Joey Huab <joey@evolution-x.org>
2023-01-31 08:42:46 +0000

    UdfpsUtils: Use config_supportsUdfps to check for all Udfps devices
    * Set to true by default as most newer devices have Udfps.
    * Legacy devices should just set to false in their trees.

--- core/java/com/android/internal/util/evolution/udfps/UdfpsUtils.java
+++ core/java/com/android/internal/util/evolution/udfps/UdfpsUtils.java
@@ -17,13 +17,8 @@ package com.android.internal.util.evolution.udfps;
 
 import android.content.Context;
 
-import com.android.internal.util.ArrayUtils;
-
 public class UdfpsUtils {
     public static boolean hasUdfpsSupport(Context context) {
-        int[] udfpsProps = context.getResources().getIntArray(
-                com.android.internal.R.array.config_udfps_sensor_props);
-
-        return !ArrayUtils.isEmpty(udfpsProps);
+        return context.getResources().getBoolean(com.android.internal.R.bool.config_supportsUdfps);
     }
 }
