e5c75a9e8846bd4b9baec299a622b5d56c6f17bc
Joey Huab <joey@evolution-x.org>
2023-02-12 15:00:53 +0000

    base: Grant MIUI Gallery storage permissions

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -959,6 +959,10 @@ final class DefaultPermissionGrantPolicy {
         // OnePlus Gallery
         grantSystemFixedPermissionsToSystemPackage(pm, "com.oneplus.gallery", userId,
                 STORAGE_PERMISSIONS);
+
+        // MIUI Gallery
+        grantSystemFixedPermissionsToSystemPackage(pm, "com.miui.gallery", userId,
+                STORAGE_PERMISSIONS);
     }
 
     private String getDefaultSystemHandlerActivityPackageForCategory(PackageManagerWrapper pm,
