2dafc02627d5411259b6232bda2d892f773dc53a
jhonboy121 <alfredmathew05@gmail.com>
2023-02-17 11:23:04 +0800

    SystemUI: Fade filter for media notification [1/2]
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6902,6 +6902,12 @@ public final class Settings {
         public static final String MEDIA_ARTWORK_BLUR_RADIUS = "media_artwork_blur_radius";
 
         /**
+         * Adjust fade percent for background media
+         * @hide
+         */
+        public static final String MEDIA_ARTWORK_FADE_PERCENT = "media_artwork_fade_percent";
+
+        /**
          * lockscreen album art blur level
          * @hide
          */
