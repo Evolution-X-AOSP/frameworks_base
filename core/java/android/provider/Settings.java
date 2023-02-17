0292154d0b6b668df58e7ffd21276447028515d3
minaripenguin <minaripenguin@users.noreply.github.com>
2023-02-17 19:36:42 +0800

    SystemUI: Fade filter for lockscreen media artwork [1/2]
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6908,6 +6908,12 @@ public final class Settings {
         public static final String LS_MEDIA_FILTER_BLUR_RADIUS = "ls_media_filter_blur_radius";
 
         /**
+         * lockscreen album art fade level
+         * @hide
+         */
+        public static final String LS_MEDIA_ARTWORK_FADE_PERCENT = "ls_media_artwork_fade_percent";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
