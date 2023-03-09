2494f7ee70b155a49d55a64055308e9dcbb11e4c
minaripenguin <minaripenguin@users.noreply.github.com>
2023-03-09 08:06:40 +0800

    libs: hwui: Decrease dequeueBuffer timeout
    * the 4000_ms is a solution for the partially broken timeout recovery to avoid slow screen-on CL[1], but with CL[2] jreck introduced another fix wherein the timeout recovery is skipped for some paths, restoring P's behaviour.
    * with the context above, the 4000_ms introduced issues stated in 9bf12d4bd5a971a4dd8f728c3efb5589f30a8ea0, which in our case introduced hiccups when turning the device screen on (noticeable with pulse feature enabled).
    * to amend the hiccups restore the default timeout before CL[1] is applied. This should be fine since we already have CL[2] addressing the timeout recovery regressions.
    
    [1] 3f333d6d72fd2d91a68b2fd1fd2ca28291e6b1d9
    [2] cd18c2271d143fa842f0de40497a4685c52e2548
    
    Change-Id: I68cc4d111754d9ee55e045d304a469df40934cca
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- libs/hwui/renderthread/CanvasContext.cpp
+++ libs/hwui/renderthread/CanvasContext.cpp
@@ -198,7 +198,7 @@ void CanvasContext::setSurface(ANativeWindow* window, bool enableTimeout) {
         mNativeSurface->init();
         if (enableTimeout) {
             // TODO: Fix error handling & re-shorten timeout
-            ANativeWindow_setDequeueTimeout(window, 4000_ms);
+            ANativeWindow_setDequeueTimeout(window, 500_ms);
         }
     } else {
         mNativeSurface = nullptr;
