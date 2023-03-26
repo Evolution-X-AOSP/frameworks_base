namaskaragusthi/android_frameworks_base-rice

SystemUI: Media notification artwork blur filter [1/2]
Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -89,6 +89,8 @@ public class SystemSettings {
         Settings.System.DISPLAY_COLOR_MODE,
         Settings.System.ALARM_ALERT,
         Settings.System.NOTIFICATION_LIGHT_PULSE,
-        Settings.System.QS_PANEL_STYLE
+        Settings.System.QS_PANEL_STYLE,
+        Settings.System.MEDIA_ARTWORK_BLUR_ENABLED,
+        Settings.System.MEDIA_ARTWORK_BLUR_RADIUS
     };
 }

Evolution-X-AOSP/frameworks_base

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -120,6 +120,8 @@ public class SystemSettings {
 	Settings.System.QS_TILE_LABEL_HIDE,
         Settings.System.QS_TILE_VERTICAL_LAYOUT,
         Settings.System.QS_LAYOUT,
-        Settings.System.QQS_LAYOUT
+        Settings.System.QQS_LAYOUT,
+        Settings.System.MEDIA_ARTWORK_BLUR_ENABLED,
+        Settings.System.MEDIA_ARTWORK_BLUR_RADIUS
     };
 }