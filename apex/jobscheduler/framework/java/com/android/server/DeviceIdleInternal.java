23429769a72552bf73b293b5ff90bf4597a0844d
Joey Huab <joey@evolution-x.org>
2023-03-29 20:21:06 +0000

    Revert "DeviceIdleJobsController: don't ignore whitelisted system apps"
    This reverts commit da84cdc404803352eee032a0d9fec0ba1f2cf910.

--- apex/jobscheduler/framework/java/com/android/server/DeviceIdleInternal.java
+++ apex/jobscheduler/framework/java/com/android/server/DeviceIdleInternal.java
@@ -73,7 +73,7 @@ public interface DeviceIdleInternal {
 
     boolean isAppOnWhitelist(int appid);
 
-    int[] getPowerSaveWhitelistAppIds();
+    int[] getPowerSaveWhitelistUserAppIds();
 
     int[] getPowerSaveTempWhitelistAppIds();
 
