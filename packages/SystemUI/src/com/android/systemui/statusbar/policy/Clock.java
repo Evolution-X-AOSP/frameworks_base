8dd0199f603f482fc62750c23b3457e27e8a124a
Joey Huab <joey@evolution-x.org>
2023-03-31 17:19:05 +0000

    [TMP][SQUASH] Remove clock/date customization for QS
    Revert "QuickStatusBarHeader: Link date in QS statusbar with clock"
    
    This reverts commit 3967c8d7ef1068d4f3ceeee1405f03785653d26f.
    
    Revert "Add tunable to disable QS date [1/2]"
    
    This reverts commit 873c86f01b041a34af8eeaf9f6cb5e5147574cbc.
    
    Revert "Add separate tunable for QS clock"
    
    This reverts commit f3b857924687d6e17f87fb8f34b215270f90194b.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -84,7 +84,6 @@ public class Clock extends TextView implements
     private static final String VISIBLE_BY_USER = "visible_by_user";
     private static final String SHOW_SECONDS = "show_seconds";
     private static final String VISIBILITY = "visibility";
-    private static final String QSHEADER = "qsheader";
 
     public static final String STATUS_BAR_CLOCK_SECONDS =
             "system:" + Settings.System.STATUS_BAR_CLOCK_SECONDS;
@@ -153,7 +152,6 @@ public class Clock extends TextView implements
     private String mClockDateFormat = null;
     private boolean mClockAutoHide;
     private int mHideDuration = HIDE_DURATION, mShowDuration = SHOW_DURATION;
-    private boolean mQsHeader;
 
     private boolean mIsStatusBar;
 
@@ -208,7 +206,6 @@ public class Clock extends TextView implements
         bundle.putBoolean(VISIBLE_BY_USER, mClockVisibleByUser);
         bundle.putBoolean(SHOW_SECONDS, mShowSeconds);
         bundle.putInt(VISIBILITY, getVisibility());
-        bundle.putBoolean(QSHEADER, mQsHeader);
 
         return bundle;
     }
@@ -232,7 +229,6 @@ public class Clock extends TextView implements
         if (bundle.containsKey(VISIBILITY)) {
             super.setVisibility(bundle.getInt(VISIBILITY));
         }
-        mQsHeader = bundle.getBoolean(QSHEADER, false);
     }
 
     @Override
@@ -370,10 +366,6 @@ public class Clock extends TextView implements
         super.setVisibility(visibility);
     }
 
-    public void setQsHeader() {
-        mQsHeader = true;
-    }
-
     public void setClockVisibleByUser(boolean visible) {
         mClockVisibleByUser = visible;
         updateClockVisibility();
@@ -397,7 +389,7 @@ public class Clock extends TextView implements
             // Do nothing
         }
         setVisibility(visibility);
-        if (!mQsHeader && mClockAutoHide && visible && mScreenOn) {
+        if (mClockAutoHide && visible && mScreenOn) {
             autoHideHandler.postDelayed(()->autoHideClock(), mShowDuration * 1000);
         }
     }
@@ -634,7 +626,7 @@ public class Clock extends TextView implements
         String timeResult = mClockFormat.format(mCalendar.getTime());
         String dateResult = "";
 
-        if (!mQsHeader && mClockDateDisplay != CLOCK_DATE_DISPLAY_GONE) {
+        if (mClockDateDisplay != CLOCK_DATE_DISPLAY_GONE) {
             Date now = new Date();
 
             if (mClockDateFormat == null || mClockDateFormat.isEmpty()) {
