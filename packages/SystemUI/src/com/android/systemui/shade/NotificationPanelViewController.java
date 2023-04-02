74abbf852c55626e139b00cef542ae980d69b54a
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-03 07:58:51 +0800

    SystemUI: Improve QS notification expansion animation
    * attempt to reduce jitter/janks caused by unnecessary animations
    
    Change-Id: I3ae45921fa20f6afd332faa223d50a68dc17b6ae
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -3729,7 +3729,8 @@ public final class NotificationPanelViewController implements Dumpable {
         if (mQsExpandImmediate && !mQsFullyExpanded) {
             alpha = 0f;
         }
-        mNotificationStackScrollLayoutController.setAlpha(alpha);
+        float finalAlpha = alpha > 0.84f ? alpha : 0f;
+        mNotificationStackScrollLayoutController.setAlpha(finalAlpha);
         if (mBarState != StatusBarState.KEYGUARD && !isFullyCollapsed() && !isPanelVisibleBecauseOfHeadsUp()) {
             mCentralSurfaces.updateDismissAllVisibility(true);
         }
