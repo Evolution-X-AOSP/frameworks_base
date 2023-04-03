44ba0b808b7999cb916d712afb50e35c673c6327
Pranav Vashi <neobuddy89@gmail.com>
2023-04-03 22:08:56 +0530

    SystemUI: Block few clock customizations in QS header
    * Block auto hide and date
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/LargeScreenShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/LargeScreenShadeHeaderController.kt
@@ -286,6 +286,8 @@ class LargeScreenShadeHeaderController @Inject constructor(
             privacyIconsController.onParentVisible()
         }
 
+        clock.setQsHeader()
+
         clock.setOnClickListener {
             activityStarter.postStartActivityDismissingKeyguard(
                 Intent(AlarmClock.ACTION_SHOW_ALARMS), 0
