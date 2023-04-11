22405433923ab04847e5ace5dbb45c851901e570
ezio84 <brabus84@gmail.com>
2023-04-11 20:20:59 +0300

    base: Allow showing Ambient instead of fully waking [1/2]
    Hey Google, please kang only good features by iOS -_-'
    
    Ido Ben-Hur (YAAP):
    Separate for dt2w and lift to wake
    Better code
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Change-Id: I4a0db9bd89abecab4ec8fce45795a37095f4c638
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13655,6 +13655,19 @@ public final class Settings {
                 RING_HOME_BUTTON_BEHAVIOR_DO_NOTHING;
 
         /**
+         * Whether to show ambient instead of waking for the dt2w gesture
+         * @hide
+         */
+        public static final String DOZE_DOUBLE_TAP_GESTURE_AMBIENT = "doze_double_tap_gesture_ambient";
+
+        /**
+         * Whether to show ambient instead of waking for the pickup gesture
+         * Do note quick pickup (device sensor) is already configured to do that
+         * @hide
+         */
+        public static final String DOZE_PICK_UP_GESTURE_AMBIENT = "doze_pick_up_gesture_ambient";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -266,5 +266,7 @@ public class SecureSettings {
         Settings.Secure.NFC_SOUNDS,
         Settings.Secure.EXTRA_DIM_AUTO_MODE,
         Settings.Secure.EXTRA_DIM_AUTO_TIME,
+        Settings.Secure.DOZE_DOUBLE_TAP_GESTURE_AMBIENT,
+        Settings.Secure.DOZE_PICK_UP_GESTURE_AMBIENT
     };
 }
