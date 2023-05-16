edb3d782f98e39f3235e0f1b22a77db44a6e17a9
Joey Huab <joey@evolution-x.org>
2023-05-16 18:52:43 +0900

    Revert "SystemUI: Dynamically adjust Lockscreen clock font spacing"
    This reverts commit 2f884603b73d3e09c3b72e83143372a99f65d5eb.

--- core/java/com/android/internal/util/evolution/EvolutionUtils.java
+++ core/java/com/android/internal/util/evolution/EvolutionUtils.java
@@ -198,23 +198,6 @@ public class EvolutionUtils {
         return false;
     }
 
-    // Method to detect whether a clock font overlay is enabled or not
-    public static boolean isClockFontEnabled(String fontName) {
-        mOverlayService = new OverlayManager();
-        try {
-            List<OverlayInfo> infos = mOverlayService.getOverlayInfosForTarget("android",
-                    UserHandle.myUserId());
-            for (int i = 0, size = infos.size(); i < size; i++) {
-                if (infos.get(i).packageName.contains(fontName)) {
-                    return infos.get(i).isEnabled();
-                }
-            }
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
     public static boolean isWifiOnly(Context context) {
         return !context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
     }
