e8176713f8ca25aba1e4a4b1dc0183bf5598953b
nift4 <nift4@protonmail.com>
2023-05-24 19:20:36 +0200

    Add pointer capture callback
    Change-Id: I6c86b7adfc9e10eae7dae5ba5a155630f4fe1a57

--- services/core/java/com/android/server/input/InputManagerService.java
+++ services/core/java/com/android/server/input/InputManagerService.java
@@ -583,6 +583,22 @@ public class InputManagerService extends IInputManager.Stub
         }
     }
 
+    // Must be called on handler
+    private void deliverCaptureChanged(boolean enabled) {
+        mTempCursorCallbacksToNotify.clear();
+        final int numListeners;
+        synchronized (mCursorCbLock) {
+            numListeners = mCursorCallbacks.size();
+            for (int i = 0; i < numListeners; i++) {
+                mTempCursorCallbacksToNotify.add(
+                        mCursorCallbacks.valueAt(i));
+            }
+        }
+        for (int i = 0; i < numListeners; i++) {
+            mTempCursorCallbacksToNotify.get(i).notifyCaptureChanged(enabled);
+        }
+    }
+
     public void start() {
         Slog.i(TAG, "Starting input manager");
         mNative.start();
@@ -1874,6 +1890,7 @@ public class InputManagerService extends IInputManager.Stub
     public void requestPointerCapture(IBinder inputChannelToken, boolean enabled) {
         Objects.requireNonNull(inputChannelToken, "event must not be null");
 
+        deliverCaptureChanged(enabled);
         mNative.requestPointerCapture(inputChannelToken, enabled);
     }
 
@@ -3877,6 +3894,16 @@ public class InputManagerService extends IInputManager.Stub
             } catch (RemoteException ex) {
                 Slog.w(TAG, "Failed to notify process " + mPid +
                         " that cursor changed, assuming it died.", ex);
+                binderDied();
+            }
+        }
+
+        public void notifyCaptureChanged(boolean enabled) {
+            try {
+                mListener.onCaptureChanged(enabled);
+            } catch (RemoteException ex) {
+                Slog.w(TAG, "Failed to notify process " + mPid +
+                        " that capture changed, assuming it died.", ex);
                 binderDied();
             }
         }
