de95240ddd7ff79a7c242ae8adcf5349a483bf10
Juhyung Park <qkrwngud825@gmail.com>
2023-05-26 23:42:32 +0900

    SystemUI: Doze: Add config to check proximity on single tap
    Based on I0aec237f12fff889ed2131ec0269c2d17d562510
    by Cosmin Tanislav <demonsingur@gmail.com>.
    
    Change-Id: I9a4b8ab0d841e158b0b7064dced47113dc130047
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
@@ -222,6 +222,7 @@ public class DozeSensors {
                         true /* touchscreen */,
                         false /* ignoresSetting */,
                         dozeParameters.singleTapUsesProx(mDevicePosture) /* requiresProx */,
+                        !dozeParameters.singleTapNeedsProximityCheck() /* performsProxCheck */,
                         true /* immediatelyReRegister */,
                         mDevicePosture,
                         false
