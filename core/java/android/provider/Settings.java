d1dc350e410921119f0924c4e04e47eb9f30d0ce
someone5678 <nemui3353@gmail.com>
2023-05-31 00:46:15 +0900

    base: Spoof current storage encryption status [1/2]
    * Some apps like Health Connect wants device to be encrypted
    * Spoof cyrpto state to encrypted while disabling encryption possible
    
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14051,6 +14051,13 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_LYRIC = "status_bar_show_lyric";
 
         /**
+         * Control whether to spoof current status of storage encryption.
+         * @hide
+         */
+        @Readable
+        public static final String SPOOF_STORAGE_ENCRYPTION_STATUS = "spoof_storage_encryption_status";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
