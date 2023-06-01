4b49d79a4f2364d017c25ae9948c404efd9958ef
minaripenguin <minaripenguin@users.noreply.github.com>
2023-06-02 06:34:53 +0800

    SystemUI: Toggle dismiss all visibility before reticker animates
    Change-Id: I63935d4176c249d2b638ee3351fb07f00118d319
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -690,7 +690,6 @@ public final class NotificationPanelViewController implements Dumpable {
     private NotificationStackScrollLayout mNotificationStackScroller;
     private boolean mReTickerStatus;
     private boolean mReTickerColored;
-    private boolean mReTickerVisible = null;
     private boolean mIsAnimatingTicker = false;
     private boolean mIsDismissRequested = false;
 
@@ -3741,7 +3740,7 @@ public final class NotificationPanelViewController implements Dumpable {
         float finalAlpha = alpha > 0.84f ? alpha : 0f;
         mNotificationStackScrollLayoutController.setAlpha(finalAlpha);
         if (mBarState != StatusBarState.KEYGUARD && !isFullyCollapsed() && !isPanelVisibleBecauseOfHeadsUp()) {
-            mCentralSurfaces.updateDismissAllVisibility(mReTickerVisible != null && mReTickerVisible ? false : true);
+            mCentralSurfaces.updateDismissAllVisibility(true);
         }
     }
 
@@ -6642,7 +6641,6 @@ public final class NotificationPanelViewController implements Dumpable {
             mReTickerComebackIcon.setImageDrawable(icon);
 
             Drawable dw = getRetickerBackgroundDrawable(pkgname, notification.color);
-            mReTickerVisible = true;
             mReTickerComeback.setBackground(dw);
             mReTickerContentTV.setText(mergedContentText);
             mReTickerContentTV.setTextAppearance(mView.getContext(), R.style.TextAppearance_Notifications_reTicker);
@@ -6676,18 +6674,18 @@ public final class NotificationPanelViewController implements Dumpable {
         mNotificationStackScroller.setVisibility(getExpandedFraction() == 0 ? View.GONE : View.VISIBLE);
         if (getExpandedFraction() > 0) {
             mReTickerComeback.setVisibility(View.GONE);
-            mReTickerVisible = false;
         }
 
         if (mReTickerComeback.getVisibility() == View.VISIBLE) {
             mReTickerComeback.getViewTreeObserver().addOnComputeInternalInsetsListener(mInsetsListener);
         } else {
             mReTickerComeback.getViewTreeObserver().removeOnComputeInternalInsetsListener(mInsetsListener);
-            mReTickerVisible = false;
         }
     }
 
     public void retickerAnimate() {
+        mCentralSurfaces.updateDismissAllVisibility(false);
+
         if (mIsAnimatingTicker) {
             return; // Animation is already running
         }
@@ -6793,7 +6791,6 @@ public final class NotificationPanelViewController implements Dumpable {
             public void onAnimationEnd(Animator animation) {
                 mReTickerComeback.setVisibility(View.GONE);
                 mNotificationStackScroller.setVisibility(View.VISIBLE);
-                mReTickerVisible = false;
                 mReTickerComeback.getViewTreeObserver().removeOnComputeInternalInsetsListener(mInsetsListener);
             }
         });
