3fcc0185beba9fa8ef53e498591f4b2efad83f7c
Ido Ben-Hur <idoybh2@gmail.com>
2023-06-14 09:24:40 +0300

    DozeSensors: We DO want UDFPS to trigger on screen off
    Why did they do that? beats me....
    See this AOSP change:
    f3b7a9aff4b6c7b7fcb740c75b33045d17d655c2

--- packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
@@ -249,7 +249,7 @@ public class DozeSensors {
                         dozeParameters.longPressUsesProx(),
                         !dozeParameters.longPressNeedsProximityCheck() /* performsProxCheck */,
                         false /* immediatelyReRegister */,
-                        true /* requiresAod */
+                        false /* requiresAod */
                 ),
                 new PluginSensor(
                         new SensorManagerPlugin.Sensor(TYPE_WAKE_DISPLAY),
