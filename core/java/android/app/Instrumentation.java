698151a201a479311083f412bca8fdf9508829c7
Joey Huab <joey@evolution-x.org>
2023-06-25 20:22:12 +0900

    PixelPropsUtils: Refactor
    * Allows some devices to fully pass safetynet's strong attestation (maybe due to prebuilt vendor)
    * Do not spoof restore, pixelmigrate and setupwizard
    * Enable Photos spoof by default
    * Keep Photos happy and not count towards storage quota
    * Separate GamesProps into GamesPropsUtils
    * Switch from POCO F4 to POCO F5
    * Transfer GMS spoofing from AttestationHooks
    
    Co-authored-by: someone5678 <nemui3353@gmail.com>
    Co-authored-by: aswin7469 <aswinas@pixysos.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -68,6 +68,7 @@ import java.util.List;
 import java.util.concurrent.TimeoutException;
 
 import com.android.internal.util.evolution.AttestationHooks;
+import com.android.internal.util.evolution.GamesPropsUtils;
 import com.android.internal.util.evolution.PixelPropsUtils;
 
 /**
@@ -1285,6 +1286,7 @@ public class Instrumentation {
                 .instantiateApplication(cl, className);
         app.attach(context);
         AttestationHooks.setProps(context);
+        GamesPropsUtils.setProps(context);
         PixelPropsUtils.setProps(context);
         return app;
     }
@@ -1304,6 +1306,7 @@ public class Instrumentation {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
         AttestationHooks.setProps(context);
+        GamesPropsUtils.setProps(context);
         PixelPropsUtils.setProps(context);
         return app;
     }
