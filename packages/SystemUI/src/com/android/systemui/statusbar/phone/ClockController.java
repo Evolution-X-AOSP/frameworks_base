4b44e65118622d0ee7ffb5773c36655d94bbdb60
AnierinB <anierin@evolution-x.org>
2023-06-28 17:45:07 -0400

    SystemUI: ClockController: Prevent NPE when clock is hidden
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ClockController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ClockController.java
@@ -66,6 +66,10 @@ public class ClockController implements TunerService.Tunable {
     }
 
     private void updateActiveClock() {
+        if (mClockPosition == CLOCK_POSITION_HIDE && mActiveClock != null) {
+            Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(mActiveClock);
+        }
+
         switch (mClockPosition) {
             case CLOCK_POSITION_RIGHT:
                 mActiveClock = mRightClock;
@@ -108,11 +112,15 @@ public class ClockController implements TunerService.Tunable {
     }
 
     public void addDarkReceiver() {
-        Dependency.get(DarkIconDispatcher.class).addDarkReceiver(mActiveClock);
+        if (mActiveClock != null) {
+            Dependency.get(DarkIconDispatcher.class).addDarkReceiver(mActiveClock);
+        }
     }
 
     public void removeDarkReceiver() {
-        Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(mActiveClock);
+        if (mActiveClock != null) {
+            Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(mActiveClock);
+        }
     }
 
     public void onDensityOrFontScaleChanged() {
