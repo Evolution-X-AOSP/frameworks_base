bb242dcca297943b7fa2e6fe195a757824eab727
someone5678 <nemui3353@gmail.com>
2023-07-12 09:40:03 +0900

    StatusBarNetworkTraffic: Remove custom clock position
    * We don't implement custom clock position
    
    Change-Id: Ia2a505c512cf25295b860c073c34aa95db46bb37

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/StatusBarNetworkTraffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/StatusBarNetworkTraffic.java
@@ -324,7 +324,6 @@ public class StatusBarNetworkTraffic extends TextView implements DarkReceiver,
             case NETWORK_TRAFFIC_ENABLED:
                 mEnabled =
                         TunerService.parseIntegerSwitch(newValue, false);
-                onEnableChange();
                 if (mEnabled) {
                     setLines(2);
                     String txtFont = getResources().getString(R.string.config_bodyFontFamily);
@@ -458,22 +457,6 @@ public class StatusBarNetworkTraffic extends TextView implements DarkReceiver,
                 mSystemIconVisible = false;
                 break;
         }
-    }
-
-    private void onEnableChange() {
-        if (mEnabled && !isClockOnLeft()) {
-            moveClockToLeft();
-        }
-    }
-
-    private boolean isClockOnLeft() {
-        return Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.STATUS_BAR_CLOCK, 2) == 2;
-    }
-
-    private void moveClockToLeft() {
-        Settings.System.putInt(mContext.getContentResolver(),
-                Settings.System.STATUS_BAR_CLOCK, 2);
     }
 
     private void updateVisibility() {
