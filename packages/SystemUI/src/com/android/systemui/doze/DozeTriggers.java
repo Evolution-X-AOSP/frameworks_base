5c92eee711f320e487edc108d224ec068f7f5654
Ido Ben-Hur <idoybh2@gmail.com>
2023-08-05 03:09:37 +0300

    base: Allow overlaying single tap gesture invocation delay
    Useful for devices that don't have the delay already
    And have both single tap and double tap enabled
    Disabled by default
    
    Change-Id: I31025b2f7952a2f7828f6818cccbc55bf3f37a08

--- packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -28,6 +28,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.hardware.display.AmbientDisplayConfiguration;
+import android.os.Looper;
+import android.os.Handler;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.text.format.Formatter;
@@ -106,9 +108,12 @@ public class DozeTriggers implements DozeMachine.Part {
     private final UserTracker mUserTracker;
     private final SelectedUserInteractor mSelectedUserInteractor;
     private final UiEventLogger mUiEventLogger;
+    private final Handler mMainHandler = new Handler(Looper.getMainLooper());
+    private final int mTapDelay;
 
     private long mNotificationPulseTime;
     private Runnable mAodInterruptRunnable;
+    private Object mTapToken;
 
     /** see {@link #onProximityFar} prox for callback */
     private boolean mWantProxSensor;
@@ -227,6 +232,8 @@ public class DozeTriggers implements DozeMachine.Part {
         mKeyguardStateController = keyguardStateController;
         mUserTracker = userTracker;
         mSelectedUserInteractor = selectedUserInteractor;
+        mTapDelay = mContext.getResources().getInteger(
+                com.android.internal.R.integer.config_singleTapDelay);
     }
 
     @Override
@@ -328,9 +335,19 @@ public class DozeTriggers implements DozeMachine.Part {
                     mDozeLog.traceSensorEventDropped(pulseReason, "prox reporting near");
                     return;
                 }
-                if (isDoubleTap || isTap) {
+                if (isDoubleTap || (isTap && mTapDelay <= 0)) {
+                    if (mTapToken != null) {
+                        mMainHandler.removeCallbacksAndMessages(mTapToken);
+                        mTapToken = null;
+                    }
                     mDozeHost.onSlpiTap(screenX, screenY);
                     gentleWakeUp(pulseReason);
+                } else if (isTap) {
+                    mMainHandler.postDelayed(() -> {
+                        mDozeHost.onSlpiTap(screenX, screenY);
+                        gentleWakeUp(pulseReason);
+                        mTapToken = null;
+                    }, mTapToken, mTapDelay);
                 } else if (isPickup) {
                     if (shouldDropPickupEvent())  {
                         mDozeLog.traceSensorEventDropped(pulseReason, "keyguard occluded");
