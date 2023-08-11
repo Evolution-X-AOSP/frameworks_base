08b252fdda1009734eea9172b995169d75b0d462
minaripenguin <minaripenguin@users.noreply.github.com>
2023-08-12 07:17:45 +0800

    Toast: Perform text scrolling for long toast texts
    Change-Id: Id3b3a465459903b5e0ce68746447f7f78e3624f4
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/widget/ToastPresenter.java
+++ core/java/android/widget/ToastPresenter.java
@@ -68,6 +68,7 @@ public class ToastPresenter {
         View view = LayoutInflater.from(context).inflate(TEXT_TOAST_LAYOUT, null);
         TextView textView = view.findViewById(com.android.internal.R.id.message);
         textView.setText(text);
+        textView.setSelected(true);
         return view;
     }
 
@@ -82,6 +83,7 @@ public class ToastPresenter {
         View view = LayoutInflater.from(context).inflate(TEXT_TOAST_LAYOUT_WITH_ICON, null);
         TextView textView = view.findViewById(com.android.internal.R.id.message);
         textView.setText(text);
+        textView.setSelected(true);
         ImageView imageView = view.findViewById(com.android.internal.R.id.icon);
         if (imageView != null) {
             imageView.setImageDrawable(icon);
