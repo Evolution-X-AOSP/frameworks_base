b45f3834479f500fde10ad4ee57a535f8338f5b3
minaripenguin <minaripenguin@users.noreply.github.com>
2023-08-24 14:31:21 +0800

    Pulse: Prevent systemui-related crashes
    Change-Id: Ib03074c31cd835c0e6e6a9a29cf029ededfba68e
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/pulse/SolidLineRenderer.java
+++ packages/SystemUI/src/com/android/systemui/pulse/SolidLineRenderer.java
@@ -185,9 +185,11 @@ public class SolidLineRenderer extends Renderer {
 
     @Override
     public void onFFTUpdate(byte[] fft) {
+        if (fft == null || fft.length < mUnits * 2 + 2) {
+            return;
+        }
         int fudgeFactor = mKeyguardShowing ? mDbFuzzFactor * 4 : mDbFuzzFactor;
-        int i = 0;
-        for (; i < (mCenterMirrored ? (mUnits / 4) : mUnits); i++) {
+        for (int i = 0; i < (mCenterMirrored ? (mUnits / 4) : mUnits) && i * 2 + 3 < fft.length; i++) {
             if (mValueAnimators[i] == null) continue;
             mValueAnimators[i].cancel();
             rfk = fft[i * 2 + 2];
@@ -220,7 +222,7 @@ public class SolidLineRenderer extends Renderer {
             mValueAnimators[i].start();
         }
         if (mCenterMirrored) {
-            for (; i < mUnits; i++) {
+            for (int i = (mUnits / 4); i < mUnits && (mUnits - (i + 1)) * 2 + 3 < fft.length; i++) {
                 int j = mUnits - (i + 1);
                 if (mValueAnimators[i] == null) continue;
                 mValueAnimators[i].cancel();
