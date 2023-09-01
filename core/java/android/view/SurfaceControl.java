1d88081e1715b1c47c72a61178bf263acf26fa30
minaripenguin <minaripenguin@users.noreply.github.com>
2023-09-01 08:45:37 +0800

    SurfaceControl: Prevent crash when surfacecontrol is null
    * while surfacecontroll is non-null there were some instances that null value is passed to checkPreconditions
    
    fixes:
    java.lang.NullPointerException: Attempt to invoke direct method 'void android.view.SurfaceControl.checkNotReleased()' on a null object reference
    
    Change-Id: I8ce78951326f19cbd753e7f9535a444a607a6c56
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/view/SurfaceControl.java
+++ core/java/android/view/SurfaceControl.java
@@ -2705,7 +2705,9 @@ public final class SurfaceControl implements Parcelable {
          * @hide
          */
         protected void checkPreconditions(SurfaceControl sc) {
-            sc.checkNotReleased();
+            if (sc != null) {
+                sc.checkNotReleased();
+            }
         }
 
         /**
