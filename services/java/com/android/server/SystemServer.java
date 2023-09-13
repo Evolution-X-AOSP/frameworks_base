7855ffbff78eb828f97c02cc468e87c6848e9d76
Joey Huab <joey@evolution-x.org>
2023-09-14 03:20:10 +0900

    SystemServer: Add configs for Lineage Health and LiveDisplay services

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -140,8 +140,9 @@ import com.android.server.input.InputManagerService;
 import com.android.server.inputmethod.InputMethodManagerService;
 import com.android.server.integrity.AppIntegrityManagerService;
 import com.android.server.lights.LightsService;
-import com.android.server.lineage.display.LiveDisplayService;
 import com.android.server.lineage.LineageHardwareService;
+import com.android.server.lineage.display.LiveDisplayService;
+import com.android.server.lineage.health.HealthInterfaceService;
 import com.android.server.locales.LocaleManagerService;
 import com.android.server.location.LocationManagerService;
 import com.android.server.logcat.LogcatManagerService;
@@ -222,7 +223,6 @@ import com.android.server.webkit.WebViewUpdateService;
 import com.android.server.wm.ActivityTaskManagerService;
 import com.android.server.wm.WindowManagerGlobalLock;
 import com.android.server.wm.WindowManagerService;
-import com.android.server.lineage.health.HealthInterfaceService;
 
 import dalvik.system.VMRuntime;
 
@@ -2578,12 +2578,18 @@ public final class SystemServer implements Dumpable {
                 t.traceBegin("StartLineageHardwareService");
                 mSystemServiceManager.startService(LineageHardwareService.class);
                 t.traceEnd();
-                t.traceBegin("StartLiveDisplayService");
-                mSystemServiceManager.startService(LiveDisplayService.class);
-                t.traceEnd();
-                t.traceBegin("StartHealthService");
-                mSystemServiceManager.startService(HealthInterfaceService.class);
-                t.traceEnd();
+                if (context.getResources().getBoolean(
+                        com.android.internal.R.bool.config_enableLiveDisplay)) {
+                    t.traceBegin("StartLiveDisplayService");
+                    mSystemServiceManager.startService(LiveDisplayService.class);
+                    t.traceEnd();
+                }
+                if (context.getResources().getBoolean(
+                        com.android.internal.R.bool.config_lineageHealthSupported)) {
+                    t.traceBegin("StartHealthService");
+                    mSystemServiceManager.startService(HealthInterfaceService.class);
+                    t.traceEnd();
+                }
             }
 
             if (context.getResources().getBoolean(
