2f8c0d865ae45b5c98e675a018d462d18519c1e9
Dhina17 <dhinalogu@gmail.com>
2023-09-20 23:31:06 +0530

    PhoneWindowManager: Pass correct invocation type for assistant launch on home long press
    When we launch assistant by long pressing home button,
    we should pass the INVOCATION_TYPE_HOME_BUTTON_LONG_PRESS as invocation type.
    
    It was modified by mistake in I8e8af90ac8fc7e96f73f2c68056de533d9157fa6.
    
    Change-Id: Id38b8f72f605a26b5fd6c976b2e37a26ed97c569
    (cherry picked from commit 0aa69a410a72297a11015ebb9fd49ab81f38766a)

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2111,6 +2111,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private void performKeyAction(Action action, KeyEvent event) {
+        // By default, pass INVOCATION_TYPE_UNKNOWN to launch assistant.
+        performKeyAction(action, event, AssistUtils.INVOCATION_TYPE_UNKNOWN);
+    }
+
+    private void performKeyAction(Action action, KeyEvent event, int assistInvocationType) {
         switch (action) {
             case NOTHING:
                 break;
@@ -2122,7 +2127,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 break;
             case SEARCH:
                 launchAssistAction(null, event.getDeviceId(), event.getEventTime(),
-                    AssistUtils.INVOCATION_TYPE_UNKNOWN);
+                    assistInvocationType);
                 break;
             case VOICE_SEARCH:
                 launchAssistLongPressAction();
@@ -2313,7 +2318,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     mHomePressed = true;
                     performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, false,
                             "Home - Long Press");
-                    performKeyAction(mHomeLongPressAction, event);
+                    // If long press home will launch assistant,
+                    // it should pass this right invocation type.
+                    performKeyAction(mHomeLongPressAction, event,
+                            AssistUtils.INVOCATION_TYPE_HOME_BUTTON_LONG_PRESS);
                     if (mHomeLongPressAction != Action.SLEEP) {
                         mHomeConsumed = true;
                     }
