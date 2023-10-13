c5d045e18c9a36e77d6b40865bb0867665fd631a
minaripenguin <minaripenguin@users.noreply.github.com>
2023-10-13 11:34:43 +0800

    services: Introduce Pocket Mode feature [1/2]
    reference used: https://github.com/minaripenguin/android_frameworks_base/commit/e3ccb262279f83e39e3935995b1d3a9178577d72
    
    Change-Id: I24ce7e6e1b81728810628227594b13418b142b10
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -445,6 +445,9 @@ public final class SystemServer implements Dumpable {
     private static final String DEVICE_LOCK_APEX_PATH =
             "/apex/com.android.devicelock/javalib/service-devicelock.jar";
 
+    private static final String POCKETLOCK_SERVICE_CLASS =
+            "org.rising.server.PocketModeService";
+
     private static final String TETHERING_CONNECTOR_CLASS = "android.net.ITetheringConnector";
 
     private static final String PERSISTENT_DATA_BLOCK_PROP = "ro.frp.pst";
@@ -2884,6 +2887,10 @@ public final class SystemServer implements Dumpable {
 
         t.traceBegin("AppCompatOverridesService");
         mSystemServiceManager.startService(APP_COMPAT_OVERRIDES_SERVICE_CLASS);
+        t.traceEnd();
+
+        t.traceBegin("StartPocketLockService");
+        mSystemServiceManager.startService(POCKETLOCK_SERVICE_CLASS);
         t.traceEnd();
 
         t.traceBegin("HealthConnectManagerService");
