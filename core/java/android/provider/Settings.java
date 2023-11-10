68fb42b12d466fd501e82a1aa42a83362c524a69
Joey Huab <joey@evolution-x.org>
2023-11-10 12:30:25 +0900

    Revert "SystemUI: Separate double tap to sleep on lockscreen [1/2]"
    This reverts commit 579bc9365f56a46778bc0525ff93e13a09a0b18b.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6464,12 +6464,6 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
-         * Double tap on lockscreen to sleep
-         * @hide
-         */
-        public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN = "double_tap_sleep_lockscreen";
-
-        /**
          * Whether to disable qs on secure lockscreen.
          * @hide
          */
