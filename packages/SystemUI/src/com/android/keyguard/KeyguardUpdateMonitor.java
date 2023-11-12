b046449e45d94855d3ffd829049af4dafd894f06
junklu <quic_junklu@quicinc.com>
2023-11-12 09:15:31 +0800

    SystemUI: Handle the SIM_STATE_NOT_READY state
    The SIM_STATE_NOT_READY state is ignored when the multi-sim
    configuration is dynamically changed between DSDS and SSSS.
    It will lead to sim pin view reappear after entering the correct pin.
    
    In this case, handle SIM_STATE_NOT_READY state when it is received.
    
    Change-Id: I95ec7d07b64c0fc7eec9056b41af94f748d94929
    CRs-Fixed: 3428634
    [adapted to aosp]
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -3256,6 +3256,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         mLogger.logSimState(subId, slotId, state);
 
         boolean becameAbsent = false;
+        boolean becameNotReady = false;
         if (!SubscriptionManager.isValidSubscriptionId(subId)) {
             mLogger.w("invalid subId in handleSimStateChange()");
             /* Only handle No SIM(ABSENT) and Card Error(CARD_IO_ERROR) due to
@@ -3274,6 +3275,13 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
                 }
             } else if (state == TelephonyManager.SIM_STATE_CARD_IO_ERROR) {
                 updateTelephonyCapable(true);
+            } else if (state == TelephonyManager.SIM_STATE_NOT_READY) {
+                becameNotReady = true;
+                for (SimData data : mSimDatas.values()) {
+                    if (data.slotId == slotId) {
+                        data.simState = TelephonyManager.SIM_STATE_NOT_READY;
+                    }
+                }
             }
         }
 
@@ -3291,7 +3299,8 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
             data.subId = subId;
             data.slotId = slotId;
         }
-        if ((changed || becameAbsent)) {
+        if ((changed || becameAbsent || becameNotReady)
+                && state != TelephonyManager.SIM_STATE_UNKNOWN) {
             for (int i = 0; i < mCallbacks.size(); i++) {
                 KeyguardUpdateMonitorCallback cb = mCallbacks.get(i).get();
                 if (cb != null) {
