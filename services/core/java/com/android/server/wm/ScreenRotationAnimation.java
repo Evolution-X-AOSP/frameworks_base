e423cad479dd22e3e73b072a79f12262b59caf50
Divya Dubey <divya.a.dubey@sony.com>
2023-11-14 11:12:33 +0900

    Fix delay in AOD rotation
    Symptom:
    There is a delay in rotation of AOD. Also sometimes screen gets stuck in
    between animation.
    
    Root cause:
    When AOD rotation happens and before animation starts, wakelock is
    acquired by WindowManagerService but released before the animation can
    finish completely. Because of which there is a lag and screen gets
    stuck in between animation.
    
    Solution:
    Acquiring a wakelock when animation starts and releasing it when the
    animation ends.
    
    Bug: 311618372
    Change-Id: I4a5a55c8be28ee0433e7ec91b7152f6401cf59c2
    Signed-off-by: ryzenforce990 <ryzenforce990@gmail.com>

--- services/core/java/com/android/server/wm/ScreenRotationAnimation.java
+++ services/core/java/com/android/server/wm/ScreenRotationAnimation.java
@@ -42,6 +42,7 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.hardware.HardwareBuffer;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.os.Trace;
 import android.util.Slog;
 import android.util.proto.ProtoOutputStream;
@@ -115,6 +116,8 @@ class ScreenRotationAnimation {
     private SurfaceControl mBackColorSurface;
     private BlackFrame mEnteringBlackFrame;
 
+    private final PowerManager.WakeLock mWakeLock;
+
     private final int mOriginalRotation;
     private final int mOriginalWidth;
     private final int mOriginalHeight;
@@ -143,6 +146,10 @@ class ScreenRotationAnimation {
         final Rect currentBounds = displayContent.getBounds();
         final int width = currentBounds.width();
         final int height = currentBounds.height();
+
+        PowerManager powerManager = (PowerManager)mContext.getSystemService(Context.POWER_SERVICE);
+        mWakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "RotationAnimation");
+        mWakeLock.setReferenceCounted(false);
 
         // Screenshot does NOT include rotation!
         final DisplayInfo displayInfo = displayContent.getDisplayInfo();
@@ -483,6 +490,7 @@ class ScreenRotationAnimation {
             }
         }
 
+        mWakeLock.acquire(1000);
         if (customAnim) {
             mSurfaceRotationAnimationController.startCustomAnimation();
         } else {
@@ -823,6 +831,7 @@ class ScreenRotationAnimation {
                     kill();
                 }
                 mService.updateRotation(false, false);
+                mWakeLock.release();
             }
         }
 
