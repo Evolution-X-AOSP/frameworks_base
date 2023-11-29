0a5bd404d6cfe62a1fd5aaad563bd6ea0d8d8147
someone5678 <someone5678@users.noreply.github.com>
2023-11-30 00:48:53 +0900

    base: Don't use AOSP QS fling animation boost impl when CLO boost is enabled
    Change-Id: Ie680fda6564a9089d3ea00d7ca7efe2f82c491de

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -2212,13 +2212,12 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                 }
             });
         }
-        if (mLocalPowerManager != null) {
-            mLocalPowerManager.setPowerBoost(Boost.DISPLAY_UPDATE_IMMINENT, 200);
-        }
-
         if (mPerf != null) {
             String currentPackage = mView.getContext().getPackageName();
             mPerf.perfHint(BoostFramework.VENDOR_HINT_SCROLL_BOOST, currentPackage, -1, BoostFramework.Scroll.PANEL_VIEW);
+        } else if (mLocalPowerManager != null 
+                && !mView.getContext().getResources().getBoolean(com.android.internal.R.bool.config_supportsBoostFramework)) {
+            mLocalPowerManager.setPowerBoost(Boost.DISPLAY_UPDATE_IMMINENT, 200);
         }
         animator.addListener(new AnimatorListenerAdapter() {
             private boolean mCancelled;
