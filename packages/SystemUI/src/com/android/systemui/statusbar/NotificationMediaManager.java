45c72a0d7546497a8266562dead05746c9f58ee4
minaripenguin <minaripenguin@users.noreply.github.com>
2023-12-01 22:16:20 +0800

    Island: Implement Now Playing music ticker
    * misc cleanups and code streamline changes
    
    Change-Id: I14cef0fb8c097aa5c7d8e5d07e90f86bded7c38b
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -39,6 +39,7 @@ import android.media.session.MediaSession;
 import android.media.session.PlaybackState;
 import android.os.AsyncTask;
 import android.os.Trace;
+import android.provider.Settings;
 import android.service.notification.NotificationStats;
 import android.service.notification.StatusBarNotification;
 import android.text.TextUtils;
@@ -50,6 +51,7 @@ import android.widget.ImageView;
 
 import com.android.app.animation.Interpolators;
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.systemui.Dependency;
 import com.android.systemui.Dumpable;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.dagger.qualifiers.Main;
@@ -72,6 +74,8 @@ import com.android.systemui.statusbar.phone.LockscreenWallpaper;
 import com.android.systemui.statusbar.phone.ScrimController;
 import com.android.systemui.statusbar.phone.ScrimState;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
+import com.android.systemui.tuner.TunerService;
+import com.android.systemui.util.NotificationUtils;
 import com.android.systemui.util.Utils;
 import com.android.systemui.util.concurrency.DelayableExecutor;
 
@@ -99,6 +103,9 @@ public class NotificationMediaManager implements Dumpable {
     public static final boolean DEBUG_MEDIA = false;
 
     private static final String NOWPLAYING_SERVICE = "com.google.android.as";
+    private static final String ISLAND_NOTIFICATION =
+            "system:" + Settings.System.ISLAND_NOTIFICATION;
+
     private final StatusBarStateController mStatusBarStateController;
     private final SysuiColorExtractor mColorExtractor;
     private final KeyguardStateController mKeyguardStateController;
@@ -159,6 +166,9 @@ public class NotificationMediaManager implements Dumpable {
 
     private LockscreenWallpaper.WallpaperDrawable mWallapperDrawable;
 
+    private boolean mIslandEnabled;
+    private NotificationUtils notifUtils;
+
     private final MediaController.Callback mMediaListener = new MediaController.Callback() {
         @Override
         public void onPlaybackStateChanged(PlaybackState state) {
@@ -167,6 +177,13 @@ public class NotificationMediaManager implements Dumpable {
                 Log.v(TAG, "DEBUG_MEDIA: onPlaybackStateChanged: " + state);
             }
             if (state != null) {
+                if (mIslandEnabled) {
+                    if (PlaybackState.STATE_PLAYING == getMediaControllerPlaybackState(mMediaController) && mMediaMetadata != null) {
+                        notifUtils.showNowPlayingNotification(mMediaMetadata);
+                    } else {
+                        notifUtils.cancelNowPlayingNotification();
+                    }
+                }
                 if (!isPlaybackActive(state.getState())) {
                     clearCurrentMediaNotification();
                 }
@@ -182,6 +199,10 @@ public class NotificationMediaManager implements Dumpable {
             }
             mMediaArtworkProcessor.clearCache();
             mMediaMetadata = metadata;
+            if (mIslandEnabled) {
+                notifUtils.cancelNowPlayingNotification();
+                notifUtils.showNowPlayingNotification(metadata);
+            }
             dispatchUpdateMediaMetaData(true /* changed */, true /* allowAnimation */);
         }
     };
@@ -227,7 +248,23 @@ public class NotificationMediaManager implements Dumpable {
         setupNotifPipeline();
 
         dumpManager.registerDumpable(this);
+
+        notifUtils = new NotificationUtils(mContext);
+        Dependency.get(TunerService.class).addTunable(mTunable, ISLAND_NOTIFICATION);
     }
+
+    private final TunerService.Tunable mTunable = new TunerService.Tunable() {
+        @Override
+        public void onTuningChanged(String key, String newValue) {
+            switch (key) {
+                case ISLAND_NOTIFICATION:
+                    mIslandEnabled = TunerService.parseIntegerSwitch(newValue, true);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     private void setupNotifPipeline() {
         mNotifPipeline.addCollectionListener(new NotifCollectionListener() {
