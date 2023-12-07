755335e32df9b982ed31cd4657be9b7a953b6845
someone5678 <someone5678@users.noreply.github.com>
2023-12-08 04:23:12 +0900

    Island: Overrides showIsland in ShadeViewControllerEmptyImpl
    * Adapt to A14 QPR1 changes
    
    Change-Id: Id465ceaa0723d90066c4189b25ecbc13e60ebe77

--- packages/SystemUI/src/com/android/systemui/shade/ShadeViewControllerEmptyImpl.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ShadeViewControllerEmptyImpl.kt
@@ -90,6 +90,7 @@ class ShadeViewControllerEmptyImpl @Inject constructor() : ShadeViewController {
 
     override val shadeHeadsUpTracker = ShadeHeadsUpTrackerEmptyImpl()
     override val shadeFoldAnimator = ShadeFoldAnimatorEmptyImpl()
+    override fun showIsland(show: Boolean) {}
 }
 
 class ShadeHeadsUpTrackerEmptyImpl : ShadeHeadsUpTracker {
