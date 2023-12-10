674a98f511fad9864da203ba16714e98ef9fe378
Pranav Vashi <neobuddy89@gmail.com>
2023-12-10 08:35:49 +0530

    Allow setting quick QS rows [1/2]
    * Default to 2 for landscape since we use split shade by default
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6722,6 +6722,16 @@ public final class Settings {
         public static final String QS_TILE_LABEL_SIZE = "qs_tile_label_size";
 
         /**
+         * @hide
+         */
+        public static final String QQS_LAYOUT_ROWS_LANDSCAPE = "qqs_layout_rows_landscape";
+
+        /**
+         * @hide
+         */
+        public static final String QQS_LAYOUT_ROWS = "qqs_layout_rows";
+
+        /**
          * Whether to enable the ripple animation on fingerprint unlock
          * @hide
          */
