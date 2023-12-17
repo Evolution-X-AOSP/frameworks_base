cfeb35659c1495d5ae8b5210950761fe98d738bc
minaripenguin <minaripenguin@users.noreply.github.com>
2023-12-17 20:39:23 +0900

    Integrate Pixel framework hooks and wrappers
    [someone5678]
    * Add collapseShade, animateCollapseShade, and animateExpandNotificationsPanel
      for Quick Tap gesture
    
    Change-Id: I6f4203391d544205ea77107303fe2a5b1468e3c7
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -398,7 +398,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     /** The current squish amount for the predictive back animation */
     private float mCurrentBackProgress = 0.0f;
     @Deprecated
-    private KeyguardBottomAreaView mKeyguardBottomArea;
+    public KeyguardBottomAreaView mKeyguardBottomArea;
     private boolean mExpanding;
     private boolean mSplitShadeEnabled;
     /** The bottom padding reserved for elements of the keyguard measuring notifications. */
@@ -1585,6 +1585,10 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             mNotificationStackScrollLayoutController.setMaxDisplayedNotifications(-1);
             mNotificationStackScrollLayoutController.setKeyguardBottomPaddingForDebug(-1f);
         }
+    }
+
+    public void updateMaxDisplayedNotificationsWrapper(boolean recompute) {
+        updateMaxDisplayedNotifications(recompute);
     }
 
     private boolean shouldAvoidChangingNotificationsCount() {
@@ -3293,6 +3297,14 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     private boolean isPulseLightEnabled() {
         return Settings.Secure.getIntForUser(mView.getContext().getContentResolver(),
                 Settings.Secure.PULSE_AMBIENT_LIGHT, 0, UserHandle.USER_CURRENT) != 0;
+    }
+
+    public void setAmbientIndicationBottomPadding(int bottomPadding) {
+        mAmbientIndicationBottomPadding = bottomPadding;
+    }
+
+    public int getAmbientIndicationBottomPadding() {
+        return mAmbientIndicationBottomPadding;
     }
 
     @Override
@@ -5432,5 +5444,15 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         if (mUseIslandNotification && mUseHeadsUp) {
             mNotifIsland.updateIslandVisibility(getExpandedFraction());
         }
+    }
+
+    @Override
+    public NotificationStackScrollLayoutController getScrollerLayoutController() {
+        return mNotificationStackScrollLayoutController;
+    }
+
+    @Override
+    public KeyguardBottomAreaView getKeyguardBottomAreaView() {
+        return mKeyguardBottomArea;
     }
 }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfaces.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfaces.java
@@ -20,6 +20,7 @@ import static com.android.wm.shell.transition.Transitions.ENABLE_SHELL_TRANSITIO
 
 import android.annotation.Nullable;
 import android.app.ActivityOptions;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -43,6 +44,9 @@ import com.android.systemui.display.data.repository.DisplayMetricsRepository;
 import com.android.systemui.navigationbar.NavigationBarView;
 import com.android.systemui.plugins.ActivityStarter.OnDismissAction;
 import com.android.systemui.qs.QSPanelController;
+import com.android.systemui.shade.NotificationShadeWindowView;
+import com.android.systemui.shade.NotificationShadeWindowViewController;
+import com.android.systemui.shade.ShadeViewController;
 import com.android.systemui.shared.system.RemoteAnimationRunnerCompat;
 import com.android.systemui.statusbar.notification.row.ExpandableNotificationRow;
 import com.android.systemui.statusbar.policy.GameSpaceManager;
@@ -332,6 +336,16 @@ public interface CentralSurfaces extends Dumpable, LifecycleOwner {
      */
     ActivityLaunchAnimator.Controller getAnimatorControllerFromNotification(
             ExpandableNotificationRow associatedView);
+
+    void startActivity(Intent intent, boolean dismiss);
+
+    void startPendingIntentDismissingKeyguard(PendingIntent intent);
+
+    ShadeViewController getNotificationPanelViewController();
+
+    void wakeUpDeviceifDozing();
+
+    NotificationShadeWindowView getNotificationShadeWindowView();
 
     void brightnessControl(MotionEvent event);
 
