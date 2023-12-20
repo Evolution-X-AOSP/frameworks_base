502f808d1f62a8f6458cc23c52b4f41cf5255b7e
minaripenguin <minaripenguin@users.noreply.github.com>
2023-12-20 08:55:56 +0800

    SettingsLib: Use San Francisco font for iOS battery styles
    * it doesn't look good with bold sans serif
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SettingsLib/src/com/android/settingslib/graph/LandscapeBatteryDrawableiOS15.kt
+++ packages/SettingsLib/src/com/android/settingslib/graph/LandscapeBatteryDrawableiOS15.kt
@@ -158,7 +158,7 @@ open class LandscapeBatteryDrawableiOS15(private val context: Context, frameColo
     }
 
     private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).also { p ->
-        p.typeface = Typeface.create("sans-serif-condensed", Typeface.BOLD)
+        p.typeface = Typeface.create("sanfrancisco", Typeface.BOLD)
         p.textAlign = Paint.Align.CENTER
     }
 
