8146afe61e97f621b8404fa643cb9696df0d5029
minaripenguin <minaripenguin@users.noreply.github.com>
2023-12-20 15:57:12 +0800

    SystemUI: Cleanup face unlock icon animation
    Change-Id: I14c4a85eda678237f42448a139690941c96c5799
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -415,6 +415,7 @@ public class KeyguardIndicationController {
             R.id.keyguard_indication_text_bottom);
         mInitialTextColorState = mTopIndicationView != null
                 ? mTopIndicationView.getTextColors() : ColorStateList.valueOf(Color.WHITE);
+        mFaceIconView.setKeyguardColorState(mInitialTextColorState);
         if (mRotateTextViewController != null) {
             mRotateTextViewController.destroy();
         }
@@ -911,8 +912,6 @@ public class KeyguardIndicationController {
             mFaceIconView.setState(FaceUnlockImageView.State.NOT_VERIFIED);
         } else if (TextUtils.equals(biometricMessage, mContext.getString(R.string.face_unlock_recognizing))) {
             mFaceIconView.setState(FaceUnlockImageView.State.SCANNING);
-        } else {
-            mFaceIconView.setState(FaceUnlockImageView.State.HIDDEN);
         }
 
         mBiometricMessage = biometricMessage;
