4d8d3fe2905192e58b13331c64bc93ae4b165c4a
Joey Huab <joey@evolution-x.org>
2023-12-28 06:22:26 +0900

    Revert "SystemUI: VolumeDialogImpl: notifyVisible after the volume panel is fully hidden"
    This reverts commit c4b7b1e4cb2cf7403f76af58b27f6d012dc3fe58.

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -1775,7 +1775,6 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                     mIsAnimatingDismiss = false;
 
                     hideRingerDrawer();
-                    mController.notifyVisible(false);
                 }, 50));
         if (!shouldSlideInVolumeTray()) {
             animator.translationX(
