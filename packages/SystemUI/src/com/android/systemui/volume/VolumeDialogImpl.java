5855fedcd15e3f64181cc0dc34fd7c9253cfe431
LuK1337 <priv.luk@gmail.com>
2023-12-28 06:25:14 +0900

    fixup! VolumeDialogImpl: Add STREAM_NOTIFICATION row

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -1860,7 +1860,6 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
     protected void dismissH(int reason) {
         Trace.beginSection("VolumeDialogImpl#dismissH");
-
         // Avoid multiple animation calls on touch spams.
         if (!mShowing) {
             return;
@@ -1942,6 +1941,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     private boolean isExpandableRowH(VolumeRow row) {
         return row != null && row != mDefaultRow && !row.defaultStream
                 && (row.stream == STREAM_RING
+                        || row.stream == STREAM_NOTIFICATION
                         || row.stream == STREAM_ALARM
                         || row.stream == STREAM_MUSIC);
     }
