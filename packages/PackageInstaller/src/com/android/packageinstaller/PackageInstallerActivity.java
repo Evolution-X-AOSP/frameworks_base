e6fddb8278598bad9f4d145ddbba28d9fff77232
John Galt <johngaltfirstrun@gmail.com>
2024-01-29 08:43:53 -0500

    PackageInstaller: Fixup 4570be5be981c18a1c9fa4e8ad1a4434ab77d935
    Fix old version showing in most cases.

--- packages/PackageInstaller/src/com/android/packageinstaller/PackageInstallerActivity.java
+++ packages/PackageInstaller/src/com/android/packageinstaller/PackageInstallerActivity.java
@@ -162,6 +162,10 @@ public class PackageInstallerActivity extends Activity {
                 oldVersionView.setVisibility(View.VISIBLE);
                 mOk.setText(R.string.update_anyway);
             } else {
+                oldVersionView = requireViewById(R.id.installed_app_version);
+                ((TextView)oldVersionView).setText(
+                        getString(R.string.old_version_number, oldInfo.versionName));
+                oldVersionView.setVisibility(View.VISIBLE);
                 mOk.setText(R.string.update);
             }
         } else {
