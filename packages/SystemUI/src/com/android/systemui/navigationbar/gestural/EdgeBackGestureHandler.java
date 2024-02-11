683e6b3a4a318ea103e726381c3604e273780d63
someone5678 <someone5678@users.noreply.github.com>
2024-02-11 16:54:56 +0900

    Gesture nav: Make it cancellable
    * Referenced from commit "Implement edge long swipe gesture to new back gesture affordance"
    * Remove long back swipe timeout setting
    * Execute long swipe action when movement changed from down to up
    * Cancel when x pointer smaller than threshold
    * Use double click effect instead of heavy click
    
    Change-Id: Icab2864703d518f4fa78a438d0e225a094fa1308
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -262,7 +262,6 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
     private boolean mIsOnLeftEdge;
     private boolean mDeferSetIsOnLeftEdge;
 
-    private int mTimeout = 2000; //ms
     private int mLeftLongSwipeAction;
     private int mRightLongSwipeAction;
     private boolean mIsExtendedSwipe;
@@ -270,6 +269,8 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
     private int mRightVerticalSwipeAction;
     private Handler mHandler;
     private boolean mImeVisible;
+    private float mStartX;
+    private float mStartY;
 
     private boolean mIsAttached;
     private boolean mIsGestureHandlingEnabled;
@@ -554,7 +555,6 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
         }
         mIsBackGestureAllowed = !mIsButtonForcedVisible;
 
-        mTimeout = mGestureNavigationSettingsObserver.getLongSwipeTimeOut();
         mLeftLongSwipeAction = mGestureNavigationSettingsObserver.getLeftLongSwipeAction();
         mRightLongSwipeAction = mGestureNavigationSettingsObserver.getRightLongSwipeAction();
         mIsExtendedSwipe = mGestureNavigationSettingsObserver.getIsExtendedSwipe();
@@ -658,9 +658,8 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
     }
 
     private void vibrateBack(boolean light) {
-            AsyncTask.execute(() ->
-                    mVibrator.vibrate(VibrationEffect.get(light ? VibrationEffect.EFFECT_CLICK :
-                        VibrationEffect.EFFECT_HEAVY_CLICK, true  /* fallback */)));
+        AsyncTask.execute(() ->
+            mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_DOUBLE_CLICK, true  /* fallback */)));
     }
 
     public void onNavBarTransientStateChanged(boolean isTransient) {
@@ -1163,6 +1162,8 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
                             mIsTrackpadThreeFingerSwipe),
                     mDisabledForQuickstep, mGestureBlockingActivityRunning, mIsInPip, mDisplaySize,
                     mEdgeWidthLeft, mLeftInset, mEdgeWidthRight, mRightInset, mExcludeRegion));
+            mStartX = ev.getX();
+            mStartY = ev.getY();
         } else if (mAllowGesture || mLogGesture) {
             if (!mThresholdCrossed) {
                 // mThresholdCrossed is true only after the first move event
@@ -1220,12 +1221,6 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
                     } else if (dx > dy && dx > mTouchSlop) {
                         if (mAllowGesture) {
                             mThresholdCrossed = true;
-                            if (!mIsExtendedSwipe && ((mLeftLongSwipeAction != 0 && mIsOnLeftEdge)
-                                || (mRightLongSwipeAction != 0 && !mIsOnLeftEdge))) {
-                                mLongSwipeAction.setIsVertical(false);
-                                mHandler.postDelayed(mLongSwipeAction, (mTimeout - elapsedTime));
-                                // mThresholdCrossed is now set to true so on next move event the handler won't get triggered again
-                            }
                             // Capture inputs
                             mInputMonitor.pilferPointers();
                             if (mBackAnimation != null) {
@@ -1244,20 +1239,24 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             boolean isUp = action == MotionEvent.ACTION_UP;
             boolean isCancel = action == MotionEvent.ACTION_CANCEL;
             boolean isMove = action == MotionEvent.ACTION_MOVE;
-            if (isMove && mIsExtendedSwipe) {
+            float longSwipeThreshold = mDisplaySize.x * 0.45f;
+            float touchTranslation = Math.abs(ev.getX() - mStartX);
+            boolean almostLongSwipe = mIsExtendedSwipe && (touchTranslation > longSwipeThreshold);
+            if (isMove && almostLongSwipe) {
                 float deltaX = Math.abs(ev.getX() - mDownPoint.x);
                 float deltaY = Math.abs(ev.getY() - mDownPoint.y);
                 // give priority to horizontal (X) swipe
                 if (deltaX  > (int)((mDisplaySize.x / 4) * 2.5f)) {
                     mLongSwipeAction.setIsVertical(false);
-                    mLongSwipeAction.run();
                 }
                 if (deltaY  > (mDisplaySize.y / 4)) {
                     mLongSwipeAction.setIsVertical(true);
-                    mLongSwipeAction.run();
                 }
             }
-            if (isUp || isCancel) {
+            if (isUp && almostLongSwipe) {
+                mLongSwipeAction.run();
+                mHandler.removeCallbacksAndMessages(null);
+            } else if (isUp || isCancel) {
                 mHandler.removeCallbacksAndMessages(null);
             }
 
