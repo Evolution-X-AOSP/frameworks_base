79bab6c64fd2d52adec01b2ddb4fe7f99ccac46c
Tim Zimmermann <tim@linux4.de>
2024-02-24 05:54:52 +0100

    SystemUI: Don't animate QS tile text when opening activity/dialog from A11QS ui
    Change-Id: I6a1cb0ae2c71904ad99be1c140c2bc36ea859a29
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
@@ -55,6 +55,7 @@ import com.android.settingslib.Utils
 import com.android.systemui.FontSizeUtils
 import com.android.systemui.animation.LaunchableView
 import com.android.systemui.animation.LaunchableViewDelegate
+import com.android.systemui.animation.view.LaunchableLinearLayout
 import com.android.systemui.plugins.qs.QSIconView
 import com.android.systemui.plugins.qs.QSTile
 import com.android.systemui.plugins.qs.QSTile.BooleanState
@@ -228,7 +229,7 @@ open class QSTileViewImpl @JvmOverloads constructor(
             val iconContainerSize = context.resources.getDimensionPixelSize(R.dimen.qs_quick_tile_size)
             radiusActive = iconContainerSize / 2f
             radiusInactive = iconContainerSize / 4f
-            iconContainer = LinearLayout(context)
+            iconContainer = LaunchableLinearLayout(context)
             iconContainer.layoutParams = LayoutParams(iconContainerSize, iconContainerSize)
             iconContainer.clipChildren = false
             iconContainer.clipToPadding = false
@@ -534,6 +535,10 @@ open class QSTileViewImpl @JvmOverloads constructor(
 
     override fun setShouldBlockVisibilityChanges(block: Boolean) {
         launchableViewDelegate.setShouldBlockVisibilityChanges(block)
+    }
+
+    override fun getAnimatedView(): LaunchableView {
+        return if (isA11Style) getIconWithBackground() as LaunchableView else this
     }
 
     override fun setVisibility(visibility: Int) {
