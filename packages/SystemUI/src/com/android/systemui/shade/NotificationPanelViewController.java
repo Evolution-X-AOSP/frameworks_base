82fe0ab1f42ec8356224e3ff4eb805330441d239
Joey <joey@evolution-x.org>
2024-03-02 04:51:04 +0900

    [SQUASH] SystemUI: Remove edge light customizations
    Revert "SystemUI: Refactor Edge light color mode"
    
    This reverts commit 22ca710035ac7c9db0d9d84bf1da0eacf9847571.
    
    Revert "EdgeLight: Fix up applying custom color"
    
    This reverts commit 7aa30238bfcdf36df30135d4dfbd55462e58a4cd.
    
    Revert "SystemUI: Prevent edge lightning from pulsing outside keyguard/doze"
    
    This reverts commit bfa8ebc9a99cbe4dc5b21cceef9fd1b3686ff1d4.
    
    Revert "SystemUI: add edge light customizations [1/2]"
    
    This reverts commit ad4bf8bb469eb331d3abfd292b16f70f1e195af6.

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -419,7 +419,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     private boolean mAnimateNextPositionUpdate;
     private final ScreenOffAnimationController mScreenOffAnimationController;
     private final UnlockedScreenOffAnimationController mUnlockedScreenOffAnimationController;
-    private final EdgeLightViewController mEdgeLightViewController;
     private TrackingStartedListener mTrackingStartedListener;
     private OpenCloseListener mOpenCloseListener;
     private GestureRecorder mGestureRecorder;
@@ -822,8 +821,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             KeyguardFaceAuthInteractor keyguardFaceAuthInteractor,
             KeyguardRootView keyguardRootView,
             TunerService tunerService,
-            Context context,
-            EdgeLightViewController edgeLightViewController) {
+            Context context) {
         keyguardStateController.addCallback(new KeyguardStateController.Callback() {
             @Override
             public void onKeyguardFadingAwayChanged() {
@@ -980,7 +978,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         mUnlockedScreenOffAnimationController = unlockedScreenOffAnimationController;
         mLastDownEvents = new NPVCDownEventState.Buffer(MAX_DOWN_EVENT_BUFFER_SIZE);
         mKeyguardFaceAuthInteractor = keyguardFaceAuthInteractor;
-        mEdgeLightViewController = edgeLightViewController;
 
         int currentMode = navigationModeController.addListener(
                 mode -> mIsGestureNavigation = QuickStepContract.isGesturalMode(mode));
@@ -1120,7 +1117,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
 
         mNotificationContainerParent = mView.findViewById(R.id.notification_container_parent);
         updateViewControllers(userAvatarContainer, keyguardUserSwitcherView);
-        mEdgeLightViewController.setEdgeLightView(mView.findViewById(R.id.edge_light_container));
+
         mNotificationStackScrollLayoutController.setOnHeightChangedListener(
                 new NsslHeightChangedListener());
         mNotificationStackScrollLayoutController.setOnEmptySpaceClickListener(
